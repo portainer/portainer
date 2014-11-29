604fd1de3b4fa2764f7fc4517c09c7a98a9a653b
Kevan Ahlquist <ahlqu039@umn.edu>
2014-11-28 22:11:56 -0600

    Moved sidebar to its own submodule

--- app/controllers.js
+++ app/controllers.js
@@ -47,16 +47,6 @@ function getChart(id) {
     return new Chart(ctx);
 }
 
-function SideBarController($scope, Container, Settings) {
-    $scope.template = 'partials/sidebar.html';
-    $scope.containers = [];
-    $scope.endpoint = Settings.endpoint;
-
-    Container.query({all: 0}, function(d) {
-        $scope.containers = d;
-    });
-}
-
 function SettingsController($scope, System, Docker, Settings, Messages) {
     $scope.info = {};
     $scope.docker = {};
