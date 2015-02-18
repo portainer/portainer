3635e951389df33304117b6ef91a3b554235f118
Kevan Ahlquist <ahlqu039@umn.edu>
2015-02-18 01:04:26 -0600

    Adding latest build to dist.

---
+++ dist/assets/js/legend.js
@@ -0,0 +1,15 @@
+function legend(parent, data) {
+    parent.className = 'legend';
+    var datas = data.hasOwnProperty('datasets') ? data.datasets : data;
+
+    datas.forEach(function(d) {
+        var title = document.createElement('span');
+        title.className = 'title';
+        title.style.borderColor = d.hasOwnProperty('strokeColor') ? d.strokeColor : d.color;
+        title.style.borderStyle = 'solid';
+        parent.appendChild(title);
+
+        var text = document.createTextNode(d.title);
+        title.appendChild(text);
+    });
+}
