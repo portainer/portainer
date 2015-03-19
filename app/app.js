f40d645fb35f44e192db275bc459e0311a07005f
Kevan Ahlquist <ahlqu039@umn.edu>
2015-03-18 20:03:50 -0500

    Bump other app version.

--- app/app.js
+++ app/app.js
@@ -14,5 +14,5 @@ angular.module('dockerui', ['dockerui.templates', 'ngRoute', 'dockerui.services'
     // You need to set this to the api endpoint without the port i.e. http://192.168.1.9
     .constant('DOCKER_ENDPOINT', 'dockerapi')
     .constant('DOCKER_PORT', '') // Docker port, leave as an empty string if no port is requred.  If you have a port, prefix it with a ':' i.e. :4243
-    .constant('UI_VERSION', 'v0.6.0')
+    .constant('UI_VERSION', 'v0.7.0-beta')
     .constant('DOCKER_API_VERSION', 'v1.17');
