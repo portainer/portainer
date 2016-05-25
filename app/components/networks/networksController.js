ef776b21913b23a5691692509d967e372a31fd92
Anthony Lapenna <lapenna.anthony@gmail.com>
2016-05-25 21:58:12 +1200

    bind network remove action

--- app/components/networks/networksController.js
+++ app/components/networks/networksController.js
@@ -3,9 +3,11 @@ angular.module('networks', [])
 function ($scope, Network, ViewSpinner, Messages, $route, errorMsgFilter) {
 
   $scope.state = {};
+  $scope.state.toggle = false;
+  $scope.state.selectedItemCount = 0;
   $scope.sortType = 'Name';
   $scope.sortReverse = true;
-  $scope.state.toggle = false;
+
   $scope.order = function(sortType) {
     $scope.sortReverse = ($scope.sortType === sortType) ? !$scope.sortReverse : false;
     $scope.sortType = sortType;
@@ -14,6 +16,44 @@ function ($scope, Network, ViewSpinner, Messages, $route, errorMsgFilter) {
   $scope.toggleSelectAll = function () {
     angular.forEach($scope.state.filteredNetworks, function (i) {
       i.Checked = $scope.state.toggle;
+    });
+    if ($scope.state.toggle) {
+      $scope.state.selectedItemCount = $scope.state.filteredNetworks.length;
+    } else {
+      $scope.state.selectedItemCount = 0;
+    }
+  };
+
+  $scope.selectItem = function (item) {
+    if (item.Checked) {
+      $scope.state.selectedItemCount++;
+    } else {
+      $scope.state.selectedItemCount--;
+    }
+  };
+
+  $scope.removeAction = function () {
+    ViewSpinner.spin();
+    var counter = 0;
+    var complete = function () {
+      counter = counter - 1;
+      if (counter === 0) {
+        ViewSpinner.stop();
+      }
+    };
+    angular.forEach($scope.networks, function (network) {
+      if (network.Checked) {
+        counter = counter + 1;
+        Network.remove({id: network.Id}, function (d) {
+          Messages.send("Network deleted", network.Id);
+          var index = $scope.networks.indexOf(network);
+          $scope.networks.splice(index, 1);
+          complete();
+        }, function (e) {
+          Messages.error("Failure", e.data);
+          complete();
+        });
+      }
     });
   };
 
