15a3cb724171c2a9e9ecbad17c82e2b7bab9e5c8
Anthony Lapenna <lapenna.anthony@gmail.com>
2016-07-27 11:10:42 +1200

    chore(grunt): use minified angular script

--- gruntFile.js
+++ gruntFile.js
@@ -150,7 +150,7 @@ module.exports = function (grunt) {
                 }
             },
             angular: {
-                src: ['bower_components/angular/angular.js',
+                src: ['bower_components/angular/angular.min.js',
                     'bower_components/angular-sanitize/angular-sanitize.min.js',
                     'bower_components/angular-cookies/angular-cookies.min.js',
                     'bower_components/angular-ui-router/release/angular-ui-router.min.js',
