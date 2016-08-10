ef257f65cf11120e1cb1bea5e3c7bed85462dfb6
Anthony Lapenna <lapenna.anthony@gmail.com>
2016-08-10 15:26:08 +1200

    fix(images): display an error message when unable to remove an image

--- gruntFile.js
+++ gruntFile.js
@@ -152,11 +152,11 @@ module.exports = function (grunt) {
                 }
             },
             angular: {
-                src: ['bower_components/angular/angular.min.js',
+                src: ['bower_components/angular/angular.js',
                     'bower_components/angular-sanitize/angular-sanitize.min.js',
                     'bower_components/angular-cookies/angular-cookies.min.js',
                     'bower_components/angular-ui-router/release/angular-ui-router.min.js',
-                    'bower_components/angular-resource/angular-resource.min.js',
+                    'bower_components/angular-resource/angular-resource.js',
                     'bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js',
                     'bower_components/angular-ui-select/dist/select.min.js'],
                 dest: '<%= distdir %>/js/angular.js'
