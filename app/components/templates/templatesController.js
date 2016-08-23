290e9402d2c8a96a4f97f8291a55332111f6aff7
Anthony Lapenna <lapenna.anthony@gmail.com>
2016-08-24 11:05:41 +1200

    refactor(lint): fix lint issue

--- app/components/templates/templatesController.js
+++ app/components/templates/templatesController.js
@@ -94,7 +94,7 @@ function generateUUID(){
   var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
     var r = (d + Math.random()*16)%16 | 0;
     d = Math.floor(d/16);
-    return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+    return (c==='x' ? r : (r&0x3|0x8)).toString(16);
   });
   return uuid;
 }
