c37eb6dd44c21886826b515458b5fd07c56c9ff1
Anthony Lapenna <lapenna.anthony@gmail.com>
2016-10-08 17:09:12 +1300

    style(templates): new effect on hover

--- app/directives/widget-custom-header.js
+++ app/directives/widget-custom-header.js
@@ -8,7 +8,7 @@ angular
       icon: '='
     },
     transclude: true,
-    template: '<div class="widget-header"><div class="row"><span class="pull-left"><img class="custom-header-ico" ng-src="{{icon}}"></img> <span class="small text-muted"> {{title}} </span> </span><span class="pull-right col-xs-6 col-sm-4" ng-transclude></span></div></div>',
+    template: '<div class="widget-header"><div class="row"><span class="pull-left"><img class="custom-header-ico" ng-src="{{icon}}"></img> <span class="text-muted"> {{title}} </span> </span><span class="pull-right col-xs-6 col-sm-4" ng-transclude></span></div></div>',
     restrict: 'E'
   };
   return directive;
