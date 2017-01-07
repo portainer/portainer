3007d07425e183e6719666e3db79f0570d892e30
Kévin LE QUILLEC <kevin.lequillec@orange.fr>
2017-01-07 18:11:04 +0100

    #476 fix(UX): Rename 'local' endpoint doesn't overwrite "unix://"

--- app/shared/services.js
+++ app/shared/services.js
@@ -374,11 +374,11 @@ angular.module('portainer.services', ['ngResource', 'ngSanitize'])
         endpoints: function() {
           return Endpoints.query({}).$promise;
         },
-        updateEndpoint: function(ID, name, URL, TLS, TLSCAFile, TLSCertFile, TLSKeyFile) {
+        updateEndpoint: function(ID, name, URL, TLS, TLSCAFile, TLSCertFile, TLSKeyFile, TYPE) {
           var endpoint = {
             id: ID,
             Name: name,
-            URL: "tcp://" + URL,
+            URL: TYPE === 'local' ? ("unix://" + URL) : ("tcp://" + URL),
             TLS: TLS
           };
           var deferred = $q.defer();
