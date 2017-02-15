64ef74321ad592ebd856665e5c07497a101b28b5
Romain <filirom1@gmail.com>
2017-02-15 23:14:56 +0100

    feat(image): add the ability to force remove an image (#497) (#562)

---
+++ app/services/modalService.js
@@ -0,0 +1,28 @@
+angular.module('portainer.services')
+.factory('ModalService', [function ModalServiceFactory() {
+  'use strict';
+  var service = {};
+  service.confirm = function(options){
+    var box = bootbox.confirm({
+      title: options.title,
+      message: options.message,
+      buttons: {
+        confirm: {
+          label: options.buttons.confirm.label,
+          className: 'btn-danger'
+        },
+         cancel: {
+          label: options.buttons.cancel.label
+         }
+      },
+      callback: options.callback
+    });
+    box.css({
+      'top': '50%',
+      'margin-top': function () {
+        return -(box.height() / 2);
+      }
+    });
+  }
+  return service;
+}]);
