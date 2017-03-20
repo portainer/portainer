039c621021539c7e1637548c393f65370a6126b8
Anthony Lapenna <lapenna.anthony@gmail.com>
2017-03-20 11:59:33 +0100

    refactor(image): indent code

--- app/components/image/imageController.js
+++ app/components/image/imageController.js
@@ -1,109 +1,109 @@
 angular.module('image', [])
 .controller('ImageController', ['$scope', '$stateParams', '$state', 'ImageService', 'Messages',
 function ($scope, $stateParams, $state, ImageService, Messages) {
-  $scope.config = {
-    Image: '',
-    Registry: ''
-  };
+	$scope.config = {
+		Image: '',
+		Registry: ''
+	};
 
-  $scope.tagImage = function() {
-    $('#loadingViewSpinner').show();
-    var image = $scope.config.Image;
-    var registry = $scope.config.Registry;
-
-    ImageService.tagImage($stateParams.id, image, registry)
-    .then(function success(data) {
-      Messages.send('Image successfully tagged');
-      $state.go('image', {id: $stateParams.id}, {reload: true});
-    })
-    .catch(function error(err) {
-      Messages.error("Failure", err, "Unable to tag image");
-    })
-    .finally(function final() {
-      $('#loadingViewSpinner').hide();
-    });
-  };
-
-  $scope.pushImage = function(tag) {
-    $('#loadingViewSpinner').show();
-    ImageService.pushImage(tag)
-    .then(function success() {
-      Messages.send('Image successfully pushed');
-    })
-    .catch(function error(err) {
-      Messages.error("Failure", err, "Unable to push image tag");
-    })
-    .finally(function final() {
-      $('#loadingViewSpinner').hide();
-    });
-  };
-
-  $scope.pullImage = function(tag) {
+	$scope.tagImage = function() {
 		$('#loadingViewSpinner').show();
 		var image = $scope.config.Image;
 		var registry = $scope.config.Registry;
 
-    ImageService.pullImage(image, registry)
-    .then(function success(data) {
-        Messages.send('Image successfully pulled', image);
-    })
-    .catch(function error(err){
-        Messages.error("Failure", err, "Unable to pull image");
-    })
-    .finally(function final() {
-        $('#loadingViewSpinner').hide();
-    });
-  };
+		ImageService.tagImage($stateParams.id, image, registry)
+		.then(function success(data) {
+			Messages.send('Image successfully tagged');
+			$state.go('image', {id: $stateParams.id}, {reload: true});
+		})
+		.catch(function error(err) {
+			Messages.error("Failure", err, "Unable to tag image");
+		})
+		.finally(function final() {
+			$('#loadingViewSpinner').hide();
+		});
+	};
+
+	$scope.pushImage = function(tag) {
+		$('#loadingViewSpinner').show();
+		ImageService.pushImage(tag)
+		.then(function success() {
+			Messages.send('Image successfully pushed');
+		})
+		.catch(function error(err) {
+			Messages.error("Failure", err, "Unable to push image tag");
+		})
+		.finally(function final() {
+			$('#loadingViewSpinner').hide();
+		});
+	};
+
+	$scope.pullImage = function(tag) {
+		$('#loadingViewSpinner').show();
+		var image = $scope.config.Image;
+		var registry = $scope.config.Registry;
+
+		ImageService.pullImage(image, registry)
+		.then(function success(data) {
+			Messages.send('Image successfully pulled', image);
+		})
+		.catch(function error(err){
+			Messages.error("Failure", err, "Unable to pull image");
+		})
+		.finally(function final() {
+			$('#loadingViewSpinner').hide();
+		});
+	};
 
 	$scope.removeTag = function(id) {
-    $('#loadingViewSpinner').show();
-    ImageService.deleteImage(id, false)
-    .then(function success() {
+		$('#loadingViewSpinner').show();
+		ImageService.deleteImage(id, false)
+		.then(function success() {
 			if ($scope.image.RepoTags.length === 1) {
 				Messages.send('Image successfully deleted', id);
 				$state.go('images', {}, {reload: true});
 			} else {
 				Messages.send('Tag successfully deleted', id);
-	      $state.go('image', {id: $stateParams.id}, {reload: true});
+				$state.go('image', {id: $stateParams.id}, {reload: true});
 			}
-    })
-    .catch(function error(err) {
-      Messages.error("Failure", err, 'Unable to remove image');
-    })
-    .finally(function final() {
-      $('#loadingViewSpinner').hide();
-    });
-  };
+		})
+		.catch(function error(err) {
+			Messages.error("Failure", err, 'Unable to remove image');
+		})
+		.finally(function final() {
+			$('#loadingViewSpinner').hide();
+		});
+	};
 
-  $scope.removeImage = function (id) {
-    $('#loadingViewSpinner').show();
-    ImageService.deleteImage(id, false)
-    .then(function success() {
+	$scope.removeImage = function (id) {
+		$('#loadingViewSpinner').show();
+		ImageService.deleteImage(id, false)
+		.then(function success() {
 			Messages.send('Image successfully deleted', id);
 			$state.go('images', {}, {reload: true});
-    })
-    .catch(function error(err) {
-      Messages.error("Failure", err, 'Unable to remove image');
-    })
-    .finally(function final() {
-      $('#loadingViewSpinner').hide();
-    });
-  };
+		})
+		.catch(function error(err) {
+			Messages.error("Failure", err, 'Unable to remove image');
+		})
+		.finally(function final() {
+			$('#loadingViewSpinner').hide();
+		});
+	};
 
-  function retrieveImageDetails() {
-    $('#loadingViewSpinner').show();
-    ImageService.image($stateParams.id)
-    .then(function success(data) {
-      $scope.image = data;
-    })
-    .catch(function error(err) {
-      Messages.error("Failure", err, "Unable to retrieve image details");
-      $state.go('images');
-    })
-    .finally(function final() {
-      $('#loadingViewSpinner').hide();
-    });
-  }
+	function retrieveImageDetails() {
+		$('#loadingViewSpinner').show();
+		ImageService.image($stateParams.id)
+		.then(function success(data) {
+			$scope.image = data;
+		})
+		.catch(function error(err) {
+			Messages.error("Failure", err, "Unable to retrieve image details");
+			$state.go('images');
+		})
+		.finally(function final() {
+			$('#loadingViewSpinner').hide();
+		});
+	}
 
-  retrieveImageDetails();
+	retrieveImageDetails();
 }]);
