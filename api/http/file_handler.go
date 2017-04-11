883cc24f189add6dc14814348830f5e07a4e44d3
Adrian Falleiro <adrian.falleiro@analogfolk.com>
2017-04-11 12:31:34 +1000

    feat(api): fallbacks onto index.html when requested asset does not exist

--- api/http/file_handler.go
+++ api/http/file_handler.go
@@ -2,17 +2,23 @@ package http
 
 import (
 	"net/http"
+	"os"
+	"path"
 	"strings"
 )
 
 // FileHandler represents an HTTP API handler for managing static files.
 type FileHandler struct {
 	http.Handler
+	AssetPath       string
+	NotFoundHandler http.Handler
 }
 
 func newFileHandler(assetPath string) *FileHandler {
 	h := &FileHandler{
-		Handler: http.FileServer(http.Dir(assetPath)),
+		Handler:         http.FileServer(http.Dir(assetPath)),
+		AssetPath:       assetPath,
+		NotFoundHandler: http.HandlerFunc(notFound),
 	}
 	return h
 }
@@ -26,11 +32,22 @@ func isHTML(acceptContent []string) bool {
 	return false
 }
 
+func notFound(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "index.html")
+}
+
 func (fileHandler *FileHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if !isHTML(r.Header["Accept"]) {
 		w.Header().Set("Cache-Control", "max-age=31536000")
 	} else {
 		w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
 	}
-	fileHandler.Handler.ServeHTTP(w, r)
+
+	_, err := os.Open(path.Join(fileHandler.AssetPath, path.Clean(r.URL.Path)))
+	if os.IsNotExist(err) {
+		fileHandler.NotFoundHandler.ServeHTTP(w, r)
+		return
+	} else {
+		fileHandler.Handler.ServeHTTP(w, r)
+	}
 }
