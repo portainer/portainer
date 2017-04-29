a3b790d88a1da5fe9e36d34e8e1224628faff8d1
gkovacs81 <gkovacs81@gmail.com>
2017-04-29 21:52:49 +0200

    Fix display of history commands

--- app/models/imageLayer.js
+++ app/models/imageLayer.js
@@ -1,7 +1,7 @@
 function ImageLayerViewModel(data) {
   this.Id = data.Id;
   this.Created = data.Created;
-  this.CreatedBy = data.CreatedBy;
+  this.CreatedBy = data.CreatedBy.replace("/bin/sh -c #(nop) ","").replace("/bin/sh -c ", "RUN "),
   this.Size = data.Size;
   this.Comment = data.Comment;
   this.Tags = data.Tags;
