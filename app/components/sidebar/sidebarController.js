f9cc5c77f6da9f7cecde5ec89201bccb0843b38f
Anthony Lapenna <lapenna.anthony@gmail.com>
2017-05-01 10:57:47 +0200

    feat(sidebar): display user management if user is team leader

--- app/components/sidebar/sidebarController.js
+++ app/components/sidebar/sidebarController.js
@@ -1,13 +1,13 @@
 angular.module('sidebar', [])
-.controller('SidebarController', ['$scope', '$state', 'Settings', 'Config', 'EndpointService', 'StateManager', 'EndpointProvider', 'Notifications', 'Authentication',
-function ($scope, $state, Settings, Config, EndpointService, StateManager, EndpointProvider, Notifications, Authentication) {
+.controller('SidebarController', ['$q', '$scope', '$state', 'Settings', 'Config', 'EndpointService', 'StateManager', 'EndpointProvider', 'Notifications', 'Authentication', 'UserService',
+function ($q, $scope, $state, Settings, Config, EndpointService, StateManager, EndpointProvider, Notifications, Authentication, UserService) {
 
   Config.$promise.then(function (c) {
     $scope.logo = c.logo;
   });
 
   $scope.uiVersion = Settings.uiVersion;
-  $scope.userRole = Authentication.getUserDetails().role;
+  $scope.endpoints = [];
 
   $scope.switchEndpoint = function(endpoint) {
     var activeEndpointID = EndpointProvider.endpointID();
@@ -17,28 +17,52 @@ function ($scope, $state, Settings, Config, EndpointService, StateManager, Endpo
       $state.go('dashboard');
     })
     .catch(function error(err) {
-      Notifications.error("Failure", err, "Unable to connect to the Docker endpoint");
+      Notifications.error('Failure', err, 'Unable to connect to the Docker endpoint');
       EndpointProvider.setEndpointID(activeEndpointID);
       StateManager.updateEndpointState(true)
       .then(function success() {});
     });
   };
 
-  function fetchEndpoints() {
-    EndpointService.endpoints()
-    .then(function success(data) {
-      $scope.endpoints = data;
-      var activeEndpointID = EndpointProvider.endpointID();
-      angular.forEach($scope.endpoints, function (endpoint) {
-        if (endpoint.Id === activeEndpointID) {
-          $scope.activeEndpoint = endpoint;
-        }
-      });
-    })
-    .catch(function error(err) {
-      $scope.endpoints = [];
+  function setActiveEndpoint(endpoints) {
+    var activeEndpointID = EndpointProvider.endpointID();
+    angular.forEach(endpoints, function (endpoint) {
+      if (endpoint.Id === activeEndpointID) {
+        $scope.activeEndpoint = endpoint;
+      }
     });
   }
 
-  fetchEndpoints();
+  function checkPermissions(memberships) {
+    var isLeader = false;
+    angular.forEach(memberships, function(membership) {
+      if (membership.Role === 1) {
+        isLeader = true;
+      }
+    });
+    $scope.isTeamLeader = isLeader;
+  }
+
+  function initView() {
+    var userDetails = Authentication.getUserDetails();
+    var isAdmin = userDetails.role === 1 ? true: false;
+    $scope.isAdmin = isAdmin;
+    $q.all({
+      endpoints: EndpointService.endpoints(),
+      memberships: !isAdmin ? UserService.userMemberships(userDetails.ID) : null
+    })
+    .then(function success(data) {
+      var endpoints = data.endpoints;
+      $scope.endpoints = endpoints;
+      setActiveEndpoint(endpoints);
+      if (!isAdmin) {
+        checkPermissions(data.memberships);
+      }
+    })
+    .catch(function error(err) {
+      Notifications.error('Failure', err, 'Unable to retrieve user information');
+    });
+  }
+
+  initView();
 }]);
