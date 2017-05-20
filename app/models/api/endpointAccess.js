d446f1b9e8339e4732b0b8187c60e59be3311868
Anthony Lapenna <lapenna.anthony@gmail.com>
2017-05-20 13:45:08 +0200

    refactor(lint): fix lint issues

--- app/models/api/endpointAccess.js
+++ app/models/api/endpointAccess.js
@@ -1,11 +1,11 @@
 function EndpointAccessUserViewModel(data) {
   this.Id = data.Id;
   this.Name = data.Username;
-  this.Type = "user";
+  this.Type = 'user';
 }
 
 function EndpointAccessTeamViewModel(data) {
   this.Id = data.Id;
   this.Name = data.Name;
-  this.Type = "team";
+  this.Type = 'team';
 }
