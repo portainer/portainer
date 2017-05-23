f0cdfef03d145154a7b6afabc293bce5416e496a
Anthony Lapenna <lapenna.anthony@gmail.com>
2017-05-23 20:17:46 +0200

    fix(image-details): fix the ability to pull an image from a tag

--- app/helpers/imageHelper.js
+++ app/helpers/imageHelper.js
@@ -1,30 +1,51 @@
 angular.module('portainer.helpers')
 .factory('ImageHelper', [function ImageHelperFactory() {
   'use strict';
-  return {
-    createImageConfigForCommit: function(imageName, registry) {
-      var imageNameAndTag = imageName.split(':');
-      var image = imageNameAndTag[0];
-      if (registry) {
-        image = registry + '/' + imageNameAndTag[0];
-      }
-      var imageConfig = {
-        repo: image,
-        tag: imageNameAndTag[1] ? imageNameAndTag[1] : 'latest'
-      };
-      return imageConfig;
-    },
-    createImageConfigForContainer: function (imageName, registry) {
-      var imageNameAndTag = imageName.split(':');
-      var image = imageNameAndTag[0];
-      if (registry) {
-        image = registry + '/' + imageNameAndTag[0];
-      }
-      var imageConfig = {
-        fromImage: image,
-        tag: imageNameAndTag[1] ? imageNameAndTag[1] : 'latest'
-      };
-      return imageConfig;
-    }
+
+  var helper = {};
+
+  helper.extractImageAndRegistryFromTag = function(tag) {
+    var slashCount = _.countBy(tag)['/'];
+		var registry = null;
+		var image = tag;
+		if (slashCount > 1) {
+			// assume something/some/thing[/...]
+			var registryAndImage = _.split(tag, '/');
+			registry = registryAndImage[0];
+			image = registryAndImage[1];
+		}
+
+    return {
+      registry: registry,
+      image: image
+    };
   };
+
+  helper.createImageConfigForCommit = function(imageName, registry) {
+    var imageNameAndTag = imageName.split(':');
+    var image = imageNameAndTag[0];
+    if (registry) {
+      image = registry + '/' + imageNameAndTag[0];
+    }
+    var imageConfig = {
+      repo: image,
+      tag: imageNameAndTag[1] ? imageNameAndTag[1] : 'latest'
+    };
+    return imageConfig;
+  };
+
+  helper.createImageConfigForContainer = function (imageName, registry) {
+    var imageNameAndTag = imageName.split(':');
+    var image = imageNameAndTag[0];
+    if (registry) {
+      image = registry + '/' + imageNameAndTag[0];
+    }
+    var imageConfig = {
+      fromImage: image,
+      tag: imageNameAndTag[1] ? imageNameAndTag[1] : 'latest'
+    };
+    return imageConfig;
+  };
+
+  return helper;
 }]);
