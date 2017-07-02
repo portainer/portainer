65cd96feca397ee84623eea46c1f63340a0e0597
Anthony Lapenna <lapenna.anthony@gmail.com>
2017-07-02 08:50:52 +0200

    feat(stacks): display stack name in networks and volumes views

--- app/models/docker/network.js
+++ app/models/docker/network.js
@@ -5,4 +5,9 @@ function NetworkViewModel(data) {
   this.Driver = data.Driver;
   this.IPAM = data.IPAM;
   this.Labels = data.Labels;
+  if (this.Labels && this.Labels['com.docker.compose.project']) {
+    this.StackName = this.Labels['com.docker.compose.project'];
+  } else if (this.Labels && this.Labels['com.docker.stack.namespace']) {
+    this.StackName = this.Labels['com.docker.stack.namespace'];
+  }
 }
