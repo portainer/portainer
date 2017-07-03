30ec5a9b0a65341abafbfd6f452075e14ce6b7e7
Thomas Krzero <thomas.kovatchitch@gmail.com>
2017-07-03 23:46:09 +0200

    Remove createContainer

--- app/components/container/containerController.js
+++ app/components/container/containerController.js
@@ -244,63 +244,10 @@ function ($scope, $state, $stateParams, $filter, Container, ContainerCommit, Con
           .finally(function final() {
             $('#loadingViewSpinner').hide();
           });
-
-          /*Container.remove({id: $scope.container.Id, v: 0, force: true}, function(d) {
-            if (d.message) {
-              Notifications.error('Error', d, 'Unable to remove container');
-            } else {
-              var c = $scope.container;
-              if (c.Metadata && c.Metadata.ResourceControl) {
-                ResourceControlService.removeContainerResourceControl(c.Metadata.ResourceControl.OwnerId, $scope.container.Id)
-                .then(function success() {
-                  Notifications.success('Container Removed', $scope.container.Id);
-                  createContainer(config);
-                })
-                .catch(function error(err) {
-                  Notifications.error('Failure', err, 'Unable to remove container ownership');
-                });
-              } else {
-                Notifications.success('Container Removed', $scope.container.Id);
-                createContainer(config);
-              }
-            }
-          });*/
         }
       }
     });
   };
-
-  function createContainer(config) {
-    Container.create(config, function (d) {
-      if (d.message) {
-        Notifications.error('Error', {}, d.message);
-      } else {
-        /*if ($scope.formValues.Ownership === 'private') {
-          ResourceControlService.setContainerResourceControl(Authentication.getUserDetails().ID, d.Id)
-          .then(function success() {
-            startContainer(d.Id);
-          })
-          .catch(function error(err) {
-            $('#createContainerSpinner').hide();
-            Notifications.error('Failure', err, 'Unable to apply resource control on container');
-          });
-        } else {*/
-        Container.start({id: d.Id}, {}, function (cd) {
-          if (cd.message) {
-            Notifications.error('Error', {}, cd.message);
-          } else {
-            Notifications.success('Container Started', d.Id);
-            $state.go('containers', {}, {reload: true});
-          }
-        }, function (e) {
-          Notifications.error('Failure', e, 'Unable to start container');
-        });
-        //}
-      }
-    }, function (e) {
-      Notifications.error('Failure', e, 'Unable to create container');
-    });
-  }
 
   $scope.containerJoinNetwork = function containerJoinNetwork(container, networkId) {
     $('#joinNetworkSpinner').show();
