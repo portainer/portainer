cefd21aa46a371fe2786d49ef19df8ab52f702da
Thomas Krzero <thomas.kovatchitch@gmail.com>
2017-07-03 23:40:51 +0200

    Add ContainerHelper.configFromContainer

--- app/helpers/containerHelper.js
+++ app/helpers/containerHelper.js
@@ -13,5 +13,48 @@ angular.module('portainer.helpers')
     }).join(' ');
   };
 
+  helper.configFromContainer = function(container) {
+    var config = container.Config;
+    // HostConfig
+    config.HostConfig = container.HostConfig;
+    // Name
+    config.name = container.Name.replace(/^\//g, '');
+    // Network
+    var mode = config.HostConfig.NetworkMode;
+    config.NetworkingConfig = {
+      'EndpointsConfig': {}
+    };
+    config.NetworkingConfig.EndpointsConfig = container.NetworkSettings.Networks;
+    if (mode.indexOf('container:') !== -1) {
+      delete config.Hostname;
+      delete config.ExposedPorts;
+    }
+    // Set volumes
+    var binds = [];
+    var volumes = {};
+    for (var v in container.Mounts) {
+      var mount = container.Mounts[v];
+      var volume = {
+        'type': mount.Type,
+        'name': mount.Name || mount.Source,
+        'containerPath': mount.Destination,
+        'readOnly': mount.RW === false
+      };
+      var name = mount.Name || mount.Source;
+      var containerPath = mount.Destination;
+      if (name && containerPath) {
+        var bind = name + ':' + containerPath;
+        volumes[containerPath] = {};
+        if (mount.RW === false) {
+          bind += ':ro';
+        }
+        binds.push(bind);
+      }
+    }
+    config.HostConfig.Binds = binds;
+    config.Volumes = volumes;
+    return config;
+  }
+
   return helper;
 }]);
