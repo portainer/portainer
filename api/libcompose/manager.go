8c1ae63c0507ecfb58b9c2b4c0ec801c3b96acc8
Anthony Lapenna <lapenna.anthony@gmail.com>
2017-07-07 10:39:17 +0200

    refactor(stacks): introduce projectManager

--- api/libcompose/manager.go
+++ api/libcompose/manager.go
@@ -1,149 +1,48 @@
 package libcompose
 
 import (
-	"path"
-	"path/filepath"
-
 	"github.com/portainer/portainer"
 
-	"github.com/portainer/libcompose/config"
-	"github.com/portainer/libcompose/docker"
-	"github.com/portainer/libcompose/docker/client"
-	"github.com/portainer/libcompose/docker/ctx"
-	"github.com/portainer/libcompose/lookup"
-	"github.com/portainer/libcompose/project"
 	"github.com/portainer/libcompose/project/options"
 	"golang.org/x/net/context"
 )
 
 // StackManager represents a service for managing stacks.
-type StackManager struct{}
+type StackManager struct {
+	projectManager *ProjectManager
+}
 
 // NewStackManager initializes a new StackManager service.
 func NewStackManager() *StackManager {
-	return &StackManager{}
+	return &StackManager{
+		projectManager: NewProjectManager(),
+	}
 }
 
 // Up will execute the Up operation against a stack inside the specified endpoint.
 func (manager *StackManager) Up(stack *portainer.Stack, endpoint *portainer.Endpoint) error {
-
-	//TODO: clientFactory should be stored once created for an endpoint
-	clientFactory, err := client.NewDefaultFactory(client.Options{
-		TLS:         endpoint.TLS,
-		TLSVerify:   endpoint.TLS,
-		Host:        endpoint.URL,
-		TLSCAFile:   endpoint.TLSCACertPath,
-		TLSCertFile: endpoint.TLSCertPath,
-		TLSKeyFile:  endpoint.TLSKeyPath,
-		APIVersion:  "1.30",
-	})
+	project, err := manager.projectManager.GetProject(stack, endpoint)
 	if err != nil {
 		return err
 	}
-
-	composeFilePath := path.Join(stack.ProjectPath, "docker-compose.yml")
-	project, err := docker.NewProject(&ctx.Context{
-		Context: project.Context{
-			ComposeFiles: []string{composeFilePath},
-			EnvironmentLookup: &lookup.ComposableEnvLookup{
-				Lookups: []config.EnvironmentLookup{
-					&lookup.EnvfileLookup{
-						Path: filepath.Join(stack.ProjectPath, ".env"),
-					},
-					&lookup.OsEnvLookup{},
-				},
-			},
-			ProjectName: stack.Name,
-		},
-		ClientFactory: clientFactory,
-	}, nil)
-
-	if err != nil {
-		return err
-	}
-
 	return project.Up(context.Background(), options.Up{})
 }
 
 // Down will execute the Down operation against a stack inside the specified endpoint.
 func (manager *StackManager) Down(stack *portainer.Stack, endpoint *portainer.Endpoint) error {
-
-	//TODO: clientFactory should be stored once created for an endpoint
-	clientFactory, err := client.NewDefaultFactory(client.Options{
-		TLS:         endpoint.TLS,
-		TLSVerify:   endpoint.TLS,
-		Host:        endpoint.URL,
-		TLSCAFile:   endpoint.TLSCACertPath,
-		TLSCertFile: endpoint.TLSCertPath,
-		TLSKeyFile:  endpoint.TLSKeyPath,
-		APIVersion:  "1.30",
-	})
+	project, err := manager.projectManager.GetProject(stack, endpoint)
 	if err != nil {
 		return err
 	}
-
-	composeFilePath := path.Join(stack.ProjectPath, "docker-compose.yml")
-	project, err := docker.NewProject(&ctx.Context{
-		Context: project.Context{
-			ComposeFiles: []string{composeFilePath},
-			EnvironmentLookup: &lookup.ComposableEnvLookup{
-				Lookups: []config.EnvironmentLookup{
-					&lookup.EnvfileLookup{
-						Path: filepath.Join(stack.ProjectPath, ".env"),
-					},
-					&lookup.OsEnvLookup{},
-				},
-			},
-			ProjectName: stack.Name,
-		},
-		ClientFactory: clientFactory,
-	}, nil)
-
-	if err != nil {
-		return err
-	}
-
 	return project.Down(context.Background(), options.Down{})
 }
 
 // Scale will execute the scale operation against a stack service inside the specified endpoint.
 func (manager *StackManager) Scale(stack *portainer.Stack, endpoint *portainer.Endpoint, service string, scale int) error {
-
-	//TODO: clientFactory should be stored once created for an endpoint
-	clientFactory, err := client.NewDefaultFactory(client.Options{
-		TLS:         endpoint.TLS,
-		TLSVerify:   endpoint.TLS,
-		Host:        endpoint.URL,
-		TLSCAFile:   endpoint.TLSCACertPath,
-		TLSCertFile: endpoint.TLSCertPath,
-		TLSKeyFile:  endpoint.TLSKeyPath,
-		APIVersion:  "1.30",
-	})
+	project, err := manager.projectManager.GetProject(stack, endpoint)
 	if err != nil {
 		return err
 	}
-
-	composeFilePath := path.Join(stack.ProjectPath, "docker-compose.yml")
-	project, err := docker.NewProject(&ctx.Context{
-		Context: project.Context{
-			ComposeFiles: []string{composeFilePath},
-			EnvironmentLookup: &lookup.ComposableEnvLookup{
-				Lookups: []config.EnvironmentLookup{
-					&lookup.EnvfileLookup{
-						Path: filepath.Join(stack.ProjectPath, ".env"),
-					},
-					&lookup.OsEnvLookup{},
-				},
-			},
-			ProjectName: stack.Name,
-		},
-		ClientFactory: clientFactory,
-	}, nil)
-
-	if err != nil {
-		return err
-	}
-
 	serviceScale := map[string]int{service: scale}
 	return project.Scale(context.Background(), 30, serviceScale)
 }
