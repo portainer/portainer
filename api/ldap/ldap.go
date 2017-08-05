7d2b9421919cad8fe31ebcac8ceae47ed5173449
Anthony Lapenna <lapenna.anthony@gmail.com>
2017-08-05 18:00:54 +0200

    refactor(ldap): remove comments

--- api/ldap/ldap.go
+++ api/ldap/ldap.go
@@ -2,7 +2,6 @@ package ldap
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/portainer/portainer"
 	"github.com/portainer/portainer/crypto"
@@ -23,25 +22,18 @@ func searchUser(username string, conn *ldap.Conn, settings []portainer.LDAPSearc
 	var userDN string
 	found := false
 	for _, searchSettings := range settings {
-		log.Println("Step 3")
 		searchRequest := ldap.NewSearchRequest(
 			searchSettings.BaseDN,
 			ldap.ScopeWholeSubtree, ldap.NeverDerefAliases, 0, 0, false,
 			fmt.Sprintf("(&%s(%s=%s))", searchSettings.Filter, searchSettings.UserNameAttribute, username),
-			// fmt.Sprintf("(&(objectClass=organizationalPerson)&(uid=%s))", username),
 			[]string{"dn"},
 			nil,
 		)
 
-		log.Println("Step 4")
 		// Deliberately skip errors on the search request so that we can jump to other search settings
 		// if any issue arise with the current one.
 		sr, _ := conn.Search(searchRequest)
-		// if err != nil {
-		// 	return "", err
-		// }
 
-		log.Println("Step 5")
 		if len(sr.Entries) == 1 {
 			found = true
 			userDN = sr.Entries[0].DN
@@ -71,60 +63,27 @@ func createConnection(settings *portainer.LDAPSettings) (*ldap.Conn, error) {
 // AuthenticateUser is used to authenticate a user against a LDAP/AD.
 func (*Service) AuthenticateUser(username, password string, settings *portainer.LDAPSettings) error {
 
-	log.Println("Step 1")
-	var connection *ldap.Conn
-
 	connection, err := createConnection(settings)
 	if err != nil {
 		return err
 	}
 	defer connection.Close()
 
-	log.Println("Step 2")
-	// dn := fmt.Sprintf("cn=%s,%s", settings.ReaderDN, settings.BaseDN)
 	err = connection.Bind(settings.ReaderDN, settings.Password)
 	if err != nil {
 		return err
 	}
 
-	// searchSettings := settings.SearchSettings[0]
-	// log.Println("Step 3")
-	// searchRequest := ldap.NewSearchRequest(
-	// 	searchSettings.BaseDN,
-	// 	ldap.ScopeWholeSubtree, ldap.NeverDerefAliases, 0, 0, false,
-	// 	fmt.Sprintf("(&%s(%s=%s))", searchSettings.Filter, searchSettings.UserNameAttribute, username),
-	// 	// fmt.Sprintf("(&(objectClass=organizationalPerson)&(uid=%s))", username),
-	// 	[]string{"dn"},
-	// 	nil,
-	// )
-	//
-	// log.Println("Step 4")
-	// sr, err := l.Search(searchRequest)
-	// if err != nil {
-	// 	return err
-	// }
-	//
-	// log.Println("Step 5")
-	// if len(sr.Entries) != 1 {
-	// 	return ErrUserNotFound
-	// }
-	// userdn := sr.Entries[0].DN
-
 	userDN, err := searchUser(username, connection, settings.SearchSettings)
 	if err != nil {
 		return err
-		// log.Fatal(err)
 	}
 
-	log.Println("Step 6")
-	// Bind as the user to verify their password
 	err = connection.Bind(userDN, password)
 	if err != nil {
 		return err
-		// log.Fatal(err)
 	}
 
-	log.Println("Step 7")
 	return nil
 }
 
