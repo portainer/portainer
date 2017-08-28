a0b1cbd74cd1f9be930ef6d72182e92cd9f03afa
Sylvain MOUQUET <sylvain.mouquet@gmail.com>
2017-08-29 00:34:43 +0200

    Add more button on container name

--- build/build_in_container.sh
+++ build/build_in_container.sh
@@ -4,6 +4,7 @@ binary="portainer-$1-$2"
 
 mkdir -p dist
 
+echo $(pwd)/api
 docker run --rm -tv $(pwd)/api:/src -e BUILD_GOOS="$1" -e BUILD_GOARCH="$2" portainer/golang-builder:cross-platform /src/cmd/portainer
 
 mv "api/cmd/portainer/$binary" dist/
