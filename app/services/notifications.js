4c200b4a96c84e5b124979f53accba0dead2f247
Anthony Lapenna <lapenna.anthony@gmail.com>
2017-09-17 16:38:45 +0200

    feat(notifications): display Docker engine error message

--- app/services/notifications.js
+++ app/services/notifications.js
@@ -13,6 +13,8 @@ angular.module('portainer.services')
       msg = e.data.message;
     } else if (e.message) {
       msg = e.message;
+    } else if (e.err && e.err.data && e.err.data.message) {
+      msg = e.err.data.message;
     } else if (e.data && e.data.length > 0 && e.data[0].message) {
       msg = e.data[0].message;
     } else if (e.err && e.err.data && e.err.data.length > 0 && e.err.data[0].message) {
