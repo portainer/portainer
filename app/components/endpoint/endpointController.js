f5749f82d822a44075ae2be7a318e794775fd5ca
Anthony Lapenna <lapenna.anthony@gmail.com>
2017-09-22 07:34:17 +0200

    fix(endpoint-details): fix an issue when updating the local endpoint (#1226)

--- app/components/endpoint/endpointController.js
+++ app/components/endpoint/endpointController.js
@@ -54,14 +54,13 @@ function ($scope, $state, $transition$, $filter, EndpointService, Notifications)
     EndpointService.endpoint($transition$.params().id)
     .then(function success(data) {
       var endpoint = data;
-      endpoint.URL = $filter('stripprotocol')(endpoint.URL);
-      $scope.endpoint = endpoint;
-
       if (endpoint.URL.indexOf('unix://') === 0) {
         $scope.endpointType = 'local';
       } else {
         $scope.endpointType = 'remote';
       }
+      endpoint.URL = $filter('stripprotocol')(endpoint.URL);
+      $scope.endpoint = endpoint;
     })
     .catch(function error(err) {
       Notifications.error('Failure', err, 'Unable to retrieve endpoint details');
