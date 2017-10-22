e109ee842751b86ec85b74d13743f6d52918d9a3
1138-4EB <1138-4EB@users.noreply.github.com>
2017-10-22 04:41:14 +0200

    feat(container-inspect) add angular-json-tree view and download button

--- app/components/containerInspect/containerInspectController.js
+++ app/components/containerInspect/containerInspectController.js
@@ -1,12 +1,13 @@
-angular.module('containerInspect', [])
+angular.module('containerInspect', ['angular-json-tree'])
 .controller('ContainerInspectController', ['$scope', '$transition$', 'Notifications', 'ContainerService',
 function ($scope, $transition$, Notifications, ContainerService) {
   function initView() {
     $('#loadingViewSpinner').show();
-    
+
     ContainerService.inspect($transition$.params().id)
     .then(function success(d) {
-      $scope.containerInfo = d;
+      $scope.containerInfo.object = d;
+      $scope.containerInfo.date = new Date();
     })
     .catch(function error(e) {
       Notifications.error('Failure', e, 'Unable to inspect container');
@@ -15,6 +16,10 @@ function ($scope, $transition$, Notifications, ContainerService) {
       $('#loadingViewSpinner').hide();
     });
   }
-  
+
+  $scope.containerInfo = { object: {} };
+  $scope.TextView = false;
+  $scope.toggle = function() { $scope.TextView = !$scope.TextView; };
+  $scope.EncodeJSON = function(obj){ return encodeURIComponent(JSON.stringify(obj, null, 2)); };
   initView();
 }]);
