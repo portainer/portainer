2df3e7d28fedaa889e7d9f7f6f52bd95908a508a
1138-4EB <1138-4EB@users.noreply.github.com>
2017-10-23 11:48:40 +0200

    feat(container-inspect) remove save button, use uib-button-radio

--- app/components/containerInspect/containerInspectController.js
+++ app/components/containerInspect/containerInspectController.js
@@ -7,7 +7,6 @@ function ($scope, $transition$, Notifications, ContainerService) {
     ContainerService.inspect($transition$.params().id)
     .then(function success(d) {
       $scope.containerInfo.object = d;
-      //$scope.containerInfo.date = new Date();
     })
     .catch(function error(e) {
       Notifications.error('Failure', e, 'Unable to inspect container');
@@ -18,8 +17,6 @@ function ($scope, $transition$, Notifications, ContainerService) {
   }
 
   $scope.containerInfo = { object: {} };
-  $scope.TextView = false;
-  $scope.toggle = function() { $scope.TextView = !$scope.TextView; };
-  //$scope.EncodeJSON = function(obj){ return encodeURIComponent(JSON.stringify(obj, null, 2)); };
+  $scope.state = { TextView: false };
   initView();
 }]);
