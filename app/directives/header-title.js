72d20ec42dba07ebc1f2c335ec301b37fefde6ab
1138-4EB <1138-4EB@users.noreply.github.com>
2017-10-28 21:59:18 +0200

    style(header) move refresh button to the left of the title, move user buttons to title line and move text to attribute

--- app/directives/header-title.js
+++ app/directives/header-title.js
@@ -10,7 +10,7 @@ angular
       scope.username = Authentication.getUserDetails().username;
     },
     transclude: true,
-    template: '<div class="page white-space-normal">{{title}}<span class="header_title_content" ng-transclude></span><span class="pull-right user-box" ng-if="username"><i class="fa fa-user-circle-o" aria-hidden="true"></i> {{username}}</span></div>',
+    template: '<div class="page white-space-normal"><span class="header_title_content" ng-transclude></span>{{title}}<span class="pull-right user-box" ng-if="username">{{username}} <i class="fa fa-user-circle-o" aria-hidden="true"></i><a title="User settings" ng-if="username" ui-sref="userSettings" style="padding-left: 5px;"><i class="fa fa-wrench" aria-hidden="true"></i></a><a title="Log out" ng-if="username" ui-sref="auth({logout: true})" class="text-danger" style="padding-left: 5px;"><i class="fa fa-sign-out" aria-hidden="true"></i></a></span></div>',
     restrict: 'E'
   };
   return directive;
