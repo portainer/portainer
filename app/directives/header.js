c1b7a3d896d9c4ecb0d9042cfdf383f132ecc981
1138-4EB <1138-4EB@users.noreply.github.com>
2017-10-29 03:22:18 +0100

    style(header) delete rd-header-content and rd-header-title, use rd-header directly

--- app/directives/header.js
+++ app/directives/header.js
@@ -1,13 +1,19 @@
 angular
 .module('portainer')
-.directive('rdHeader', function rdHeader() {
+.directive('rdHeader', ['Authentication', function rdHeader(Authentication) {
   var directive = {
     scope: {
-      'ngModel': '='
+      'ngModel': '=',
+      'title': '@'
+    },
+    link: function (scope, iElement, iAttrs) {
+      scope.username = Authentication.getUserDetails().username;
     },
     transclude: true,
-    template: '<div class="row header"><div class="col-xs-12"><div class="meta" ng-transclude></div></div></div>',
+    template: '<div class="row header"><div class="meta col-xs-12"><div class="page white-space-normal"><span class="header_title_content" ng-transclude></span><span class="pull-right user-box" ng-if="username">{{username}} <i class="fa fa-user-circle-o" aria-hidden="true"></i><a title="User settings" ng-if="username" ui-sref="userSettings" style="padding-left: 6px;"><i class="fa fa-wrench" aria-hidden="true"></i></a><a title="Log out" ng-if="username" ui-sref="auth({logout: true})" class="text-danger" style="padding-left: 6px;"><i class="fa fa-sign-out" aria-hidden="true"></i></a></span></div></div></div>',
     restrict: 'EA'
   };
   return directive;
-});
+}]);
+
+//{{title}} corresponds to the attribute. Not uset ATM.
