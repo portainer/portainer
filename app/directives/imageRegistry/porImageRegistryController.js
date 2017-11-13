0e7e2d7d268893ede612336650a2a0db87f8f263
Yassir Hannoun <yassir.hannoun@gmail.com>
2017-11-14 00:16:30 +0100

    Auto complete feature for images when creating services and containers

--- app/directives/imageRegistry/porImageRegistryController.js
+++ app/directives/imageRegistry/porImageRegistryController.js
@@ -1,27 +1,34 @@
 angular.module('portainer')
-.controller('porImageRegistryController', ['$q', 'RegistryService', 'DockerHubService', 'Notifications',
-function ($q, RegistryService, DockerHubService, Notifications) {
-  var ctrl = this;
+  .controller('porImageRegistryController', ['$q', 'RegistryService', 'DockerHubService', 'ImageService', 'Notifications',
+    function ($q, RegistryService, DockerHubService, ImageService, Notifications) {
+      var ctrl = this;
 
-  function initComponent() {
-    $q.all({
-      registries: RegistryService.registries(),
-      dockerhub: DockerHubService.dockerhub()
-    })
-    .then(function success(data) {
-      var dockerhub = data.dockerhub;
-      var registries = data.registries;
-      ctrl.availableRegistries = [dockerhub].concat(registries);
-      if (!ctrl.registry.Id) {
-        ctrl.registry = dockerhub;
-      } else {
-        ctrl.registry = _.find(ctrl.availableRegistries, { 'Id': ctrl.registry.Id });
+      function initComponent() {
+        $q.all({
+          registries: RegistryService.registries(),
+          dockerhub: DockerHubService.dockerhub(),
+          availableImages: ctrl.autoComplete ? ImageService.images() : []
+        })
+          .then(function success(data) {
+            var dockerhub = data.dockerhub;
+            var registries = data.registries;
+            ctrl.availableImages = _.flatten(_.map(data.availableImages, function (image) {
+              _.remove(image.RepoTags, function(item){
+                return item.indexOf("<none>") != -1; 
+              });
+              return image.RepoTags ? _.uniqWith(image.RepoTags, _.isEqual) : [];
+            }));
+            ctrl.availableRegistries = [dockerhub].concat(registries);
+            if (!ctrl.registry.Id) {
+              ctrl.registry = dockerhub;
+            } else {
+              ctrl.registry = _.find(ctrl.availableRegistries, { 'Id': ctrl.registry.Id });
+            }
+          })
+          .catch(function error(err) {
+            Notifications.error('Failure', err, 'Unable to retrieve registries');
+          });
       }
-    })
-    .catch(function error(err) {
-      Notifications.error('Failure', err, 'Unable to retrieve registries');
-    });
-  }
 
-  initComponent();
-}]);
+      initComponent();
+    }]);
