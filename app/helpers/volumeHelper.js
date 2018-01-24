6fc7196157dcb0a4b812ca6be0d965efff9b69d3
Anthony Lapenna <lapenna.anthony@gmail.com>
2018-01-24 18:45:09 +0100

    feat(volumes): check if volumes are used in service definitions

--- app/helpers/volumeHelper.js
+++ app/helpers/volumeHelper.js
@@ -11,5 +11,20 @@ angular.module('portainer.helpers')
     return options;
   };
 
+  service.isVolumeUsedByAService = function(volume, services) {
+    for (var i = 0; i < services.length; i++) {
+      var service = services[i];
+      var mounts = service.Mounts;
+      for (var j = 0; j < mounts.length; j++) {
+        var mount = mounts[j];
+        if (mount.Source === volume.Id) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  };
+
   return helper;
 }]);
