b87832dfef5af5dee163acf43e85d78f5e5d9ac7
Thomas Krzero <thomas.kovatchitch@gmail.com>
2018-02-11 00:16:53 +0100

    Add reworked history
    Signed-off-by: Thomas Krzero <thomas.kovatchitch@gmail.com>

--- app/portainer/models/registry/manifests.js
+++ app/portainer/models/registry/manifests.js
@@ -2,9 +2,12 @@ function RegistryManifestsViewModel(data) {
   this.RepositoryName = data.name;
   this.TagName = data.tag;
   this.Architecture = data.architecture;
-  this.History = data.history;
   this.LayersCount = data.fsLayers.length;
   this.Layers = data.fsLayers.map(function (elem) {
     return elem.blobSum;
   });  
+  this.History = data.history.map(function (elem) {
+    var legacyHistory = JSON.parse(elem.v1Compatibility);
+    return legacyHistory.container_config.Cmd.join(' ');
+  });
 }
