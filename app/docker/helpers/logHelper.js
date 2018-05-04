70fc89bcbdfa8317b1ce12954bac889fe1e18b3c
Anthony Lapenna <lapenna.anthony@gmail.com>
2018-05-04 09:43:22 +0200

    fix(log-viewer): always remove ANSI sequences

--- app/docker/helpers/logHelper.js
+++ app/docker/helpers/logHelper.js
@@ -4,13 +4,15 @@ angular.module('portainer.docker')
   var helper = {};
 
   // Return an array with each line being an entry.
-  // It will also strip the 8 first characters of each line and remove any ANSI code related character sequences.
+  // It will also remove any ANSI code related character sequences.
+  // If the skipHeaders param is specified, it will strip the 8 first characters of each line.
   helper.formatLogs = function(logs, skipHeaders) {
+    logs = logs.replace(
+      /[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g, '');
+
     if (skipHeaders) {
       logs = logs.substring(8);
       logs = logs.replace(/\n(.{8})/g, '\n\r');
-      logs = logs.replace(
-        /[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g, '');
     }
 
     return logs.split('\n');
