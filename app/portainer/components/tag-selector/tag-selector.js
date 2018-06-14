88eb70ce268cb61264e71f27df5c3790a8031848
Anthony Lapenna <lapenna.anthony@gmail.com>
2018-06-14 10:21:06 +0200

    feat(tags): update tag selector UX

--- app/portainer/components/tag-selector/tag-selector.js
+++ app/portainer/components/tag-selector/tag-selector.js
@@ -1,5 +1,6 @@
 angular.module('portainer.app').component('tagSelector', {
   templateUrl: 'app/portainer/components/tag-selector/tagSelector.html',
+  controller: 'TagSelectorController',
   bindings: {
     tags: '<',
     model: '='
