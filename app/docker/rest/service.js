e78ba6c6fd3ad07d47581168cc4d873c016122f5
Anthony Lapenna <lapenna.anthony@gmail.com>
2018-06-20 16:44:51 +0300

    feat(services): rollback version header value to 1.29

--- app/docker/rest/service.js
+++ app/docker/rest/service.js
@@ -10,21 +10,23 @@ function ServiceFactory($resource, API_ENDPOINT_ENDPOINTS, EndpointProvider, Htt
     query: { method: 'GET', isArray: true, params: {filters: '@filters'} },
     create: {
       method: 'POST', params: {action: 'create'},
-      // TODO: This is a temporary work-around that allows us to leverage digest pinning on
-      // the Docker daemon side. It has been moved client-side since Docker API version > 1.30.
-      // We should do digest pinning in Portainer as well.
       headers: {
         'X-Registry-Auth': HttpRequestHelper.registryAuthenticationHeader,
-        'version': '1.30'
+        // TODO: This is a temporary work-around that allows us to leverage digest pinning on
+        // the Docker daemon side. It has been moved client-side since Docker API version > 1.29.
+        // We should introduce digest pinning in Portainer as well.
+        'version': '1.29'
       },
       ignoreLoadingBar: true
     },
     update: {
       method: 'POST', params: { id: '@id', action: 'update', version: '@version' },
-      // TODO: This is a temporary work-around that allows us to leverage digest pinning on
-      // the Docker daemon side. It has been moved client-side since Docker API version > 1.30.
-      // We should do digest pinning in Portainer as well.
-      headers: { 'version': '1.30' }
+      headers: {
+        // TODO: This is a temporary work-around that allows us to leverage digest pinning on
+        // the Docker daemon side. It has been moved client-side since Docker API version > 1.29.
+        // We should introduce digest pinning in Portainer as well.
+        'version': '1.29'
+      }
     },
     remove: { method: 'DELETE', params: {id: '@id'} },
     logs: {
