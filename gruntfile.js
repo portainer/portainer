252af86ceaaefee53809067b7505fd8fd82c5cdf
Anthony Lapenna <lapenna.anthony@gmail.com>
2018-07-28 20:35:01 +0200

    fix(build-system): fix an invalid condition in shell_downloadDockerBinary task

--- gruntfile.js
+++ gruntfile.js
@@ -281,9 +281,9 @@ function shell_downloadDockerBinary(p, a) {
   var as = { 'amd64': 'x86_64', 'arm': 'armhf', 'arm64': 'aarch64' };
   var ip = ((ps[p] === undefined) ? p : ps[p]);
   var ia = ((as[a] === undefined) ? a : as[a]);
-  var binaryVersion = (( p === 'win' ? '<%= shippedDockerVersionWindows %>' : '<%= shippedDockerVersion %>' ));
+  var binaryVersion = (( p === 'windows' ? '<%= shippedDockerVersionWindows %>' : '<%= shippedDockerVersion %>' ));
   return [
-    'if [ -f '+(( p === 'win' ) ? 'dist/docker.exe' : 'dist/docker')+' ]; then',
+    'if [ -f '+(( p === 'windows' ) ? 'dist/docker.exe' : 'dist/docker')+' ]; then',
       'echo "Docker binary exists";',
     'else',
       'build/download_docker_binary.sh ' + ip + ' ' + ia + ' ' + binaryVersion + ';',
