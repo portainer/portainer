17b11adb03d96dcae264e6940f66f3bb4a214014
baron_l <baron_l@epitech.eu>
2018-08-06 16:24:05 +0200

    feat(services): constraints on node/engine labels are now supported

--- app/docker/helpers/constraintsHelper.js
+++ app/docker/helpers/constraintsHelper.js
@@ -3,9 +3,10 @@ var ConstraintOperator = {
   NEQ: 'neq'
 };
 
-function ConstraintModel(op, value) {
+function ConstraintModel(op, key, value) {
   this.op = op;
   this.value = value;
+  this.key = key;
 }
 // node.id        node.id==2ivku8v2gvtg4
 // node.hostname  node.hostname!=node-2
@@ -17,24 +18,37 @@ var patterns = {
   id: {
     nodeId: /(node\.id)/,
     nodeHostname: /(node\.hostname)/,
-    nodeRole: /(node\.role)/
+    nodeRole: /(node\.role)/,
+    nodeLabels: /(node\.labels)/,
+    engineLabels: /(node\.labels)/
   },
   op: {
+    both: /(\s?(=|!)=\s?)/,
     eq: /(\s?==\s?)/,
     neq: /(\s?!=\s?)/
   },
-  value: /(node\.(id|hostname|role)\s?(=|!)=\s?)/
+  labelKey: /((node|engine)\.labels\.)/,
+  value: /((node\.(id|hostname|role|(labels\..*)))|(engine\.labels\..*))\s?(=|!)=\s?/
 };
 
 function matchesConstraint(value, constraint) {
-  if (constraint) {
-    if ((constraint.op === ConstraintOperator.EQ &&
+  if (constraint && ((constraint.op === ConstraintOperator.EQ &&
         value !== constraint.value) ||
       (constraint.op === ConstraintOperator.NEQ &&
-        value === constraint.value))
-      return false;
+        value === constraint.value))) {
+    return false;
   }
   return true;
+}
+
+function matchesLabel(labels, constraint) {
+  if (!constraint) {
+    return true;
+  }
+  var found = labels.find(function (label) {
+    return label.key === constraint.key && label.value === constraint.value;
+  });
+  return found !== undefined;
 }
 
 angular.module('portainer.docker')
@@ -44,32 +58,44 @@ angular.module('portainer.docker')
       transformConstraints: function (constraints) {
         var transform = {};
         angular.forEach(constraints, function (cons) {
-          var value = cons.replace(patterns.value, '');
+          var value = cons.replace(patterns.value, '').trim();
+          var key = cons.replace(patterns.labelKey, '').replace(patterns.op.both, '').replace(value, '');
           var op;
 
-          if (cons.match(patterns.op.eq))
+          if (cons.match(patterns.op.eq)) {
             op = ConstraintOperator.EQ;
-          else if (cons.match(patterns.op.NEQ))
+          } else if (cons.match(patterns.op.NEQ)) {
             op = ConstraintOperator.NEQ;
-          if (cons.match(patterns.id.nodeId))
-            transform.nodeId = new ConstraintModel(op, value);
-          else if (cons.match(patterns.id.nodeHostname))
-            transform.nodeHostname = new ConstraintModel(op, value);
-          else if (cons.match(patterns.id.nodeRole))
-            transform.nodeRole = new ConstraintModel(op, value);
+          }
+
+          if (cons.match(patterns.id.nodeId)) {
+            transform.nodeId = new ConstraintModel(op, key, value);
+          } else if (cons.match(patterns.id.nodeHostname)) {
+            transform.nodeHostname = new ConstraintModel(op, key, value);
+          } else if (cons.match(patterns.id.nodeRole)) {
+            transform.nodeRole = new ConstraintModel(op, key, value);
+          } else if (cons.match(patterns.id.nodeLabels)) {
+            transform.nodeLabels = new ConstraintModel(op, key, value);
+          } else if (cons.match(patterns.id.engineLabels)) {
+            transform.engineLabels = new ConstraintModel(op, key, value);
+          }
         });
         return transform;
       },
 
       matchesServiceConstraints: function (service, node) {
-        if (service.Constraints === undefined || service.Constraints.length === 0) return true;
+        if (service.Constraints === undefined || service.Constraints.length === 0) {
+          return true;
+        }
         var constraints = this.transformConstraints(angular.copy(service.Constraints));
-
         if (matchesConstraint(node.Id, constraints.nodeId) &&
           matchesConstraint(node.Hostname, constraints.nodeHostname) &&
-          matchesConstraint(node.Role, constraints.nodeRole)
-        )
+          matchesConstraint(node.Role, constraints.nodeRole) &&
+          matchesLabel(node.Labels, constraints.nodeLabels) &&
+          matchesLabel(node.EngineLabels, constraints.engineLabels)
+        ) {
           return true;
+        }
         return false;
       }
     };
