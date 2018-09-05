d612ec9cee22e06f07e0f542229219a0cd640255
Chaim Lando <chiptus@gmail.com>
2018-09-05 12:23:30 +0300

    feat(engine-details): add host-view container component

--- app/docker/components/host-overview/host-overview-controller.js
+++ app/docker/components/host-overview/host-overview-controller.js
@@ -1,22 +1,3 @@
-angular.module('portainer.docker')
-.controller('HostOverviewController', ['$q', '$scope', 'SystemService', 'Notifications',
-function HostOverviewController($q, $scope, SystemService, Notifications) {
-
-  function initView() {
-    $q.all({
-      version: SystemService.version(),
-      info: SystemService.info()
-    })
-    .then(function success(data) {
-      $scope.version = data.version;
-      $scope.info = data.info;
-    })
-    .catch(function error(err) {
-      $scope.info = {};
-      $scope.version = {};
-      Notifications.error('Failure', err, 'Unable to retrieve engine details');
-    });
-  }
-
-  initView();
-}]);
+angular
+  .module('portainer.docker')
+  .controller('HostOverviewController', [function HostOverviewController() {}]);
---
+++ app/docker/views/host/host-view-controller.js
@@ -0,0 +1,49 @@
+angular.module('portainer.docker').controller('HostViewController', [
+  '$q',
+  '$scope',
+  'SystemService',
+  'Notifications',
+  function HostViewController($q, $scope, SystemService, Notifications) {
+    var ctrl = this;
+    this.$onInit = initView;
+
+    this.engineDetails = {};
+    this.hostDetails = {};
+
+    function initView() {
+      $q.all({
+        version: SystemService.version(),
+        info: SystemService.info()
+      })
+        .then(function success(data) {
+          ctrl.engineDetails = buildEngineDetails(data);
+          ctrl.hostDetails = buildHostDetails(data);
+        })
+        .catch(function error(err) {
+          Notifications.error(
+            'Failure',
+            err,
+            'Unable to retrieve engine details'
+          );
+        });
+    }
+
+    function buildEngineDetails(data) {
+      var versionDetails = data.version;
+      var info = data.info;
+      return {
+        releaseVersion: versionDetails.Version,
+        apiVersion: versionDetails.ApiVersion,
+        rootDirectory: info.DockerRootDir,
+        storageDriver: info.Driver,
+        loggingDriver: info.LoggingDriver,
+        volumePlugins: info.Plugins.Volume,
+        networkPlugins: info.Plugins.Network
+      };
+    }
+
+    function buildHostDetails() {
+      return {};
+    }
+  }
+]);
