77fabe6d5a3eb3fea4c36f8af48304fe9f8ba6a5
Chaim Lando <chiptus@gmail.com>
2018-09-17 11:24:08 +0300

    feat(stack-details): combine migration and duplication forms

---
+++ app/portainer/components/stack-duplication-form/stack-duplication-form-controller.js
@@ -0,0 +1,60 @@
+angular.module('portainer.app').controller('StackDuplicationFormController', [
+  'Notifications',
+  function StackDuplicationFormController(Notifications) {
+    var ctrl = this;
+
+    ctrl.state = {
+      duplicationInProgress: false,
+      migrationInProgress: false
+    };
+
+    ctrl.formValues = {
+      endpoint: null,
+      newName: ''
+    };
+
+    ctrl.isFormValidForDuplication = isFormValidForDuplication;
+    ctrl.isFormValidForMigration = isFormValidForMigration;
+    ctrl.duplicateStack = duplicateStack;
+    ctrl.migrateStack = migrateStack;
+
+    function isFormValidForMigration() {
+      return (
+        ctrl.formValues.endpoint &&
+        ctrl.formValues.endpoint.Id 
+      );
+    }
+
+    function isFormValidForDuplication() {
+      return isFormValidForMigration() && ctrl.formValues.newName;
+    }
+
+    function duplicateStack() {
+      if (!ctrl.formValues.newName) {
+        Notifications.error('Failure', null, 'Stack name is required for duplication');
+        return;
+      }
+      ctrl.state.duplicationInProgress = true;
+      ctrl
+        .onDuplicate({
+          endpointId: ctrl.formValues.endpoint.Id,
+          name: ctrl.formValues.newName ? ctrl.formValues.newName : undefined
+        })
+        .finally(function() {
+          ctrl.state.duplicationInProgress = false;
+        });
+    }
+
+    function migrateStack() {
+      ctrl.state.migrationInProgress = true;
+      ctrl
+        .onMigrate({
+          endpointId: ctrl.formValues.endpoint.Id,
+          name: ctrl.formValues.newName ? ctrl.formValues.newName : undefined
+        })
+        .finally(function() {
+          ctrl.state.migrationInProgress = false;
+        });
+    }
+  }
+]);
---
+++ app/portainer/components/stack-duplication-form/stack-duplication-form.js
@@ -0,0 +1,11 @@
+angular.module('portainer.app').component('stackDuplicationForm', {
+  templateUrl:
+    'app/portainer/components/stack-duplication-form/stack-duplication-form.html',
+  controller: 'StackDuplicationFormController',
+  bindings: {
+    onDuplicate: '&',
+    onMigrate: '&',
+    endpoints: '<',
+    groups: '<'
+  }
+});
