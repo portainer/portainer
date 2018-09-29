344810f099a825606466fb6349c3fc54dbded19e
ssbkang <skan070@gmail.com>
2018-09-29 20:11:41 +1200

    Update build_in_container.ps1

--- build/build_in_container.ps1
+++ build/build_in_container.ps1
@@ -5,7 +5,9 @@ param (
 
 $binary = "portainer-$($platform)-$($arch)"
 
-New-Item -Name dist -ItemType Directory
+New-Item -Name dist -ItemType Directory | Out-Null
+
+docker pull portainer/golang-builder:cross-platform
 
 docker run --rm -tv "$(pwd)\api:/src" -e BUILD_GOOS="$($platform)" -e BUILD_GOARCH="$($arch)" portainer/golang-builder:cross-platform /src/cmd/portainer
 
