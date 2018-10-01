2f1ac94943a708a8ccd376476465b88fd5fe091e
ssbkang <skan070@gmail.com>
2018-10-01 15:00:07 +1300

    Update build_in_container.ps1

--- build/build_in_container.ps1
+++ build/build_in_container.ps1
@@ -20,6 +20,10 @@ Set-Location -Path "C:\projects\portainer\api\cmd\portainer"
 C:\go\bin\go.exe get -t -d -v ./...
 C:\go\bin\go.exe build -v
 
+ls "C:\projects\portainer\api\cmd\portainer"
+
+Move-Item -Path "C:\projects\portainer\api\cmd\portainer\$($binary)" -Destination "C:\projects\portainer\dist"
+
 ls C:\projects\portainer\dist
 
 #docker run -e CGO_ENABLED=0 -v "C:\projects\portainer\api:C:\gopath" -w C:\gopath\cmd\portainer golang:1.10.4-windowsservercore-ltsc2016 ls C:\gopath; ls C:\gopath\cmd\portainer; ls C:\gopath\src; go get -t -d -v ./...; go build -v
