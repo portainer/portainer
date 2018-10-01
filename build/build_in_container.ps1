fa957d5395f9b0bbe729f68488725c3d367f678d
ssbkang <skan070@gmail.com>
2018-10-01 15:03:37 +1300

    Update build_in_container.ps1

--- build/build_in_container.ps1
+++ build/build_in_container.ps1
@@ -7,13 +7,11 @@ $binary = "portainer-$($platform)-$($arch)"
 
 Set-Item env:GOPATH "C:\projects\portainer\api"
 
-New-Item -Name dist -Path "C:\projects\portainer" -ItemType Directory
-New-Item -Name portainer -Path "C:\projects\portainer\api\src\github.com\" -ItemType Directory
+New-Item -Name dist -Path "C:\projects\portainer" -ItemType Directory | Out-Null
+New-Item -Name portainer -Path "C:\projects\portainer\api\src\github.com\" -ItemType Directory | Out-Null
 
 Copy-Item -Path "C:\projects\portainer\api" -Destination "C:\projects\portainer\api\src\github.com\portainer" -Recurse -Force -ErrorAction:SilentlyContinue
 Rename-Item -Path "C:\projects\portainer\api\src\github.com\portainer\api" -NewName "portainer" -ErrorAction:SilentlyContinue
-
-ls "C:\projects\portainer\api\src\github.com\portainer\portainer\"
 
 Set-Location -Path "C:\projects\portainer\api\cmd\portainer"
 
