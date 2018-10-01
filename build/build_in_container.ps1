9fd936c49d9db5df53885c27707645e88c497db8
ssbkang <skan070@gmail.com>
2018-10-01 13:08:43 +1300

    Update build_in_container.ps1

--- build/build_in_container.ps1
+++ build/build_in_container.ps1
@@ -10,6 +10,6 @@ New-Item -Name dist -Path "C:\projects\portainer" -ItemType Directory
 ls "C:\projects\portainer"
 ls "C:\projects\portainer\api"
 
-docker run -e CGO_ENABLED=0 -e GOPATH=C:\gopath -v "C:\projects\portainer\api:C:\gopath" -w C:\gopath\cmd\portainer golang:1.10.4-windowsservercore-ltsc2016 go get -t -d -v ./...; go build -v
+docker run -e CGO_ENABLED=0 -v "C:\projects\portainer\api:C:\gopath" -w C:\gopath\cmd\portainer golang:1.10.4-windowsservercore-ltsc2016 ls C:\gopath; ls C:\gopath\cmd\portainer; ls C:\gopath\src; go get -t -d -v ./...; go build -v
 
 Move-Item -Path "C:\projects\portainer\api\cmd\portainer\$($binary)" -Destination "C:\projects\portainer\dist"
