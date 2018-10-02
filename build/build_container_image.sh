8a4e540e26d7e623203d558e774db186a0897722
ssbkang <skan070@gmail.com>
2018-10-03 12:12:31 +1300

    Test

--- build/build_container_image.sh
+++ build/build_container_image.sh
@@ -1,14 +1,5 @@
-
-echo $IMAGE
-echo $ARCH
-echo $PORTAINER_VERSION
-
-echo "${IMAGE}"
-echo "${ARCH}"
-echo "${PORTAINER_VERSION}"
-
-#docker build -t "ssbkang/portainer:${IMAGE}-${ARCH}-${PORTAINER_VERSION}" -f build/linux/Dockerfile .
-#docker tag "ssbkang/portainer:${IMAGE}-${ARCH}-${PORTAINER_VERSION}" "ssbkang/portainer:${IMAGE}-${ARCH}"
-#docker login -u "${DOCKER_USER}" -p "${DOCKER_PASS}"
-#docker push "ssbkang/portainer:${IMAGE}-${ARCH}-${PORTAINER_VERSION}"
-#docker push "ssbkang/portainer:${IMAGE}-${ARCH}"
+docker build -t "ssbkang/portainer:$1-$2-$3" -f build/linux/Dockerfile .
+docker tag "ssbkang/portainer:$1-$2-$3" "ssbkang/portainer:$1-$2"
+docker login -u "$4" -p "$4"
+docker push "ssbkang/portainer:$1-$2-$3"
+docker push "ssbkang/portainer:$1-$2"
