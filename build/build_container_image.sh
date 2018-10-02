3d0e8f109dff48a36c9d738ff652c33ba476e6d1
ssbkang <skan070@gmail.com>
2018-10-03 12:00:02 +1300

    Echo Testing

--- build/build_container_image.sh
+++ build/build_container_image.sh
@@ -1,5 +1,14 @@
-docker build -t "ssbkang/portainer:${IMAGE}-${ARCH}-${PORTAINER_VERSION}" -f build/linux/Dockerfile .
-docker tag "ssbkang/portainer:${IMAGE}-${ARCH}-${PORTAINER_VERSION}" "ssbkang/portainer:${IMAGE}-${ARCH}"
-docker login -u "${DOCKER_USER}" -p "${DOCKER_PASS}"
-docker push "ssbkang/portainer:${IMAGE}-${ARCH}-${PORTAINER_VERSION}"
-docker push "ssbkang/portainer:${IMAGE}-${ARCH}"
+
+echo $IMAGE
+echo $ARCH
+echo $PORTAINER_VERSION
+
+echo "${IMAGE}"
+echo "${ARCH}"
+echo "${PORTAINER_VERSION}"
+
+#docker build -t "ssbkang/portainer:${IMAGE}-${ARCH}-${PORTAINER_VERSION}" -f build/linux/Dockerfile .
+#docker tag "ssbkang/portainer:${IMAGE}-${ARCH}-${PORTAINER_VERSION}" "ssbkang/portainer:${IMAGE}-${ARCH}"
+#docker login -u "${DOCKER_USER}" -p "${DOCKER_PASS}"
+#docker push "ssbkang/portainer:${IMAGE}-${ARCH}-${PORTAINER_VERSION}"
+#docker push "ssbkang/portainer:${IMAGE}-${ARCH}"
