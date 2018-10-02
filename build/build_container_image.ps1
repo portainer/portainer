1d6ece351208dc27516336edcf87b58bdf6f848c
ssbkang <skan070@gmail.com>
2018-10-03 10:30:54 +1300

    Clean-up Appveyor.yml

---
+++ build/build_container_image.ps1
@@ -0,0 +1,9 @@
+docker build -t ssbkang/portainer:$((Get-Item ENV:IMAGE).Value)-$((Get-Item ENV:ARCH).Value)-$((Get-Item ENV:PORTAINER_VERSION).Value) -f build\windows2016\nanoserver\Dockerfile .
+docker tag ssbkang/portainer:$((Get-Item ENV:IMAGE).Value)-$((Get-Item ENV:ARCH).Value)-$((Get-Item ENV:PORTAINER_VERSION).Value) ssbkang/portainer:$((Get-Item ENV:IMAGE).Value)-$((Get-Item ENV:ARCH).Value)
+docker login -u "$((Get-Item ENV:DOCKER_USER).Value)" -p "$((Get-Item ENV:DOCKER_PASS).Value)"
+docker push ssbkang/portainer:$((Get-Item ENV:IMAGE).Value)-$((Get-Item ENV:ARCH).Value)-$((Get-Item ENV:PORTAINER_VERSION).Value)
+docker push ssbkang/portainer:$((Get-Item ENV:IMAGE).Value)-$((Get-Item ENV:ARCH).Value)
+rebase-docker-image ssbkang/portainer:$((Get-Item ENV:IMAGE).Value)-$((Get-Item ENV:ARCH).Value) -t ssbkang/portainer:$((Get-Item ENV:IMAGE).Value)1709-$((Get-Item ENV:ARCH).Value)-$((Get-Item ENV:PORTAINER_VERSION).Value) -b microsoft/nanoserver:1709
+rebase-docker-image ssbkang/portainer:$((Get-Item ENV:IMAGE).Value)-$((Get-Item ENV:ARCH).Value) -t ssbkang/portainer:$((Get-Item ENV:IMAGE).Value)1709-$((Get-Item ENV:ARCH).Value) -b microsoft/nanoserver:1709
+rebase-docker-image ssbkang/portainer:$((Get-Item ENV:IMAGE).Value)-$((Get-Item ENV:ARCH).Value) -t ssbkang/portainer:$((Get-Item ENV:IMAGE).Value)1803-$((Get-Item ENV:ARCH).Value)-$((Get-Item ENV:PORTAINER_VERSION).Value) -b microsoft/nanoserver:1803
+rebase-docker-image ssbkang/portainer:$((Get-Item ENV:IMAGE).Value)-$((Get-Item ENV:ARCH).Value) -t ssbkang/portainer:$((Get-Item ENV:IMAGE).Value)1803-$((Get-Item ENV:ARCH).Value) -b microsoft/nanoserver:1803
---
+++ build/build_container_image.sh
@@ -0,0 +1,5 @@
+docker build -t ssbkang/portainer:$IMAGE-$ARCH-$PORTAINER_VERSION -f build/linux/Dockerfile .
+docker tag ssbkang/portainer:$IMAGE-$ARCH-$PORTAINER_VERSION ssbkang/portainer:$IMAGE-$ARCH
+docker login -u $DOCKER_USER -p $DOCKER_PASS
+docker push ssbkang/portainer:$IMAGE-$ARCH-$PORTAINER_VERSION
+docker push ssbkang/portainer:$IMAGE-$ARCH
