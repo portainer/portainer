6eda62ccb69ad73454810778526760cc8e79e465
ssbkang <skan070@gmail.com>
2018-10-03 12:15:45 +1300

    Fix variable

--- build/build_container_image.sh
+++ build/build_container_image.sh
@@ -1,5 +1,5 @@
 docker build -t "ssbkang/portainer:$1-$2-$3" -f build/linux/Dockerfile .
 docker tag "ssbkang/portainer:$1-$2-$3" "ssbkang/portainer:$1-$2"
-docker login -u "$4" -p "$4"
+docker login -u "$4" -p "$5"
 docker push "ssbkang/portainer:$1-$2-$3"
 docker push "ssbkang/portainer:$1-$2"
