089d919ca63e9b18ae8a1005b60e3cefa659494b
ssbkang <skan070@gmail.com>
2018-10-03 20:22:45 +1300

    Artifact Testing

--- build/build_container_image.ps1
+++ build/build_container_image.ps1
@@ -2,8 +2,6 @@ New-Item -Path portainer -ItemType Directory | Out-Null
 Copy-Item -Path dist\* -Destination portainer -Recurse
 tar -cvpf "portainer-$((Get-Item ENV:PORTAINER_VERSION).Value)-$((Get-Item ENV:IMAGE).Value)-$((Get-Item ENV:ARCH).Value).tar.gz" portainer
 
-ls
-
 docker build `
     -t ssbkang/portainer:$((Get-Item ENV:IMAGE).Value)-$((Get-Item ENV:ARCH).Value)-$((Get-Item ENV:PORTAINER_VERSION).Value) `
     -f build\windows2016\nanoserver\Dockerfile .
