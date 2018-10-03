ea3fbbca3bdf9dfda6cdf3bca6864ffe9b289b5d
ssbkang <skan070@gmail.com>
2018-10-03 22:19:10 +1300

    Fix the binary issue

--- build/build_in_container.sh
+++ build/build_in_container.sh
@@ -1,7 +1,7 @@
-binary="portainer"
+binary="portainer-$1-$2"
 
 mkdir -p dist
 
 docker run --rm -tv "$(pwd)/api:/src" -e BUILD_GOOS="$1" -e BUILD_GOARCH="$2" portainer/golang-builder:cross-platform /src/cmd/portainer
 
-mv "api/cmd/portainer/$binary" dist/
+mv "api/cmd/portainer/$binary" dist/portiner
