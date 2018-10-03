5b79e4ec716cd67b9fab0df6ca791997ec3d1259
ssbkang <skan070@gmail.com>
2018-10-03 15:01:11 +1300

    Manifest Test

--- build/build_container_image.sh
+++ build/build_container_image.sh
@@ -5,11 +5,8 @@ docker push "ssbkang/portainer:$1-$2-$3"
 docker push "ssbkang/portainer:$1-$2"
 
 if [ "${2}" == 's390x' ] ; then
-  ls ~/.docker
-  cat ~/.docker/config.json
+  go get github.com/estesp/manifest-tool
   git clone -q --branch=master $6 /home/appveyor/projects/docker-manifest
-  ls /home/appveyor/projects/docker-manifest
-  ls /home/appveyor/projects/docker-manifest/portainer
-  docker manifest push /home/appveyor/projects/docker-manifest/portainer-1-19-2.yml
-  docker manifest push /home/appveyor/projects/docker-manifest/portainer.yml
+  manifest-tool push from-spec /home/appveyor/projects/docker-manifest/portainer-1-19-2.yml
+  manifest-tool push from-spec /home/appveyor/projects/docker-manifest/portainer.yml
 fi
