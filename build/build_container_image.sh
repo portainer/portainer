376ff70faabf75444767a5a26da07c11b3ae158d
ssbkang <skan070@gmail.com>
2018-10-03 14:47:32 +1300

    Manifest Test

--- build/build_container_image.sh
+++ build/build_container_image.sh
@@ -6,6 +6,8 @@ docker push "ssbkang/portainer:$1-$2"
 
 if [ "${2}" == 's390x' ] ; then
   git clone -q --branch=master $6 /home/appveyor/projects/docker-manifest
+  ls /home/appveyor/projects/docker-manifest
+  ls /home/appveyor/projects/docker-manifest/portainer
   docker manifest push /home/appveyor/projects/docker-manifest/portainer-1-19-2.yml
   docker manifest push /home/appveyor/projects/docker-manifest/portainer.yml
 fi
