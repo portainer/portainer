7683cc583cbc9d0e2371e794f84c51801e7bfc0e
ssbkang <skan070@gmail.com>
2018-10-04 20:21:04 +1300

    Echo

--- build/build_container_image.sh
+++ build/build_container_image.sh
@@ -6,9 +6,15 @@ DOCKER_PASS="$5"
 GITHUB_MANIFEST_URL="$6"
 APPVEYOR_PULL_REQUEST_NUMBER="$7"
 
+echo "${IMAGE}"
+echo "${ARCH}"
+echo "${PORTAINER_VERSION}"
+echo "${DOCKER_USER}"
+echo "${DOCKER_PASS}"
+echo "${GITHUB_MANIFEST_URL}"
 echo "${APPVEYOR_PULL_REQUEST_NUMBER}"
 
-if [ ! -z "${APPVEYOR_PULL_REQUEST_NUMBER}" ] ; then
+if [ ! -z "${7}" ] ; then
   tag="pr${APPVEYOR_PULL_REQUEST_NUMBER}"
   docker build -t "ssbkang/portainer:$tag" -f build/linux/Dockerfile .
   docker login -u "${DOCKER_USER}" -p "${DOCKER_PASS}"
