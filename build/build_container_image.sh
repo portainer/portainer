713a0c007f6ca4ace1d58b4e7c2380995d60c6b0
ssbkang <skan070@gmail.com>
2018-10-04 19:20:28 +1300

    CI Testing

--- build/build_container_image.sh
+++ build/build_container_image.sh
@@ -1,4 +1,9 @@
-if [ -z "${7}"] ; then
+if [ ${7} ] ; then
+  tag="pr$7"
+  docker build -t "ssbkang/portainer:$tag" -f build/linux/Dockerfile .
+  docker login -u "$4" -p "$5"
+  docker push "ssbkang/portainer:$tag"
+else
   mkdir -pv portainer
   cp -r dist/* portainer
   tar cvpfz "portainer-$3-$1-$2.tar.gz" portainer
@@ -8,7 +13,7 @@ if [ -z "${7}"] ; then
   docker tag "ssbkang/portainer:$1-$2-$3" "ssbkang/portainer:$1-$2"
   docker login -u "$4" -p "$5"
   docker push "ssbkang/portainer:$1-$2-$3"
-  docker push "ssbkang/portainer:$1-$2"
+  docker push "ssbkang/portainer:$1-$2" 
 
   if [ "${2}" == 's390x' ] ; then
     wget https://github.com/estesp/manifest-tool/releases/download/v0.8.0/manifest-tool-linux-amd64
@@ -19,9 +24,5 @@ if [ -z "${7}"] ; then
     ./manifest-tool-linux-amd64 push from-spec /home/appveyor/projects/docker-manifest/portainer/portainer-1-19-2.yml
     ./manifest-tool-linux-amd64 push from-spec /home/appveyor/projects/docker-manifest/portainer/portainer.yml
   fi
-else
-  tag="pr$7"
-  docker build -t "ssbkang/portainer:$tag" -f build/linux/Dockerfile .
-  docker login -u "$4" -p "$5"
-  docker push "ssbkang/portainer:$tag"
 fi
+
