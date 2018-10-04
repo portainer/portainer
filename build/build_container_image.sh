9608d7788c6a64be270e93c53f0ec5560eae5b1f
ssbkang <skan070@gmail.com>
2018-10-04 18:08:16 +1300

    Update the build_container_image.sh with PR support

--- build/build_container_image.sh
+++ build/build_container_image.sh
@@ -1,19 +1,21 @@
-mkdir -pv portainer
-cp -r dist/* portainer
-tar cvpfz "portainer-$3-$1-$2.tar.gz" portainer
+if [ -z "$7"] ; then
+  if [ "${2}" == 's390x' ] ; then
+    wget https://github.com/estesp/manifest-tool/releases/download/v0.8.0/manifest-tool-linux-amd64
+    git clone -q --branch=master $6 /home/appveyor/projects/docker-manifest
+
+    chmod 755 manifest-tool-linux-amd64
+    
+    ./manifest-tool-linux-amd64 push from-spec /home/appveyor/projects/docker-manifest/portainer/portainer-1-19-2.yml
+    ./manifest-tool-linux-amd64 push from-spec /home/appveyor/projects/docker-manifest/portainer/portainer.yml
+  fi
+
+  mkdir -pv portainer
+  cp -r dist/* portainer
+  tar cvpfz "portainer-$3-$1-$2.tar.gz" portainer
+fi
 
 docker build -t "ssbkang/portainer:$1-$2-$3" -f build/linux/Dockerfile .
 docker tag "ssbkang/portainer:$1-$2-$3" "ssbkang/portainer:$1-$2"
 docker login -u "$4" -p "$5"
 docker push "ssbkang/portainer:$1-$2-$3"
 docker push "ssbkang/portainer:$1-$2"
-
-if [ "${2}" == 's390x' ] ; then
-  wget https://github.com/estesp/manifest-tool/releases/download/v0.8.0/manifest-tool-linux-amd64
-  git clone -q --branch=master $6 /home/appveyor/projects/docker-manifest
-
-  chmod 755 manifest-tool-linux-amd64
-  
-  ./manifest-tool-linux-amd64 push from-spec /home/appveyor/projects/docker-manifest/portainer/portainer-1-19-2.yml
-  ./manifest-tool-linux-amd64 push from-spec /home/appveyor/projects/docker-manifest/portainer/portainer.yml
-fi
