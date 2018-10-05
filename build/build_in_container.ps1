2444f629018977db860f7ae3e26047514c84c3d5
ssbkang <skan070@gmail.com>
2018-10-05 16:06:36 +1300

    Multi Arch for CI

--- build/build_in_container.ps1
+++ build/build_in_container.ps1
@@ -7,17 +7,17 @@ $ErrorActionPreference = "Stop";
 
 $binary = "portainer.exe"
 
-Set-Item env:GOPATH "C:\projects\portainer\api"
+Set-Item env:GOPATH "$PWD\api"
 
-New-Item -Name dist -Path "C:\projects\portainer" -ItemType Directory | Out-Null
-New-Item -Name portainer -Path "C:\projects\portainer\api\src\github.com\" -ItemType Directory | Out-Null
+New-Item -Name dist -Path "$PWD" -ItemType Directory | Out-Null
+New-Item -Name portainer -Path "$PWD\api\src\github.com\" -ItemType Directory | Out-Null
 
-Copy-Item -Path "C:\projects\portainer\api" -Destination "C:\projects\portainer\api\src\github.com\portainer" -Recurse -Force -ErrorAction:SilentlyContinue
-Rename-Item -Path "C:\projects\portainer\api\src\github.com\portainer\api" -NewName "portainer" -ErrorAction:SilentlyContinue
+Copy-Item -Path "$PWD\api" -Destination "$PWD\api\src\github.com\portainer" -Recurse -Force -ErrorAction:SilentlyContinue
+Rename-Item -Path "$PWD\api\src\github.com\portainer\api" -NewName "portainer" -ErrorAction:SilentlyContinue
 
-Set-Location -Path "C:\projects\portainer\api\cmd\portainer"
+Set-Location -Path "$PWD\api\cmd\portainer"
 
 C:\go\bin\go.exe get -t -d -v ./...
 C:\go\bin\go.exe build -v
 
-Move-Item -Path "C:\projects\portainer\api\cmd\portainer\$($binary)" -Destination "C:\projects\portainer\dist"
+Move-Item -Path "$PWD\api\cmd\portainer\$($binary)" -Destination "$PWD\dist"
