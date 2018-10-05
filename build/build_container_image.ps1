5b4972809e0f2cf3ae4e2568574c4b4968329234
ssbkang <skan070@gmail.com>
2018-10-05 15:33:37 +1300

    Replace manifest with Docker Manifest

--- build/build_container_image.ps1
+++ build/build_container_image.ps1
@@ -1,38 +1,42 @@
 $ErrorActionPreference = "Stop";
 
-New-Item -Path portainer -ItemType Directory | Out-Null
-Copy-Item -Path dist\* -Destination portainer -Recurse
-tar cvpfz "portainer-$((Get-Item ENV:PORTAINER_VERSION).Value)-$((Get-Item ENV:IMAGE).Value)-$((Get-Item ENV:ARCH).Value).tar.gz" portainer
+if ($ENV:APPVEYOR_PULL_REQUEST_NUMBER) {
 
-docker build `
-    -t ssbkang/portainer:$((Get-Item ENV:IMAGE).Value)-$((Get-Item ENV:ARCH).Value)-$((Get-Item ENV:PORTAINER_VERSION).Value) `
-    -f build\windows2016\nanoserver\Dockerfile .
-    
-docker tag ssbkang/portainer:$((Get-Item ENV:IMAGE).Value)-$((Get-Item ENV:ARCH).Value)-$((Get-Item ENV:PORTAINER_VERSION).Value) ssbkang/portainer:$((Get-Item ENV:IMAGE).Value)-$((Get-Item ENV:ARCH).Value)
+} else {
+    New-Item -Path portainer -ItemType Directory | Out-Null
+    Copy-Item -Path dist\* -Destination portainer -Recurse
+    tar cvpfz "portainer-$((Get-Item ENV:PORTAINER_VERSION).Value)-$((Get-Item ENV:IMAGE).Value)-$((Get-Item ENV:ARCH).Value).tar.gz" portainer
 
-docker login `
-    -u "$((Get-Item ENV:DOCKER_USER).Value)" `
-    -p "$((Get-Item ENV:DOCKER_PASS).Value)"
+    docker build `
+        -t ssbkang/portainer:$((Get-Item ENV:IMAGE).Value)-$((Get-Item ENV:ARCH).Value)-$((Get-Item ENV:PORTAINER_VERSION).Value) `
+        -f build\windows2016\nanoserver\Dockerfile .
+        
+    docker tag ssbkang/portainer:$((Get-Item ENV:IMAGE).Value)-$((Get-Item ENV:ARCH).Value)-$((Get-Item ENV:PORTAINER_VERSION).Value) ssbkang/portainer:$((Get-Item ENV:IMAGE).Value)-$((Get-Item ENV:ARCH).Value)
 
-docker push ssbkang/portainer:$((Get-Item ENV:IMAGE).Value)-$((Get-Item ENV:ARCH).Value)-$((Get-Item ENV:PORTAINER_VERSION).Value)
-docker push ssbkang/portainer:$((Get-Item ENV:IMAGE).Value)-$((Get-Item ENV:ARCH).Value)
+    docker login `
+        -u "$((Get-Item ENV:DOCKER_USER).Value)" `
+        -p "$((Get-Item ENV:DOCKER_PASS).Value)"
 
-rebase-docker-image `
-    ssbkang/portainer:$((Get-Item ENV:IMAGE).Value)-$((Get-Item ENV:ARCH).Value) `
-    -t ssbkang/portainer:$((Get-Item ENV:IMAGE).Value)1709-$((Get-Item ENV:ARCH).Value)-$((Get-Item ENV:PORTAINER_VERSION).Value) `
-    -b microsoft/nanoserver:1709
+    docker push ssbkang/portainer:$((Get-Item ENV:IMAGE).Value)-$((Get-Item ENV:ARCH).Value)-$((Get-Item ENV:PORTAINER_VERSION).Value)
+    docker push ssbkang/portainer:$((Get-Item ENV:IMAGE).Value)-$((Get-Item ENV:ARCH).Value)
 
-rebase-docker-image `
-    ssbkang/portainer:$((Get-Item ENV:IMAGE).Value)-$((Get-Item ENV:ARCH).Value) `
-    -t ssbkang/portainer:$((Get-Item ENV:IMAGE).Value)1709-$((Get-Item ENV:ARCH).Value) `
-    -b microsoft/nanoserver:1709
+    rebase-docker-image `
+        ssbkang/portainer:$((Get-Item ENV:IMAGE).Value)-$((Get-Item ENV:ARCH).Value) `
+        -t ssbkang/portainer:$((Get-Item ENV:IMAGE).Value)1709-$((Get-Item ENV:ARCH).Value)-$((Get-Item ENV:PORTAINER_VERSION).Value) `
+        -b microsoft/nanoserver:1709
 
-rebase-docker-image `
-    ssbkang/portainer:$((Get-Item ENV:IMAGE).Value)-$((Get-Item ENV:ARCH).Value) `
-    -t ssbkang/portainer:$((Get-Item ENV:IMAGE).Value)1803-$((Get-Item ENV:ARCH).Value)-$((Get-Item ENV:PORTAINER_VERSION).Value) `
-    -b microsoft/nanoserver:1803
+    rebase-docker-image `
+        ssbkang/portainer:$((Get-Item ENV:IMAGE).Value)-$((Get-Item ENV:ARCH).Value) `
+        -t ssbkang/portainer:$((Get-Item ENV:IMAGE).Value)1709-$((Get-Item ENV:ARCH).Value) `
+        -b microsoft/nanoserver:1709
 
-rebase-docker-image `
-    ssbkang/portainer:$((Get-Item ENV:IMAGE).Value)-$((Get-Item ENV:ARCH).Value) `
-    -t ssbkang/portainer:$((Get-Item ENV:IMAGE).Value)1803-$((Get-Item ENV:ARCH).Value) `
-    -b microsoft/nanoserver:1803
+    rebase-docker-image `
+        ssbkang/portainer:$((Get-Item ENV:IMAGE).Value)-$((Get-Item ENV:ARCH).Value) `
+        -t ssbkang/portainer:$((Get-Item ENV:IMAGE).Value)1803-$((Get-Item ENV:ARCH).Value)-$((Get-Item ENV:PORTAINER_VERSION).Value) `
+        -b microsoft/nanoserver:1803
+
+    rebase-docker-image `
+        ssbkang/portainer:$((Get-Item ENV:IMAGE).Value)-$((Get-Item ENV:ARCH).Value) `
+        -t ssbkang/portainer:$((Get-Item ENV:IMAGE).Value)1803-$((Get-Item ENV:ARCH).Value) `
+        -b microsoft/nanoserver:1803
+}
