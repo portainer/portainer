d8796cc8b5ed177aa899c0280f8a6c332e0a653e
baron_l <baron_l@epitech.eu>
2018-10-15 23:21:21 +0200

    refactor(offline): remove redundant binding of informationPanel + endpointStatusInterceptor patter as service

--- app/portainer/components/information-panel/information-panel.js
+++ app/portainer/components/information-panel/information-panel.js
@@ -2,8 +2,7 @@ angular.module('portainer.app').component('informationPanel', {
   templateUrl: 'app/portainer/components/information-panel/informationPanel.html',
   bindings: {
     titleText: '@',
-    dismissAction: '&',
-    cantDismiss: '<'
+    dismissAction: '&?'
   },
   transclude: true
 });
