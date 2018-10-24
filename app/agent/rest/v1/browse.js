ee5df0b69885fe9e7386c4ba979d87a10f5a0f92
Chaim Lev-Ari <chiptus@gmail.com>
2018-10-24 17:08:29 +0300

    fix(volumes): add volumeID to browse api path

--- app/agent/rest/v1/browse.js
+++ app/agent/rest/v1/browse.js
@@ -1,7 +1,7 @@
 angular.module('portainer.agent')
 .factory('BrowseVersion1', ['$resource', 'API_ENDPOINT_ENDPOINTS', 'EndpointProvider', function BrowseFactory($resource, API_ENDPOINT_ENDPOINTS, EndpointProvider) {
   'use strict';
-  return $resource(API_ENDPOINT_ENDPOINTS + '/:endpointId/docker/browse/:action', {
+  return $resource(API_ENDPOINT_ENDPOINTS + '/:endpointId/docker/browse/:volumeID/:action', {
     endpointId: EndpointProvider.endpointID
   },
   {
