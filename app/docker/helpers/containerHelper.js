f64188ced89ff3b23eb7a20aa9901ce2e53cfb0f
Dmitriy Larionov <dml@crtweb.ru>
2018-10-30 18:52:49 +0500

    feat(containers): allow escaped quotes in command field

--- app/docker/helpers/containerHelper.js
+++ app/docker/helpers/containerHelper.js
@@ -4,7 +4,7 @@ angular.module('portainer.docker')
   var helper = {};
 
   helper.commandStringToArray = function(command) {
-    return splitargs(command);
+    return splitargs(command, undefined, true);
   };
 
   helper.commandArrayToString = function(array) {
