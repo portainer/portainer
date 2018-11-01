e6ff098860cbe6dce562505d838c7092c9bd2f30
Anthony Lapenna <lapenna.anthony@gmail.com>
2018-11-02 11:07:39 +1300

    feat(store): mockup 2

--- app/portainer/views/support/product/productController.js
+++ app/portainer/views/support/product/productController.js
@@ -2,6 +2,11 @@ angular.module('portainer.app')
 .controller('SupportProductController', ['$scope', '$transition$',
 function ($scope, $transition$) {
 
+  $scope.formValues = {
+    instances: 1,
+    hosts: 10
+  };
+
   function initView() {
     $scope.product = $transition$.params().product;
   }
