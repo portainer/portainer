7661b559206ca765c79afea2bbca627c650b69f6
Chaim Lev-Ari <chiptus@gmail.com>
2018-11-01 10:35:09 +0200

    fix(webpack): fix production config

--- webpack/webpack.production.js
+++ webpack/webpack.production.js
@@ -1,4 +1,6 @@
 const webpackMerge = require('webpack-merge');
 const commonConfig = require('./webpack.common.js');
 
-module.exports = webpackMerge(commonConfig);
+module.exports = webpackMerge(commonConfig, {
+  mode: 'production'
+});
