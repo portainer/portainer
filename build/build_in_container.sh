e32b9c7085274f263cf1eb4d09b20c21fcbd8b8c
ssbkang <skan070@gmail.com>
2018-11-05 10:04:21 +1300

    Update appveyor-ci and container build script

--- build/build_in_container.sh
+++ build/build_in_container.sh
@@ -1,7 +1,12 @@
-binary="portainer-$1-$2"
+binary="portainer"
 
 mkdir -p dist
 
-docker run --rm -tv "$(pwd)/api:/src" -e BUILD_GOOS="$1" -e BUILD_GOARCH="$2" portainer/golang-builder:cross-platform /src/cmd/portainer
+cd 'api/cmd/portainer'
+
+go get -t -d -v ./...
+GOOS=$1 GOARCH=$2 CGO_ENABLED=0 go build -a --installsuffix cgo --ldflags '-s'
+
+#docker run --rm -tv "$(pwd)/api:/src" -e BUILD_GOOS="$1" -e BUILD_GOARCH="$2" portainer/golang-builder:cross-platform /src/cmd/portainer
 
 mv "api/cmd/portainer/$binary" dist/portainer
