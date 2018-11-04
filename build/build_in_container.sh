698123e828517d901c9e8f99c3428a6fd754fa3c
ssbkang <skan070@gmail.com>
2018-11-05 11:09:15 +1300

    Test Go Build

--- build/build_in_container.sh
+++ build/build_in_container.sh
@@ -4,7 +4,6 @@ binary="portainer"
 mkdir -p dist
 mkdir -p api/src/github.com/portainer/
 
-echo $GOPATH
 cp -R api/ api/src/github.com/portainer/portainer/
 
 cd 'api/cmd/portainer'
@@ -14,6 +13,6 @@ GOOS=$1 GOARCH=$2 CGO_ENABLED=0 go build -a --installsuffix cgo --ldflags '-s'
 
 #docker run --rm -tv "$(pwd)/api:/src" -e BUILD_GOOS="$1" -e BUILD_GOARCH="$2" portainer/golang-builder:cross-platform /src/cmd/portainer
 
-mv "$APPVEYOR_BUILD_FOLDER/api/cmd/portainer/$binary" dist/portainer
+mv "$APPVEYOR_BUILD_FOLDER/api/cmd/portainer/$binary" "$APPVEYOR_BUILD_FOLDER/dist/portainer"
 
 sha256sum "$APPVEYOR_BUILD_FOLDER/dist/$binary" > portainer-checksum.txt
