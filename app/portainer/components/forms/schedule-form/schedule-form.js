4889b145700d0cc32e3518b5bc1e2eee35a44bb7
Anthony Lapenna <lapenna.anthony@gmail.com>
2018-11-07 11:54:53 +1300

    feat(schedules): add form validation and details about cron expression

--- app/portainer/components/forms/schedule-form/schedule-form.js
+++ app/portainer/components/forms/schedule-form/schedule-form.js
@@ -3,6 +3,21 @@ angular.module('portainer.app').component('scheduleForm', {
   controller: function() {
     var ctrl = this;
 
+    ctrl.state = {
+      formValidationError: ''
+    };
+
+    this.action = function() {
+      ctrl.state.formValidationError = '';
+
+      if (ctrl.model.Job.Method === 'editor' && ctrl.model.Job.FileContent === '') {
+        ctrl.state.formValidationError = 'Script file content must not be empty';
+        return;
+      }
+
+      ctrl.formAction();
+    };
+
     this.editorUpdate = function(cm) {
       ctrl.model.Job.FileContent = cm.getValue();
     };
