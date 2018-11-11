1b57b657bdbb26b8aadc3f607f8fc78b36c39758
Chaim Lev-Ari <chiptus@gmail.com>
2018-11-11 10:23:57 +0200

    fix(webpack): import missing htmls

--- app/docker/components/host-view-panels/swarm-node-details-panel/swarm-node-details-panel.js
+++ app/docker/components/host-view-panels/swarm-node-details-panel/swarm-node-details-panel.js
@@ -1,6 +1,5 @@
 angular.module('portainer.docker').component('swarmNodeDetailsPanel', {
-  templateUrl:
-    'app/docker/components/host-view-panels/swarm-node-details-panel/swarm-node-details-panel.html',
+  templateUrl: './swarm-node-details-panel.html',
   controller: 'SwarmNodeDetailsPanelController',
   bindings: {
     details: '<',
