4cfdcfdc78bccd0b333dd91d508d76858950b707
baron_l <baron_l@epitech.eu>
2018-11-12 17:55:47 +0100

    feat(app): global loading screen -- need tweak for low latency env

---
+++ app/portainer/components/rd-body/body.js
@@ -0,0 +1,32 @@
+angular.module('portainer.app')
+.directive('rdBody', ['$rootScope', '$timeout', function rdBody($rootScope, $timeout) {
+  var directive = {
+    scope: {
+      'ngModel': '='
+    },
+    transclude: true,
+    templateUrl: 'app/portainer/components/rd-body/body.template.html',
+    restrict: 'EA',
+    link: function(scope) {
+      var removeLoadingScreen = function() {
+        scope.viewLoading = false;
+        scope.viewLoadingTimeoutPromise = null;
+      };
+
+      scope.viewLoading = true;
+      scope.viewLoadingTimeoutPromise = $timeout(removeLoadingScreen, 1000);
+      $rootScope.$on('cfpLoadingBar:started', function() {
+        if (scope.viewLoadingTimeoutPromise) {
+          $timeout.cancel(scope.viewLoadingTimeoutPromise);
+          scope.viewLoadingTimeoutPromise = null;
+        }
+        scope.viewLoading = true;
+      });
+      
+      $rootScope.$on('cfpLoadingBar:completed', function() {
+        scope.viewLoadingTimeoutPromise = $timeout(removeLoadingScreen, 1000);
+      });
+    }
+  };
+  return directive;
+}]);
