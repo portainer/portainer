b0677b06574d155bb0bf8dd104bcb12f39db41cb
Steven Kang <skan070@gmail.com>
2018-11-15 18:43:43 +1300

    ssbkang/portainer to portainer/portainer

--- build/release-linux.sh
+++ build/release-linux.sh
@@ -12,34 +12,34 @@ sha256sum --tag "portainer-$PORTAINER_VERSION-$IMAGE-$ARCH.tar.gz" > "portainer-
 
 tag="$IMAGE-$ARCH"
 
-docker build -t "ssbkang/portainer:$IMAGE-$ARCH-$PORTAINER_VERSION" -f build/linux/Dockerfile .
-docker tag "ssbkang/portainer:$IMAGE-$ARCH-$PORTAINER_VERSION" "ssbkang/portainer:$IMAGE-$ARCH"
+docker build -t "portainer/portainer:$IMAGE-$ARCH-$PORTAINER_VERSION" -f build/linux/Dockerfile .
+docker tag "portainer/portainer:$IMAGE-$ARCH-$PORTAINER_VERSION" "portainer/portainer:$IMAGE-$ARCH"
 docker login -u "$DOCKER_USER" -p "$DOCKER_PASS"
-docker push "ssbkang/portainer:$IMAGE-$ARCH-$PORTAINER_VERSION"
-docker push "ssbkang/portainer:$IMAGE-$ARCH"
+docker push "portainer/portainer:$IMAGE-$ARCH-$PORTAINER_VERSION"
+docker push "portainer/portainer:$IMAGE-$ARCH"
 
 if [ "${2}" == 's390x' ] ; then
-  docker -D manifest create "ssbkang/portainer:latest" \
-    "ssbkang/portainer:linux-amd64" \
-    "ssbkang/portainer:linux-arm" \
-    "ssbkang/portainer:linux-arm64" \
-    "ssbkang/portainer:linux-ppc64le" \
-    "ssbkang/portainer:linux-s390x" \
-    "ssbkang/portainer:windows-amd64" \
-    "ssbkang/portainer:windows1709-amd64" \
-    "ssbkang/portainer:windows1803-amd64"
+  docker -D manifest create "portainer/portainer:latest" \
+    "portainer/portainer:linux-amd64" \
+    "portainer/portainer:linux-arm" \
+    "portainer/portainer:linux-arm64" \
+    "portainer/portainer:linux-ppc64le" \
+    "portainer/portainer:linux-s390x" \
+    "portainer/portainer:windows-amd64" \
+    "portainer/portainer:windows1709-amd64" \
+    "portainer/portainer:windows1803-amd64"
 
-  docker manifest push "ssbkang/portainer:latest"
+  docker manifest push "portainer/portainer:latest"
 
-  docker -D manifest create "ssbkang/portainer:${PORTAINER_VERSION}" \
-    "ssbkang/portainer:linux-amd64-${PORTAINER_VERSION}" \
-    "ssbkang/portainer:linux-arm-${PORTAINER_VERSION}" \
-    "ssbkang/portainer:linux-arm64-${PORTAINER_VERSION}" \
-    "ssbkang/portainer:linux-ppc64le-${PORTAINER_VERSION}" \
-    "ssbkang/portainer:linux-s390x-${PORTAINER_VERSION}" \
-    "ssbkang/portainer:windows-amd64-${PORTAINER_VERSION}" \
-    "ssbkang/portainer:windows1709-amd64-${PORTAINER_VERSION}" \
-    "ssbkang/portainer:windows1803-amd64-${PORTAINER_VERSION}"
+  docker -D manifest create "portainer/portainer:${PORTAINER_VERSION}" \
+    "portainer/portainer:linux-amd64-${PORTAINER_VERSION}" \
+    "portainer/portainer:linux-arm-${PORTAINER_VERSION}" \
+    "portainer/portainer:linux-arm64-${PORTAINER_VERSION}" \
+    "portainer/portainer:linux-ppc64le-${PORTAINER_VERSION}" \
+    "portainer/portainer:linux-s390x-${PORTAINER_VERSION}" \
+    "portainer/portainer:windows-amd64-${PORTAINER_VERSION}" \
+    "portainer/portainer:windows1709-amd64-${PORTAINER_VERSION}" \
+    "portainer/portainer:windows1803-amd64-${PORTAINER_VERSION}"
 
-  docker manifest push "ssbkang/portainer:${PORTAINER_VERSION}"
+  docker manifest push "portainer/portainer:${PORTAINER_VERSION}"
 fi
