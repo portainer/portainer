d70388f2097fce1fc18d217de075e10e6a5b30ed
Anthony Lapenna <lapenna.anthony@gmail.com>
2018-12-07 14:51:41 +1300

    feat(extensions): update support logos

--- app/portainer/views/support/product/productController.js
+++ app/portainer/views/support/product/productController.js
@@ -1,14 +1,14 @@
 angular.module('portainer.app')
-.controller('SupportProductController', ['$scope', '$transition$',
-function ($scope, $transition$) {
+  .controller('SupportProductController', ['$scope', '$transition$',
+    function($scope, $transition$) {
 
-  $scope.formValues = {
-    hostCount: 10
-  };
+      $scope.formValues = {
+        hostCount: 10
+      };
 
-  function initView() {
-    $scope.product = $transition$.params().product;
-  }
+      function initView() {
+        $scope.product = $transition$.params().product;
+      }
 
-  initView();
-}]);
+      initView();
+    }]);
