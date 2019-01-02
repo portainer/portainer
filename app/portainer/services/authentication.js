38f24683a656ca0e5dcb08a645a5c1854ba951d5
Chaim Lev Ari <chiptus@gmail.com>
2019-01-02 15:59:38 +0200

    refactor(auth): remove empty $q.deffered

--- app/portainer/services/authentication.js
+++ app/portainer/services/authentication.js
@@ -1,79 +1,66 @@
 angular.module('portainer.app')
-.factory('Authentication', ['$q', 'Auth', 'OAuth', 'jwtHelper', 'LocalStorage', 'StateManager', 'EndpointProvider', function AuthenticationFactory($q, Auth, OAuth, jwtHelper, LocalStorage, StateManager, EndpointProvider) {
-  'use strict';
+.factory('Authentication', [
+  'Auth', 'OAuth', 'jwtHelper', 'LocalStorage', 'StateManager', 'EndpointProvider', 
+  function AuthenticationFactory(Auth, OAuth, jwtHelper, LocalStorage, StateManager, EndpointProvider) {
+    'use strict';
 
-  var service = {};
-  var user = {};
+    var service = {};
+    var user = {};
 
-  service.init = init;
-  service.oAuthLogin = oAuthLogin;
-  service.login = login;
-  service.logout = logout;
-  service.isAuthenticated = isAuthenticated;
-  service.getUserDetails = getUserDetails;
+    service.init = init;
+    service.oAuthLogin = oAuthLogin;
+    service.login = login;
+    service.logout = logout;
+    service.isAuthenticated = isAuthenticated;
+    service.getUserDetails = getUserDetails;
 
-  function init() {
-    var jwt = LocalStorage.getJWT();
+    function init() {
+      var jwt = LocalStorage.getJWT();
 
-    if (jwt) {
-      var tokenPayload = jwtHelper.decodeToken(jwt);
-      user.username = tokenPayload.username;
-      user.ID = tokenPayload.id;
-      user.role = tokenPayload.role;
+      if (jwt) {
+        var tokenPayload = jwtHelper.decodeToken(jwt);
+        user.username = tokenPayload.username;
+        user.ID = tokenPayload.id;
+        user.role = tokenPayload.role;
+      }
     }
-  }
 
-  function oAuthLogin(code) {
-    var deferred = $q.defer();
+    function oAuthLogin(code) {
+      return OAuth.login({code: code}).$promise
+      .then(function success(data) {
+        LocalStorage.storeJWT(data.jwt);
+        var tokenPayload = jwtHelper.decodeToken(data.jwt);
+        user.username = tokenPayload.username;
+        user.ID = tokenPayload.id;
+        user.role = tokenPayload.role;
+      });
+    }
 
-    OAuth.login({code: code}).$promise
-    .then(function success(data) {
-      LocalStorage.storeJWT(data.jwt);
-      var tokenPayload = jwtHelper.decodeToken(data.jwt);
-      user.username = tokenPayload.username;
-      user.ID = tokenPayload.id;
-      user.role = tokenPayload.role;
-      deferred.resolve();
-    })
-    .catch(function error() {
-      deferred.reject();
-    });
-    return deferred.promise;
-  }
+    function login(username, password) {
+      return Auth.login({username: username, password: password}).$promise
+      .then(function success(data) {
+        LocalStorage.storeJWT(data.jwt);
+        var tokenPayload = jwtHelper.decodeToken(data.jwt);
+        user.username = username;
+        user.ID = tokenPayload.id;
+        user.role = tokenPayload.role;
+      });
+    }
 
-  function login(username, password) {
-    var deferred = $q.defer();
+    function logout() {
+      StateManager.clean();
+      EndpointProvider.clean();
+      LocalStorage.clean();
+    }
 
-    Auth.login({username: username, password: password}).$promise
-    .then(function success(data) {
-      LocalStorage.storeJWT(data.jwt);
-      var tokenPayload = jwtHelper.decodeToken(data.jwt);
-      user.username = username;
-      user.ID = tokenPayload.id;
-      user.role = tokenPayload.role;
-      deferred.resolve();
-    })
-    .catch(function error() {
-      deferred.reject();
-    });
+    function isAuthenticated() {
+      var jwt = LocalStorage.getJWT();
+      return jwt && !jwtHelper.isTokenExpired(jwt);
+    }
 
-    return deferred.promise;
-  }
+    function getUserDetails() {
+      return user;
+    }
 
-  function logout() {
-    StateManager.clean();
-    EndpointProvider.clean();
-    LocalStorage.clean();
-  }
-
-  function isAuthenticated() {
-    var jwt = LocalStorage.getJWT();
-    return jwt && !jwtHelper.isTokenExpired(jwt);
-  }
-
-  function getUserDetails() {
-    return user;
-  }
-
-  return service;
+    return service;
 }]);
