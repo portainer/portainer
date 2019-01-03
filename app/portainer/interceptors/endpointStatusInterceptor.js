b29bcb4cf1ec939ef3a18423126634d5dcefd02b
baron_l <baron_l@epitech.eu>
2019-01-03 17:38:58 +0100

    fix(interceptor): remove endpoint status update on 502/503 http return

--- app/portainer/interceptors/endpointStatusInterceptor.js
+++ app/portainer/interceptors/endpointStatusInterceptor.js
@@ -18,21 +18,17 @@ angular.module('portainer.app')
     }
 
     function responseInterceptor(response) {
-      var EndpointService = $injector.get('EndpointService');
       var url = response.config.url;
       if (response.status === 200 && canBeOffline(url) && EndpointProvider.offlineMode()) {
         EndpointProvider.setOfflineMode(false);
-        EndpointService.updateEndpoint(EndpointProvider.endpointID(), {Status: EndpointProvider.endpointStatusFromOfflineMode(false)});
       }
       return response || $q.when(response);
     }
 
     function responseErrorInterceptor(rejection) {
-      var EndpointService = $injector.get('EndpointService');
       var url = rejection.config.url;
       if ((rejection.status === 502 || rejection.status === 503 || rejection.status === -1) && canBeOffline(url) && !EndpointProvider.offlineMode()) {
         EndpointProvider.setOfflineMode(true);
-        EndpointService.updateEndpoint(EndpointProvider.endpointID(), {Status: EndpointProvider.endpointStatusFromOfflineMode(true)});
       }
       return $q.reject(rejection);
     }
