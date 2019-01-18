3699b794ebf468f022697526397e016b18a37a9c
Chaim Lev Ari <chiptus@gmail.com>
2019-01-18 12:14:12 +0200

    feat(oauth): add providers selectors

---
+++ app/extensions/oauth/components/oauth-providers-selector/oauth-providers-selector.js
@@ -0,0 +1,4 @@
+angular.module('portainer.extensions.oauth')
+  .component('oauthProvidersSelector', {
+    templateUrl: 'app/extensions/oauth/components/oauth-providers-selector/oauth-providers-selector.html'
+  });
