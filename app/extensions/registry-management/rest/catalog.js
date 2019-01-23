d702c6d765974997a9d6b22c62d1b47b22c412ed
baron_l <baron_l@epitech.eu>
2019-01-23 13:45:29 +0100

    refactor(registry-management): change regex usage to a reusable interceptor function

--- app/extensions/registry-management/rest/catalog.js
+++ app/extensions/registry-management/rest/catalog.js
@@ -1,22 +1,13 @@
 angular.module('portainer.extensions.registrymanagement')
-.factory('RegistryCatalog', ['$resource', 'API_ENDPOINT_REGISTRIES', function RegistryCatalogFactory($resource, API_ENDPOINT_REGISTRIES) {
+.factory('RegistryCatalog', ['$resource', 'API_ENDPOINT_REGISTRIES', 'RegistryAPILinkInterceptor',
+function RegistryCatalogFactory($resource, API_ENDPOINT_REGISTRIES, RegistryAPILinkInterceptor) {
   'use strict';
   return $resource(API_ENDPOINT_REGISTRIES + '/:id/v2/:action', {},
   {
     get: {
       method: 'GET',
       params: { id: '@id', action: '_catalog' },
-      interceptor: {
-        response: function versionInterceptor(response) {
-          var instance = response.data;
-          var link = response.headers('link');
-          if (link) {
-            instance.last = _.split(_.split(link, 'last=')[1], />|&/)[0];
-            instance.n = _.split(_.split(link, /\?n=|&n=/)[1], />|&/)[0];
-          }
-          return instance;
-        }
-      }
+      interceptor: RegistryAPILinkInterceptor
     },
     ping: {
       method: 'GET',
