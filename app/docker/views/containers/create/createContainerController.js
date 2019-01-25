2ca3a6759258baa9528e9dd0cb8727c79858fd11
Mike C <mcrosson_git at nusku dot net>
2019-01-25 13:15:30 -0500

    Allow empty values for labels

--- app/docker/views/containers/create/createContainerController.js
+++ app/docker/views/containers/create/createContainerController.js
@@ -228,8 +228,13 @@ function ($q, $scope, $state, $timeout, $transition$, $filter, Container, Contai
   function prepareLabels(config) {
     var labels = {};
     $scope.formValues.Labels.forEach(function (label) {
-      if (label.name && label.value) {
-        labels[label.name] = label.value;
+      if (label.name) {
+        if (label.value) {
+            labels[label.name] = label.value;
+        }
+        else {
+            labels[label.name] = '';
+        }
       }
     });
     config.Labels = labels;
