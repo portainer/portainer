90281fd7f0e751ca53f54b56e4de4e9a4eb81e76
Chaim Lev Ari <chiptus@gmail.com>
2019-01-25 10:57:40 +0200

    feat(oauth): add providers to providers-selector

--- app/extensions/oauth/components/oauth-providers-selector/oauth-providers-selector.js
+++ app/extensions/oauth/components/oauth-providers-selector/oauth-providers-selector.js
@@ -1,4 +1,20 @@
-angular.module('portainer.extensions.oauth')
-  .component('oauthProvidersSelector', {
-    templateUrl: 'app/extensions/oauth/components/oauth-providers-selector/oauth-providers-selector.html'
-  });
+angular.module('portainer.extensions.oauth').component('oauthProvidersSelector', {
+  templateUrl: 'app/extensions/oauth/components/oauth-providers-selector/oauth-providers-selector.html',
+  bindings: {
+    onSelect: '<'
+  },
+  controller: function oauthProvidersSelectorController() {
+    this.providers = [
+      {
+        name: 'Facebook',
+        authUrl: 'https://www.facebook.com/v3.2/dialog/oauth',
+        accessTokenUrl: 'https://graph.facebook.com/v3.2/oauth/access_token',
+        resourceUrl: 'https://graph.facebook.com/v3.2/me?fields=email',
+        userIdentifier: 'email'
+      },
+      {
+        name: 'Custom'
+      }
+    ];
+  }
+});
--- app/extensions/oauth/components/oauth-settings/oauth-settings-controller.js
+++ app/extensions/oauth/components/oauth-settings/oauth-settings-controller.js
@@ -1,6 +1,11 @@
 angular.module('portainer.extensions.oauth')
-  .controller('OAuthSettingsController', function OAuthSettingsController() {
-    this.providers = [
-      'facebook'
-    ]
-  });
+.controller('OAuthSettingsController', function OAuthSettingsController() {
+  this.onSelectProvider = onSelectProvider;
+
+  function onSelectProvider(provider) {
+    this.settings.AuthorizationURI = provider.authUrl;
+    this.settings.AccessTokenURI = provider.accessTokenUrl;
+    this.settings.ResourceURI = provider.resourceUrl;
+    this.settings.UserIdentifier = provider.userIdentifier;
+  }
+});
