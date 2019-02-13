36731859c1acd61ac9896c9bb256353522541599
baron_l <baron_l@epitech.eu>
2019-02-13 16:30:35 +0100

    feat(storidge): update storidge routes

--- app/extensions/storidge/rest/storidge.js
+++ app/extensions/storidge/rest/storidge.js
@@ -5,11 +5,11 @@ angular.module('extension.storidge')
     endpointId: EndpointProvider.endpointID
   },
   {
-    rebootCluster: { method: 'POST', params: { resource: 'cluster', action: 'reboot' } },
-    shutdownCluster: { method: 'POST', params: { resource: 'cluster', action: 'shutdown' } },
-    queryEvents: { method: 'GET', params: { resource: 'events' }, timeout: 4500, ignoreLoadingBar: true, isArray: true },
-    getVersion: { method: 'GET', params: { resource: 'version' } },
-    getInfo: { method: 'GET', params: { resource: 'info' }, timeout: 4500, ignoreLoadingBar: true },
+    rebootCluster: { method: 'POST', params: { resource: 'clusters', action: 'reboot' } },
+    shutdownCluster: { method: 'POST', params: { resource: 'clusters', action: 'shutdown' } },
+    queryEvents: { method: 'GET', params: { resource: 'clusters', action: 'events' }, timeout: 4500, ignoreLoadingBar: true, isArray: true },
+    getVersion: { method: 'GET', params: { resource: 'clusters', action: 'version' } },
+    getInfo: { method: 'GET', params: { resource: 'clusters', action: 'info' }, timeout: 4500, ignoreLoadingBar: true },
     queryNodes: { method: 'GET', params: { resource: 'nodes' } },
     queryProfiles: { method: 'GET', params: { resource: 'profiles' } },
     getProfile: { method: 'GET', params: { resource: 'profiles' } },
