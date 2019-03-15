0c7d69eb1768443bb6b0a248315f55ffe5c7fc96
Steven Kang <skan070@gmail.com>
2019-03-15 19:08:35 +1300

    fix(build-system): fix an issue with Windows build in CI (#2780)
    * Change GOPATH outside of the current project path
    
    * Fix ENV Variable
    
    * Fix locations
    
    * Fix dist directory location

--- build/build_binary_devops.ps1
+++ build/build_binary_devops.ps1
@@ -6,19 +6,19 @@ param (
 $ErrorActionPreference = "Stop";
 
 $binary = "portainer.exe"
-$project_path = (Get-ITEM -Path env:BUILD_SOURCESDIRECTORY).Value
+$go_path = "$($(Get-ITEM -Path env:AGENT_HOMEDIRECTORY).Value)\go"
 
-Set-Item env:GOPATH "$project_path\api"
+Set-Item env:GOPATH "$go_path"
 
-New-Item -Name dist -Path "$project_path" -ItemType Directory | Out-Null
-New-Item -Name portainer -Path "$project_path\api\src\github.com\" -ItemType Directory | Out-Null
+New-Item -Name dist -Path "." -ItemType Directory | Out-Null
+New-Item -Name portainer -Path "$go_path\src\github.com\" -ItemType Directory | Out-Null
 
-Copy-Item -Path "$project_path\api" -Destination "$project_path\api\src\github.com\portainer" -Recurse -Force -ErrorAction:SilentlyContinue
-Rename-Item -Path "$project_path\api\src\github.com\portainer\api" -NewName "portainer" -ErrorAction:SilentlyContinue
+Copy-Item -Path "api" -Destination "$go_path\src\github.com\portainer" -Recurse -Force -ErrorAction:SilentlyContinue
+Rename-Item -Path "$go_path\src\github.com\portainer\api" -NewName "portainer" -ErrorAction:SilentlyContinue
 
-Set-Location -Path "$project_path\api\cmd\portainer"
+Set-Location -Path "api\cmd\portainer"
 
 go.exe get -t -d -v ./...
 go.exe build -v
 
-Move-Item -Path "$project_path\api\cmd\portainer\$($binary)" -Destination "$project_path\dist"
+Move-Item -Path "$($binary)" -Destination "dist"
