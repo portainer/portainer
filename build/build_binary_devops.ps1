623ba68852bb393d3891a42c07faa347c4aace93
Steven <skan070@gmail.com>
2019-03-15 15:28:39 +1300

    Fix locations

--- build/build_binary_devops.ps1
+++ build/build_binary_devops.ps1
@@ -6,19 +6,19 @@ param (
 $ErrorActionPreference = "Stop";
 
 $binary = "portainer.exe"
-$go_path = (Get-ITEM -Path env:AGENT_HOMEDIRECTORY).Value
+$go_path = "$($(Get-ITEM -Path env:AGENT_HOMEDIRECTORY).Value)\go"
 
-Set-Item env:GOPATH "$go_path\api"
+Set-Item env:GOPATH "$go_path"
 
-New-Item -Name dist -Path "$go_path" -ItemType Directory | Out-Null
-New-Item -Name portainer -Path "$go_path\api\src\github.com\" -ItemType Directory | Out-Null
+New-Item -Name dist -Path "." -ItemType Directory | Out-Null
+New-Item -Name portainer -Path "$go_path\src\github.com\" -ItemType Directory | Out-Null
 
-Copy-Item -Path "$go_path\api" -Destination "$go_path\api\src\github.com\portainer" -Recurse -Force -ErrorAction:SilentlyContinue
-Rename-Item -Path "$go_path\api\src\github.com\portainer\api" -NewName "portainer" -ErrorAction:SilentlyContinue
+Copy-Item -Path "api" -Destination "$go_path\src\github.com\portainer" -Recurse -Force -ErrorAction:SilentlyContinue
+Rename-Item -Path "$go_path\src\github.com\portainer\api" -NewName "portainer" -ErrorAction:SilentlyContinue
 
-Set-Location -Path "$go_path\api\cmd\portainer"
+Set-Location -Path "api\cmd\portainer"
 
 go.exe get -t -d -v ./...
 go.exe build -v
 
-Move-Item -Path "$go_path\api\cmd\portainer\$($binary)" -Destination "$go_path\dist"
+Move-Item -Path "api\cmd\portainer\$($binary)" -Destination "dist"
