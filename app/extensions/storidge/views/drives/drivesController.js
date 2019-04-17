8fb8aba0755b7aaea1df845a4c90136e743b7649
Anthony Lapenna <lapenna.anthony@gmail.com>
2019-04-17 13:31:32 +1200

    feat(storidge): update 6

--- app/extensions/storidge/views/drives/drivesController.js
+++ app/extensions/storidge/views/drives/drivesController.js
@@ -1,63 +1,6 @@
 angular.module('extension.storidge')
-.controller('StoridgeDrivesController', ['$q', '$scope', '$state', 'Notifications', 'StoridgeDriveService',
-function ($q, $scope, $state, Notifications, StoridgeDriveService) {
-
-  // $scope.removeAction = function(selectedItems) {
-  //   ModalService.confirm({
-  //     title: 'Are you sure?',
-  //     message: 'Do you want really want to remove the drives from the storage pool?',
-  //     buttons: {
-  //       confirm: {
-  //         label: 'Remove',
-  //         className: 'btn-danger'
-  //       }
-  //     },
-  //     callback: function onConfirm(confirmed) {
-  //       if(!confirmed) { return; }
-  //       var actionCount = selectedItems.length;
-  //       selectedItems = selectedItems.filter(function (item) {
-  //         return item.Status === 'faulty';
-  //       });
-  //       angular.forEach(selectedItems, function (drive) {
-  //         StoridgeDriveService.remove(drive.Id)
-  //         .then(function success() {
-  //           Notifications.success('Drive successfully removed', drive.Id);
-  //         })
-  //         .catch(function error(err) {
-  //           Notifications.error('Failure', err, 'Unable to remove drive');
-  //         })
-  //         .finally(function final() {
-  //           --actionCount;
-  //           if (actionCount === 0) {
-  //             $state.reload();
-  //           }
-  //         });
-  //       });
-  //     }
-  //   });
-  // };
-
-  // $scope.addAction = function (selectedItems) {
-  //   var actionCount = selectedItems.length;
-  //   selectedItems = selectedItems.filter(function (item) {
-  //     return item.Status === 'available';
-  //   });
-  //   angular.forEach(selectedItems, function (drive) {
-  //     StoridgeDriveService.add(drive.Device, drive.Node)
-  //     .then(function success() {
-  //       Notifications.success('Drive ' + drive.Device + ' successfully added on node ' + drive.Node);
-  //     })
-  //     .catch(function error(err) {
-  //       Notifications.error('Failure', err, 'Unable to add drive');
-  //     })
-  //     .finally(function final() {
-  //       --actionCount;
-  //       if (actionCount === 0) {
-  //         $state.reload();
-  //       }
-  //     });
-  //   });
-  // };
+.controller('StoridgeDrivesController', ['$scope', '$state', 'Notifications', 'StoridgeDriveService',
+function ($scope, $state, Notifications, StoridgeDriveService) {
 
   $scope.rescanAction = function () {
     StoridgeDriveService.rescan()
