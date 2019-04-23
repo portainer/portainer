3c0fbfc82611919b3ecb31aec8c0d8e40a2f9d3a
Kai <494931023@qq.com>
2019-04-23 15:46:11 +0800

    Update websocket_exec.go

--- api/http/handler/websocket/websocket_exec.go
+++ api/http/handler/websocket/websocket_exec.go
@@ -253,12 +253,16 @@ func streamFromWebsocketConnToTCPConn(websocketConn *websocket.Conn, tcpConn net
 }
 
 func streamFromTCPConnToWebsocketConn(websocketConn *websocket.Conn, br *bufio.Reader, errorChan chan error) {
-	out, err := ioutil.ReadAll(br)
-	if err != nil {
-		errorChan <- err
-	}
-	err = websocketConn.WriteMessage(websocket.TextMessage, out)
-	if err != nil {
-		errorChan <- err
+	for{
+		out, err := ioutil.ReadAll(br)
+		if err != nil {
+			errorChan <- err
+			break
+		}
+		err = websocketConn.WriteMessage(websocket.TextMessage, out)
+		if err != nil {
+			errorChan <- err
+			break
+		}
 	}
 }
