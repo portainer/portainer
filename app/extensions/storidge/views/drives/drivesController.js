83868a63d3b57b9cef3430875bf34d831c1aaa90
baron_l <baron_l@epitech.eu>
2019-04-26 16:44:34 +0200

    feat(storidge): update 8 - add drive

--- app/extensions/storidge/views/drives/drivesController.js
+++ app/extensions/storidge/views/drives/drivesController.js
@@ -2,6 +2,28 @@ angular.module('extension.storidge')
 .controller('StoridgeDrivesController', ['$scope', '$state', 'Notifications', 'StoridgeDriveService',
 function ($scope, $state, Notifications, StoridgeDriveService) {
 
+  $scope.addAction = function (selectedItems) {
+    var actionCount = selectedItems.length;
+    selectedItems = selectedItems.filter(function (item) {
+      return item.Status === 'available';
+    });
+    angular.forEach(selectedItems, function (drive) {
+      StoridgeDriveService.add(drive.Device, drive.Node)
+      .then(function success() {
+        Notifications.success('Drive ' + drive.Device + ' successfully added on node ' + drive.Node);
+      })
+      .catch(function error(err) {
+        Notifications.error('Failure', err, 'Unable to add drive');
+      })
+      .finally(function final() {
+        --actionCount;
+        if (actionCount === 0) {
+          $state.reload();
+        }
+      });
+    });
+  };
+
   $scope.rescanAction = function () {
     StoridgeDriveService.rescan()
     .then(function sucess() {
