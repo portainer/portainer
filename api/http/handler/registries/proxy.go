407328f9ede8efb57c80f520d6539599632a87db
Anthony Lapenna <lapenna.anthony@gmail.com>
2019-05-27 09:15:50 +1200

    fix(api): remove admin restriction for registry proxy endpoint

--- api/http/handler/registries/proxy.go
+++ api/http/handler/registries/proxy.go
@@ -5,8 +5,6 @@ import (
 	"net/http"
 	"strconv"
 
-	"github.com/portainer/portainer/api/http/security"
-
 	httperror "github.com/portainer/libhttp/error"
 	"github.com/portainer/libhttp/request"
 	"github.com/portainer/portainer/api"
@@ -24,15 +22,6 @@ func (handler *Handler) proxyRequestsToRegistryAPI(w http.ResponseWriter, r *htt
 		return &httperror.HandlerError{http.StatusNotFound, "Unable to find a registry with the specified identifier inside the database", err}
 	} else if err != nil {
 		return &httperror.HandlerError{http.StatusInternalServerError, "Unable to find a registry with the specified identifier inside the database", err}
-	}
-
-	securityContext, err := security.RetrieveRestrictedRequestContext(r)
-	if err != nil {
-		return &httperror.HandlerError{http.StatusInternalServerError, "Unable to retrieve info from request context", err}
-	}
-
-	if !securityContext.IsAdmin {
-		return &httperror.HandlerError{http.StatusForbidden, "Access denied to resource", portainer.ErrResourceAccessDenied}
 	}
 
 	err = handler.requestBouncer.RegistryAccess(r, registry)
