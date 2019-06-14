b4f784d8358d89b56a49159c1635136f0aa87131
baron_l <baron_l@epitech.eu>
2019-06-14 18:30:33 +0200

    fix(async): $async with multiple params was not working

--- app/portainer/services/async.js
+++ app/portainer/services/async.js
@@ -2,16 +2,15 @@ angular.module('portainer').factory('$async', ['$q',
   function($q) {
     return function(asyncFunc, ...args) {
       const def = $q.defer();
-
       const wrapper = function(params) {
         const deferred = $q.defer();
-        asyncFunc(params)
+        asyncFunc(...params)
           .then(deferred.resolve)
           .catch(deferred.reject);
         return deferred.promise;
       };
 
-      wrapper(...args).then(def.resolve).catch(def.reject)
+      wrapper(args).then(def.resolve).catch(def.reject)
       return def.promise;
     };
   }
