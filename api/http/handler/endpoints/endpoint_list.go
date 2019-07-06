af46607b1290bb53b52eb7eb98c85b71d9f97d3f
Anthony Lapenna <lapenna.anthony@gmail.com>
2019-07-07 05:05:36 +1200

    feat(api): implement filter for EndpointList operation

--- api/http/handler/endpoints/endpoint_list.go
+++ api/http/handler/endpoints/endpoint_list.go
@@ -3,6 +3,9 @@ package endpoints
 import (
 	"net/http"
 	"strconv"
+	"strings"
+
+	portainer "github.com/portainer/portainer/api"
 
 	"github.com/portainer/libhttp/request"
 
@@ -15,7 +18,7 @@ import (
 func (handler *Handler) endpointList(w http.ResponseWriter, r *http.Request) *httperror.HandlerError {
 	start, _ := request.RetrieveNumericQueryParameter(r, "start", false)
 	limit, _ := request.RetrieveNumericQueryParameter(r, "limit", false)
-	//filter, _ := request.RetrieveQueryParameter(r, "filter", false)
+	filter, _ := request.RetrieveQueryParameter(r, "filter", false)
 
 	if start == 0 {
 		start = 1
@@ -30,14 +33,14 @@ func (handler *Handler) endpointList(w http.ResponseWriter, r *http.Request) *ht
 		return &httperror.HandlerError{http.StatusInternalServerError, "Unable to retrieve endpoints from the database", err}
 	}
 
-	endpointCount, err := handler.EndpointService.EndpointCount()
-	if err != nil {
-		return &httperror.HandlerError{http.StatusInternalServerError, "Unable to retrieve endpoints from the database", err}
-	}
-
 	endpointGroups, err := handler.EndpointGroupService.EndpointGroups()
 	if err != nil {
 		return &httperror.HandlerError{http.StatusInternalServerError, "Unable to retrieve endpoint groups from the database", err}
+	}
+
+	endpointCount, err := handler.EndpointService.EndpointCount()
+	if err != nil {
+		return &httperror.HandlerError{http.StatusInternalServerError, "Unable to retrieve endpoints from the database", err}
 	}
 
 	securityContext, err := security.RetrieveRestrictedRequestContext(r)
@@ -47,10 +50,58 @@ func (handler *Handler) endpointList(w http.ResponseWriter, r *http.Request) *ht
 
 	filteredEndpoints := security.FilterEndpoints(endpoints, endpointGroups, securityContext)
 
+	if filter != "" {
+		filter = strings.ToLower(filter)
+
+		nfilteredendpoints := make([]portainer.Endpoint, 0)
+		for _, endpoint := range filteredEndpoints {
+
+			if strings.Contains(strings.ToLower(endpoint.Name), filter) {
+				nfilteredendpoints = append(nfilteredendpoints, endpoint)
+				continue
+			} else if strings.Contains(strings.ToLower(endpoint.URL), filter) {
+				nfilteredendpoints = append(nfilteredendpoints, endpoint)
+				continue
+			}
+
+			if endpoint.Status == portainer.EndpointStatusUp && filter == "up" {
+				nfilteredendpoints = append(nfilteredendpoints, endpoint)
+				continue
+			} else if endpoint.Status == portainer.EndpointStatusDown && filter == "down" {
+				nfilteredendpoints = append(nfilteredendpoints, endpoint)
+				continue
+			}
+
+			for _, tag := range endpoint.Tags {
+				if strings.Contains(strings.ToLower(tag), filter) {
+					nfilteredendpoints = append(nfilteredendpoints, endpoint)
+					continue
+				}
+			}
+
+			endpointGroup := getAssociatedGroup(&endpoint, endpointGroups)
+			if strings.Contains(strings.ToLower(endpointGroup.Name), filter) {
+				nfilteredendpoints = append(nfilteredendpoints, endpoint)
+				continue
+			}
+		}
+
+		filteredEndpoints = nfilteredendpoints
+	}
+
 	for idx := range filteredEndpoints {
 		hideFields(&filteredEndpoints[idx])
 	}
 
 	w.Header().Set("X-Total-Count", strconv.Itoa(endpointCount))
 	return response.JSON(w, filteredEndpoints)
+}
+
+func getAssociatedGroup(endpoint *portainer.Endpoint, groups []portainer.EndpointGroup) *portainer.EndpointGroup {
+	for _, group := range groups {
+		if group.ID == endpoint.GroupID {
+			return &group
+		}
+	}
+	return nil
 }
