f4bf0b1068f6e2d0f4b5e42708207b883e0a53c5
xAt0mZ <baron_l@epitech.eu>
2019-07-22 14:43:52 +0200

    fix(app): UI settings persistency

--- app/portainer/services/localStorage.js
+++ app/portainer/services/localStorage.js
@@ -60,40 +60,40 @@ angular.module('portainer.app')
       localStorageService.remove('JWT');
     },
     storePaginationLimit: function(key, count) {
-      localStorageService.set('datatable_pagination_' + key, count);
+      localStorageService.cookie.set('datatable_pagination_' + key, count);
     },
     getPaginationLimit: function(key) {
-      return localStorageService.get('datatable_pagination_' + key);
+      return localStorageService.cookie.get('datatable_pagination_' + key);
     },
     getDataTableOrder: function(key) {
-      return localStorageService.get('datatable_order_' + key);
+      return localStorageService.cookie.get('datatable_order_' + key);
     },
     storeDataTableOrder: function(key, data) {
-      localStorageService.set('datatable_order_' + key, data);
+      localStorageService.cookie.set('datatable_order_' + key, data);
     },
     getDataTableTextFilters: function(key) {
-      return localStorageService.get('datatable_text_filter_' + key);
+      return localStorageService.cookie.get('datatable_text_filter_' + key);
     },
     storeDataTableTextFilters: function(key, data) {
-      localStorageService.set('datatable_text_filter_' + key, data);
+      localStorageService.cookie.set('datatable_text_filter_' + key, data);
     },
     getDataTableFilters: function(key) {
-      return localStorageService.get('datatable_filters_' + key);
+      return localStorageService.cookie.get('datatable_filters_' + key);
     },
     storeDataTableFilters: function(key, data) {
-      localStorageService.set('datatable_filters_' + key, data);
+      localStorageService.cookie.set('datatable_filters_' + key, data);
     },
     getDataTableSettings: function(key) {
-      return localStorageService.get('datatable_settings_' + key);
+      return localStorageService.cookie.get('datatable_settings_' + key);
     },
     storeDataTableSettings: function(key, data) {
-      localStorageService.set('datatable_settings_' + key, data);
+      localStorageService.cookie.set('datatable_settings_' + key, data);
     },
     getDataTableExpandedItems: function(key) {
-      return localStorageService.get('datatable_expandeditems_' + key);
+      return localStorageService.cookie.get('datatable_expandeditems_' + key);
     },
     storeDataTableExpandedItems: function(key, data) {
-      localStorageService.set('datatable_expandeditems_' + key, data);
+      localStorageService.cookie.set('datatable_expandeditems_' + key, data);
     },
     getDataTableSelectedItems: function(key) {
       return localStorageService.get('datatable_selecteditems_' + key);
@@ -102,16 +102,16 @@ angular.module('portainer.app')
       localStorageService.set('datatable_selecteditems_' + key, data);
     },
     storeSwarmVisualizerSettings: function(key, data) {
-      localStorageService.set('swarmvisualizer_' + key, data);
+      localStorageService.cookie.set('swarmvisualizer_' + key, data);
     },
     getSwarmVisualizerSettings: function(key) {
-      return localStorageService.get('swarmvisualizer_' + key);
+      return localStorageService.cookie.get('swarmvisualizer_' + key);
     },
     storeColumnVisibilitySettings: function(key, data) {
-      localStorageService.set('col_visibility_' + key, data);
+      localStorageService.cookie.set('col_visibility_' + key, data);
     },
     getColumnVisibilitySettings: function(key) {
-      return localStorageService.get('col_visibility_' + key);
+      return localStorageService.cookie.get('col_visibility_' + key);
     },
     storeJobImage: function(data) {
       localStorageService.set('job_image', data);
