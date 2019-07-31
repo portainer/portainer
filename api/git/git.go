13f11436afaf79d4386f40ff83d3b0039891f044
ssbkang <skan070@gmail.com>
2019-07-31 21:53:27 +1200

    fix(stack): Skip SSL Verification

--- api/git/git.go
+++ api/git/git.go
@@ -1,11 +1,16 @@
 package git
 
 import (
+	"crypto/tls"
+	"net/http"
 	"net/url"
 	"strings"
+	"time"
 
 	"gopkg.in/src-d/go-git.v4"
 	"gopkg.in/src-d/go-git.v4/plumbing"
+	"gopkg.in/src-d/go-git.v4/plumbing/transport/client"
+	githttp "gopkg.in/src-d/go-git.v4/plumbing/transport/http"
 )
 
 // Service represents a service for managing Git.
@@ -33,14 +38,24 @@ func (service *Service) ClonePrivateRepositoryWithBasicAuth(repositoryURL, refer
 }
 
 func cloneRepository(repositoryURL, referenceName string, destination string) error {
+	customClient := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		},
+
+		Timeout: 15 * time.Second,
+	}
+
+	client.InstallProtocol("https", githttp.NewClient(customClient))
+
 	options := &git.CloneOptions{
 		URL: repositoryURL,
 	}
-
+	
 	if referenceName != "" {
 		options.ReferenceName = plumbing.ReferenceName(referenceName)
 	}
-
+	
 	_, err := git.PlainClone(destination, false, options)
 	return err
 }
