bcf3dd293ba632e32d25439397f3992807ff36f3
ssbkang <skan070@gmail.com>
2019-09-04 11:10:26 +1200

    feat(docker): copy the required .dll files for Windows image

--- build/download_docker_binary.ps1
+++ build/download_docker_binary.ps1
@@ -11,4 +11,8 @@ $docker_base_version = $docker_version.Split(".")[0] + "." + $docker_version.Spl
 
 Invoke-WebRequest -O "$($download_folder)/docker-binaries.zip" "https://download.docker.com/components/engine/windows-server/$($docker_base_version)/docker-$($docker_version).zip"
 Expand-Archive -Path "$($download_folder)/docker-binaries.zip" -DestinationPath "$($download_folder)"
-Move-Item -Path "$($download_folder)/docker/docker.exe" -Destination "dist"
+Move-Item -Path "$($download_folder)/docker/*" -Destination "dist/"
+Remove-Item -Path "dist/cli-plugins" -Recurse -Force -Confirm:$false
+Remove-Item -Path "dist/dockerd.exe" -Recurse -Force -Confirm:$false
+
+ls "dist/"
