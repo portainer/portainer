36bdbfe6557d334fa7ebadaeae66691263ab346c
Anthony Lapenna <anthony.lapenna@portainer.io>
2019-09-13 01:37:58 +1200

    Fix k8s v0.2 (#10)
    * fix(kube/nodes): fix invalid role detection
    
    * fix(kube/services): fix an issue with load balancer services

--- app/kubernetes/models/node.js
+++ app/kubernetes/models/node.js
@@ -4,7 +4,7 @@ export default function KubernetesSecretViewModel(data) {
   this.Id = data.metadata.uid;
   const hostName = _.find(data.status.addresses, {type: 'Hostname'});
   this.Name = hostName ? hostName.address : data.metadata.Name;
-  this.Role = data.metadata.labels['node-role.kubernetes.io/master'] ? 'Manager' : 'Worker';
+  this.Role = _.has(data.metadata.labels, 'node-role.kubernetes.io/master') ? 'Manager' : 'Worker';
   const readyStatus = _.find(data.status.conditions, {type: 'Ready'});
   this.Status = readyStatus && readyStatus.status === "True" ? 'Ready' : 'Warning';
   this.CPU = data.status.capacity.cpu;
--- app/kubernetes/models/service.js
+++ app/kubernetes/models/service.js
@@ -8,9 +8,11 @@ export default function KubernetesServiceViewModel(data) {
   this.RunningPodsCount = data.status.availableReplicas || data.status.replicas - data.status.unavailableReplicas;
   this.TotalPodsCount = data.status.replicas;
   this.PublishedPorts = _.flatMapDeep(_.map(data.BoundServices, (service) => {
-    if (service.spec.Type === 'LoadBalancer') {
+    if (service.spec.type === 'LoadBalancer') {
       return _.map(service.spec.ports, (port) => {
-        port.IPAddress = service.status.loadbalancer.ip || service.status.loadBalancer.hostname;
+        if (service.status.loadBalancer.ingress.length > 0) {
+          port.IPAddress = service.status.loadBalancer.ingress[0].ip || service.status.loadBalancer.ingress[0].hostname;
+        }
         return port;
       });
     }
