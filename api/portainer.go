636adeec54d58060ae45a768a0de10ef416edd24
Anthony Lapenna <lapenna.anthony@gmail.com>
2019-09-25 17:23:30 +1200

    refactor(api): rollback to latest version

--- api/portainer.go
+++ api/portainer.go
@@ -901,7 +901,7 @@ type (
 
 const (
 	// APIVersion is the version number of the Portainer API
-	APIVersion = "1.21.0"
+	APIVersion = "1.22.0"
 	// DBVersion is the version number of the Portainer database
 	DBVersion = 20
 	// AssetsServerURL represents the URL of the Portainer asset server
