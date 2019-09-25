ea05d96c7382eb7ddf94099a0c147db969ba0c00
Anthony Lapenna <anthony.lapenna@portainer.io>
2019-09-26 08:38:11 +1200

    feat(sidebar): add update notification (#3196)
    * feat(sidebar): add update notification
    
    * style(sidebar): update notification color palette
    
    * refactor(api): rollback to latest version
    
    * feat(sidebar): update style
    
    * style(sidebar): fix color override

--- app/portainer/services/api/statusService.js
+++ app/portainer/services/api/statusService.js
@@ -1,4 +1,4 @@
-import { StatusViewModel } from "../../models/status";
+import {StatusVersionViewModel, StatusViewModel} from '../../models/status';
 
 angular.module('portainer.app')
 .factory('StatusService', ['$q', 'Status', function StatusServiceFactory($q, Status) {
@@ -15,6 +15,21 @@ angular.module('portainer.app')
     })
     .catch(function error(err) {
       deferred.reject({ msg: 'Unable to retrieve application status', err: err });
+    });
+
+    return deferred.promise;
+  };
+
+  service.version = function() {
+    var deferred = $q.defer();
+
+    Status.version().$promise
+    .then(function success(data) {
+      var status = new StatusVersionViewModel(data);
+      deferred.resolve(status);
+    })
+    .catch(function error(err) {
+      deferred.reject({ msg: 'Unable to retrieve application version info', err: err });
     });
 
     return deferred.promise;
