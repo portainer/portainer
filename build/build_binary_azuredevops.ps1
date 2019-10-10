8b4d16dd3bf42b3b4b79e1befa41fae50e79bdf1
Anthony Lapenna <lapenna.anthony@gmail.com>
2019-10-10 14:01:42 +1300

    fix(build-system): fix build system on CI for Windows

--- build/build_binary_azuredevops.ps1
+++ build/build_binary_azuredevops.ps1
@@ -20,7 +20,6 @@ Set-Location -Path "api\cmd\portainer"
 go get -t -d -v ./...
 # $env:CGO_ENABLED = "0"
 # go build -a --installsuffix cgo --ldflags '-s'
-
 & cmd /c 'go build -v 2>&1'
 
 Copy-Item -Path "portainer.exe" -Destination "$($env:BUILD_SOURCESDIRECTORY)\dist\portainer.exe" -Force -ErrorAction:SilentlyContinue
