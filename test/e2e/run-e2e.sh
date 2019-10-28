36de0aee7bc8fd7f3d742dab6317e11cf14de81f
Anthony Lapenna <lapenna.anthony@gmail.com>
2019-10-29 11:38:38 +1300

    feat(test): update e2e setup

---
+++ test/e2e/run-e2e.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+exec_in() { docker-compose exec -T $@; }
+
+# Up all dinds nodes
+docker-compose up -d manager1 manager2 worker1 worker2
+
+# Manager1 init
+exec_in manager1 docker swarm init
+TOKEN_WORKER="$(exec_in manager1 docker swarm join-token -q worker)"
+TOKEN_MANAGER="$(exec_in manager1 docker swarm join-token -q manager)"
+
+# Manager2 join
+exec_in manager2 docker swarm join --token $TOKEN_MANAGER manager1:2377
+
+# Worker1 join
+exec_in worker1 docker swarm join --token $TOKEN_WORKER manager1:2377
+
+# Worker2 join
+exec_in worker2 docker swarm join --token $TOKEN_WORKER manager1:2377
+
+# Up portainer
+docker-compose up --exit-code-from cypress
