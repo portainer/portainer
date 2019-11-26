281bfe97001993105c85b1615dc988cdefe92b1c
xAt0mZ <baron_l@epitech.eu>
2019-11-26 16:52:57 +0100

    feat(app): autocomplete on registry selector

--- app/docker/components/imageRegistry/porImageRegistryController.js
+++ app/docker/components/imageRegistry/porImageRegistryController.js
@@ -1,26 +1,49 @@
 import angular from 'angular';
 import _ from 'lodash-es';
+import { DockerHubViewModel } from 'Portainer/models/dockerhub';
 
 class porImageRegistryController {
   /* @ngInject */
-  constructor($async, RegistryService, DockerHubService, ImageService, Notifications) {
+  constructor($async, $scope, RegistryService, DockerHubService, ImageService, Notifications) {
     this.$async = $async;
+    this.$scope = $scope;
     this.RegistryService = RegistryService;
     this.DockerHubService = DockerHubService;
     this.ImageService = ImageService;
     this.Notifications = Notifications;
 
     this.onInit = this.onInit.bind(this);
+    this.onRegistryChange = this.onRegistryChange.bind(this);
+
+    this.$scope.$watch(() => this.model.Registry, this.onRegistryChange);
+  }
+
+  isKnownRepository(registry) {
+    return !(registry instanceof DockerHubViewModel) && registry.URL;
+  }
+
+  onRegistryChange() {
+    let images = [];
+    const registry = this.model.Registry;
+    if (this.isKnownRepository(registry)) {
+      const registryImages = _.filter(this.images, (image) => _.includes(image, this.model.Registry.URL));
+      images = _.map(registryImages, (image) => _.replace(image, registry.URL + '/', ''));
+    } else {
+      const registries = _.filter(this.availableRegistries, (reg) => this.isKnownRepository(reg));
+      const registryImages = _.flatMap(registries, (registry) => _.filter(this.images, (image) => _.includes(image, registry.URL)));
+      images = _.difference(this.images, registryImages);
+    }
+    this.availableImages = images;
   }
 
   async onInit() {
     try {
-      const [registries, dockerhub, availableImages] = await Promise.all([
+      const [registries, dockerhub, images] = await Promise.all([
         this.RegistryService.registries(),
         this.DockerHubService.dockerhub(),
         this.autoComplete ? this.ImageService.images() : []
       ]);
-      this.availableImages = this.ImageService.getUniqueTagListFromImages(availableImages);
+      this.images = this.ImageService.getUniqueTagListFromImages(images);
       this.availableRegistries = _.concat(dockerhub, registries);
 
       const id = this.model.Registry.Id;
