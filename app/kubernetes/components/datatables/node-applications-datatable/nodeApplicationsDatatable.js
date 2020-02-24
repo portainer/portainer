44bbe41bc5a0e724a000d5f03e6434b1988ec7ad
Maxime Bajeux <max.bajeux@gmail.com>
2020-02-24 01:33:08 +0100

    refacto(kubernetes): no line break for params

--- app/kubernetes/components/datatables/node-applications-datatable/nodeApplicationsDatatable.js
+++ app/kubernetes/components/datatables/node-applications-datatable/nodeApplicationsDatatable.js
@@ -1,18 +1,13 @@
-angular.module(
-  'portainer.kubernetes'
-).component(
-  'kubernetesNodeApplicationsDatatable',
-  {
-    templateUrl: './nodeApplicationsDatatable.html',
-    controller: 'GenericDatatableController',
-    bindings: {
-      titleText: '@',
-      titleIcon: '@',
-      dataset: '<',
-      tableKey: '@',
-      orderBy: '@',
-      reverseOrder: '<',
-      refreshCallback: '<'
-    }
+angular.module('portainer.kubernetes').component('kubernetesNodeApplicationsDatatable', {
+  templateUrl: './nodeApplicationsDatatable.html',
+  controller: 'GenericDatatableController',
+  bindings: {
+    titleText: '@',
+    titleIcon: '@',
+    dataset: '<',
+    tableKey: '@',
+    orderBy: '@',
+    reverseOrder: '<',
+    refreshCallback: '<'
   }
-);
+});
