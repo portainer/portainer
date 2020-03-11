c288fb3e8f53a3db89553b3ffc7d4487961420e8
itsconquest <william.conquest@portainer.io>
2020-03-11 21:36:23 +1300

    feat(support): add new offerings

--- app/portainer/views/support/product/productController.js
+++ app/portainer/views/support/product/productController.js
@@ -3,11 +3,21 @@ angular.module('portainer.app')
     function($scope, $transition$) {
 
       $scope.formValues = {
-        hostCount: 10
+        hostCount: 0
       };
 
       function initView() {
         $scope.product = $transition$.params().product;
+        
+        if ($scope.product.Id == 1) {
+          $scope.formValues.hostCount = 1;
+        }
+        if ($scope.product.Id == 2 || $scope.product.Id == 3) {
+          $scope.formValues.hostCount = 4;
+        }
+        if ($scope.product.Id == 4 || $scope.product.Id == 5) {
+          $scope.formValues.hostCount = 10;
+        }
       }
 
       initView();
