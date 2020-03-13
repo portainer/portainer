095a8690930f3cd060a90c35e69a34304ecdaec6
itsconquest <william.conquest@portainer.io>
2020-03-13 15:06:39 +1300

    feat(support): refactor for simplicity

--- app/portainer/views/support/product/productController.js
+++ app/portainer/views/support/product/productController.js
@@ -6,17 +6,32 @@ angular.module('portainer.app')
         hostCount: 0
       };
 
+      $scope.state = {
+        minHosts: 0,
+        placeholder: 0
+      };
+
+      $scope.buttonChecker = function () {
+        return !$scope.formValues.hostCount || ($scope.formValues.hostCount < $scope.state.minHosts);
+      };
+
       function initView() {
         $scope.product = $transition$.params().product;
         
         if ($scope.product.Id == 1) {
           $scope.formValues.hostCount = 1;
+          $scope.state.minHosts = 1;
+          $scope.state.placeholder = 1;
         }
         if ($scope.product.Id == 2 || $scope.product.Id == 3) {
           $scope.formValues.hostCount = 4;
+          $scope.state.minHosts = 4;
+          $scope.state.placeholder = 4;
         }
         if ($scope.product.Id == 4 || $scope.product.Id == 5) {
           $scope.formValues.hostCount = 10;
+          $scope.state.minHosts = 10;
+          $scope.state.placeholder = 10;
         }
       }
 
