5e9b8caa81f68cd6db8b7531e71757c40f81c94b
xAt0mZ <xAt0mZ@users.noreply.github.com>
2020-03-19 22:57:19 +0100

    fix(kubernetes): isolated volumes showed as unused even when used (#116)

--- app/kubernetes/helpers/volumeHelper.js
+++ app/kubernetes/helpers/volumeHelper.js
@@ -3,8 +3,9 @@ import _ from 'lodash-es';
 class KubernetesVolumeHelper {
   static getUsingApplications(volume, applications) {
     return _.filter(applications, (app) => {
-      const names = _.map(app.Volumes, 'name');
-      return _.includes(names, volume.PersistentVolumeClaim.Name)
+      const names = _.without(_.map(app.Volumes, 'persistentVolumeClaim.claimName'), undefined);
+      const matchingNames = _.filter(names, (name) => _.startsWith(volume.PersistentVolumeClaim.Name, name));
+      return volume.ResourcePool.Namespace.Name === app.ResourcePool && matchingNames.length;
     });
   }
 }
