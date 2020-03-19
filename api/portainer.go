f79e24047059ee78022ea326689665d97b00ece8
Anthony Lapenna <lapenna.anthony@gmail.com>
2020-03-20 11:37:35 +1300

    chore(version): bump version number

--- api/portainer.go
+++ api/portainer.go
@@ -945,7 +945,7 @@ type (
 
 const (
 	// APIVersion is the version number of the Portainer API
-	APIVersion = "1.23.2"
+	APIVersion = "1.24.0-dev"
 	// DBVersion is the version number of the Portainer database
 	DBVersion = 23
 	// AssetsServerURL represents the URL of the Portainer asset server
