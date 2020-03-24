0c34b147a40062a0e74f1f126001d7ab7fb98720
Chaim Lev Ari <chiptus@gmail.com>
2020-03-24 08:32:06 +0200

    refactor(home): rearange code to match former style

--- app/portainer/components/endpoint-list/endpoint-item/endpoint-item-controller.js
+++ app/portainer/components/endpoint-list/endpoint-item/endpoint-item-controller.js
@@ -1,39 +1,44 @@
-angular.module('portainer.app').controller('EndpointItemController', function EndpointItemController() {
-  var ctrl = this;
-  this.endpointTags = '';
+angular.module('portainer.app').controller('EndpointItemController', 
+  function EndpointItemController() {
+    var ctrl = this;
 
-  ctrl.editEndpoint = editEndpoint;
+    ctrl.editEndpoint = editEndpoint;
 
-  this.$onInit = function $onInit() {
-    this.endpointTags = this.joinTags();
-  };
-  this.$onChanges = function $onChanges(changesObj) {
-    this.handleTagsChange(changesObj);
-  };
-  this.handleTagsChange = function handleTagsChange({ tags, model }) {
-    if ((!tags && !model) || (!tags.currentValue && !model.currentValue)) {
-      return;
+    function editEndpoint(event) {
+      event.stopPropagation();
+      ctrl.onEdit(ctrl.model.Id);
     }
-    this.endpointTags = this.joinTags();
-  };
 
-  function editEndpoint(event) {
-    event.stopPropagation();
-    ctrl.onEdit(ctrl.model.Id);
-  }
 
-  this.joinTags = function joinTags() {
-    if (!this.tags) {
-      return 'Loading tags...'
-    }
-    return this.model.TagIds.map(tagId => {
-      const tag = this.tags.find(tag => tag.Id === tagId);
-      if (!tag) {
-        return null;
+    this.$onInit = function $onInit() {
+      this.endpointTags = this.joinTags();
+    };
+
+    this.$onChanges = function $onChanges(changesObj) {
+      this.handleTagsChange(changesObj);
+    };
+
+    this.handleTagsChange = function handleTagsChange({ tags, model }) {
+      if ((!tags && !model) || (!tags.currentValue && !model.currentValue)) {
+        return;
       }
-      return tag.Name;
-    })
-      .filter(Boolean)
-      .join(',');
-  }
-});
+      this.endpointTags = this.joinTags();
+    };
+
+    this.joinTags = function joinTags() {
+      if (!this.tags) {
+        return 'Loading tags...'
+      }
+      if (!this.model.TagIds) {
+        return '';
+      }
+      const findTagName = tagId => {
+        const tag = this.tags.find(tag => tag.Id === tagId);
+        if (!tag) {
+          return null;
+        }
+        return tag.Name;
+      }
+      return this.model.TagIds.map(findTagName).filter(Boolean).join(',');
+    }
+  });
