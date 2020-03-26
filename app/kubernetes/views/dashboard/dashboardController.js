f3866d7adef776d4727983a6e5bee343333895c9
Anthony Lapenna <anthony.lapenna@portainer.io>
2020-03-27 09:58:15 +1300

    feat(kubernetes): add volumes and configurations to dashboard (#152)

--- app/kubernetes/views/dashboard/dashboardController.js
+++ app/kubernetes/views/dashboard/dashboardController.js
@@ -2,13 +2,15 @@ import angular from 'angular';
 
 class KubernetesDashboardController {
   /* @ngInject */
-  constructor($async, Notifications, EndpointService, EndpointProvider, KubernetesNamespaceService, KubernetesApplicationService) {
+  constructor($async, Notifications, EndpointService, EndpointProvider, KubernetesNamespaceService, KubernetesApplicationService, KubernetesConfigurationService, KubernetesVolumeService) {
     this.$async = $async;
     this.Notifications = Notifications;
     this.EndpointService = EndpointService;
     this.EndpointProvider = EndpointProvider;
     this.KubernetesNamespaceService = KubernetesNamespaceService;
     this.KubernetesApplicationService = KubernetesApplicationService;
+    this.KubernetesConfigurationService = KubernetesConfigurationService;
+    this.KubernetesVolumeService = KubernetesVolumeService;
 
     this.onInit = this.onInit.bind(this);
     this.getAll = this.getAll.bind(this);
@@ -18,14 +20,18 @@ class KubernetesDashboardController {
   async getAllAsync() {
     try {
       const endpointId = this.EndpointProvider.endpointID();
-      const [endpoint, pools, applications] = await Promise.all([
+      const [endpoint, pools, applications, configurations, volumes] = await Promise.all([
         this.EndpointService.endpoint(endpointId),
         this.KubernetesNamespaceService.get(), // TODO: review, use ResourcePools instead of namespaces
         this.KubernetesApplicationService.get(),
+        this.KubernetesConfigurationService.get(),
+        this.KubernetesVolumeService.get()
       ]);
       this.endpoint = endpoint;
       this.pools = pools;
       this.applications = applications;
+      this.configurations = configurations;
+      this.volumes = volumes;
     } catch (err) {
       this.Notifications.error('Failure', err, 'Unable to load dashboard data');
     }
