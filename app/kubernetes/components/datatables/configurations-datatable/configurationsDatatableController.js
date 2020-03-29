139875bf476773d6ec3bf4321e9b68012252fc34
xAt0mZ <xAt0mZ@users.noreply.github.com>
2020-03-29 23:44:34 +0200

    feat(kubernetes): hide default-tokens + prevent remove (#183)
    * feat(kubernetes): hide default-tokens + prevent remove
    
    * feat(kubernetes): do not display unused label for system configurations
    
    * fix(kubernetes): minor fix around showing system configurations
    
    Co-authored-by: Anthony Lapenna <lapenna.anthony@gmail.com>

--- app/kubernetes/components/datatables/configurations-datatable/configurationsDatatableController.js
+++ app/kubernetes/components/datatables/configurations-datatable/configurationsDatatableController.js
@@ -1,10 +1,12 @@
+import _ from 'lodash-es';
+
 angular.module('portainer.docker')
   .controller('KubernetesConfigurationsDatatableController', ['$scope', '$controller', 'KubernetesNamespaceHelper', 'DatatableService', 'Authentication',
     function ($scope, $controller, KubernetesNamespaceHelper, DatatableService, Authentication) {
 
       angular.extend(this, $controller('GenericDatatableController', {$scope: $scope}));
 
-      var ctrl = this;
+      const ctrl = this;
 
       this.settings = Object.assign(this.settings, {
         showSystem: false,
@@ -18,35 +20,43 @@ angular.module('portainer.docker')
         return KubernetesNamespaceHelper.isSystemNamespace(item.Namespace);
       };
 
+      this.isSystemToken = function(item) {
+        return _.startsWith(item.Name, 'default-token-');
+      }
+
+      this.isSystemConfig = function(item) {
+        return ctrl.isSystemNamespace(item) || ctrl.isSystemToken(item);
+      }
+
       this.isDisplayed = function(item) {
-        return !ctrl.isSystemNamespace(item) || ctrl.settings.showSystem;
+        return !ctrl.isSystemConfig(item) || (ctrl.settings.showSystem && ctrl.isAdmin);
       };
 
       /**
        * Do not allow configurations in system namespaces to be selected
        */
       this.allowSelection = function(item) {
-        return !this.isSystemNamespace(item) && !item.Used;
+        return !this.isSystemConfig(item) && !item.Used;
       }
 
       this.$onInit = function() {
         this.isAdmin = Authentication.isAdmin();
         this.setDefaults();
         this.prepareTableFromDataset();
-    
+
         this.state.orderBy = this.orderBy;
         var storedOrder = DatatableService.getDataTableOrder(this.tableKey);
         if (storedOrder !== null) {
           this.state.reverseOrder = storedOrder.reverse;
           this.state.orderBy = storedOrder.orderBy;
         }
-    
+
         var textFilter = DatatableService.getDataTableTextFilters(this.tableKey);
         if (textFilter !== null) {
           this.state.textFilter = textFilter;
           this.onTextFilterChange();
         }
-    
+
         var storedFilters = DatatableService.getDataTableFilters(this.tableKey);
         if (storedFilters !== null) {
           this.filters = storedFilters;
@@ -54,7 +64,7 @@ angular.module('portainer.docker')
         if (this.filters && this.filters.state) {
           this.filters.state.open = false;
         }
-    
+
         var storedSettings = DatatableService.getDataTableSettings(this.tableKey);
         if (storedSettings !== null) {
           this.settings = storedSettings;
