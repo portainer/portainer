741681acc9b2f8a51159d24b78c7492b8bfc0a12
Chaim Lev Ari <chiptus@gmail.com>
2020-04-02 09:23:24 +0300

    refactor(edge-compute): import angular

--- app/edge/rest/edge-groups.js
+++ app/edge/rest/edge-groups.js
@@ -1,6 +1,7 @@
+import angular from 'angular';
+
 angular.module('portainer.edge')
 .factory('EdgeGroups', function EdgeGroupsFactory($resource, API_ENDPOINT_EDGE_GROUPS) {
-  'use strict';
   return $resource(API_ENDPOINT_EDGE_GROUPS + '/:id/:action', {}, {
     create: { method: 'POST', ignoreLoadingBar: true },
     query: { method: 'GET', isArray: true },
--- app/edge/views/groups/edgeGroupsView.js
+++ app/edge/views/groups/edgeGroupsView.js
@@ -1,3 +1,5 @@
+import angular from 'angular';
+
 angular.module('portainer.edge').component('edgeGroupsView', {
   templateUrl: './edgeGroupsView.html',
   controller: 'EdgeGroupsController'
