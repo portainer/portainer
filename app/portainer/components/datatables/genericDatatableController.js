d73397f5124e1dfaf9c84c839b448f72798abf43
Chaim Lev-Ari <chiptus@users.noreply.github.com>
2020-04-05 08:20:59 +0300

    Edge group list (#3644)
    * feat(edge-groups): add edge module
    
    * feat(edge-groups):  add edge-group service
    
    * feat(edge-group): add groups list view
    
    * feat(edge-groups): add link to groups in the sidebar
    
    * feat(edge-group): show endpoints count and group type
    
    * feat(edge-group): enable removal of edge groups
    
    * refactor(edge-groups): replace datatable controller with class
    
    * refactor(edge-groups): replace function with class
    
    * fix(edge-groups): sort items by endpoints count and group type
    
    * refactor(edge-groups): use generic datatable-header component
    
    * feat(app): add trace for ui router
    
    * fix(edge-compute): add ng injection to onEnter guard
    
    * fix(edge-compute): add ng injection to onEnter guard
    
    * style(edge-compute): remove space
    
    * refactor(edge-compute): import angular
    
    * fix(app): remove ui router trace
    
    * refactor(product): revert app.js
    
    * fix(edge-compute): remove admin guard from edge routes
    
    * fix(edge-groups): change label of empty datatable
    
    Co-Authored-By: Anthony Lapenna <anthony.lapenna@portainer.io>
    
    * refactor(edge-groups): rename service
    
    * fix(edge-groups): replace icon in sidebar
    
    Co-Authored-By: Anthony Lapenna <anthony.lapenna@portainer.io>
    
    * refactor(edge-groups): remove datatable controller
    
    * refactor(edge-groups): move datatable icon to binding
    
    * refactor(edge-groups): use vanilla datatable header
    
    * refactor(datatable): remove datatable header
    
    Co-authored-by: Anthony Lapenna <anthony.lapenna@portainer.io>

--- app/portainer/components/datatables/genericDatatableController.js
+++ app/portainer/components/datatables/genericDatatableController.js
@@ -42,11 +42,12 @@ angular.module('portainer.app').controller('GenericDatatableController', [
       DatatableService.setDataTableTextFilters(this.tableKey, this.state.textFilter);
     };
 
-    this.changeOrderBy = function (orderField) {
+    this.changeOrderBy = changeOrderBy.bind(this);
+    function changeOrderBy(orderField) {
       this.state.reverseOrder = this.state.orderBy === orderField ? !this.state.reverseOrder : false;
       this.state.orderBy = orderField;
       DatatableService.setDataTableOrder(this.tableKey, orderField, this.state.reverseOrder);
-    };
+    }
 
     this.selectItem = function (item, event) {
       // Handle range select using shift
