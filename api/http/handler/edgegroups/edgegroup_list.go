cf10ebd86cf2b36e62b70c8aa7900c46f71fb0eb
Anthony Lapenna <lapenna.anthony@gmail.com>
2020-04-06 09:11:09 +1200

    refactor(edge): rename edge group to Edge group

--- api/http/handler/edgegroups/edgegroup_list.go
+++ api/http/handler/edgegroups/edgegroup_list.go
@@ -11,14 +11,14 @@ import (
 func (handler *Handler) edgeGroupList(w http.ResponseWriter, r *http.Request) *httperror.HandlerError {
 	edgeGroups, err := handler.EdgeGroupService.EdgeGroups()
 	if err != nil {
-		return &httperror.HandlerError{http.StatusInternalServerError, "Unable to retrieve edge groups from the database", err}
+		return &httperror.HandlerError{http.StatusInternalServerError, "Unable to retrieve Edge groups from the database", err}
 	}
 	decoratedEdgeGroups := []portainer.EdgeGroup{}
 	for _, edgeGroup := range edgeGroups {
 		if edgeGroup.Dynamic {
 			endpoints, err := handler.getEndpointsByTags(edgeGroup.TagIDs)
 			if err != nil {
-				return &httperror.HandlerError{http.StatusInternalServerError, "Unable to retrieve endpoints and endpoint groups for edge group", err}
+				return &httperror.HandlerError{http.StatusInternalServerError, "Unable to retrieve endpoints and endpoint groups for Edge group", err}
 			}
 
 			edgeGroup.Endpoints = endpoints
