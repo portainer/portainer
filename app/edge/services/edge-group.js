e57901985323c9daa0e669a1cd0f74d73e6cde76
Chaim Lev-Ari <chiptus@users.noreply.github.com>
2020-04-05 08:20:59 +0300

    Edge group list (#3644)
    * feat(edge-groups): add edge module
    
    * feat(edge-groups):  add edge-group service
    
    * feat(edge-group): add groups list view
    
    * feat(edge-groups): add link to groups in the sidebar
    
    * feat(edge-group): show endpoints count and group type
    
    * feat(edge-group): enable removal of edge groups
    
    * refactor(edge-groups): replace datatable controller with class
    
    * refactor(edge-groups): replace function with class
    
    * fix(edge-groups): sort items by endpoints count and group type
    
    * refactor(edge-groups): use generic datatable-header component
    
    * feat(app): add trace for ui router
    
    * fix(edge-compute): add ng injection to onEnter guard
    
    * fix(edge-compute): add ng injection to onEnter guard
    
    * style(edge-compute): remove space
    
    * refactor(edge-compute): import angular
    
    * fix(app): remove ui router trace
    
    * refactor(product): revert app.js
    
    * fix(edge-compute): remove admin guard from edge routes
    
    * fix(edge-groups): change label of empty datatable
    
    Co-Authored-By: Anthony Lapenna <anthony.lapenna@portainer.io>
    
    * refactor(edge-groups): rename service
    
    * fix(edge-groups): replace icon in sidebar
    
    Co-Authored-By: Anthony Lapenna <anthony.lapenna@portainer.io>
    
    * refactor(edge-groups): remove datatable controller
    
    * refactor(edge-groups): move datatable icon to binding
    
    * refactor(edge-groups): use vanilla datatable header
    
    * refactor(datatable): remove datatable header
    
    Co-authored-by: Anthony Lapenna <anthony.lapenna@portainer.io>

---
+++ app/edge/services/edge-group.js
@@ -0,0 +1,24 @@
+import angular from 'angular';
+
+angular.module('portainer.app').factory('EdgeGroupService', function EdgeGroupServiceFactory(EdgeGroups) {
+  var service = {};
+
+  // service.group = async function group(groupId) {
+  // };
+
+  service.groups = function groups() {
+    return EdgeGroups.query({}).$promise;
+  };
+
+  service.remove = async function remove(groupId) {
+    return EdgeGroups.remove({ id: groupId }).$promise;
+  };
+
+  // service.create = async function create(group) {
+  // };
+
+  // service.update = async function update(groupId, group) {
+  // };
+
+  return service;
+});
