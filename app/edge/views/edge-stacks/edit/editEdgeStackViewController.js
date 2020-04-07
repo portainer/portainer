0567a0465af9c9bc2b52c726d413997c011a5459
Chaim Lev-Ari <chiptus@users.noreply.github.com>
2020-04-07 12:58:06 +0300

    feat(edge-stacks): add basic views and sidebar link (#3689)
    * feat(edge-stacks): add mock routes
    
    * feat(edge-stacks): add link to stacks on sidebar
    
    * feat(edge-stacks): add edge stacks view
    
    * feat(edge-stacks): add create view
    
    * feat(edge-stacks): add edit view
    
    * fix(edge-stacks): use class in controller

---
+++ app/edge/views/edge-stacks/edit/editEdgeStackViewController.js
@@ -0,0 +1,9 @@
+import angular from 'angular';
+
+class EditEdgeStackViewController {
+  constructor() {}
+}
+
+angular.module('portainer.edge')
+  .controller('EditEdgeStackViewController', EditEdgeStackViewController);
+export default EditEdgeStackViewController;
