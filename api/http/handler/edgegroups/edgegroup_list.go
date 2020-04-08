5094485974c15175cc0bacf0445ecfce8d3612c1
Chaim Lev Ari <chiptus@gmail.com>
2020-04-08 17:21:25 +0300

    feat(edge-groups): fetch associated endpoints (AND and OR)

--- api/http/handler/edgegroups/edgegroup_list.go
+++ api/http/handler/edgegroups/edgegroup_list.go
@@ -16,7 +16,7 @@ func (handler *Handler) edgeGroupList(w http.ResponseWriter, r *http.Request) *h
 	decoratedEdgeGroups := []portainer.EdgeGroup{}
 	for _, edgeGroup := range edgeGroups {
 		if edgeGroup.Dynamic {
-			endpoints, err := handler.getEndpointsByTags(edgeGroup.TagIDs)
+			endpoints, err := handler.getEndpointsByTags(edgeGroup.TagIDs, edgeGroup.MustHaveAllTags)
 			if err != nil {
 				return &httperror.HandlerError{http.StatusInternalServerError, "Unable to retrieve endpoints and endpoint groups for Edge group", err}
 			}
