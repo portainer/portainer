07fe6157dcbd519c9ceffd764add321e71d5b462
Chaim Lev-Ari <chiptus@users.noreply.github.com>
2020-04-13 02:24:35 +0300

    feat(edge-stacks): create basic edge stack service (#3704)
    Co-authored-by: Anthony Lapenna <anthony.lapenna@portainer.io>

---
+++ app/edge/rest/edge-stacks.js
@@ -0,0 +1,15 @@
+import angular from 'angular';
+
+angular.module('portainer.edge').factory('EdgeStacks', function EdgeStacksFactory($resource, API_ENDPOINT_EDGE_STACKS) {
+  return $resource(
+    API_ENDPOINT_EDGE_STACKS + '/:id/:action',
+    {},
+    {
+      create: { method: 'POST', ignoreLoadingBar: true },
+      query: { method: 'GET', isArray: true },
+      get: { method: 'GET', params: { id: '@id' } },
+      update: { method: 'PUT', params: { id: '@Id' } },
+      remove: { method: 'DELETE', params: { id: '@id' } },
+    }
+  );
+});
