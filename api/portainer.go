6bb025155c11dd6bb3f29a1d9b1c35f3fe752763
Chaim Lev-Ari <chiptus@users.noreply.github.com>
2020-04-13 09:53:37 +0300

    feat(edge-groups): Provide a switch to use AND or OR for tags (#3695)
    * feat(edge-groups): add switch to form
    
    * feat(project): add property to EdgeGroup
    
    * feat(edge-groups): save mustHaveAllTags
    
    * feat(edge-groups): fetch associated endpoints (AND and OR)
    
    * feat(edge-groups): add AND selector
    
    * feat(edge-groups): default to AND
    
    * fix(edge-groups): rewrite selector options
    
    Co-Authored-By: Anthony Lapenna <anthony.lapenna@portainer.io>
    
    * refactor(endpoints): move margin to schedule form
    
    * fix(edge-groups): move the selector to top of group
    
    * refactor(edge-groups): replace partialMatch property
    
    Co-authored-by: Anthony Lapenna <anthony.lapenna@portainer.io>

--- api/portainer.go
+++ api/portainer.go
@@ -86,11 +86,12 @@ type (
 
 	// EdgeGroup represents an Edge group
 	EdgeGroup struct {
-		ID        EdgeGroupID  `json:"Id"`
-		Name      string       `json:"Name"`
-		Dynamic   bool         `json:"Dynamic"`
-		TagIDs    []TagID      `json:"TagIds"`
-		Endpoints []EndpointID `json:"Endpoints"`
+		ID           EdgeGroupID  `json:"Id"`
+		Name         string       `json:"Name"`
+		Dynamic      bool         `json:"Dynamic"`
+		TagIDs       []TagID      `json:"TagIds"`
+		Endpoints    []EndpointID `json:"Endpoints"`
+		PartialMatch bool         `json:"PartialMatch"`
 	}
 
 	// EdgeGroupID represents an Edge group identifier
