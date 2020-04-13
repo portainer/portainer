f80f8869c9afd75ad9398b601031f7bea7c6f484
Chaim Lev-Ari <chiptus@users.noreply.github.com>
2020-04-13 09:56:01 +0300

    feat(edge-stacks): add Edge stack creation view (#3705)
    * feat(edge-stacks): basic creation view
    
    * feat(edge-stacks): add group selector
    
    * feat(edge-stack): create edge stack
    
    * fix(code-editor): apply digest cycle after editor is changed
    
    * style(project): reformat constants file
    
    * feat(edge-stacks): add a note about missing edge groups
    
    * fix(edge-stacks): add groups when creating stack from file

--- app/edge/services/edge-stack.js
+++ app/edge/services/edge-stack.js
@@ -1,6 +1,6 @@
 import angular from 'angular';
 
-angular.module('portainer.edge').factory('EdgeStackService', function EdgeStackServiceFactory(EdgeStacks) {
+angular.module('portainer.edge').factory('EdgeStackService', function EdgeStackServiceFactory(EdgeStacks, FileUploadService) {
   var service = {};
 
   service.stack = function stack(stackId) {
@@ -15,8 +15,43 @@ angular.module('portainer.edge').factory('EdgeStackService', function EdgeStackS
     return EdgeStacks.remove({ id: stackId }).$promise;
   };
 
-  service.create = function create(stack) {
-    return EdgeStacks.create(stack).$promise;
+  service.createStackFromFileContent = async function createStackFromFileContent(name, stackFileContent, edgeGroups) {
+    var payload = {
+      Name: name,
+      StackFileContent: stackFileContent,
+      EdgeGroups: edgeGroups,
+    };
+    try {
+      return await EdgeStacks.create({ method: 'string' }, payload).$promise;
+    } catch (err) {
+      throw { msg: 'Unable to create the stack', err };
+    }
+  };
+
+  service.createStackFromFileUpload = async function createStackFromFileUpload(name, stackFile, edgeGroups) {
+    try {
+      return await FileUploadService.createEdgeStack(name, stackFile, edgeGroups);
+    } catch (err) {
+      throw { msg: 'Unable to create the stack', err };
+    }
+  };
+
+  service.createStackFromGitRepository = async function createStackFromGitRepository(name, repositoryOptions, edgeGroups) {
+    var payload = {
+      Name: name,
+      RepositoryURL: repositoryOptions.RepositoryURL,
+      RepositoryReferenceName: repositoryOptions.RepositoryReferenceName,
+      ComposeFilePathInRepository: repositoryOptions.ComposeFilePathInRepository,
+      RepositoryAuthentication: repositoryOptions.RepositoryAuthentication,
+      RepositoryUsername: repositoryOptions.RepositoryUsername,
+      RepositoryPassword: repositoryOptions.RepositoryPassword,
+      EdgeGroups: edgeGroups,
+    };
+    try {
+      return await EdgeStacks.create({ method: 'repository' }, payload).$promise;
+    } catch (err) {
+      throw { msg: 'Unable to create the stack', err };
+    }
   };
 
   service.update = function update(stack) {
