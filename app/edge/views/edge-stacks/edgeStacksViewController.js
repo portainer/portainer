224f7c09e70569473445442d028dcbbba13f79f5
Chaim Lev Ari <chiptus@gmail.com>
2020-04-14 12:33:11 +0300

    feat(edge-stacks): remove stack

--- app/edge/views/edge-stacks/edgeStacksViewController.js
+++ app/edge/views/edge-stacks/edgeStacksViewController.js
@@ -1,27 +1,28 @@
 import angular from 'angular';
+import _ from 'lodash-es';
 
 angular.module('portainer.edge').controller('EdgeStacksViewController', function EdgeStacksViewController($state, Notifications, EdgeStackService, ModalService, $scope, $async) {
   this.stacks = undefined;
 
   this.getStacks = getStacks.bind(this);
   this.removeAction = removeAction.bind(this);
+  this.removeActionAsync = removeActionAsync.bind(this);
 
   this.$onInit = function $onInit() {
     this.getStacks();
   };
 
   function removeAction(stacks) {
-    return $async(removeActionAsync, stacks);
+    return $async(this.removeActionAsync, stacks);
   }
 
   async function removeActionAsync(stacks) {
     await Promise.all(
       stacks.map(async (stack) => {
         try {
-          await EdgeStackService.remove(stack);
+          await EdgeStackService.remove(stack.Id);
           Notifications.success('Stack successfully removed', stack.Name);
-          const index = this.stacks.indexOf(stack);
-          this.stacks.splice(index, 1);
+          _.remove(this.stacks, stack);
         } catch (err) {
           Notifications.error('Failure', err, 'Unable to remove stack ' + stack.Name);
         }
