409b78a77ab17bae5bf78dcc998e86074018721b
Anthony Lapenna <lapenna.anthony@gmail.com>
2020-04-15 17:30:35 +1200

    feat(templates): remove template management features

--- app/portainer/models/settings.js
+++ app/portainer/models/settings.js
@@ -9,7 +9,6 @@ export function SettingsViewModel(data) {
   this.AllowVolumeBrowserForRegularUsers = data.AllowVolumeBrowserForRegularUsers;
   this.SnapshotInterval = data.SnapshotInterval;
   this.TemplatesURL = data.TemplatesURL;
-  this.ExternalTemplates = data.ExternalTemplates;
   this.EnableHostManagementFeatures = data.EnableHostManagementFeatures;
   this.EdgeAgentCheckinInterval = data.EdgeAgentCheckinInterval;
 }
@@ -20,7 +19,6 @@ export function PublicSettingsViewModel(settings) {
   this.AllowVolumeBrowserForRegularUsers = settings.AllowVolumeBrowserForRegularUsers;
   this.AuthenticationMethod = settings.AuthenticationMethod;
   this.EnableHostManagementFeatures = settings.EnableHostManagementFeatures;
-  this.ExternalTemplates = settings.ExternalTemplates;
   this.LogoURL = settings.LogoURL;
   this.OAuthLoginURI = settings.OAuthLoginURI;
 }
