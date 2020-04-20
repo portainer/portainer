fb677ca78188cf33033e99df510bbc0bd5b09e01
Maxime Bajeux <max.bajeux@gmail.com>
2020-04-20 23:27:04 +0200

    feat(templates): Support templates versioning format

--- app/portainer/models/template.js
+++ app/portainer/models/template.js
@@ -1,32 +1,44 @@
 import _ from 'lodash-es';
 import { PorImageRegistryModel } from 'Docker/models/porImageRegistry';
 
-export function TemplateViewModel(data) {
-  this.Id = data.Id;
-  this.Title = data.title;
-  this.Type = data.type;
-  this.Description = data.description;
-  this.AdministratorOnly = data.AdministratorOnly;
-  this.Name = data.name;
-  this.Note = data.note;
-  this.Categories = data.categories ? data.categories : [];
-  this.Platform = data.platform ? data.platform : '';
-  this.Logo = data.logo;
-  this.Repository = data.repository;
-  this.Hostname = data.hostname;
-  this.RegistryModel = new PorImageRegistryModel();
-  this.RegistryModel.Image = data.image;
-  this.RegistryModel.Registry.URL = data.registry || '';
-  this.Command = data.command ? data.command : '';
-  this.Network = data.network ? data.network : '';
-  this.Privileged = data.privileged ? data.privileged : false;
-  this.Interactive = data.interactive ? data.interactive : false;
-  this.RestartPolicy = data.restart_policy ? data.restart_policy : 'always';
-  this.Labels = data.labels ? data.labels : [];
-  this.Hosts = data.hosts ? data.hosts : [];
-  this.Env = templateEnv(data);
-  this.Volumes = templateVolumes(data);
-  this.Ports = templatePorts(data);
+export class TemplateViewModel {
+  constructor(data, version) {
+    switch (version) {
+      case '2':
+        this.setTemplatesV2(data);
+        break;
+      default:
+        throw new Error('Unknown template version');
+    }
+  }
+
+  setTemplatesV2(data) {
+    this.Id = data.Id;
+    this.Title = data.title;
+    this.Type = data.type;
+    this.Description = data.description;
+    this.AdministratorOnly = data.AdministratorOnly;
+    this.Name = data.name;
+    this.Note = data.note;
+    this.Categories = data.categories ? data.categories : [];
+    this.Platform = data.platform ? data.platform : '';
+    this.Logo = data.logo;
+    this.Repository = data.repository;
+    this.Hostname = data.hostname;
+    this.RegistryModel = new PorImageRegistryModel();
+    this.RegistryModel.Image = data.image;
+    this.RegistryModel.Registry.URL = data.registry || '';
+    this.Command = data.command ? data.command : '';
+    this.Network = data.network ? data.network : '';
+    this.Privileged = data.privileged ? data.privileged : false;
+    this.Interactive = data.interactive ? data.interactive : false;
+    this.RestartPolicy = data.restart_policy ? data.restart_policy : 'always';
+    this.Labels = data.labels ? data.labels : [];
+    this.Hosts = data.hosts ? data.hosts : [];
+    this.Env = templateEnv(data);
+    this.Volumes = templateVolumes(data);
+    this.Ports = templatePorts(data);
+  }
 }
 
 function templatePorts(data) {
