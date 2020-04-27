85a4e70b87e246d523ceaad28674d0f3a6ab5556
Anthony Lapenna <anthony.lapenna@portainer.io>
2020-04-27 17:58:24 +1200

    feat(templates): leftovers cleanup (#3762)
    * feat(templates): leftovers cleanup
    
    * feat(templates): update CLIFlags structure

--- app/constants.js
+++ app/constants.js
@@ -19,7 +19,6 @@ angular
   .constant('API_ENDPOINT_TEAM_MEMBERSHIPS', 'api/team_memberships')
   .constant('API_ENDPOINT_TEMPLATES', 'api/templates')
   .constant('API_ENDPOINT_WEBHOOKS', 'api/webhooks')
-  .constant('DEFAULT_TEMPLATES_URL', 'https://raw.githubusercontent.com/portainer/templates/master/templates.json')
   .constant('PAGINATION_MAX_ITEMS', 10)
   .constant('APPLICATION_CACHE_VALIDITY', 3600)
   .constant('CONSOLE_COMMANDS_LABEL_PREFIX', 'io.portainer.commands.')
