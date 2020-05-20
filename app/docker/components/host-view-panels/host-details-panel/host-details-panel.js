6d74df59f9e89824359a51f86f81d59cd72e7035
Chaim Lev-Ari <chiptus@gmail.com>
2020-05-20 13:29:29 +0300

    refactor(host): remove jobs table

--- app/docker/components/host-view-panels/host-details-panel/host-details-panel.js
+++ app/docker/components/host-view-panels/host-details-panel/host-details-panel.js
@@ -2,9 +2,7 @@ angular.module('portainer.docker').component('hostDetailsPanel', {
   templateUrl: './host-details-panel.html',
   bindings: {
     host: '<',
-    isJobEnabled: '<',
     isBrowseEnabled: '<',
     browseUrl: '@',
-    jobUrl: '@',
   },
 });
