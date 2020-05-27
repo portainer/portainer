fff34eca342f1ff7243fece29a7464d650c6e40f
Chaim Lev-Ari <chiptus@gmail.com>
2020-05-27 09:31:13 +0300

    chore(client): rename generators

--- plopfile.js
+++ plopfile.js
@@ -1,4 +1,4 @@
-module.exports = function(plop) {
+module.exports = function (plop) {
   // use of INIT_CWD instead of process.cwd() because yarn changes the cwd
   const cwd = process.env.INIT_CWD;
   plop.addHelper('cwd', () => cwd);
@@ -7,33 +7,33 @@ module.exports = function(plop) {
       {
         type: 'input',
         name: 'name',
-        message: 'component name please'
+        message: 'component name please',
       },
       {
         type: 'input',
         name: 'module',
         message: 'module name please',
-        default: `${getCurrentPortainerModule(cwd)}`
+        default: `${getCurrentPortainerModule(cwd)}`,
         // when: false
-      }
+      },
     ], // array of inquirer prompts
     actions: [
       {
         type: 'add',
-        path: `{{cwd}}/{{dashCase name}}/{{camelCase name}}.js`,
-        templateFile: './plop-templates/component.js.hbs'
+        path: `{{cwd}}/{{dashCase name}}/index.js`,
+        templateFile: './plop-templates/component.js.hbs',
       },
       {
         type: 'add',
         path: `{{cwd}}/{{dashCase name}}/{{camelCase name}}Controller.js`,
-        templateFile: './plop-templates/component-controller.js.hbs'
+        templateFile: './plop-templates/component-controller.js.hbs',
       },
       {
         type: 'add',
         path: `{{cwd}}/{{dashCase name}}/{{camelCase name}}.html`,
-        templateFile: './plop-templates/component.html.hbs'
-      }
-    ] // array of actions
+        templateFile: './plop-templates/component.html.hbs',
+      },
+    ], // array of actions
   });
 };
 
