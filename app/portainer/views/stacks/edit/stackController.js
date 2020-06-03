5588f425040badfcdcb09e5bae196076fa37a0c0
Anthony Lapenna <lapenna.anthony@gmail.com>
2020-06-03 13:09:18 +1200

    feat(stacks): use runtime endpoint ID

--- app/portainer/views/stacks/edit/stackController.js
+++ app/portainer/views/stacks/edit/stackController.js
@@ -151,15 +151,7 @@ angular.module('portainer.app').controller('StackController', [
       var env = FormHelper.removeInvalidEnvVars($scope.stack.Env);
       var prune = $scope.formValues.Prune;
       var stack = $scope.stack;
-
-      // TODO: this is a work-around for stacks created with Portainer version >= 1.17.1
-      // The EndpointID property is not available for these stacks, we can pass
-      // the current endpoint identifier as a part of the update request. It will be used if
-      // the EndpointID property is not defined on the stack.
-      var endpointId = EndpointProvider.endpointID();
-      if (stack.EndpointId === 0) {
-        stack.EndpointId = endpointId;
-      }
+      stack.EndpointId = EndpointProvider.endpointID();
 
       $scope.state.actionInProgress = true;
       StackService.updateStack(stack, stackFile, env, prune)
