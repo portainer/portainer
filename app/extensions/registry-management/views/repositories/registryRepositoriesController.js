b0ad21285812dbe6786768620ecfe9d4f1ac27e0
Chaim Lev-Ari <chiptus@users.noreply.github.com>
2020-07-07 01:59:33 +0300

    fix(registries): hide zero tags repositories (#3985)

--- app/extensions/registry-management/views/repositories/registryRepositoriesController.js
+++ app/extensions/registry-management/views/repositories/registryRepositoriesController.js
@@ -54,8 +54,8 @@ angular.module('portainer.extensions.registrymanagement').controller('RegistryRe
             .then(function success() {
               return RegistryServiceSelector.repositories($scope.registry);
             })
-            .then(function success(data) {
-              $scope.repositories = data;
+            .then(function success(repositories) {
+              $scope.repositories = _.filter(repositories, (repository) => repository.TagsCount > 0);
             })
             .catch(function error() {
               $scope.state.displayInvalidConfigurationMessage = true;
