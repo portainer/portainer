67b0ffed4214f8e6c81345d8e7e562f52181621a
Chaim Lev-Ari <chiptus@gmail.com>
2020-07-06 13:47:46 +0300

    refactor(sidebar): remove refernce to endpointId

--- app/portainer/views/sidebar/sidebarController.js
+++ app/portainer/views/sidebar/sidebarController.js
@@ -1,39 +1,38 @@
-import angular from 'angular';
-
-function SidebarController($q, $scope, $transitions, EndpointProvider, StateManager, Notifications, Authentication, UserService) {
-  function checkPermissions(memberships) {
-    var isLeader = false;
-    angular.forEach(memberships, function (membership) {
-      if (membership.Role === 1) {
-        isLeader = true;
-      }
-    });
-    $scope.isTeamLeader = isLeader;
-  }
-
-  function initView() {
-    $scope.uiVersion = StateManager.getState().application.version;
-    $scope.logo = StateManager.getState().application.logo;
-    $scope.endpointId = EndpointProvider.endpointID();
-
-    let userDetails = Authentication.getUserDetails();
-    let isAdmin = Authentication.isAdmin();
-    $scope.isAdmin = isAdmin;
-
-    $q.when(!isAdmin ? UserService.userMemberships(userDetails.ID) : [])
-      .then(function success(data) {
-        checkPermissions(data);
-      })
-      .catch(function error(err) {
-        Notifications.error('Failure', err, 'Unable to retrieve user memberships');
+angular.module('portainer.app').controller('SidebarController', [
+  '$q',
+  '$scope',
+  'StateManager',
+  'Notifications',
+  'Authentication',
+  'UserService',
+  function ($q, $scope, StateManager, Notifications, Authentication, UserService) {
+    function checkPermissions(memberships) {
+      var isLeader = false;
+      angular.forEach(memberships, function (membership) {
+        if (membership.Role === 1) {
+          isLeader = true;
+        }
       });
+      $scope.isTeamLeader = isLeader;
+    }
 
-    $transitions.onSuccess({}, () => {
-      $scope.endpointId = EndpointProvider.endpointID();
-    });
-  }
+    function initView() {
+      $scope.uiVersion = StateManager.getState().application.version;
+      $scope.logo = StateManager.getState().application.logo;
 
-  initView();
-}
+      let userDetails = Authentication.getUserDetails();
+      let isAdmin = Authentication.isAdmin();
+      $scope.isAdmin = isAdmin;
 
-angular.module('portainer.app').controller('SidebarController', SidebarController);
+      $q.when(!isAdmin ? UserService.userMemberships(userDetails.ID) : [])
+        .then(function success(data) {
+          checkPermissions(data);
+        })
+        .catch(function error(err) {
+          Notifications.error('Failure', err, 'Unable to retrieve user memberships');
+        });
+    }
+
+    initView();
+  },
+]);
