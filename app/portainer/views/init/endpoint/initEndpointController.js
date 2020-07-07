1da56fcf87522db3add98c2c3c56c329e8d9e027
Anthony Lapenna <lapenna.anthony@gmail.com>
2020-07-07 16:26:45 +1200

    feat(endpoint-init): remove Azure ACI and remote Docker options

--- app/portainer/views/init/endpoint/initEndpointController.js
+++ app/portainer/views/init/endpoint/initEndpointController.js
@@ -1,12 +1,10 @@
 import _ from 'lodash-es';
 import angular from 'angular';
-import { PortainerEndpointInitFormValues, PortainerEndpointInitFormValueEndpointSections } from 'Portainer/models/endpoint/formValues';
-import { PortainerEndpointTypes, PortainerEndpointConnectionTypes } from 'Portainer/models/endpoint/models';
+import { PortainerEndpointInitFormValueEndpointSections, PortainerEndpointInitFormValues } from 'Portainer/models/endpoint/formValues';
+import { PortainerEndpointConnectionTypes, PortainerEndpointTypes } from 'Portainer/models/endpoint/models';
 
 require('./includes/localDocker.html');
 require('./includes/localKubernetes.html');
-require('./includes/remote.html');
-require('./includes/azure.html');
 require('./includes/agent.html');
 
 class InitEndpointController {
@@ -23,8 +21,6 @@ class InitEndpointController {
     this.createLocalEndpointAsync = this.createLocalEndpointAsync.bind(this);
     this.createLocalKubernetesEndpointAsync = this.createLocalKubernetesEndpointAsync.bind(this);
     this.createAgentEndpointAsync = this.createAgentEndpointAsync.bind(this);
-    this.createAzureEndpointAsync = this.createAzureEndpointAsync.bind(this);
-    this.createRemoteEndpointAsync = this.createRemoteEndpointAsync.bind(this);
   }
 
   $onInit() {
@@ -43,30 +39,12 @@ class InitEndpointController {
     this.PortainerEndpointConnectionTypes = PortainerEndpointConnectionTypes;
   }
 
-  isRemoteConnectButtonDisabled() {
-    return (
-      this.state.actionInProgress ||
-      !this.formValues.Name ||
-      !this.formValues.URL ||
-      (this.formValues.TLS &&
-        ((this.formValues.TLSVerify && !this.formValues.TLSCACert) || (!this.formValues.TLSSKipClientVerify && (!this.formValues.TLSCert || !this.formValues.TLSKey))))
-    );
-  }
-
-  isAzureConnectButtonDisabled() {
-    return this.state.actionInProgress || !this.formValues.Name || !this.formValues.AzureApplicationId || !this.formValues.AzureTenantId || !this.formValues.AzureAuthenticationKey;
-  }
-
   isConnectButtonDisabled() {
     switch (this.formValues.ConnectionType) {
       case PortainerEndpointConnectionTypes.DOCKER_LOCAL:
         return this.state.actionInProgress;
       case PortainerEndpointConnectionTypes.KUBERNETES_LOCAL:
         return this.state.actionInProgress;
-      case PortainerEndpointConnectionTypes.REMOTE:
-        return this.isRemoteConnectButtonDisabled();
-      case PortainerEndpointConnectionTypes.AZURE:
-        return this.isAzureConnectButtonDisabled();
       case PortainerEndpointConnectionTypes.AGENT:
         return this.state.actionInProgress || !this.formValues.Name || !this.formValues.URL;
       default:
@@ -80,10 +58,6 @@ class InitEndpointController {
         return this.createLocalEndpoint();
       case PortainerEndpointConnectionTypes.KUBERNETES_LOCAL:
         return this.createLocalKubernetesEndpoint();
-      case PortainerEndpointConnectionTypes.REMOTE:
-        return this.createRemoteEndpoint();
-      case PortainerEndpointConnectionTypes.AZURE:
-        return this.createAzureEndpoint();
       case PortainerEndpointConnectionTypes.AGENT:
         return this.createAgentEndpoint();
       default:
@@ -163,58 +137,6 @@ class InitEndpointController {
   }
 
   createAgentEndpoint() {
-    return this.$async(this.createAgentEndpointAsync);
-  }
-
-  /**
-   * DOCKER REMOTE (1)
-   */
-  async createRemoteEndpointAsync() {
-    try {
-      this.state.actionInProgress = true;
-      const name = this.formValues.Name;
-      const type = PortainerEndpointTypes.DockerEnvironment;
-      const URL = this.formValues.URL;
-      const PublicURL = URL.split(':')[0];
-      const TLS = this.formValues.TLS;
-      const TLSSkipVerify = TLS && this.formValues.TLSSkipVerify;
-      const TLSSKipClientVerify = TLS && this.formValues.TLSSKipClientVerify;
-      const TLSCAFile = TLSSkipVerify ? null : this.formValues.TLSCACert;
-      const TLSCertFile = TLSSKipClientVerify ? null : this.formValues.TLSCert;
-      const TLSKeyFile = TLSSKipClientVerify ? null : this.formValues.TLSKey;
-      await this.EndpointService.createRemoteEndpoint(name, type, URL, PublicURL, 1, [], TLS, TLSSkipVerify, TLSSKipClientVerify, TLSCAFile, TLSCertFile, TLSKeyFile);
-      this.$state.go('portainer.home');
-    } catch (err) {
-      this.Notifications.error('Failure', err, 'Unable to connect to the Docker environment');
-    } finally {
-      this.state.actionInProgress = false;
-    }
-  }
-
-  createRemoteEndpoint() {
-    return this.$async(this.createAgentEndpointAsync);
-  }
-
-  /**
-   * AZURE (4)
-   */
-  async createAzureEndpointAsync() {
-    try {
-      this.state.actionInProgress = true;
-      var name = this.formValues.Name;
-      var applicationId = this.formValues.AzureApplicationId;
-      var tenantId = this.formValues.AzureTenantId;
-      var authenticationKey = this.formValues.AzureAuthenticationKey;
-      await this.EndpointService.createAzureEndpoint(name, applicationId, tenantId, authenticationKey, 1, []);
-      this.$state.go('portainer.home');
-    } catch (err) {
-      this.Notifications.error('Failure', err, 'Unable to connect to the Azure environment');
-    } finally {
-      this.state.actionInProgress = false;
-    }
-  }
-
-  createAzureEndpoint() {
     return this.$async(this.createAgentEndpointAsync);
   }
 }
