22230497f442de7cf4e8a462c6ba6d56ad8aae68
Chaim Lev-Ari <chiptus@gmail.com>
2020-07-07 19:46:20 +0300

    feat(crypto): use secure cipher suites

--- api/crypto/tls.go
+++ api/crypto/tls.go
@@ -12,6 +12,17 @@ func CreateTLSConfigurationFromBytes(caCert, cert, key []byte, skipClientVerific
 	config := &tls.Config{}
 	config.InsecureSkipVerify = skipServerVerification
 	config.MinVersion = tls.VersionTLS12
+	config.CipherSuites = []uint16{
+		tls.TLS_AES_128_GCM_SHA256,
+		tls.TLS_AES_256_GCM_SHA384,
+		tls.TLS_CHACHA20_POLY1305_SHA256,
+		tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+		tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+		tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+		tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+		tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+		tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+	}
 
 	if !skipClientVerification {
 		certificate, err := tls.X509KeyPair(cert, key)
@@ -36,6 +47,17 @@ func CreateTLSConfigurationFromDisk(caCertPath, certPath, keyPath string, skipSe
 	config := &tls.Config{}
 	config.InsecureSkipVerify = skipServerVerification
 	config.MinVersion = tls.VersionTLS12
+	config.CipherSuites = []uint16{
+		tls.TLS_AES_128_GCM_SHA256,
+		tls.TLS_AES_256_GCM_SHA384,
+		tls.TLS_CHACHA20_POLY1305_SHA256,
+		tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+		tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+		tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+		tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+		tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+		tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+	}
 
 	if certPath != "" && keyPath != "" {
 		cert, err := tls.LoadX509KeyPair(certPath, keyPath)
