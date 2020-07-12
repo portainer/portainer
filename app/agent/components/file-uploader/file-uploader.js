68ed252c70af223005fe1898d3b352262619f0eb
Chaim Lev-Ari <chiptus@gmail.com>
2020-07-12 18:19:24 +0300

    refactor(host): convert fileUploader to es6

--- app/agent/components/file-uploader/file-uploader.js
+++
@@ -1,7 +0,0 @@
-angular.module('portainer.agent').component('fileUploader', {
-  templateUrl: './file-uploader.html',
-  controller: 'FileUploaderController',
-  bindings: {
-    uploadFile: '<onFileSelected',
-  },
-});
