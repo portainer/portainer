f8bd075ce44fe7e577e4e2839b1c10a1c2f5c7ed
Chaim Lev-Ari <chiptus@users.noreply.github.com>
2020-07-13 13:26:23 +0300

    feat(containers): disable edit container on security setting restricting regular users (#4033)
    * feat(settings): add info about container edit disable
    
    * feat(settings): set security settings
    
    * feat(containers): hide recreate button when setting is enabled
    
    * feat(settings): rephrase security notice
    
    * fix(settings): save allowHostNamespaceForRegularUsers to state

--- app/portainer/views/settings/settingsController.js
+++ app/portainer/views/settings/settingsController.js
@@ -38,6 +38,11 @@ angular.module('portainer.app').controller('SettingsController', [
       allowDeviceMappingForRegularUsers: false,
     };
 
+    $scope.isContainerEditDisabled = function isContainerEditDisabled() {
+      const { restrictBindMounts, restrictHostNamespaceForRegularUsers, restrictPrivilegedMode, disableDeviceMappingForRegularUsers } = this.formValues;
+      return restrictBindMounts || restrictHostNamespaceForRegularUsers || restrictPrivilegedMode || disableDeviceMappingForRegularUsers;
+    };
+
     $scope.removeFilteredContainerLabel = function (index) {
       var settings = $scope.settings;
       settings.BlackListedLabels.splice(index, 1);
@@ -92,6 +97,8 @@ angular.module('portainer.app').controller('SettingsController', [
           StateManager.updateAllowStackManagementForRegularUsers(settings.AllowStackManagementForRegularUsers);
           StateManager.updateAllowHostNamespaceForRegularUsers(settings.AllowHostNamespaceForRegularUsers);
           StateManager.updateAllowDeviceMappingForRegularUsers(settings.AllowDeviceMappingForRegularUsers);
+          StateManager.updateAllowPrivilegedModeForRegularUsers(settings.AllowPrivilegedModeForRegularUsers);
+          StateManager.updateAllowBindMountsForRegularUsers(settings.AllowBindMountsForRegularUsers);
           $state.reload();
         })
         .catch(function error(err) {
