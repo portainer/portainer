27aa51aab2c3dbb5526c9b5ec8c952616982cacf
Chaim Lev-Ari <chiptus@gmail.com>
2020-07-20 09:30:14 +0300

    refactor(agent): refactor agentv1 to es6

--- app/agent/rest/v1/agent.js
+++ app/agent/rest/v1/agent.js
@@ -1,17 +1,15 @@
-angular.module('portainer.agent').factory('AgentVersion1', [
-  '$resource',
-  'API_ENDPOINT_ENDPOINTS',
-  'EndpointProvider',
-  function AgentFactory($resource, API_ENDPOINT_ENDPOINTS, EndpointProvider) {
-    'use strict';
-    return $resource(
-      API_ENDPOINT_ENDPOINTS + '/:endpointId/docker/agents',
-      {
-        endpointId: EndpointProvider.endpointID,
-      },
-      {
-        query: { method: 'GET', isArray: true },
-      }
-    );
-  },
-]);
+import angular from 'angular';
+
+angular.module('portainer.agent').factory('AgentVersion1', AgentFactory);
+
+function AgentFactory($resource, API_ENDPOINT_ENDPOINTS, EndpointProvider) {
+  return $resource(
+    `${API_ENDPOINT_ENDPOINTS}/:endpointId/docker/agents`,
+    {
+      endpointId: EndpointProvider.endpointID,
+    },
+    {
+      query: { method: 'GET', isArray: true },
+    }
+  );
+}
