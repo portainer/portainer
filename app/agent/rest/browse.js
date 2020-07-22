c2f8d4f422279fb223ed28e590f6dc604a58f788
Chaim Lev-Ari <chiptus@gmail.com>
2020-07-22 14:07:36 +0300

    refactor(agent): refactor browse to es6

--- app/agent/rest/browse.js
+++ app/agent/rest/browse.js
@@ -1,39 +1,34 @@
 import { browseGetResponse } from './response/browse';
 
-angular.module('portainer.agent').factory('Browse', [
-  '$resource',
-  'API_ENDPOINT_ENDPOINTS',
-  'EndpointProvider',
-  'StateManager',
-  function BrowseFactory($resource, API_ENDPOINT_ENDPOINTS, EndpointProvider, StateManager) {
-    'use strict';
-    return $resource(
-      API_ENDPOINT_ENDPOINTS + '/:endpointId/docker/v:version/browse/:action',
-      {
-        endpointId: EndpointProvider.endpointID,
-        version: StateManager.getAgentApiVersion,
+angular.module('portainer.agent').factory('Browse', BrowseFactory);
+
+function BrowseFactory($resource, API_ENDPOINT_ENDPOINTS, EndpointProvider, StateManager) {
+  return $resource(
+    `${API_ENDPOINT_ENDPOINTS}/:endpointId/docker/v:version/browse/:action`,
+    {
+      endpointId: EndpointProvider.endpointID,
+      version: StateManager.getAgentApiVersion,
+    },
+    {
+      ls: {
+        method: 'GET',
+        isArray: true,
+        params: { action: 'ls' },
       },
-      {
-        ls: {
-          method: 'GET',
-          isArray: true,
-          params: { action: 'ls' },
-        },
-        get: {
-          method: 'GET',
-          params: { action: 'get' },
-          transformResponse: browseGetResponse,
-          responseType: 'arraybuffer',
-        },
-        delete: {
-          method: 'DELETE',
-          params: { action: 'delete' },
-        },
-        rename: {
-          method: 'PUT',
-          params: { action: 'rename' },
-        },
-      }
-    );
-  },
-]);
+      get: {
+        method: 'GET',
+        params: { action: 'get' },
+        transformResponse: browseGetResponse,
+        responseType: 'arraybuffer',
+      },
+      delete: {
+        method: 'DELETE',
+        params: { action: 'delete' },
+      },
+      rename: {
+        method: 'PUT',
+        params: { action: 'rename' },
+      },
+    }
+  );
+}
