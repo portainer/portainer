7eb8d5449a68e2564aa9d6178edd5e4b42ca4a57
Chaim Lev-Ari <chiptus@users.noreply.github.com>
2020-07-23 10:45:01 +0300

    refactor(agent): refactor rest factories to es6 (#4090)
    * refactor(agent): replace v1 browse with es6 module
    
    * refactor(agent): refactor agentv1 to es6
    
    * refactor(agent): replace agent factory with es6
    
    * refactor(agent): refactor browse response to es6
    
    * refactor(agent): refactor browse to es6
    
    * refactor(agent): import angular
    
    * refactor(agent): refactor host to es6
    
    * refactor(agent): refactor ping to es6

--- app/agent/rest/agent.js
+++ app/agent/rest/agent.js
@@ -1,19 +1,16 @@
-angular.module('portainer.agent').factory('Agent', [
-  '$resource',
-  'API_ENDPOINT_ENDPOINTS',
-  'EndpointProvider',
-  'StateManager',
-  function AgentFactory($resource, API_ENDPOINT_ENDPOINTS, EndpointProvider, StateManager) {
-    'use strict';
-    return $resource(
-      API_ENDPOINT_ENDPOINTS + '/:endpointId/docker/v:version/agents',
-      {
-        endpointId: EndpointProvider.endpointID,
-        version: StateManager.getAgentApiVersion,
-      },
-      {
-        query: { method: 'GET', isArray: true },
-      }
-    );
-  },
-]);
+import angular from 'angular';
+
+angular.module('portainer.agent').factory('Agent', AgentFactory);
+
+function AgentFactory($resource, API_ENDPOINT_ENDPOINTS, EndpointProvider, StateManager) {
+  return $resource(
+    `${API_ENDPOINT_ENDPOINTS}/:endpointId/docker/v:version/agents`,
+    {
+      endpointId: EndpointProvider.endpointID,
+      version: StateManager.getAgentApiVersion,
+    },
+    {
+      query: { method: 'GET', isArray: true },
+    }
+  );
+}
--- app/agent/rest/host.js
+++ app/agent/rest/host.js
@@ -1,19 +1,16 @@
-angular.module('portainer.agent').factory('Host', [
-  '$resource',
-  'API_ENDPOINT_ENDPOINTS',
-  'EndpointProvider',
-  'StateManager',
-  function AgentFactory($resource, API_ENDPOINT_ENDPOINTS, EndpointProvider, StateManager) {
-    'use strict';
-    return $resource(
-      API_ENDPOINT_ENDPOINTS + '/:endpointId/docker/v:version/host/:action',
-      {
-        endpointId: EndpointProvider.endpointID,
-        version: StateManager.getAgentApiVersion,
-      },
-      {
-        info: { method: 'GET', params: { action: 'info' } },
-      }
-    );
-  },
-]);
+import angular from 'angular';
+
+angular.module('portainer.agent').factory('Host', HostFactory);
+
+function HostFactory($resource, API_ENDPOINT_ENDPOINTS, EndpointProvider, StateManager) {
+  return $resource(
+    `${API_ENDPOINT_ENDPOINTS}/:endpointId/docker/v:version/host/:action`,
+    {
+      endpointId: EndpointProvider.endpointID,
+      version: StateManager.getAgentApiVersion,
+    },
+    {
+      info: { method: 'GET', params: { action: 'info' } },
+    }
+  );
+}
