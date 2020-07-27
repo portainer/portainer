530f668425140782f3043f95d5e9f68754f22dee
Chaim Lev-Ari <chiptus@gmail.com>
2020-07-27 12:49:31 +0300

    refactor(stacks): revert file

--- api/http/handler/stacks/create_swarm_stack.go
+++ api/http/handler/stacks/create_swarm_stack.go
@@ -10,7 +10,7 @@ import (
 	"github.com/asaskevich/govalidator"
 	httperror "github.com/portainer/libhttp/error"
 	"github.com/portainer/libhttp/request"
-	portainer "github.com/portainer/portainer/api"
+	"github.com/portainer/portainer/api"
 	"github.com/portainer/portainer/api/filesystem"
 	"github.com/portainer/portainer/api/http/security"
 )
@@ -341,7 +341,7 @@ func (handler *Handler) deploySwarmStack(config *swarmStackDeploymentConfig) err
 		return err
 	}
 
-	if (!settings.AllowBindMountsForRegularUsers) && !isAdminOrEndpointAdmin {
+	if !settings.AllowBindMountsForRegularUsers && !isAdminOrEndpointAdmin {
 		composeFilePath := path.Join(config.stack.ProjectPath, config.stack.EntryPoint)
 
 		stackContent, err := handler.FileService.GetFileContent(composeFilePath)
