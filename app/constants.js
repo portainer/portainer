c3c6d66f58b44e5be027ba8c9b7f9628f66a38e6
Chaim Lev-Ari <chiptus@gmail.com>
2020-07-29 15:44:08 +0300

    refactor(app): duplicate constants as es6 exports

--- app/constants.js
+++ app/constants.js
@@ -1,3 +1,33 @@
+export const API_ENDPOINT_AUTH = 'api/auth';
+export const API_ENDPOINT_DOCKERHUB = 'api/dockerhub';
+export const API_ENDPOINT_CUSTOM_TEMPLATES = 'api/custom_templates';
+export const API_ENDPOINT_EDGE_GROUPS = 'api/edge_groups';
+export const API_ENDPOINT_EDGE_JOBS = 'api/edge_jobs';
+export const API_ENDPOINT_EDGE_STACKS = 'api/edge_stacks';
+export const API_ENDPOINT_EDGE_TEMPLATES = 'api/edge_templates';
+export const API_ENDPOINT_ENDPOINTS = 'api/endpoints';
+export const API_ENDPOINT_ENDPOINT_GROUPS = 'api/endpoint_groups';
+export const API_ENDPOINT_MOTD = 'api/motd';
+export const API_ENDPOINT_EXTENSIONS = 'api/extensions';
+export const API_ENDPOINT_REGISTRIES = 'api/registries';
+export const API_ENDPOINT_RESOURCE_CONTROLS = 'api/resource_controls';
+export const API_ENDPOINT_SETTINGS = 'api/settings';
+export const API_ENDPOINT_STACKS = 'api/stacks';
+export const API_ENDPOINT_STATUS = 'api/status';
+export const API_ENDPOINT_SUPPORT = 'api/support';
+export const API_ENDPOINT_USERS = 'api/users';
+export const API_ENDPOINT_TAGS = 'api/tags';
+export const API_ENDPOINT_TEAMS = 'api/teams';
+export const API_ENDPOINT_TEAM_MEMBERSHIPS = 'api/team_memberships';
+export const API_ENDPOINT_TEMPLATES = 'api/templates';
+export const API_ENDPOINT_WEBHOOKS = 'api/webhooks';
+export const DEFAULT_TEMPLATES_URL = 'https://raw.githubusercontent.com/portainer/templates/master/templates.json';
+export const PAGINATION_MAX_ITEMS = 10;
+export const APPLICATION_CACHE_VALIDITY = 3600;
+export const CONSOLE_COMMANDS_LABEL_PREFIX = 'io.portainer.commands.';
+export const PREDEFINED_NETWORKS = ['host', 'bridge', 'none'];
+export const KUBERNETES_SYSTEM_NAMESPACES = ['kube-system', 'kube-public', 'kube-node-lease', 'portainer'];
+
 angular
   .module('portainer')
   .constant('API_ENDPOINT_AUTH', 'api/auth')
