bcbcb3308a8b9824a5d11e2e429fd932bc27bf40
Chaim Lev-Ari <chiptus@gmail.com>
2020-08-01 13:20:58 +0300

    feat(telemetry): set matomo url

--- app/matomo-setup.js
+++ app/matomo-setup.js
@@ -2,13 +2,13 @@ const _paq = (window._paq = window._paq || []);
 /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
 _paq.push(['enableLinkTracking']);
 
-const u = '//188.166.250.54/';
+var u = 'https://portainer-ce.matomo.cloud/';
 _paq.push(['setTrackerUrl', u + 'matomo.php']);
 _paq.push(['setSiteId', '1']);
-const d = document,
+var d = document,
   g = d.createElement('script'),
   s = d.getElementsByTagName('script')[0];
 g.type = 'text/javascript';
 g.async = true;
-g.src = u + 'matomo.js';
+g.src = '//cdn.matomo.cloud/portainer-ce.matomo.cloud/matomo.js';
 s.parentNode.insertBefore(g, s);
