f4dd52dd3a79c949d4e02f778b22dcc0f855296e
DarkAEther <30438425+DarkAEther@users.noreply.github.com>
2020-08-06 00:33:25 +0530

    fix(registries) uniform usage of string replace function for registry update

--- app/portainer/services/api/registryService.js
+++ app/portainer/services/api/registryService.js
@@ -63,7 +63,7 @@ angular.module('portainer.app').factory('RegistryService', [
 
     service.updateRegistry = function (registry) {
       registry.URL = _.replace(registry.URL, /^https?\:\/\//i, '');
-      registry.URL = registry.URL.replace(/\/$/, '');
+      registry.URL = _.replace(registry.URL, /\/$/, '');
       return Registries.update({ id: registry.Id }, registry).$promise;
     };
 
