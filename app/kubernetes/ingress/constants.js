5ecbc5b2f0de67dcac8b1dd53fb31e2e03d06868
Anthony Lapenna <lapenna.anthony@gmail.com>
2020-08-19 20:36:18 +1200

    fix(k8s/resource-pool): update ingress rewrite target annotation value

--- app/kubernetes/ingress/constants.js
+++ app/kubernetes/ingress/constants.js
@@ -3,8 +3,8 @@ export const KubernetesIngressClassAnnotation = 'kubernetes.io/ingress.class';
 // keys must match KubernetesIngressClassTypes values to map them quickly using the ingress type
 // KubernetesIngressClassRewriteTargetAnnotations[KubernetesIngressClassTypes.NGINX] for example
 export const KubernetesIngressClassRewriteTargetAnnotations = Object.freeze({
-  nginx: { 'nginx.ingress.kubernetes.io/rewrite-target': '/$1' },
-  traefik: { 'traefik.ingress.kubernetes.io/rewrite-target': '/$1' },
+  nginx: { 'nginx.ingress.kubernetes.io/rewrite-target': '/' },
+  traefik: { 'traefik.ingress.kubernetes.io/rewrite-target': '/' },
 });
 
 export const KubernetesIngressClassTypes = Object.freeze({
