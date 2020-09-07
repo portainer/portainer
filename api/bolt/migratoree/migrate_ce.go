5be246f7da46651de25e660bce486125cbb2b795
Chaim Lev-Ari <chiptus@users.noreply.github.com>
2020-09-07 13:15:38 +0300

    feat(bolt): handle migrations from ce to ee (#22)
    * feat(db): add edition value to db
    
    * feat(bolt): handle migrations from ce to ee
    
    * refactor(bolt): merge if branches
    
    * refactor(bolt): rename migration function
    
    * feat(bolt): change migration message
    
    * feat(bolt): add edition to migration messages
    
    * feat(bolt): add log tags
    
    * feat(portainer): add edition
    
    * feat(db): set initial db version
    
    * feat(bolt): cache current version
    
    * refactor(portainer): remove current edition const

---
+++ api/bolt/migratoree/migrate_ce.go
@@ -0,0 +1,20 @@
+package migratoree
+
+import (
+	"github.com/portainer/portainer/api"
+)
+
+// MigrateFromCEdbv25 will migrate the db from latest ce version to latest ee version
+func (m *Migrator) MigrateFromCEdbv25() error {
+	err := m.versionService.StoreDBVersion(portainer.DBVersionBE)
+	if err != nil {
+		return err
+	}
+
+	err = m.versionService.StoreEdition(portainer.PortainerBE)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
--- api/bolt/version/version.go
+++ api/bolt/version/version.go
@@ -4,6 +4,7 @@ import (
 	"strconv"
 
 	"github.com/boltdb/bolt"
+	"github.com/portainer/portainer/api"
 	"github.com/portainer/portainer/api/bolt/errors"
 	"github.com/portainer/portainer/api/bolt/internal"
 )
@@ -13,6 +14,7 @@ const (
 	BucketName  = "version"
 	versionKey  = "DB_VERSION"
 	instanceKey = "INSTANCE_ID"
+	editionKey  = "EDITION"
 )
 
 // Service represents a service to manage stored versions.
@@ -32,48 +34,63 @@ func NewService(db *bolt.DB) (*Service, error) {
 	}, nil
 }
 
-// DBVersion retrieves the stored database version.
-func (service *Service) DBVersion() (int, error) {
-	var data []byte
-
-	err := service.db.View(func(tx *bolt.Tx) error {
-		bucket := tx.Bucket([]byte(BucketName))
-
-		value := bucket.Get([]byte(versionKey))
-		if value == nil {
-			return errors.ErrObjectNotFound
-		}
-
-		data = make([]byte, len(value))
-		copy(data, value)
-
-		return nil
-	})
+// Edition retrieves the stored portainer edition.
+func (service *Service) Edition() (portainer.SoftwareEdition, error) {
+	editionData, err := service.getKey(editionKey)
 	if err != nil {
 		return 0, err
 	}
 
-	return strconv.Atoi(string(data))
+	edition, err := strconv.Atoi(string(editionData))
+	if err != nil {
+		return 0, err
+	}
+
+	return portainer.SoftwareEdition(edition), nil
+}
+
+// StoreEdition store the portainer edition.
+func (service *Service) StoreEdition(edition portainer.SoftwareEdition) error {
+	return service.setKey(editionKey, strconv.Itoa(int(edition)))
+}
+
+// DBVersion retrieves the stored database version.
+func (service *Service) DBVersion() (int, error) {
+	version, err := service.getKey(versionKey)
+	if err != nil {
+		return 0, err
+	}
+
+	return strconv.Atoi(string(version))
 }
 
 // StoreDBVersion store the database version.
 func (service *Service) StoreDBVersion(version int) error {
-	return service.db.Update(func(tx *bolt.Tx) error {
-		bucket := tx.Bucket([]byte(BucketName))
-
-		data := []byte(strconv.Itoa(version))
-		return bucket.Put([]byte(versionKey), data)
-	})
+	return service.setKey(versionKey, strconv.Itoa(version))
 }
 
 // InstanceID retrieves the stored instance ID.
 func (service *Service) InstanceID() (string, error) {
+	instanceID, err := service.getKey(instanceKey)
+	if err != nil {
+		return "", err
+	}
+
+	return string(instanceID), nil
+}
+
+// StoreInstanceID store the instance ID.
+func (service *Service) StoreInstanceID(ID string) error {
+	return service.setKey(instanceKey, ID)
+}
+
+func (service *Service) getKey(key string) ([]byte, error) {
 	var data []byte
 
 	err := service.db.View(func(tx *bolt.Tx) error {
 		bucket := tx.Bucket([]byte(BucketName))
 
-		value := bucket.Get([]byte(instanceKey))
+		value := bucket.Get([]byte(key))
 		if value == nil {
 			return errors.ErrObjectNotFound
 		}
@@ -83,19 +100,19 @@ func (service *Service) InstanceID() (string, error) {
 
 		return nil
 	})
+
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
-	return string(data), nil
+	return data, nil
 }
 
-// StoreInstanceID store the instance ID.
-func (service *Service) StoreInstanceID(ID string) error {
+func (service *Service) setKey(key string, value string) error {
 	return service.db.Update(func(tx *bolt.Tx) error {
 		bucket := tx.Bucket([]byte(BucketName))
 
-		data := []byte(ID)
-		return bucket.Put([]byte(instanceKey), data)
+		data := []byte(value)
+		return bucket.Put([]byte(key), data)
 	})
 }
