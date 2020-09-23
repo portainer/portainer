80317e647ee01ec9408f3c45ca7dc0606f9cde8b
itsconquest <william.conquest@portainer.io>
2020-09-23 21:06:10 +1200

    feat(ci): download binaries direct to dist directory

--- build/download_kompose_binary.ps1
+++ build/download_kompose_binary.ps1
@@ -4,9 +4,4 @@ param (
 
 $ErrorActionPreference = "Stop";
 
-New-Item -Path "kompose-binary" -ItemType Directory | Out-Null
-
-$download_folder = "kompose-binary"
-
-Invoke-WebRequest -O "$($download_folder)/kompose.exe" "https://github.com/kubernetes/kompose/releases/download/$($kompose_version)/kompose-windows-amd64.exe"
-Move-Item -Path "$($download_folder)/kubectl.exe" -Destination "dist"
+Invoke-WebRequest -O "dist/kompose.exe" "https://github.com/kubernetes/kompose/releases/download/$($kompose_version)/kompose-windows-amd64.exe"
--- build/download_kubectl_binary.ps1
+++ build/download_kubectl_binary.ps1
@@ -4,9 +4,4 @@ param (
 
 $ErrorActionPreference = "Stop";
 
-New-Item -Path "kubectl-binary" -ItemType Directory | Out-Null
-
-$download_folder = "kubectl-binary"
-
-Invoke-WebRequest -O "$($download_folder)/kubectl.exe" "https://storage.googleapis.com/kubernetes-release/release/$($kubectl_version)/bin/windows/amd64/kubectl.exe"
-Move-Item -Path "$($download_folder)/kubectl.exe" -Destination "dist"
+Invoke-WebRequest -O "dist/kubectl.exe" "https://storage.googleapis.com/kubernetes-release/release/$($kubectl_version)/bin/windows/amd64/kubectl.exe"
