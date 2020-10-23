a0424506eeb3a6abbdaa4f24df93fec75c321dcb
Olli Janatuinen <olli.janatuinen@gmail.com>
2020-10-23 23:09:15 +0300

    dep(api): Support Docker Stack 3.8
    - Linux: Update Docker binary to version 19.03.13
    - Windows: Update Docker binary to version 19.03.12

--- build/download_docker_binary.ps1
+++ build/download_docker_binary.ps1
@@ -9,6 +9,7 @@ New-Item -Path "docker-binary" -ItemType Directory | Out-Null
 
 $download_folder = "docker-binary"
 
-Invoke-WebRequest -O "$($download_folder)/docker-binaries.zip" "https://download.docker.com/win/static/stable/x86_64/docker-$($docker_version).zip"
+Invoke-WebRequest -O "$($download_folder)/docker-binaries.zip" "https://dockermsft.azureedge.net/dockercontainer/docker-$($docker_version).zip"
 Expand-Archive -Path "$($download_folder)/docker-binaries.zip" -DestinationPath "$($download_folder)"
 Move-Item -Path "$($download_folder)/docker/docker.exe" -Destination "dist"
+Move-Item -Path "$($download_folder)/docker/*.dll" -Destination "dist"
