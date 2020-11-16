a6954d1da2e23b5e7c8a554d067ffacb7301d761
Chaim Lev-Ari <chiptus@gmail.com>
2020-11-16 11:20:31 +0200

    chore(webpack): add source maps

--- webpack/webpack.develop.js
+++ webpack/webpack.develop.js
@@ -4,6 +4,7 @@ const commonConfig = require('./webpack.common.js');
 
 module.exports = webpackMerge(commonConfig, {
   mode: 'development',
+  devtool: 'eval-source-map',
   module: {
     rules: [
       {
