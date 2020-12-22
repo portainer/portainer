58c04bdbe362f41bc78212e366200cc6f2661e29
Yi Chen <69284638+yi-portainer@users.noreply.github.com>
2020-12-22 13:47:11 +1300

    + silently continue when downloading artifacts in windows (#4637)

--- build/download_kompose_binary.ps1
+++ build/download_kompose_binary.ps1
@@ -3,5 +3,6 @@ param (
 )
 
 $ErrorActionPreference = "Stop";
+$ProgressPreference = "SilentlyContinue";
 
 Invoke-WebRequest -O "dist/kompose.exe" "https://github.com/kubernetes/kompose/releases/download/$($kompose_version)/kompose-windows-amd64.exe"
