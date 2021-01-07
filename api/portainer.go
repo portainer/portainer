1b82b450d76cc68d635c7dba47ae303583df0919
Yi Chen <69284638+yi-portainer@users.noreply.github.com>
2021-01-07 14:47:32 +1300

    * bump the APIVersion to 2.0.1 (#4688)

--- api/portainer.go
+++ api/portainer.go
@@ -1119,7 +1119,7 @@ type (
 
 const (
 	// APIVersion is the version number of the Portainer API
-	APIVersion = "2.0.0"
+	APIVersion = "2.0.1"
 	// DBVersion is the version number of the Portainer database
 	DBVersion = 25
 	// AssetsServerURL represents the URL of the Portainer asset server
