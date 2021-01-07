9fbda9fb99a26daa8a6f79a251650992f30c9cb7
Yi Chen <69284638+yi-portainer@users.noreply.github.com>
2021-01-07 13:38:01 +1300

    Merge in release fixes to develop (#4687)
    * fix(frontend) unable to retrieve config map error when trying to manage newly created resource pool (ce#180) (#4618)
    
    * fix(frontend) unable to retrieve config map error when trying to manage newly created resource pool (ce#180)
    
    * fix(frontend) rephrase comments (#4629)
    
    Co-authored-by: Stéphane Busso <sbusso@users.noreply.github.com>
    
    Co-authored-by: Simon Meng <simon.meng@portainer.io>
    Co-authored-by: Stéphane Busso <sbusso@users.noreply.github.com>
    
    * + silently continue when downloading artifacts in windows (#4637)
    
    * fix(docker/stack-details): do not display editor tab for external stack (#4650)
    
    * Revert "chore(build): bump Kompose version (#4475)" (#4676)
    
    This reverts commit 380f106571330e778e2ef8790cc6f2a617643e97.
    
    Co-authored-by: Stéphane Busso <sbusso@users.noreply.github.com>
    
    Co-authored-by: cong meng <mcpacino@gmail.com>
    Co-authored-by: Simon Meng <simon.meng@portainer.io>
    Co-authored-by: Stéphane Busso <sbusso@users.noreply.github.com>
    Co-authored-by: Anthony Lapenna <anthony.lapenna@portainer.io>

--- build/download_kubectl_binary.ps1
+++ build/download_kubectl_binary.ps1
@@ -3,5 +3,6 @@ param (
 )
 
 $ErrorActionPreference = "Stop";
+$ProgressPreference = "SilentlyContinue";
 
 Invoke-WebRequest -O "dist/kubectl.exe" "https://storage.googleapis.com/kubernetes-release/release/$($kubectl_version)/bin/windows/amd64/kubectl.exe"
