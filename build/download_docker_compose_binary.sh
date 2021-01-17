0c0bda1361fd00a3ea1f5d34cd62270eb470a92b
Stéphane Busso <stephane.busso@gmail.com>
2021-01-18 10:00:37 +1300

    Fix platofrm specific docker-compose download

--- build/download_docker_compose_binary.sh
+++ build/download_docker_compose_binary.sh
@@ -4,10 +4,11 @@ PLATFORM=$1
 ARCH=$2
 DOCKER_COMPOSE_VERSION=$3
 
-if [ "${PLATFORM}" == 'win' ]; then
-  wget -O "dist/docker-compose.exe" "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-Windows-x86_64.exe"
-elif [ "${PLATFORM}" == 'linux' ] && [ "${ARCH}" == 'amd64' ]; then
+if [ "${PLATFORM}" == 'linux' ] && [ "${ARCH}" == 'amd64' ]; then
   wget -O "dist/docker-compose" "https://github.com/portainer/docker-compose-linux-amd64-static-binary/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose"
+  chmod +x "dist/docker-compose"
+elif [ "${PLATFORM}" == 'mac' ]; then
+  wget -O "dist/docker-compose" "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-Darwin-x86_64"
   chmod +x "dist/docker-compose"
 fi
 
