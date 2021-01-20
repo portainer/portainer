2c7ba3d79ba3a8f21df71cb47ccfba0df7e4cde9
Dmitry Salakhov <to@dimasalakhov.com>
2021-01-20 13:01:50 +1300

    refactor: remove unused code

--- api/http/proxy/factory/dockercompose/transport.go
+++ api/http/proxy/factory/dockercompose/transport.go
@@ -4,49 +4,16 @@ import (
 	"net/http"
 
 	portainer "github.com/portainer/portainer/api"
-	"github.com/portainer/portainer/api/crypto"
 )
 
 type (
-	LocalTransport struct {
-		httpTransport      *http.Transport
-		endpointIdentifier portainer.EndpointID
-	}
-
+	// AgentTransport is an http.Transport wrapper that adds custom http headers to communicate to an Agent
 	AgentTransport struct {
 		httpTransport      *http.Transport
 		signatureService   portainer.DigitalSignatureService
 		endpointIdentifier portainer.EndpointID
 	}
-
-	EdgeTransport struct {
-		httpTransport        *http.Transport
-		reverseTunnelService portainer.ReverseTunnelService
-		endpointIdentifier   portainer.EndpointID
-	}
 )
-
-// NewLocalTransport returns a new transport that can be used to send requests to the local Kubernetes API
-func NewLocalTransport() (*LocalTransport, error) {
-	config, err := crypto.CreateTLSConfigurationFromBytes(nil, nil, nil, true, true)
-	if err != nil {
-		return nil, err
-	}
-
-	transport := &LocalTransport{
-		httpTransport: &http.Transport{
-			TLSClientConfig: config,
-		},
-	}
-
-	return transport, nil
-}
-
-// RoundTrip is the implementation of the the http.RoundTripper interface
-func (transport *LocalTransport) RoundTrip(request *http.Request) (*http.Response, error) {
-
-	return transport.httpTransport.RoundTrip(request)
-}
 
 // NewAgentTransport returns a new transport that can be used to send signed requests to a Portainer agent
 func NewAgentTransport(signatureService portainer.DigitalSignatureService, httpTransport *http.Transport) *AgentTransport {
@@ -70,29 +37,4 @@ func (transport *AgentTransport) RoundTrip(request *http.Request) (*http.Respons
 	request.Header.Set(portainer.PortainerAgentSignatureHeader, signature)
 
 	return transport.httpTransport.RoundTrip(request)
-}
-
-// NewAgentTransport returns a new transport that can be used to send signed requests to a Portainer Edge agent
-func NewEdgeTransport(reverseTunnelService portainer.ReverseTunnelService, endpointIdentifier portainer.EndpointID, httpTransport *http.Transport) *EdgeTransport {
-	transport := &EdgeTransport{
-		httpTransport:        httpTransport,
-		reverseTunnelService: reverseTunnelService,
-		endpointIdentifier:   endpointIdentifier,
-	}
-
-	return transport
-}
-
-// RoundTrip is the implementation of the the http.RoundTripper interface
-func (transport *EdgeTransport) RoundTrip(request *http.Request) (*http.Response, error) {
-
-	response, err := transport.httpTransport.RoundTrip(request)
-
-	if err == nil {
-		transport.reverseTunnelService.SetTunnelStatusToActive(transport.endpointIdentifier)
-	} else {
-		transport.reverseTunnelService.SetTunnelStatusToIdle(transport.endpointIdentifier)
-	}
-
-	return response, err
 }
