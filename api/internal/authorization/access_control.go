00a9f29df1884a0222b5cdc3f627cbfba7cb26bd
Chaim Lev-Ari <chiptus@gmail.com>
2021-02-08 13:49:50 +0200

    feat(authorization): use stackutils

--- api/internal/authorization/access_control.go
+++ api/internal/authorization/access_control.go
@@ -1,10 +1,10 @@
 package authorization
 
 import (
-	"fmt"
 	"strconv"
 
 	portainer "github.com/portainer/portainer/api"
+	"github.com/portainer/portainer/api/internal/stackutils"
 )
 
 // NewAdministratorsOnlyResourceControl will create a new administrators only resource control associated to the resource specified by the
@@ -111,7 +111,7 @@ func NewRestrictedResourceControl(resourceIdentifier string, resourceType portai
 func DecorateStacks(stacks []portainer.Stack, resourceControls []portainer.ResourceControl) []portainer.Stack {
 	for idx, stack := range stacks {
 
-		resourceControl := GetResourceControlByResourceIDAndType(fmt.Sprintf("%d_%s", stack.EndpointID, stack.Name), portainer.StackResourceControl, resourceControls)
+		resourceControl := GetResourceControlByResourceIDAndType(stackutils.ResourceControlID(stack.EndpointID, stack.Name), portainer.StackResourceControl, resourceControls)
 		if resourceControl != nil {
 			stacks[idx].ResourceControl = resourceControl
 		}
