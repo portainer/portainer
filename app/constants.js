951abb98ecc3fe047df655126c739c14044ea9de
Chaim Lev-Ari <chiptus@gmail.com>
2021-02-18 13:03:09 +0200

    refactor(app/constants): use es6 constants

--- app/constants.js
+++ app/constants.js
@@ -23,33 +23,31 @@ export const API_ENDPOINT_TEAM_MEMBERSHIPS = 'api/team_memberships';
 export const API_ENDPOINT_TEMPLATES = 'api/templates';
 export const API_ENDPOINT_WEBHOOKS = 'api/webhooks';
 export const PAGINATION_MAX_ITEMS = 10;
-export const APPLICATION_CACHE_VALIDITY = 3600;
 export const CONSOLE_COMMANDS_LABEL_PREFIX = 'io.portainer.commands.';
 export const PREDEFINED_NETWORKS = ['host', 'bridge', 'none'];
 export const KUBERNETES_SYSTEM_NAMESPACES = ['kube-system', 'kube-public', 'kube-node-lease', 'portainer'];
 
 angular
   .module('portainer')
-  .constant('API_ENDPOINT_AUTH', 'api/auth')
-  .constant('API_ENDPOINT_DOCKERHUB', 'api/dockerhub')
-  .constant('API_ENDPOINT_CUSTOM_TEMPLATES', 'api/custom_templates')
-  .constant('API_ENDPOINT_ENDPOINTS', 'api/endpoints')
-  .constant('API_ENDPOINT_ENDPOINT_GROUPS', 'api/endpoint_groups')
-  .constant('API_ENDPOINT_MOTD', 'api/motd')
-  .constant('API_ENDPOINT_REGISTRIES', 'api/registries')
-  .constant('API_ENDPOINT_RESOURCE_CONTROLS', 'api/resource_controls')
-  .constant('API_ENDPOINT_SETTINGS', 'api/settings')
-  .constant('API_ENDPOINT_STACKS', 'api/stacks')
-  .constant('API_ENDPOINT_STATUS', 'api/status')
-  .constant('API_ENDPOINT_SUPPORT', 'api/support')
-  .constant('API_ENDPOINT_USERS', 'api/users')
-  .constant('API_ENDPOINT_TAGS', 'api/tags')
-  .constant('API_ENDPOINT_TEAMS', 'api/teams')
-  .constant('API_ENDPOINT_TEAM_MEMBERSHIPS', 'api/team_memberships')
-  .constant('API_ENDPOINT_TEMPLATES', 'api/templates')
-  .constant('API_ENDPOINT_WEBHOOKS', 'api/webhooks')
-  .constant('PAGINATION_MAX_ITEMS', 10)
-  .constant('APPLICATION_CACHE_VALIDITY', 3600)
+  .constant('API_ENDPOINT_AUTH', API_ENDPOINT_AUTH)
+  .constant('API_ENDPOINT_DOCKERHUB', API_ENDPOINT_DOCKERHUB)
+  .constant('API_ENDPOINT_CUSTOM_TEMPLATES', API_ENDPOINT_CUSTOM_TEMPLATES)
+  .constant('API_ENDPOINT_ENDPOINTS', API_ENDPOINT_ENDPOINTS)
+  .constant('API_ENDPOINT_ENDPOINT_GROUPS', API_ENDPOINT_ENDPOINT_GROUPS)
+  .constant('API_ENDPOINT_MOTD', API_ENDPOINT_MOTD)
+  .constant('API_ENDPOINT_REGISTRIES', API_ENDPOINT_REGISTRIES)
+  .constant('API_ENDPOINT_RESOURCE_CONTROLS', API_ENDPOINT_RESOURCE_CONTROLS)
+  .constant('API_ENDPOINT_SETTINGS', API_ENDPOINT_SETTINGS)
+  .constant('API_ENDPOINT_STACKS', API_ENDPOINT_STACKS)
+  .constant('API_ENDPOINT_STATUS', API_ENDPOINT_STATUS)
+  .constant('API_ENDPOINT_SUPPORT', API_ENDPOINT_SUPPORT)
+  .constant('API_ENDPOINT_USERS', API_ENDPOINT_USERS)
+  .constant('API_ENDPOINT_TAGS', API_ENDPOINT_TAGS)
+  .constant('API_ENDPOINT_TEAMS', API_ENDPOINT_TEAMS)
+  .constant('API_ENDPOINT_TEAM_MEMBERSHIPS', API_ENDPOINT_TEAM_MEMBERSHIPS)
+  .constant('API_ENDPOINT_TEMPLATES', API_ENDPOINT_TEMPLATES)
+  .constant('API_ENDPOINT_WEBHOOKS', API_ENDPOINT_WEBHOOKS)
+  .constant('PAGINATION_MAX_ITEMS', PAGINATION_MAX_ITEMS)
   .constant('CONSOLE_COMMANDS_LABEL_PREFIX', 'io.portainer.commands.')
   .constant('PREDEFINED_NETWORKS', ['host', 'bridge', 'none'])
   .constant('KUBERNETES_DEFAULT_NAMESPACE', 'default')
