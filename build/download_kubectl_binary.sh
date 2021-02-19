86c378b561e428467cbbfb3f93574606fbbef8ec
itsconquest <william.conquest@portainer.io>
2021-02-19 14:31:43 +1300

    fix(build): fix kubectl download script (#187)

--- build/download_kubectl_binary.sh
+++ build/download_kubectl_binary.sh
@@ -4,7 +4,6 @@ PLATFORM=$1
 ARCH=$2
 KUBECTL_VERSION=$3
 
-exit 0
 
 if [ "${PLATFORM}" == 'linux' ]; then
   wget -O "dist/kubectl" "https://storage.googleapis.com/kubernetes-release/release/${KUBECTL_VERSION}/bin/${PLATFORM}/${ARCH}/kubectl"
@@ -13,3 +12,5 @@ elif [ "${PLATFORM}" == 'windows' ]; then
   wget -O "dist/kubectl.exe" "https://storage.googleapis.com/kubernetes-release/release/${KUBECTL_VERSION}/bin/windows/amd64/kubectl.exe"
   chmod +x "dist/kubectl.exe"
 fi
+
+exit 0
