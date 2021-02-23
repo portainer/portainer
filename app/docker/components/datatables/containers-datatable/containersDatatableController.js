18842045fcbf5a73ae2333ec73480a65df57c1da
Alice Groux <alice.grx@gmail.com>
2021-02-23 20:45:37 +0100

    feat(app/containers): display IP (#4435)

--- app/docker/components/datatables/containers-datatable/containersDatatableController.js
+++ app/docker/components/datatables/containers-datatable/containersDatatableController.js
@@ -57,6 +57,10 @@ angular.module('portainer.docker').controller('ContainersDatatableController', [
           label: 'Created',
           display: true,
         },
+        ip: {
+          label: 'IP Address',
+          display: true,
+        },
         host: {
           label: 'Host',
           display: true,
