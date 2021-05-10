67d1d7f12bbc4f10a742d9d3c5122f6ea118457d
Chaim Lev-Ari <chiptus@gmail.com>
2021-05-10 17:11:05 +0300

    feat(stacks): redeploy git stack api
    feat(stacks): update git stack
    
    feat(stacks): pull repository
    
    feat(stacks): replace pull with clear and clone

--- api/internal/testhelpers/git_service.go
+++ api/internal/testhelpers/git_service.go
@@ -10,3 +10,7 @@ func NewGitService() *gitService {
 func (service *gitService) CloneRepository(destination string, repositoryURL, referenceName string, auth bool, username, password string) error {
 	return nil
 }
+
+func (service *gitService) PullRepository(path string, referenceName string, auth bool, username, password string) error {
+	return nil
+}
