c7c164c3d24a4e25be29808b674f285084254819
Chaim Lev-Ari <chiptus@gmail.com>
2021-05-10 07:53:49 +0300

    refactor(backup): move demo flag

--- api/http/handler/backup/handler.go
+++ api/http/handler/backup/handler.go
@@ -16,7 +16,6 @@ import (
 // Handler is an http handler responsible for backup and restore portainer state
 type Handler struct {
 	*mux.Router
-	demoEnvironment bool
 	bouncer         *security.RequestBouncer
 	dataStore       portainer.DataStore
 	gate            *offlinegate.OfflineGate
@@ -26,10 +25,9 @@ type Handler struct {
 }
 
 // NewHandler creates an new instance of backup handler
-func NewHandler(bouncer *security.RequestBouncer, dataStore portainer.DataStore, gate *offlinegate.OfflineGate, filestorePath string, shutdownTrigger context.CancelFunc, adminMonitor *adminmonitor.Monitor, demo bool) *Handler {
+func NewHandler(bouncer *security.RequestBouncer, dataStore portainer.DataStore, gate *offlinegate.OfflineGate, filestorePath string, shutdownTrigger context.CancelFunc, adminMonitor *adminmonitor.Monitor, isDemo bool) *Handler {
 	h := &Handler{
 		Router:          mux.NewRouter(),
-		demoEnvironment: demo,
 		bouncer:         bouncer,
 		dataStore:       dataStore,
 		gate:            gate,
@@ -38,8 +36,8 @@ func NewHandler(bouncer *security.RequestBouncer, dataStore portainer.DataStore,
 		adminMonitor:    adminMonitor,
 	}
 
-	h.Handle("/backup", h.restrictDemoEnv(bouncer.RestrictedAccess(adminAccess(httperror.LoggerHandler(h.backup))))).Methods(http.MethodPost)
-	h.Handle("/restore", h.restrictDemoEnv(bouncer.PublicAccess(httperror.LoggerHandler(h.restore)))).Methods(http.MethodPost)
+	h.Handle("/backup", restrictDemoEnv(isDemo, bouncer.RestrictedAccess(adminAccess(httperror.LoggerHandler(h.backup))))).Methods(http.MethodPost)
+	h.Handle("/restore", restrictDemoEnv(isDemo, bouncer.PublicAccess(httperror.LoggerHandler(h.restore)))).Methods(http.MethodPost)
 
 	return h
 }
@@ -49,21 +47,11 @@ func adminAccess(next http.Handler) http.Handler {
 		securityContext, err := security.RetrieveRestrictedRequestContext(r)
 		if err != nil {
 			httperror.WriteError(w, http.StatusInternalServerError, "Unable to retrieve user info from request context", err)
+			return
 		}
 
 		if !securityContext.IsAdmin {
 			httperror.WriteError(w, http.StatusUnauthorized, "User is not authorized to perform the action", nil)
-		}
-
-		next.ServeHTTP(w, r)
-	})
-}
-
-// restrict backup functionality on demo environments
-func (handler *Handler) restrictDemoEnv(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if handler.demoEnvironment {
-			httperror.WriteError(w, http.StatusBadRequest, "This feature is not available in the demo version of Portainer", errors.New("this feature is not available in the demo version of Portainer"))
 			return
 		}
 
@@ -71,10 +59,14 @@ func (handler *Handler) restrictDemoEnv(next http.Handler) http.Handler {
 	})
 }
 
-func systemWasInitialized(dataStore portainer.DataStore) (bool, error) {
-	users, err := dataStore.User().UsersByRole(portainer.AdministratorRole)
-	if err != nil {
-		return false, err
-	}
-	return len(users) > 0, nil
+// restrict backup functionality on demo environments
+func restrictDemoEnv(isDemo bool, next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if isDemo {
+			httperror.WriteError(w, http.StatusBadRequest, "This feature is not available in the demo version of Portainer", errors.New("this feature is not available in the demo version of Portainer"))
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
--- api/http/handler/backup/handler_test.go
+++ api/http/handler/backup/handler_test.go
@@ -1,17 +1,12 @@
 package backup
 
 import (
-	"context"
 	"io"
 	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
-	"time"
 
-	"github.com/portainer/portainer/api/adminmonitor"
-	"github.com/portainer/portainer/api/http/offlinegate"
-	i "github.com/portainer/portainer/api/internal/testhelpers"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -19,16 +14,22 @@ func Test_demoEnvironment_shouldFail(t *testing.T) {
 	r := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(`{}`))
 	w := httptest.NewRecorder()
 
-	gate := offlinegate.NewOfflineGate()
-	adminMonitor := adminmonitor.New(time.Hour, nil, context.Background())
-
-	handler := NewHandler(nil, i.NewDatastore(), gate, "./test_assets/handler_test", nil, func() {}, adminMonitor, true)
-
-	handler.restrictDemoEnv(http.DefaultServeMux).ServeHTTP(w, r)
+	restrictDemoEnv(true, http.DefaultServeMux).ServeHTTP(w, r)
 
 	response := w.Result()
-	assert.Equal(t, response.StatusCode, http.StatusBadRequest)
+	assert.Equal(t, http.StatusBadRequest, response.StatusCode)
 
 	body, _ := io.ReadAll(response.Body)
 	assert.Contains(t, string(body), "This feature is not available in the demo version of Portainer")
+}
+
+func Test_notDemoEnvironment_shouldSucceed(t *testing.T) {
+	r := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(`{}`))
+	w := httptest.NewRecorder()
+
+	restrictDemoEnv(false, http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {})).ServeHTTP(w, r)
+
+	response := w.Result()
+	assert.Equal(t, http.StatusOK, response.StatusCode)
+
 }
