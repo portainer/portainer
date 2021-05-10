c76463c54a57af98b666c4cfae6e240c96020cf4
LP B <xAt0mZ@users.noreply.github.com>
2021-05-10 17:36:17 +0200

    feat(app/docker): backport docker/views/container changes

--- app/docker/views/containers/edit/containerController.js
+++ app/docker/views/containers/edit/containerController.js
@@ -21,7 +21,6 @@ angular.module('portainer.docker').controller('ContainerController', [
   'ImageService',
   'HttpRequestHelper',
   'Authentication',
-  'StateManager',
   'endpoint',
   function (
     $q,
@@ -42,9 +41,9 @@ angular.module('portainer.docker').controller('ContainerController', [
     ImageService,
     HttpRequestHelper,
     Authentication,
-    StateManager,
     endpoint
   ) {
+    $scope.endpoint = endpoint;
     $scope.activityTime = 0;
     $scope.portBindings = [];
     $scope.displayRecreateButton = false;
