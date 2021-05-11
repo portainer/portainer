75da9ac2aa91756350c67fea8d0d9b3a18d09d5b
Chaim Lev-Ari <chiptus@gmail.com>
2021-05-11 16:38:10 +0300

    feat(stacks): redeploy git stacks form
    [EE-666]

---
+++ app/portainer/components/forms/git-form/git-form.controller.js
@@ -0,0 +1,18 @@
+export default class GitFormController {
+  /* @ngInject */
+  constructor() {
+    this.onChangeField = this.onChangeField.bind(this);
+    this.onChangeURL = this.onChangeField('RepositoryURL');
+    this.onChangeRefName = this.onChangeField('RepositoryReferenceName');
+    this.onChangeComposePath = this.onChangeField('ComposeFilePathInRepository');
+  }
+
+  onChangeField(field) {
+    return (value) => {
+      this.onChange({
+        ...this.model,
+        [field]: value,
+      });
+    };
+  }
+}
--- app/portainer/services/api/stackService.js
+++ app/portainer/services/api/stackService.js
@@ -13,7 +13,9 @@ angular.module('portainer.app').factory('StackService', [
   'EndpointProvider',
   function StackServiceFactory($q, $async, Stack, FileUploadService, StackHelper, ServiceService, ContainerService, SwarmService, EndpointProvider) {
     'use strict';
-    var service = {};
+    var service = {
+      updateGit,
+    };
 
     service.stack = function (id) {
       var deferred = $q.defer();
@@ -347,6 +349,20 @@ angular.module('portainer.app').factory('StackService', [
     service.stop = stop;
     function stop(id) {
       return Stack.stop({ id }).$promise;
+    }
+
+    function updateGit(id, endpointId, env, prune, gitConfig) {
+      return Stack.updateGit(
+        { endpointId, id },
+        {
+          env,
+          prune,
+          RepositoryReferenceName: gitConfig.RefName,
+          RepositoryAuthentication: gitConfig.RepositoryAuthentication,
+          RepositoryUsername: gitConfig.RepositoryUsername,
+          RepositoryPassword: gitConfig.RepositoryPassword,
+        }
+      ).$promise;
     }
 
     return service;
