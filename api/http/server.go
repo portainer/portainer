a4ac66ad8340d18845cb9f9b987ac14d669003a1
LP B <xAt0mZ@users.noreply.github.com>
2021-05-17 17:42:50 +0200

    feat(api): backport private registries backend changes (#5072)
    * feat(api/bolt): backport bolt changes
    
    * feat(api/exec): backport exec changes
    
    * feat(api/http): backport http/handler/dockerhub changes
    
    * feat(api/http): backport http/handler/endpoints changes
    
    * feat(api/http): backport http/handler/registries changes
    
    * feat(api/http): backport http/handler/stacks changes
    
    * feat(api/http): backport http/handler changes
    
    * feat(api/http): backport http/proxy/factory/azure changes
    
    * feat(api/http): backport http/proxy/factory/docker changes
    
    * feat(api/http): backport http/proxy/factory/utils changes
    
    * feat(api/http): backport http/proxy/factory/kubernetes changes
    
    * feat(api/http): backport http/proxy/factory changes
    
    * feat(api/http): backport http/security changes
    
    * feat(api/http): backport http changes
    
    * feat(api/internal): backport internal changes
    
    * feat(api): backport api changes
    
    * feat(api/kubernetes): backport kubernetes changes
    
    * fix(api/http): changes on backend following backport

--- api/http/server.go
+++ api/http/server.go
@@ -16,7 +16,6 @@ import (
 	"github.com/portainer/portainer/api/http/handler/auth"
 	"github.com/portainer/portainer/api/http/handler/backup"
 	"github.com/portainer/portainer/api/http/handler/customtemplates"
-	"github.com/portainer/portainer/api/http/handler/dockerhub"
 	"github.com/portainer/portainer/api/http/handler/edgegroups"
 	"github.com/portainer/portainer/api/http/handler/edgejobs"
 	"github.com/portainer/portainer/api/http/handler/edgestacks"
@@ -109,9 +108,6 @@ func (server *Server) Start() error {
 	customTemplatesHandler.FileService = server.FileService
 	customTemplatesHandler.GitService = server.GitService
 
-	var dockerHubHandler = dockerhub.NewHandler(requestBouncer)
-	dockerHubHandler.DataStore = server.DataStore
-
 	var edgeGroupsHandler = edgegroups.NewHandler(requestBouncer)
 	edgeGroupsHandler.DataStore = server.DataStore
 
@@ -157,6 +153,7 @@ func (server *Server) Start() error {
 	registryHandler.DataStore = server.DataStore
 	registryHandler.FileService = server.FileService
 	registryHandler.ProxyManager = server.ProxyManager
+	registryHandler.K8sClientFactory = server.KubernetesClientFactory
 
 	var resourceControlHandler = resourcecontrols.NewHandler(requestBouncer)
 	resourceControlHandler.DataStore = server.DataStore
@@ -215,7 +212,6 @@ func (server *Server) Start() error {
 		AuthHandler:            authHandler,
 		BackupHandler:          backupHandler,
 		CustomTemplatesHandler: customTemplatesHandler,
-		DockerHubHandler:       dockerHubHandler,
 		EdgeGroupsHandler:      edgeGroupsHandler,
 		EdgeJobsHandler:        edgeJobsHandler,
 		EdgeStacksHandler:      edgeStacksHandler,
