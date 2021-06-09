3304f53885d19b0f42905e6e0825415b05a89616
Chaim Lev-Ari <chiptus@gmail.com>
2021-06-09 09:40:41 +0300

    feat(db): move migration to db 31

--- api/bolt/migrator/migrator.go
+++ api/bolt/migrator/migrator.go
@@ -365,23 +365,20 @@ func (m *Migrator) Migrate() error {
 		}
 	}
 
-	// Portainer 2.5.0
+	// Portainer 2.7.0
 	if m.currentDBVersion < 31 {
 		err := m.updateSettingsToDB31()
 		if err != nil {
 			return err
 		}
-	}
 
-	// Portainer CE-2.5.0
-	if m.currentDBVersion < 30 {
-		err := m.updateRegistriesToDB30()
+		err = m.updateRegistriesToDB30()
 		if err != nil {
 			return err
 		}
 		migrateLog.Info("Successful migration of registries to DB version 30")
 
-		err = m.UpdateDockerhubToDB30()
+		err = m.updateDockerhubToDB30()
 		if err != nil {
 			return err
 		}
