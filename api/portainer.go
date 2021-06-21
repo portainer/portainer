e47de58161bff42de51e659f16ebacc69a03cd70
zees-dev <63374656+zees-dev@users.noreply.github.com>
2021-06-21 14:30:02 +1200

    feat(kubernetes/shell): backport kubeconfig generation backend functionality EE-1004 (#5213)
    * backported core backend kubectl shell functionality
    
    * refactored backend to match EE
    
    * - backported kubernetes backend handler implementation
    - backported kubernetes config endpoint
    - backported kubeconfig file generation
    - backported kubeconfig and yaml unit tests
    - backported updates to kubeclient interfaces

--- api/portainer.go
+++ api/portainer.go
@@ -8,6 +8,7 @@ import (
 
 	gittypes "github.com/portainer/portainer/api/git/types"
 	v1 "k8s.io/api/core/v1"
+	clientV1 "k8s.io/client-go/tools/clientcmd/api/v1"
 )
 
 type (
@@ -1193,6 +1194,7 @@ type (
 		DeleteRegistrySecret(registry *Registry, namespace string) error
 		CreateRegistrySecret(registry *Registry, namespace string) error
 		IsRegistrySecret(namespace, secretName string) (bool, error)
+		GetKubeConfig(ctx context.Context, apiServerURL string, bearerToken string, tokenData *TokenData) (*clientV1.Config, error)
 	}
 
 	// KubernetesDeployer represents a service to deploy a manifest inside a Kubernetes endpoint
