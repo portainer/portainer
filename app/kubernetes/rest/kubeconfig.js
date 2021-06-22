addd61ec1156bcd91e86244347847919a5001f4a
zees-dev <dev.786zshan@gmail.com>
2021-06-23 10:39:49 +1200

    using angularjs constant instead of hardcoded URL

--- app/kubernetes/rest/kubeconfig.js
+++ app/kubernetes/rest/kubeconfig.js
@@ -1,9 +1,9 @@
 angular.module('portainer.kubernetes').factory('KubernetesConfig', [
   '$http',
   'EndpointProvider',
-  function KubernetesConfigFactory($http, EndpointProvider) {
+  'API_ENDPOINT_KUBERNETES',
+  function KubernetesConfigFactory($http, EndpointProvider, API_ENDPOINT_KUBERNETES) {
     'use strict';
-    const BASE_URL = '/api/kubernetes';
 
     return { get };
 
@@ -11,7 +11,7 @@ angular.module('portainer.kubernetes').factory('KubernetesConfig', [
       const endpointID = EndpointProvider.endpointID();
       return $http({
         method: 'GET',
-        url: `${BASE_URL}/${endpointID}/config`,
+        url: `${API_ENDPOINT_KUBERNETES}/${endpointID}/config`,
         responseType: 'blob',
         headers: {
           Accept: 'text/yaml',
