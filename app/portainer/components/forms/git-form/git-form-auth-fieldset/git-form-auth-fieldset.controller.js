cb28050eeef2b3d420e0e899bdab2c2e23c62b9b
Chaim Lev-Ari <chiptus@gmail.com>
2021-06-22 09:26:36 +0300

    fix(git-form): empty auth values when auth is off

--- app/portainer/components/forms/git-form/git-form-auth-fieldset/git-form-auth-fieldset.controller.js
+++ app/portainer/components/forms/git-form/git-form-auth-fieldset/git-form-auth-fieldset.controller.js
@@ -1,8 +1,13 @@
 class GitFormComposeAuthFieldsetController {
   /* @ngInject */
   constructor() {
+    this.authValues = {
+      username: '',
+      password: '',
+    };
+
     this.onChangeField = this.onChangeField.bind(this);
-    this.onChangeAuth = this.onChangeField('RepositoryAuthentication');
+    this.onChangeAuth = this.onChangeAuth.bind(this);
     this.onChangeUsername = this.onChangeField('RepositoryUsername');
     this.onChangePassword = this.onChangeField('RepositoryPassword');
   }
@@ -14,6 +19,35 @@ class GitFormComposeAuthFieldsetController {
         [field]: value,
       });
     };
+  }
+
+  onChangeAuth(auth) {
+    if (!auth) {
+      this.authValues.username = this.model.RepositoryUsername;
+      this.authValues.password = this.model.RepositoryPassword;
+      this.onChange({
+        ...this.model,
+        RepositoryAuthentication: true,
+        RepositoryUsername: '',
+        RepositoryPassword: '',
+      });
+
+      return;
+    }
+
+    this.onChange({
+      ...this.model,
+      RepositoryAuthentication: false,
+      RepositoryUsername: this.authValues.username,
+      RepositoryPassword: this.authValues.password,
+    });
+  }
+
+  $onInit() {
+    if (this.model.RepositoryAuthentication) {
+      this.authValues.username = this.model.RepositoryUsername;
+      this.authValues.password = this.model.RepositoryPassword;
+    }
   }
 }
 
