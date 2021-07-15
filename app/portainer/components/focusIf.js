6af762d073dbe81a405c506bff9d909855fede6e
Felix Han <felix.han@portainer.io>
2021-07-15 23:25:17 +1200

    added focus-if directive

---
+++ app/portainer/components/focusIf.js
@@ -0,0 +1,20 @@
+angular.module('portainer.app').directive('focusIf', function ($timeout) {
+  return {
+    restrict: 'A',
+    link: function ($scope, $element, $attrs) {
+      var dom = $element[0];
+      if ($attrs.focusIf) {
+        $scope.$watch($attrs.focusIf, focus);
+      } else {
+        focus(true);
+      }
+      function focus(condition) {
+        if (condition) {
+          $timeout(function () {
+            dom.focus();
+          }, $scope.$eval($attrs.focusDelay) || 0);
+        }
+      }
+    },
+  };
+});
