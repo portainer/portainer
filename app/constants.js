7608e1eb5c5cb4b53cc55004c808229e72fdc650
zees-dev <dev.786zshan@gmail.com>
2021-07-16 17:48:36 +1200

    removed redundant dockerhub api endpoint variable

--- app/constants.js
+++ app/constants.js
@@ -1,7 +1,6 @@
 angular
   .module('portainer')
   .constant('API_ENDPOINT_AUTH', 'api/auth')
-  .constant('API_ENDPOINT_DOCKERHUB', 'api/dockerhub')
   .constant('API_ENDPOINT_KUBERNETES', 'api/kubernetes')
   .constant('API_ENDPOINT_CUSTOM_TEMPLATES', 'api/custom_templates')
   .constant('API_ENDPOINT_EDGE_GROUPS', 'api/edge_groups')
