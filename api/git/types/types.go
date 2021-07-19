2f81cea0650f33c3bbebe4616b1d17974e40d61c
ArrisLee <arris_li@hotmail.com>
2021-07-20 02:53:08 +1200

    update examples

--- api/git/types/types.go
+++ api/git/types/types.go
@@ -1,10 +1,10 @@
 package gittypes
 
 type RepoConfig struct {
-	// Git repo URL
-	URL string `example:"https://github.com/portainer/portainer"`
-	// Git reference name
-	ReferenceName string `example:"master"`
-	// Entry point file path
+	// The repo url
+	URL string `example:"https://github.com/portainer/portainer-ee.git"`
+	// The reference name
+	ReferenceName string `example:"refs/heads/branch_name"`
+	// Path to where the config file is in this url/refName
 	ConfigFilePath string `example:"docker-compose.yml"`
 }
