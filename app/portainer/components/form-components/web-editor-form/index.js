d57fd20b158449b57326ae615ed5dbdfeafbd62a
Chaim Lev-Ari <chiptus@gmail.com>
2021-08-12 12:07:07 +0300

    feat(app): introduce web-editor form component

---
+++ app/portainer/components/form-components/web-editor-form/index.js
@@ -0,0 +1,19 @@
+import controller from './web-editor-form.controller.js';
+
+export const webEditorForm = {
+  templateUrl: './web-editor-form.html',
+  controller,
+
+  bindings: {
+    identifier: '@',
+    placeholder: '@',
+    yml: '<',
+    value: '<',
+
+    onChange: '<',
+  },
+
+  transclude: {
+    description: '?editorDescription',
+  },
+};
