3a330d9d1425c418c0063e71f1c0a582df6a37d8
Richard Wei <54336863+WaysonWei@users.noreply.github.com>
2021-08-15 19:50:54 +1200

    feat(helm): add helm chart backport to ce EE-1409 (#5425)
    * EE-1311 Helm Chart Backport from EE
    
    * backport to ce
    
    Co-authored-by: Matt Hook <hookenz@gmail.com>

--- api/http/handler/helm/helm_show.go
+++ api/http/handler/helm/helm_show.go
@@ -4,9 +4,9 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/portainer/libhelm/options"
 	httperror "github.com/portainer/libhttp/error"
 	"github.com/portainer/libhttp/request"
+	"github.com/portainer/portainer/api/exec/helm"
 )
 
 // @id HelmList
@@ -23,9 +23,9 @@ import (
 // @failure 500 "Server error"
 // @router /templates/helm/{chart}/{command} [get]
 func (handler *Handler) helmShow(w http.ResponseWriter, r *http.Request) *httperror.HandlerError {
-	settings, err := handler.dataStore.Settings().Settings()
-	if err != nil {
-		return &httperror.HandlerError{StatusCode: http.StatusInternalServerError, Message: "Unable to retrieve settings", Err: err}
+	repo, httperr := handler.getHelmRepositoryUrl()
+	if httperr != nil {
+		return httperr
 	}
 
 	chart, err := request.RetrieveRouteVariableValue(r, "chart")
@@ -43,12 +43,12 @@ func (handler *Handler) helmShow(w http.ResponseWriter, r *http.Request) *httper
 		log.Printf("[DEBUG] [internal,helm] [message: command not provided, defaulting to %s]", cmd)
 	}
 
-	showOptions := options.ShowOptions{
-		OutputFormat: options.ShowOutputFormat(cmd),
+	showOptions := helm.ShowOptions{
+		OutputFormat: helm.ShowOutputFormat(cmd),
 		Chart:        chart,
-		Repo:         settings.HelmRepositoryURL,
+		Repo:         repo,
 	}
-	result, err := handler.helmPackageManager.Show(showOptions)
+	result, err := handler.HelmPackageManager.Show(showOptions)
 	if err != nil {
 		return &httperror.HandlerError{
 			StatusCode: http.StatusInternalServerError,
