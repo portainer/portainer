89e71aabab89b9e33f7bd236edef1427772d9456
Matt Hook <hookenz@gmail.com>
2021-08-20 17:19:26 +1200

    feat(helm) helm chart backport from ee EE-1311 (#5436)
    * Add missing defaultHelmRepoUrl and mock testing
    
    * Backport EE-1477
    
    * Backport updates to helm tests from EE
    
    * add https by default changes and ssl to tls renaming from EE
    
    * Port install integration test. Disabled by default to pass CI checks
    
    * merged changes from EE for the integration test
    
    * kube proxy whitelist updated to support internal helm install command
    
    Co-authored-by: zees-dev <dev.786zshan@gmail.com>

--- api/exec/helm/helm_package.go
+++ api/exec/helm/helm_package.go
@@ -2,23 +2,22 @@ package helm
 
 import (
 	"bytes"
-	// "errors"
 	"fmt"
-	"log"
 	"os/exec"
 	"path"
 	"runtime"
 	"strings"
 
 	"github.com/pkg/errors"
+	portainer "github.com/portainer/portainer/api"
 	"github.com/portainer/portainer/api/exec/helm/release"
 	"github.com/portainer/portainer/api/kubernetes"
+	log "github.com/sirupsen/logrus"
 )
 
 // HelmPackageManager represents a service that interfaces with Helm
 type HelmPackageManager interface {
-	Run(command string, args []string, serverURL, authToken string) (string, error) // TODO remove
-	Install(installOpts InstallOptions, serverURL, authToken string) (*release.Release, error)
+	Install(installOpts InstallOptions, endpointId portainer.EndpointID, authToken string) (*release.Release, error)
 	Show(showOpts ShowOptions) (string, error)
 	SearchRepo(searchOpts SearchRepoOptions) (string, error)
 }
@@ -30,38 +29,44 @@ type helmBinaryPackageManager struct {
 }
 
 // NewHelmBinaryPackageManager initializes a new HelmPackageManager service.
-func NewHelmBinaryPackageManager(kubeConfigService kubernetes.KubeConfigService, binaryPath string) HelmPackageManager {
+func NewHelmBinaryPackageManager(kubeConfigService kubernetes.KubeConfigService, binaryPath string) *helmBinaryPackageManager {
 	return &helmBinaryPackageManager{
 		kubeConfigService: kubeConfigService,
 		binaryPath:        binaryPath,
 	}
 }
 
-// Run will execute helm command against the provided Kubernetes cluster.
-// The serverURL and authToken are dynamic params (based on the user) that allow helm to execute commands
+// runWithKubeConfig will execute run against the provided Kubernetes cluster with kubeconfig as cli arguments.
+func (hbpm *helmBinaryPackageManager) runWithKubeConfig(command string, args []string, endpointId portainer.EndpointID, authToken string) (string, error) {
+	cmdArgs := make([]string, 0)
+	if hbpm.kubeConfigService.IsSecure() {
+		clusterAccess := hbpm.kubeConfigService.GetKubeConfigInternal(endpointId, authToken)
+		cmdArgs = append(cmdArgs, "--kube-apiserver", clusterAccess.ClusterServerURL)
+		cmdArgs = append(cmdArgs, "--kube-token", clusterAccess.AuthToken)
+		cmdArgs = append(cmdArgs, "--kube-ca-file", clusterAccess.CertificateAuthorityFile)
+	}
+	cmdArgs = append(cmdArgs, args...)
+	return hbpm.run(command, cmdArgs)
+}
+
+// run will execute helm command against the provided Kubernetes cluster.
+// The endpointId and authToken are dynamic params (based on the user) that allow helm to execute commands
 // in the context of the current user against specified k8s cluster.
-func (hbpm *helmBinaryPackageManager) Run(command string, args []string, serverURL, authToken string) (string, error) {
+func (hbpm *helmBinaryPackageManager) run(command string, args []string) (string, error) {
+	cmdArgs := make([]string, 0)
+	cmdArgs = append(cmdArgs, command)
+	cmdArgs = append(cmdArgs, args...)
+
 	helmPath := path.Join(hbpm.binaryPath, "helm")
 	if runtime.GOOS == "windows" {
 		helmPath = path.Join(hbpm.binaryPath, "helm.exe")
 	}
 
-	clusterAccess := hbpm.kubeConfigService.GetKubeConfigCore(serverURL, authToken)
-
-	cmdArgs := make([]string, 0)
-	if hbpm.kubeConfigService.IsSecure() {
-		cmdArgs = append(cmdArgs, "--kube-apiserver", clusterAccess.ClusterServerURL)
-		cmdArgs = append(cmdArgs, "--kube-token", clusterAccess.AuthToken)
-		cmdArgs = append(cmdArgs, "--kube-ca-file", clusterAccess.CertificateAuthorityFile)
-	}
-	cmdArgs = append(cmdArgs, command)
-	cmdArgs = append(cmdArgs, args...)
-
-	log.Printf("[DEBUG] [internal,helm] [message: executing helm command: %s]", fmt.Sprintf("%s %s", helmPath, strings.Join(cmdArgs, " ")))
-
 	var stderr bytes.Buffer
 	cmd := exec.Command(helmPath, cmdArgs...)
 	cmd.Stderr = &stderr
+
+	log.Debug(fmt.Sprintf("executing helm command: %s %s", helmPath, strings.Join(cmdArgs, " ")))
 
 	output, err := cmd.Output()
 	if err != nil {
