5d261590c26b5ad05a87904b5dbbe6e9c7ccf459
zees-dev <dev.786zshan@gmail.com>
2021-08-27 00:52:04 +1200

    - kube-config -> kube-config-download-button - fixed kubeconfig file name (bug)

--- app/kubernetes/components/kube-config/kube-config.js
+++
@@ -1,7 +0,0 @@
-import angular from 'angular';
-import controller from './kube-config.controller';
-
-angular.module('portainer.kubernetes').component('kubeConfig', {
-  templateUrl: './kube-config.html',
-  controller,
-});
