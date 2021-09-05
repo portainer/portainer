1597717e3de6dfad78b3f9b1c800256ed55720d5
zees-dev <dev.786zshan@gmail.com>
2021-09-06 11:02:04 +1200

    dependency injection in helm factory

--- app/kubernetes/helm/rest.js
+++ app/kubernetes/helm/rest.js
@@ -2,6 +2,7 @@ import angular from 'angular';
 
 angular.module('portainer.kubernetes').factory('HelmFactory', HelmFactory);
 
+/* @ngInject */
 function HelmFactory($resource, API_ENDPOINT_ENDPOINTS) {
   const helmUrl = API_ENDPOINT_ENDPOINTS + '/:endpointId/kubernetes/helm';
   const templatesUrl = '/api/templates/helm';
