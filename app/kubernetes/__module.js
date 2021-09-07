81a06c559a26fc240fe2b6b07f0c94b62b220a82
zees-dev <dev.786zshan@gmail.com>
2021-09-07 18:30:18 +1200

    fixed helm release page for non-default namespaces

--- app/kubernetes/__module.js
+++ app/kubernetes/__module.js
@@ -47,7 +47,7 @@ angular.module('portainer.kubernetes', ['portainer.app', registriesModule, custo
 
     const helmApplication = {
       name: 'kubernetes.helm',
-      url: '/helm/:name',
+      url: '/helm/:namespace/:name',
       views: {
         'content@': {
           component: 'kubernetesHelmApplicationView',
