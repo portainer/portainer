5ad522a930b69a967cab33e03d329e375e7afa4c
Chaim Lev-Ari <chiptus@gmail.com>
2021-09-19 11:15:37 +0300

    feat(featureflags): expose is feature limited to BE

--- app/portainer/components/be-feature-indicator/be-feature-indicator.controller.js
+++ app/portainer/components/be-feature-indicator/be-feature-indicator.controller.js
@@ -1,5 +1,3 @@
-import { STATES } from '@/portainer/feature-flags/enums';
-
 const BE_URL = 'https://www.portainer.io/business-upsell?from=';
 
 export default class BeIndicatorController {
@@ -13,9 +11,8 @@ export default class BeIndicatorController {
   $onInit() {
     if (this.feature) {
       this.url = `${BE_URL}${this.feature}`;
-      const state = this.featureService.selectShow(this.feature);
 
-      this.limitedToBE = state === STATES.LIMITED_BE;
+      this.limitedToBE = this.featureService.isLimitedToBE(this.feature);
     }
   }
 }
