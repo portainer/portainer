e1f725d01af9662efca95f54f3e0bc1b3319b826
Richard Wei <54336863+WaysonWei@users.noreply.github.com>
2021-09-22 16:01:28 +1200

    fix(k8s): fix error handling for namespace restricted user EE-1703 (#5692)
    * fix error handler for user has no namespace access

--- app/kubernetes/views/deploy/deployController.js
+++ app/kubernetes/views/deploy/deployController.js
@@ -107,7 +107,9 @@ class KubernetesDeployController {
       this.state.BuildMethod === KubernetesDeployBuildMethods.WEB_EDITOR && _.isEmpty(this.formValues.EditorContent) && _.isEmpty(this.formValues.Namespace);
     const isURLFormInvalid = this.state.BuildMethod == KubernetesDeployBuildMethods.WEB_EDITOR.URL && _.isEmpty(this.formValues.ManifestURL);
 
-    return isGitFormInvalid || isWebEditorInvalid || isURLFormInvalid || this.state.actionInProgress;
+    const isNamespaceInvalid = _.isEmpty(this.formValues.Namespace);
+
+    return isGitFormInvalid || isWebEditorInvalid || isURLFormInvalid || this.state.actionInProgress || isNamespaceInvalid;
   }
 
   onChangeFormValues(values) {
@@ -230,7 +232,9 @@ class KubernetesDeployController {
       });
 
       this.namespaces = namespaces;
-      this.formValues.Namespace = this.namespaces[0].Name;
+      if (this.namespaces.length > 0) {
+        this.formValues.Namespace = this.namespaces[0].Name;
+      }
     } catch (err) {
       this.Notifications.error('Failure', err, 'Unable to load namespaces data');
     }
