89d9a485467ba694f6ceffcb0e52f1bcf6e7f64a
Chaim Lev-Ari <chiptus@users.noreply.github.com>
2021-09-22 10:16:47 +0300

    refactor(settings): backport auth views (#5705)

---
+++ app/portainer/settings/authentication/ldap/ad-settings/index.js
@@ -0,0 +1,12 @@
+import controller from './ad-settings.controller';
+
+export const adSettings = {
+  templateUrl: './ad-settings.html',
+  controller,
+  bindings: {
+    settings: '=',
+    tlscaCert: '=',
+    state: '=',
+    connectivityCheck: '<',
+  },
+};
--- app/portainer/settings/index.js
+++ app/portainer/settings/index.js
@@ -1,5 +1,6 @@
 import angular from 'angular';
 
+import authenticationModule from './authentication';
 import generalModule from './general';
 
-export default angular.module('portainer.settings', [generalModule]).name;
+export default angular.module('portainer.settings', [authenticationModule, generalModule]).name;
