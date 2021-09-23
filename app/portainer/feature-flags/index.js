b466d0ce57007d18aebb7b6e87308dd0bbc95d81
Chaim Lev-Ari <chiptus@gmail.com>
2021-09-23 11:50:25 +0300

    refactor(feature-flags): move css to module

--- app/portainer/feature-flags/index.js
+++ app/portainer/feature-flags/index.js
@@ -2,5 +2,6 @@ import angular from 'angular';
 
 import { limitedFeatureDirective } from './limited-feature.directive';
 import { featureService } from './feature-flags.service';
+import './feature-flags.css';
 
 export default angular.module('portainer.feature-flags', []).directive('limitedFeatureDir', limitedFeatureDirective).factory('featureService', featureService).name;
