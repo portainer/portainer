56131c2ae63ff5bbd1a61463e20f8184efa1b81b
Matt Hook <hookenz@gmail.com>
2021-09-23 12:00:56 +1200

    add overall failure back in. diff log file

--- api/bolt/migrate_data.go
+++ api/bolt/migrate_data.go
@@ -58,6 +58,7 @@ func (store *Store) MigrateData(force bool) error {
 		migrateLog.Info(fmt.Sprintf("Migrating database from version %v to %v.\n", migrator.Version(), portainer.DBVersion))
 		err = store.FailSafeMigrate(migrator)
 		if err != nil {
+			migrateLog.Error("An error occurred during database migration", err)
 			return err
 		}
 	}
