9a5f16d5b09238cb232880a0b2431fdb090dccce
Stéphane Busso <stephane.busso@gmail.com>
2021-09-23 12:38:08 +1200

    Update env tasks

--- gruntfile.js
+++ gruntfile.js
@@ -74,7 +74,7 @@ module.exports = function (grunt) {
       'shell:download_helm_binary:' + p + ':' + a,
       'shell:download_kompose_binary:' + p + ':' + a,
       'shell:download_kubectl_binary:' + p + ':' + a,
-      'webpack:prod',
+      'webpack:${env}',
     ]);
   });
 
@@ -90,7 +90,7 @@ module.exports = function (grunt) {
       'shell:download_helm_binary:' + p + ':' + a,
       'shell:download_kompose_binary:' + p + ':' + a,
       'shell:download_kubectl_binary:' + p + ':' + a,
-      'webpack:prod',
+      'webpack:${env}',
     ]);
   });
 };
