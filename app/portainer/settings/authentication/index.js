594626d8cb020a95de9ddffa872107fa185414f0
Chaim Lev-Ari <chiptus@gmail.com>
2021-09-29 23:11:38 +0300

    refactor(oauth): remove unused membership toggle

--- app/portainer/settings/authentication/index.js
+++ app/portainer/settings/authentication/index.js
@@ -3,10 +3,8 @@ import angular from 'angular';
 import ldapModule from './ldap';
 
 import { autoUserProvisionToggle } from './auto-user-provision-toggle';
-import { autoTeamMembershipToggle } from './auto-team-membership-toggle';
 
 export default angular
   .module('portainer.settings.authentication', [ldapModule])
 
-  .component('autoUserProvisionToggle', autoUserProvisionToggle)
-  .component('autoTeamMembershipToggle', autoTeamMembershipToggle).name;
+  .component('autoUserProvisionToggle', autoUserProvisionToggle).name;
