24a40dc787c94d1033f0733abf8717511dd33883
Chaim Lev-Ari <chiptus@users.noreply.github.com>
2021-10-01 05:43:10 +0300

    feat(oauth): highlight BE features [EE-1665] (#5787)
    * refactor(oauth): backport ee changes
    
    * feat(oauth): limit options to BE
    
    * feat(oauth): limit oauth fields to BE
    
    * refactor(oauth): remove unused membership toggle

--- app/portainer/oauth/components/oauth-providers-selector/index.js
+++ app/portainer/oauth/components/oauth-providers-selector/index.js
@@ -1,8 +1,11 @@
+import angular from 'angular';
+import controller from './oauth-provider-selector.controller';
+
 angular.module('portainer.oauth').component('oauthProvidersSelector', {
   templateUrl: './oauth-providers-selector.html',
   bindings: {
-    onSelect: '<',
-    provider: '=',
+    onChange: '<',
+    value: '<',
   },
-  controller: 'OAuthProviderSelectorController',
+  controller,
 });
