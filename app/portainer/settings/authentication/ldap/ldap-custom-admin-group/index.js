a87cb8785c8aa8b43deeef92493be68a78765a2b
Felix Han <felix.han@portainer.io>
2021-10-12 10:10:29 +1300

    feat(ldap): added ldap custom admin group component

---
+++ app/portainer/settings/authentication/ldap/ldap-custom-admin-group/index.js
@@ -0,0 +1,13 @@
+import controller from './ldap-custom-admin-group.controller';
+
+export const ldapCustomAdminGroup = {
+  templateUrl: './ldap-custom-admin-group.html',
+  controller,
+  bindings: {
+    settings: '=',
+    selectedAdminGroups: '=',
+    defaultAdminGroupSearchFilter: '<',
+    onSearchClick: '<',
+    limitedFeatureId: '<',
+  },
+};
