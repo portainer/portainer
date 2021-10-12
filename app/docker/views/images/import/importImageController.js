97e043fd2b46f0564ff13106fa7a57f2ffd10036
Prabhat Khera <prabhat.khera@portainer.io>
2021-10-12 17:32:32 +1300

    feat(ui): allow tags when importing docker image

--- app/docker/views/images/import/importImageController.js
+++ app/docker/views/images/import/importImageController.js
@@ -1,17 +1,40 @@
+import { PorImageRegistryModel } from 'Docker/models/porImageRegistry';
+
 angular.module('portainer.docker').controller('ImportImageController', [
   '$scope',
   '$state',
   'ImageService',
   'Notifications',
   'HttpRequestHelper',
-  function ($scope, $state, ImageService, Notifications, HttpRequestHelper) {
+  'Authentication',
+  'ImageHelper',
+  'endpoint',
+  function ($scope, $state, ImageService, Notifications, HttpRequestHelper, Authentication, ImageHelper, endpoint) {
     $scope.state = {
       actionInProgress: false,
     };
 
+    $scope.endpoint = endpoint;
+
+    $scope.isAdmin = Authentication.isAdmin();
+
     $scope.formValues = {
       UploadFile: null,
       NodeName: null,
+      RegistryModel: new PorImageRegistryModel(),
+    };
+
+    $scope.setPullImageValidity = setPullImageValidity;
+    function setPullImageValidity(validity) {
+      $scope.state.pullImageValidity = validity;
+    };
+
+    function tagImage(id) {
+      const registryModel = $scope.formValues.RegistryModel;
+      if (registryModel.Image) {
+        const image = ImageHelper.createImageConfigForContainer(registryModel);
+        ImageService.tagImage(id, image.fromImage);
+      }
     };
 
     $scope.uploadImage = function () {
@@ -21,7 +44,13 @@ angular.module('portainer.docker').controller('ImportImageController', [
       HttpRequestHelper.setPortainerAgentTargetHeader(nodeName);
       var file = $scope.formValues.UploadFile;
       ImageService.uploadImage(file)
-        .then(function success() {
+        .then(async function success(res) {
+          var regex = /Loaded.*?: (.*?)\n$/g;
+          var imageIds = regex.exec(res.data.stream);
+          if (imageIds && imageIds.length == 2) {
+            await tagImage(imageIds[1]);
+            $state.go('docker.images.image', { id: imageIds[1] }, { reload: true });
+          }
           Notifications.success('Images successfully uploaded');
         })
         .catch(function error(err) {
