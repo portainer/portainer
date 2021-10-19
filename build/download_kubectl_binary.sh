1f7338f1733ed697af98da694c9bf5b76c086bdd
cheloRydel <marcelorydel26@gmail.com>
2021-10-19 10:11:43 -0300

    fixes

--- build/download_kubectl_binary.sh
+++ build/download_kubectl_binary.sh
@@ -4,7 +4,6 @@ PLATFORM=$1
 ARCH=$2
 KUBECTL_VERSION=$3
 
-
 if [ "${PLATFORM}" == 'linux' ]; then
   wget -O "dist/kubectl" "https://storage.googleapis.com/kubernetes-release/release/${KUBECTL_VERSION}/bin/${PLATFORM}/${ARCH}/kubectl"
   chmod +x "dist/kubectl"
