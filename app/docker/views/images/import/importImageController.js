bc9b41c84d54134fef542c401318ebd227cee194
Prabhat Khera <prabhat.khera@portainer.io>
2021-10-27 13:26:48 +1300

    show notification when uploading tar with multiple images

--- app/docker/views/images/import/importImageController.js
+++ app/docker/views/images/import/importImageController.js
@@ -27,15 +27,17 @@ angular.module('portainer.docker').controller('ImportImageController', [
     $scope.setPullImageValidity = setPullImageValidity;
     function setPullImageValidity(validity) {
       $scope.state.pullImageValidity = validity;
-    };
+    }
 
     function tagImage(id) {
       const registryModel = $scope.formValues.RegistryModel;
       if (registryModel.Image) {
         const image = ImageHelper.createImageConfigForContainer(registryModel);
-        ImageService.tagImage(id, image.fromImage);
+        ImageService.tagImage(id, image.fromImage).catch(function error(err) {
+          Notifications.error('Failure', err, 'Unable to tag image');
+        });
       }
-    };
+    }
 
     $scope.uploadImage = function () {
       $scope.state.actionInProgress = true;
@@ -45,13 +47,17 @@ angular.module('portainer.docker').controller('ImportImageController', [
       var file = $scope.formValues.UploadFile;
       ImageService.uploadImage(file)
         .then(async function success(res) {
-          var regex = /Loaded.*?: (.*?)\n$/g;
-          var imageIds = regex.exec(res.data.stream);
-          if (imageIds && imageIds.length == 2) {
-            await tagImage(imageIds[1]);
-            $state.go('docker.images.image', { id: imageIds[1] }, { reload: true });
+          if (res.data.stream) {
+            var regex = /Loaded.*?: (.*?)\n$/g;
+            var imageIds = regex.exec(res.data.stream);
+            if (imageIds && imageIds.length == 2) {
+              await tagImage(imageIds[1]);
+              $state.go('docker.images.image', { id: imageIds[1] }, { reload: true });
+            }
+            Notifications.success('Images successfully uploaded');
+          } else {
+            Notifications.success('The uploaded tar file contained multiple images. The provided tag therefore has been ignored.');
           }
-          Notifications.success('Images successfully uploaded');
         })
         .catch(function error(err) {
           Notifications.error('Failure', err, 'Unable to upload image');
