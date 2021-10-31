3cac8ff02ed8aefade628776fa031d5fb98f6926
zees-dev <63374656+zees-dev@users.noreply.github.com>
2021-11-01 10:33:56 +1300

    feat(access-token): Multi-auth middleware support EE-1891 (#5936)
    * AnyAuth middleware initial implementation with tests
    
    * using mux.MiddlewareFunc instead of custom definition
    
    * removed redundant comments
    
    * - ExtractBearerToken bouncer func made private
    - changed helm token handling functionality to use jwt service to convert token to jwt string
    - updated tests
    - fixed helm list broken test due to missing token in request context
    
    * rename mwCheckAuthentication -> mwCheckJWTAuthentication
    
    * - introduce initial api-key auth support using X-API-KEY header
    - added tests to validate x-api-key request header presence
    
    * updated core mwAuthenticatedUser middleware to support multiple auth paradigms
    
    * - simplified anyAuth middleware
    - enforcing authmiddleware to implement verificationFunc interface
    - created tests for middleware
    
    * simplify bouncer
    
    Co-authored-by: Dmitry Salakhov <to@dimasalakhov.com>

--- api/http/handler/helm/handler.go
+++ api/http/handler/helm/handler.go
@@ -26,17 +26,19 @@ type Handler struct {
 	*mux.Router
 	requestBouncer     requestBouncer
 	dataStore          portainer.DataStore
+	jwtService         portainer.JWTService
 	kubeConfigService  kubernetes.KubeConfigService
 	kubernetesDeployer portainer.KubernetesDeployer
 	helmPackageManager libhelm.HelmPackageManager
 }
 
 // NewHandler creates a handler to manage endpoint group operations.
-func NewHandler(bouncer requestBouncer, dataStore portainer.DataStore, kubernetesDeployer portainer.KubernetesDeployer, helmPackageManager libhelm.HelmPackageManager, kubeConfigService kubernetes.KubeConfigService) *Handler {
+func NewHandler(bouncer requestBouncer, dataStore portainer.DataStore, jwtService portainer.JWTService, kubernetesDeployer portainer.KubernetesDeployer, helmPackageManager libhelm.HelmPackageManager, kubeConfigService kubernetes.KubeConfigService) *Handler {
 	h := &Handler{
 		Router:             mux.NewRouter(),
 		requestBouncer:     bouncer,
 		dataStore:          dataStore,
+		jwtService:         jwtService,
 		kubernetesDeployer: kubernetesDeployer,
 		helmPackageManager: helmPackageManager,
 		kubeConfigService:  kubeConfigService,
@@ -91,7 +93,12 @@ func (handler *Handler) getHelmClusterAccess(r *http.Request) (*options.Kubernet
 		return nil, &httperror.HandlerError{http.StatusNotFound, "Unable to find an environment on request context", err}
 	}
 
-	bearerToken, err := security.ExtractBearerToken(r)
+	tokenData, err := security.RetrieveTokenData(r)
+	if err != nil {
+		return nil, &httperror.HandlerError{http.StatusInternalServerError, "Unable to retrieve user authentication token", err}
+	}
+
+	bearerToken, err := handler.jwtService.GenerateToken(tokenData)
 	if err != nil {
 		return nil, &httperror.HandlerError{http.StatusUnauthorized, "Unauthorized", err}
 	}
---
+++ api/http/security/bouncer_test.go
@@ -0,0 +1,201 @@
+package security
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	portainer "github.com/portainer/portainer/api"
+	"github.com/portainer/portainer/api/bolt"
+	httperrors "github.com/portainer/portainer/api/http/errors"
+	"github.com/portainer/portainer/api/jwt"
+	"github.com/stretchr/testify/assert"
+)
+
+// testHandler200 is a simple handler which returns HTTP status 200 OK
+var testHandler200 = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+})
+
+func tokenLookupSucceed(dataStore portainer.DataStore, jwtService portainer.JWTService) tokenLookup {
+	return func(r *http.Request) *portainer.TokenData {
+		uid := portainer.UserID(1)
+		dataStore.User().CreateUser(&portainer.User{ID: uid})
+		jwtService.GenerateToken(&portainer.TokenData{ID: uid})
+		return &portainer.TokenData{ID: 1}
+	}
+}
+
+func tokenLookupFail(r *http.Request) *portainer.TokenData {
+	return nil
+}
+
+func Test_mwAuthenticateFirst(t *testing.T) {
+	is := assert.New(t)
+
+	store, teardown := bolt.MustNewTestStore(true)
+	defer teardown()
+
+	jwtService, err := jwt.NewService("1h", nil)
+	assert.NoError(t, err, "failed to create a copy of service")
+
+	bouncer := NewRequestBouncer(store, jwtService)
+
+	tests := []struct {
+		name                   string
+		verificationMiddlwares []tokenLookup
+		wantStatusCode         int
+	}{
+		{
+			name:                   "mwAuthenticateFirst middleware passes with no middleware",
+			verificationMiddlwares: nil,
+			wantStatusCode:         http.StatusUnauthorized,
+		},
+		{
+			name: "mwAuthenticateFirst middleware succeeds with passing middleware",
+			verificationMiddlwares: []tokenLookup{
+				tokenLookupSucceed(store, jwtService),
+			},
+			wantStatusCode: http.StatusOK,
+		},
+		{
+			name: "mwAuthenticateFirst fails with failing middleware",
+			verificationMiddlwares: []tokenLookup{
+				tokenLookupFail,
+			},
+			wantStatusCode: http.StatusUnauthorized,
+		},
+		{
+			name: "mwAuthenticateFirst succeeds if first middleware successfully handles request",
+			verificationMiddlwares: []tokenLookup{
+				tokenLookupSucceed(store, jwtService),
+				tokenLookupFail,
+			},
+			wantStatusCode: http.StatusOK,
+		},
+		{
+			name: "mwAuthenticateFirst succeeds if last middleware successfully handles request",
+			verificationMiddlwares: []tokenLookup{
+				tokenLookupFail,
+				tokenLookupSucceed(store, jwtService),
+			},
+			wantStatusCode: http.StatusOK,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			req := httptest.NewRequest(http.MethodGet, "/", nil)
+			rr := httptest.NewRecorder()
+
+			h := bouncer.mwAuthenticateFirst(tt.verificationMiddlwares, testHandler200)
+			h.ServeHTTP(rr, req)
+
+			is.Equal(tt.wantStatusCode, rr.Code, fmt.Sprintf("Status should be %d", tt.wantStatusCode))
+		})
+	}
+}
+
+func Test_extractBearerToken(t *testing.T) {
+	is := assert.New(t)
+
+	tt := []struct {
+		name               string
+		requestHeader      string
+		requestHeaderValue string
+		wantToken          string
+		doesError          bool
+	}{
+		{
+			name:               "missing request header",
+			requestHeader:      "",
+			requestHeaderValue: "",
+			wantToken:          "",
+			doesError:          true,
+		},
+		{
+			name:               "invalid authorization request header",
+			requestHeader:      "authorisation", // note: `s`
+			requestHeaderValue: "abc",
+			wantToken:          "",
+			doesError:          true,
+		},
+		{
+			name:               "valid authorization request header",
+			requestHeader:      "AUTHORIZATION",
+			requestHeaderValue: "abc",
+			wantToken:          "abc",
+			doesError:          false,
+		},
+		{
+			name:               "valid authorization request header case-insensitive canonical check",
+			requestHeader:      "authorization",
+			requestHeaderValue: "def",
+			wantToken:          "def",
+			doesError:          false,
+		},
+	}
+
+	for _, test := range tt {
+		req := httptest.NewRequest(http.MethodGet, "/", nil)
+		req.Header.Set(test.requestHeader, test.requestHeaderValue)
+		apiKey, err := extractBearerToken(req)
+		is.Equal(test.wantToken, apiKey)
+		if test.doesError {
+			is.Error(err, "Should return error")
+			is.ErrorIs(err, httperrors.ErrUnauthorized)
+		} else {
+			is.NoError(err)
+		}
+	}
+}
+
+func Test_extractAPIKey(t *testing.T) {
+	is := assert.New(t)
+
+	tt := []struct {
+		name               string
+		requestHeader      string
+		requestHeaderValue string
+		wantApiKey         string
+		succeeds           bool
+	}{
+		{
+			name:               "missing request header",
+			requestHeader:      "",
+			requestHeaderValue: "",
+			wantApiKey:         "",
+			succeeds:           false,
+		},
+		{
+			name:               "invalid api-key request header",
+			requestHeader:      "api-key",
+			requestHeaderValue: "abc",
+			wantApiKey:         "",
+			succeeds:           false,
+		},
+		{
+			name:               "valid api-key request header",
+			requestHeader:      apiKeyHeader,
+			requestHeaderValue: "abc",
+			wantApiKey:         "abc",
+			succeeds:           true,
+		},
+		{
+			name:               "valid api-key request header case-insensitive canonical check",
+			requestHeader:      "x-api-key",
+			requestHeaderValue: "def",
+			wantApiKey:         "def",
+			succeeds:           true,
+		},
+	}
+
+	for _, test := range tt {
+		req := httptest.NewRequest(http.MethodGet, "/", nil)
+		req.Header.Set(test.requestHeader, test.requestHeaderValue)
+		apiKey, ok := extractAPIKey(req)
+		is.Equal(test.wantApiKey, apiKey)
+		is.Equal(test.succeeds, ok)
+	}
+}
