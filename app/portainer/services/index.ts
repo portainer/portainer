8722cc4fd0861b058e087952821f14c20fae1804
Chaim Lev-Ari <chiptus@users.noreply.github.com>
2021-11-10 17:05:07 +0200

    refactor(app): remove angular from http-request [EE-1899] (#6016)

--- app/portainer/services/index.ts
+++ app/portainer/services/index.ts
@@ -2,8 +2,10 @@ import angular from 'angular';
 
 import { Notifications } from './notifications';
 import { ModalServiceAngular } from './modal.service';
+import { HttpRequestHelperAngular } from './http-request.helper';
 
 export default angular
   .module('portainer.app.services', [])
   .factory('Notifications', Notifications)
-  .factory('ModalService', ModalServiceAngular).name;
+  .factory('ModalService', ModalServiceAngular)
+  .factory('HttpRequestHelper', HttpRequestHelperAngular).name;
