6b8ea1d80b56239ee0cadd9e045a62f5b4c2c458
cheloRydel <marcelorydel26@gmail.com>
2021-11-10 09:00:15 -0300

    add component

--- app/portainer/settings/general/index.js
+++ app/portainer/settings/general/index.js
@@ -1,5 +1,7 @@
 import angular from 'angular';
 
+import { ReactExampleAngular } from '@/portainer/components/ReactExample';
+
 import { sslCertificate } from './ssl-certificate';
 
-export default angular.module('portainer.settings.general', []).component('sslCertificateSettings', sslCertificate).name;
+export default angular.module('portainer.settings.general', []).component('sslCertificateSettings', sslCertificate).component('intelOpenAmtSettings', ReactExampleAngular).name;
