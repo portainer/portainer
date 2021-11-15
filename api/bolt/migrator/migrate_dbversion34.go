1c448adf6fd24e8048b375be0882d95d8dd129b6
Matt Hook <hookenz@gmail.com>
2021-11-15 17:03:40 +1300

    fix(migration) make dockerhub registry migration idempotent EE-2042

---
+++ api/bolt/migrator/migrate_dbversion34.go
@@ -0,0 +1,11 @@
+package migrator
+
+func (m *Migrator) migrateDBVersionToDB35() error {
+	// These should have been migrated already, but due to an earlier bug and a bunch of duplicates,
+	// calling it again will now fix the issue as the function has been repaired.
+	err := m.updateDockerhubToDB32()
+	if err != nil {
+		return err
+	}
+	return nil
+}
