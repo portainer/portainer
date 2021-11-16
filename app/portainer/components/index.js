1a6af5d58ff3579b676d37e3a6d0ec5ec35daa59
Chaim Lev-Ari <chiptus@users.noreply.github.com>
2021-11-16 16:11:18 +0200

    feat(app): add tooltip component [EE-2047] (#6088)

--- app/portainer/components/index.js
+++ app/portainer/components/index.js
@@ -6,7 +6,9 @@ import porAccessManagementModule from './accessManagement';
 import formComponentsModule from './form-components';
 
 import { ReactExampleAngular } from './ReactExample';
+import { TooltipAngular } from './Tooltip';
 
 export default angular
   .module('portainer.app.components', [sidebarModule, gitFormModule, porAccessManagementModule, formComponentsModule])
+  .component('portainerTooltip', TooltipAngular)
   .component('reactExample', ReactExampleAngular).name;
