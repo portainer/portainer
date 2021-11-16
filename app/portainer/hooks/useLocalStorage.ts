27f12c14f1692c5275251ad58db1c255afb423b4
Chaim Lev-Ari <chiptus@users.noreply.github.com>
2021-11-16 10:51:50 +0200

    feat(app): add axios [EE-2035](#6077)

---
+++ app/portainer/hooks/useLocalStorage.ts
@@ -0,0 +1,46 @@
+import { useState, useCallback } from 'react';
+
+const localStoragePrefix = 'portainer';
+
+function keyBuilder(key: string) {
+  return `${localStoragePrefix}.${key}`;
+}
+
+export function useLocalStorage<T>(
+  key: string,
+  defaultValue: T,
+  storage = localStorage
+): [T, (value: T) => void] {
+  const [value, setValue] = useState(get<T>(key, defaultValue, storage));
+
+  const handleChange = useCallback(
+    (value) => {
+      setValue(value);
+      set<T>(key, value, storage);
+    },
+    [key, storage]
+  );
+
+  return [value, handleChange];
+}
+
+export function get<T>(
+  key: string,
+  defaultValue: T,
+  storage = localStorage
+): T {
+  const value = storage.getItem(keyBuilder(key));
+  if (!value) {
+    return defaultValue;
+  }
+
+  try {
+    return JSON.parse(value);
+  } catch (e) {
+    return defaultValue;
+  }
+}
+
+export function set<T>(key: string, value: T, storage = localStorage) {
+  storage.setItem(keyBuilder(key), JSON.stringify(value));
+}
