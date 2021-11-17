0ee403c1b2f00e2cba27047e2d84d76bbc2c225a
Marcelo Rydel <marcelorydel26@gmail.com>
2021-11-17 11:32:57 -0700

    feat(app): added Input components [EE-2007] (#6028)

---
+++ app/portainer/components/form-components/FormControl/FormControl.tsx
@@ -0,0 +1,41 @@
+import { PropsWithChildren, ReactNode } from 'react';
+
+import { Tooltip } from '@/portainer/components/Tooltip';
+
+export interface Props {
+  inputId: string;
+  label: string | ReactNode;
+  tooltip?: string;
+  children: ReactNode;
+  errors?: string | ReactNode;
+}
+
+export function FormControl({
+  inputId,
+  label,
+  tooltip = '',
+  children,
+  errors,
+}: PropsWithChildren<Props>) {
+  return (
+    <div>
+      <div className="form-group">
+        <label
+          htmlFor={inputId}
+          className="col-sm-3 col-lg-2 control-label text-left"
+        >
+          {label}
+          {tooltip && <Tooltip message={tooltip} />}
+        </label>
+
+        <div className="col-sm-9 col-lg-10">{children}</div>
+      </div>
+
+      {errors && (
+        <div className="form-group col-md-12">
+          <div className="small text-warning">{errors}</div>
+        </div>
+      )}
+    </div>
+  );
+}
---
+++ app/portainer/components/form-components/Input/TextInput.tsx
@@ -0,0 +1,34 @@
+import clsx from 'clsx';
+import { HTMLInputTypeAttribute } from 'react';
+
+import { BaseInput } from './BaseInput';
+import { ChangeProps, InputProps } from './types';
+
+interface TextInputProps extends InputProps, ChangeProps<string> {
+  type?: HTMLInputTypeAttribute;
+  readonly?: boolean;
+}
+
+export function TextInput({
+  id,
+  type = 'text',
+  value,
+  className,
+  onChange,
+  disabled,
+  readonly,
+  required,
+}: TextInputProps) {
+  return (
+    <BaseInput
+      id={id}
+      type={type}
+      className={clsx(className, 'form-control')}
+      value={value}
+      onChange={onChange}
+      disabled={disabled}
+      readonly={readonly}
+      required={required}
+    />
+  );
+}
