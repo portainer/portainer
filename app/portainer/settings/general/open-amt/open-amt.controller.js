0368f6f678a8ff8a1f29de0d7d864475b604d8d2
cheloRydel <marcelorydel26@gmail.com>
2021-11-17 10:31:10 -0300

    feat(openamt): properly read .pfx cert file

--- app/portainer/settings/general/open-amt/open-amt.controller.js
+++ app/portainer/settings/general/open-amt/open-amt.controller.js
@@ -41,11 +41,31 @@ class OpenAmtController {
     return Object.entries(this.originalValues).some(([key, value]) => value !== this.formValues[key]);
   }
 
+  async readFile() {
+    return new Promise((resolve, reject) => {
+      const file = this.formValues.certFile;
+      if (file) {
+        const fileReader = new FileReader();
+        fileReader.fileName = file.name;
+        fileReader.onload = (e) => {
+          const base64 = e.target.result;
+          const index = base64.indexOf('base64,');
+          const cert = base64.substring(index + 7, base64.length);
+          resolve(cert);
+        };
+        fileReader.onerror = () => {
+          reject(new Error('error reading provisioning certificate file'));
+        };
+        fileReader.readAsDataURL(file);
+      }
+    });
+  }
+
   async save() {
     return this.$async(async () => {
       this.state.actionInProgress = true;
       try {
-        this.formValues.certFileText = this.formValues.certFile ? await this.formValues.certFile.text() : null;
+        this.formValues.certFileText = this.formValues.certFile ? await this.readFile(this.formValues.certFile) : null;
         await this.OpenAMTService.submit(this.formValues);
 
         await new Promise((resolve) => setTimeout(resolve, 2000));
