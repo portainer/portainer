5f50580632edee338d003425437866e359a8a3c4
Chaim Lev-Ari <chiptus@gmail.com>
2021-11-17 15:54:25 +0200

    feat(feature): add be-indicator in react [EE-2005]

--- app/portainer/components/index.js
+++ app/portainer/components/index.js
@@ -8,8 +8,10 @@ import widgetModule from './widget';
 
 import { ReactExampleAngular } from './ReactExample';
 import { TooltipAngular } from './Tooltip';
+import { beFeatureIndicatorAngular } from './BEFeatureIndicator';
 
 export default angular
   .module('portainer.app.components', [widgetModule, sidebarModule, gitFormModule, porAccessManagementModule, formComponentsModule])
   .component('portainerTooltip', TooltipAngular)
+  .component('beFeatureIndicator', beFeatureIndicatorAngular)
   .component('reactExample', ReactExampleAngular).name;
