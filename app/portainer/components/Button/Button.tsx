cc36777080b3139b7327cf3730eafad2b3586e51
zees-dev <dev.786zshan@gmail.com>
2021-11-22 08:22:23 +1300

    Button component updated to be more dynamic

--- app/portainer/components/Button/Button.tsx
+++ app/portainer/components/Button/Button.tsx
@@ -3,12 +3,14 @@ import clsx from 'clsx';
 
 type Type = 'submit' | 'reset' | 'button';
 type Color = 'default' | 'primary' | 'success' | 'warning' | 'danger' | 'link';
-type Size = 'xsmall' | 'small' | 'large';
+type Size = 'xsmall' | 'small' | 'medium' | 'large';
 export interface Props {
   type?: Type;
   color?: Color;
   size?: Size;
   disabled?: boolean;
+  title?: string;
+  className?: string;
   onClick: () => void;
 }
 
@@ -17,7 +19,9 @@ export function Button({
   color = 'primary',
   size = 'small',
   disabled = false,
+  className,
   onClick,
+  title,
   children,
 }: PropsWithChildren<Props>) {
   return (
@@ -25,8 +29,9 @@ export function Button({
       /* eslint-disable-next-line react/button-has-type */
       type={type}
       disabled={disabled}
-      className={clsx('btn', `btn-${color}`, sizeClass(size))}
+      className={clsx('btn', `btn-${color}`, sizeClass(size), className)}
       onClick={onClick}
+      title={title}
     >
       {children}
     </button>
@@ -37,6 +42,8 @@ function sizeClass(size?: Size) {
   switch (size) {
     case 'large':
       return 'btn-lg';
+    case 'medium':
+      return 'btn-md';
     case 'xsmall':
       return 'btn-xs';
     default:
