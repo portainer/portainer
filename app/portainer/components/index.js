202703e73e4570aba833aba727cf54de02e7d2da
cheloRydel <marcelorydel26@gmail.com>
2021-11-30 15:45:36 -0300

    feature(openamt): move component declaration

--- app/portainer/components/index.js
+++ app/portainer/components/index.js
@@ -1,6 +1,5 @@
 import angular from 'angular';
 
-import { KVMControlAngular } from "@/portainer/views/endpoints/kvm/KVMControl";
 import sidebarModule from './sidebar';
 import gitFormModule from './forms/git-form';
 import porAccessManagementModule from './accessManagement';
@@ -10,9 +9,7 @@ import widgetModule from './widget';
 import { ReactExampleAngular } from './ReactExample';
 import { TooltipAngular } from './Tooltip';
 
-
 export default angular
   .module('portainer.app.components', [widgetModule, sidebarModule, gitFormModule, porAccessManagementModule, formComponentsModule])
   .component('portainerTooltip', TooltipAngular)
-  .component('reactExample', ReactExampleAngular)
-    .component('kvmControl', KVMControlAngular).name;
+  .component('reactExample', ReactExampleAngular).name;
