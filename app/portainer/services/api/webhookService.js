ac6988dbc1c8d1d0d2078f82827901cbbd1a0f27
Simon Meng <simon.meng@portainer.io>
2021-12-02 18:05:14 +1300

    feat(webhook) EE-2125 send registryID to backend when creating a service with webhook

--- app/portainer/services/api/webhookService.js
+++ app/portainer/services/api/webhookService.js
@@ -25,8 +25,8 @@ angular.module('portainer.app').factory('WebhookService', [
       return deferred.promise;
     };
 
-    service.createServiceWebhook = function (serviceID, endpointID) {
-      return Webhooks.create({ ResourceID: serviceID, EndpointID: endpointID, WebhookType: 1 }).$promise;
+    service.createServiceWebhook = function (serviceID, endpointID, registryID) {
+      return Webhooks.create({ ResourceID: serviceID, EndpointID: endpointID, WebhookType: 1, registryID }).$promise;
     };
 
     service.deleteWebhook = function (id) {
