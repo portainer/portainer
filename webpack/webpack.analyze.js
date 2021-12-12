44518b6b85a2701e0f151d81257da051d73dd95d
Chaim Lev-Ari <chiptus@gmail.com>
2021-12-12 11:42:29 +0200

    chore(build): use single dep (lodash,moment)

--- webpack/webpack.analyze.js
+++ webpack/webpack.analyze.js
@@ -2,11 +2,11 @@ const SpeedMeasurePlugin = require('speed-measure-webpack-plugin');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const smp = new SpeedMeasurePlugin();
 const merge = require('webpack-merge');
-// const webpackDevelop = require('./webpack.develop.js');
-const prodConfig = require('./webpack.production.js');
+const webpackConfig = require('./webpack.production.js');
+// const webpackConfig = require('./webpack.develop.js');
 
 module.exports = smp.wrap(
-  merge(prodConfig, {
+  merge(webpackConfig, {
     plugins: [new BundleAnalyzerPlugin()],
   })
 );
