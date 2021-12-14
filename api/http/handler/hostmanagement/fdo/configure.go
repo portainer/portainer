cefa174426e99b56e6f4769a0c6db8de717e3eb6
andres-portainer <andres-portainer@users.noreply.github.com>
2021-12-14 10:41:23 -0300

    Fix function documentation comments.

--- api/http/handler/hostmanagement/fdo/configure.go
+++ api/http/handler/hostmanagement/fdo/configure.go
@@ -41,9 +41,7 @@ func (payload *deviceConfigurePayload) Validate(r *http.Request) error {
 // @tags intel
 // @security jwt
 // @produce json
-// @param edgekey query string true "Environment edge key"
-// @param name query string true "Environment name"
-// @param profile query string true "FDO deployment profile name"
+// @param body body deviceConfigurePayload true "Device Configuration"
 // @success 200 "Success"
 // @failure 400 "Invalid request"
 // @failure 403 "Permission denied to access settings"
