a84ec025e8732b883b7fcb62c9966511ca14e4a2
Marcelo Rydel <marcelorydel26@gmail.com>
2021-12-16 09:29:03 -0300

    feat(openamt): preload existing AMT settings (#6283)

--- app/portainer/settings/general/open-amt/open-amt.controller.js
+++ app/portainer/settings/general/open-amt/open-amt.controller.js
@@ -11,6 +11,7 @@ class OpenAmtController {
       mpsPassword: '',
       domainName: '',
       certFile: null,
+      certFileText: '',
       certPassword: '',
       useWirelessConfig: false,
       wifiAuthenticationMethod: '4',
@@ -42,27 +43,35 @@ class OpenAmtController {
   }
 
   isFormValid() {
-    return !this.formValues.enableOpenAMT || this.formValues.certFile != null;
+    if (!this.formValues.enableOpenAMT) {
+      return true;
+    }
+    return this.formValues.certFile != null || this.formValues.certFileText !== '';
   }
 
-  async readFile() {
-    return new Promise((resolve, reject) => {
-      const file = this.formValues.certFile;
+  onCertFileSelected(file) {
+    return this.$scope.$evalAsync(async () => {
       if (file) {
-        const fileReader = new FileReader();
-        fileReader.fileName = file.name;
-        fileReader.onload = (e) => {
-          const base64 = e.target.result;
-          // remove prefix of "data:application/x-pkcs12;base64," returned by "readAsDataURL()"
-          const index = base64.indexOf('base64,');
-          const cert = base64.substring(index + 7, base64.length);
-          resolve(cert);
-        };
-        fileReader.onerror = () => {
-          reject(new Error('error reading provisioning certificate file'));
-        };
-        fileReader.readAsDataURL(file);
+        this.formValues.certFileText = '';
       }
+    });
+  }
+
+  async readFile(file) {
+    return new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.fileName = file.name;
+      fileReader.onload = (e) => {
+        const base64 = e.target.result;
+        // remove prefix of "data:application/x-pkcs12;base64," returned by "readAsDataURL()"
+        const index = base64.indexOf('base64,');
+        const cert = base64.substring(index + 7, base64.length);
+        resolve(cert);
+      };
+      fileReader.onerror = () => {
+        reject(new Error('error reading provisioning certificate file'));
+      };
+      fileReader.readAsDataURL(file);
     });
   }
 
@@ -70,13 +79,21 @@ class OpenAmtController {
     return this.$async(async () => {
       this.state.actionInProgress = true;
       try {
-        this.formValues.certFileText = this.formValues.certFile ? await this.readFile(this.formValues.certFile) : null;
+        if (this.formValues.certFile) {
+          this.formValues.certFileText = await this.readFile(this.formValues.certFile);
+        }
         await this.OpenAMTService.submit(this.formValues);
 
         await new Promise((resolve) => setTimeout(resolve, 2000));
         this.Notifications.success(`OpenAMT successfully ${this.formValues.enableOpenAMT ? 'enabled' : 'disabled'}`);
+        this.originalValues = {
+          ...this.formValues,
+        };
       } catch (err) {
         this.Notifications.error('Failure', err, 'Failed applying changes');
+        if (this.originalValues.certFileText === '') {
+          this.formValues.certFileText = '';
+        }
       }
       this.state.actionInProgress = false;
     });
@@ -94,7 +111,10 @@ class OpenAmtController {
             enableOpenAMT: config.Enabled,
             mpsServer: config.MPSServer,
             mpsUser: config.Credentials.MPSUser,
+            mpsPassword: config.Credentials.MPSPassword,
             domainName: config.DomainConfiguration.DomainName,
+            certPassword: config.DomainConfiguration.CertPassword,
+            certFileText: config.DomainConfiguration.CertFileText,
           };
 
           if (config.WirelessConfiguration) {
