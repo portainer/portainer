67752cfb2a1b4d2d6adb1cfcc9d6fc68a533b94d
Chaim Lev-Ari <chiptus@gmail.com>
2021-12-23 14:39:42 +0200

    refactor(app): create InformationPanel

--- app/portainer/components/information-panel/information-panel.js
+++
@@ -1,8 +0,0 @@
-angular.module('portainer.app').component('informationPanel', {
-  templateUrl: './informationPanel.html',
-  bindings: {
-    titleText: '@',
-    dismissAction: '&?',
-  },
-  transclude: true,
-});
--- app/portainer/home/MotdPanel.tsx
+++ app/portainer/home/MotdPanel.tsx
@@ -3,8 +3,7 @@ import _ from 'lodash-es';
 
 import { useUIState } from '@/common/hooks/UIStateProvider';
 
-import { Button } from '../components/Button';
-import { Widget, WidgetBody } from '../components/widget';
+import { InformationPanel } from '../components/InformationPanel/InformationPanel';
 
 import { getMotd } from './home.service';
 
@@ -18,31 +17,20 @@ export function MotdPanel() {
   }
 
   return (
-    <div className="row">
-      <div className="col-sm-12">
-        <Widget>
-          <WidgetBody className="motd-body">
-            {!!motd.Style && <style>{motd.Style}</style>}
-            <div style={camelCaseKeys(motd.ContentLayout)}>
-              <div className="col-sm-12 form-section-title">
-                <span style={{ float: 'left' }}>{motd.Title}</span>
-                <span className="small" style={{ float: 'right' }}>
-                  <Button color="link" onClick={() => onDismiss(motd.Hash)}>
-                    <i className="fa fa-times" /> dismiss
-                  </Button>
-                </span>
-              </div>
-              <div className="form-group">
-                <span className="text-muted">
-                  {/* eslint-disable-next-line react/no-danger */}
-                  <p dangerouslySetInnerHTML={{ __html: motd.Message }} />
-                </span>
-              </div>
-            </div>
-          </WidgetBody>
-        </Widget>
-      </div>
-    </div>
+    <>
+      {!!motd.Style && <style>{motd.Style}</style>}
+      <InformationPanel
+        onDismiss={() => onDismiss(motd.Hash)}
+        title={motd.Title}
+        wrapperStyle={camelCaseKeys(motd.ContentLayout)}
+        bodyClassName="motd-body"
+      >
+        <span className="text-muted">
+          {/* eslint-disable-next-line react/no-danger */}
+          <p dangerouslySetInnerHTML={{ __html: motd.Message }} />
+        </span>
+      </InformationPanel>
+    </>
   );
 
   function onDismiss(hash: string) {
