7911e05a258c1709a2f47d8ab10aaf05bf72375e
Chaim Lev-Ari <chiptus@gmail.com>
2021-12-28 16:22:55 +0200

    refactor(app): parse axios error and loading bar

--- app/global.d.ts
+++ app/global.d.ts
@@ -8,3 +8,13 @@ declare module '*.png' {
 declare module '*.css';
 
 declare module '@open-amt-cloud-toolkit/ui-toolkit-react/reactjs/src/kvm.bundle';
+
+declare module 'axios-progress-bar' {
+  import { AxiosInstance } from 'axios';
+  import { NProgressOptions } from 'nprogress';
+
+  export function loadProgressBar(
+    config?: Partial<NProgressOptions>,
+    instance?: AxiosInstance
+  ): void;
+}
