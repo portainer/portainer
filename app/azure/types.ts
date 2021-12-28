ea785d01a7dd7f0574084f96b0a62ea4ca771994
Chaim Lev-Ari <chiptus@gmail.com>
2021-12-28 17:15:20 +0200

    feat(azure): create instance

--- app/azure/types.ts
+++ app/azure/types.ts
@@ -1,3 +1,22 @@
+import { PortMapping } from './ContainerInstances/PortsMappingField';
+
+export interface ContainerInstanceFormValues {
+  name: string;
+  location: string;
+  subscription: string;
+  resourceGroup: string;
+  image: string;
+  os: string;
+  memory: number;
+  cpu: number;
+  ports: PortMapping[];
+  allocatePublicIP: boolean;
+}
+
+export interface ContainerGroup {
+  name: string;
+}
+
 export interface Subscription {
   subscriptionId: string;
   displayName: string;
