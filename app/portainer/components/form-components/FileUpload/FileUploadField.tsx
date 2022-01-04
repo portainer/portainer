5d6e14f5aa68ad4f31421b9aa4730a28cc056803
cheloRydel <marcelorydel26@gmail.com>
2022-01-04 09:33:02 -0300

    feat(react): remove useState, peer review

--- app/portainer/components/form-components/FileUpload/FileUploadField.tsx
+++ app/portainer/components/form-components/FileUpload/FileUploadField.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, createRef, useState } from 'react';
+import { ChangeEvent, createRef } from 'react';
 
 import { r2a } from '@/react-tools/react2angular';
 import { Button } from '@/portainer/components/Button';
@@ -7,32 +7,19 @@ import styles from './FileUploadField.module.css';
 
 export interface Props {
   onChange(value: unknown): void;
+    value?: File;
   title?: string;
-  file?: File;
   required?: boolean;
 }
 
 export function FileUploadField({
-  required = false,
-  title = 'Select a file',
   onChange,
-  file,
+  value,
+  title = 'Select a file',
+  required = false,
 }: Props) {
-  const fileRef = createRef<HTMLInputElement>();
-  function handleButtonClick() {
-    if (fileRef && fileRef.current) {
-      fileRef.current.click();
-    }
-  }
 
-  const [selectedFile, setSelectedFile] = useState(file);
-  function changeHandler(event: ChangeEvent<HTMLInputElement>) {
-    if (event.target && event.target.files && event.target.files.length > 0) {
-      const file = event.target.files[0];
-      setSelectedFile(file);
-      onChange(file);
-    }
-  }
+  const fileRef = createRef<HTMLInputElement>();
 
   return (
     <div className="file-upload-field">
@@ -44,27 +31,39 @@ export function FileUploadField({
         onChange={changeHandler}
       />
       <Button
-        type="button"
-        className="btn btn-sm btn-primary"
+          size="small"
+          color="primary"
         onClick={handleButtonClick}
       >
         {title}
       </Button>
 
       <span className="space-left">
-        {selectedFile ? (
-          selectedFile.name
+        {value ? (
+            value.name
         ) : (
           <i className="fa fa-times red-icon" aria-hidden="true" />
         )}
       </span>
     </div>
   );
+
+  function handleButtonClick() {
+    if (fileRef && fileRef.current) {
+      fileRef.current.click();
+    }
+  }
+
+    function changeHandler(event: ChangeEvent<HTMLInputElement>) {
+        if (event.target && event.target.files && event.target.files.length > 0) {
+            onChange(event.target.files[0]);
+        }
+    }
 }
 
 export const FileUploadFieldAngular = r2a(FileUploadField, [
-  'required',
-  'title',
   'onChange',
-  'file',
+  'value',
+  'title',
+  'required',
 ]);
