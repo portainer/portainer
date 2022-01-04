b3c484bb1537dc60ec993be410ab947545634f6c
cheloRydel <marcelorydel26@gmail.com>
2022-01-04 10:37:07 -0300

    feat(react): add FileUploadField and FileUploadForm tests

---
+++ app/portainer/components/form-components/FileUpload/FileUploadField.test.tsx
@@ -0,0 +1,25 @@
+import { fireEvent, render } from '@/react-tools/test-utils';
+
+import {FileUploadField} from "./FileUploadField";
+
+test('render should make the file button clickable and fie onChange event', async () => {
+
+    const onClick = jest.fn();
+    const { findByText } = render(
+        <FileUploadField title="test button" onChange={onClick} />
+    );
+
+    const button = await findByText('test button');
+    expect(button).toBeVisible();
+
+    const input = button.previousElementSibling;
+    expect(input).not.toBeNull();
+
+    const mockFile = new File([], 'file.txt');
+    if (input) {
+        fireEvent.change(input, {
+            target: {files: [mockFile]},
+        });
+    }
+    expect(onClick).toHaveBeenCalledTimes(1);
+});
