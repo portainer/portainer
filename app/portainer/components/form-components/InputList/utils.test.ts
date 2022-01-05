43d2e2e1657459f3dc94f7022c3a8f2dcb8ef3d1
cheloRydel <marcelorydel26@gmail.com>
2022-01-05 13:08:01 -0300

    fmt

--- app/portainer/components/form-components/InputList/utils.test.ts
+++ app/portainer/components/form-components/InputList/utils.test.ts
@@ -2,18 +2,7 @@ import { arrayMove } from './utils';
 
 it('moves items in an array', () => {
   expect(arrayMove(['a', 'b', 'c'], 2, 0)).toEqual(['c', 'a', 'b']);
-  expect(
-    arrayMove(
-      [
-        { name: 'Fred' },
-        { name: 'Barney' },
-        { name: 'Wilma' },
-        { name: 'Betty' },
-      ],
-      2,
-      1
-    )
-  ).toEqual([
+    expect(arrayMove([{ name: 'Fred' }, { name: 'Barney' }, { name: 'Wilma' }, { name: 'Betty' }], 2, 1)).toEqual([
     { name: 'Fred' },
     { name: 'Wilma' },
     { name: 'Barney' },
