5e3e1fa17cb8531e0e034c81d20b8e3a96afbc75
cheloRydel <marcelorydel26@gmail.com>
2022-01-05 13:06:48 -0300

    fmt

--- app/portainer/views/account/CreateAccessToken/index.ts
+++ app/portainer/views/account/CreateAccessToken/index.ts
@@ -2,10 +2,5 @@ import { react2angular } from '@/react-tools/react2angular';
 
 import { CreateAccessToken } from './CreateAccessToken';
 
-const CreateAccessTokenAngular = react2angular(CreateAccessToken, [
-  'userId',
-  'onSubmit',
-  'onSuccess',
-  'onError',
-]);
+const CreateAccessTokenAngular = react2angular(CreateAccessToken, ['userId', 'onSubmit', 'onSuccess', 'onError']);
 export { CreateAccessToken, CreateAccessTokenAngular };
