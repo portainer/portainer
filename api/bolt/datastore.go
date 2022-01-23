b5f6cb49f5f801ee8f37f85177a56e7ffe488997
chaogeng77977 <chao.geng@portainer.io>
2022-01-23 21:24:07 +0800

    EE-2167 remove pre-commit

--- api/bolt/datastore.go
+++ api/bolt/datastore.go
@@ -104,13 +104,11 @@ func simpleCopyFile(src, dst string) (int64, error) {
 		return 0, err
 	}
 	defer source.Close()
-
 	destination, err := os.Create(dst)
 	if err != nil {
 		return 0, err
 	}
 	defer destination.Close()
-
 	nBytes, err := io.Copy(destination, source)
 	return nBytes, err
 }
