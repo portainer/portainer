5aeaafd2233dec2b21fbc511095eadc84361b757
Matt Hook <hookenz@gmail.com>
2022-01-27 09:04:09 +1300

    fix(restore): restore the database file correctly EE-2435 (#6448)
    * prevent restoring db to the wrong name or potentially having two dbs
    
    * fix for case where we started encrypted and restore unencrypted.  We don't want to have two databases

--- api/backup/restore.go
+++ api/backup/restore.go
@@ -10,6 +10,7 @@ import (
 	"github.com/pkg/errors"
 	"github.com/portainer/portainer/api/archive"
 	"github.com/portainer/portainer/api/crypto"
+	"github.com/portainer/portainer/api/database/boltdb"
 	"github.com/portainer/portainer/api/dataservices"
 	"github.com/portainer/portainer/api/filesystem"
 	"github.com/portainer/portainer/api/http/offlinegate"
@@ -42,8 +43,7 @@ func RestoreArchive(archive io.Reader, password string, filestorePath string, ga
 		return errors.Wrap(err, "Failed to stop db")
 	}
 
-	dbFile := datastore.Connection().GetDatabaseFileName()
-	if err = restoreFiles(restorePath, filestorePath, dbFile); err != nil {
+	if err = restoreFiles(restorePath, filestorePath); err != nil {
 		return errors.Wrap(err, "failed to restore the system state")
 	}
 
@@ -59,7 +59,7 @@ func extractArchive(r io.Reader, destinationDirPath string) error {
 	return archive.ExtractTarGz(r, destinationDirPath)
 }
 
-func restoreFiles(srcDir string, destinationDir string, databaseFile string) error {
+func restoreFiles(srcDir string, destinationDir string) error {
 	for _, filename := range filesToRestore {
 		err := filesystem.CopyPath(filepath.Join(srcDir, filename), destinationDir)
 		if err != nil {
@@ -67,5 +67,19 @@ func restoreFiles(srcDir string, destinationDir string, databaseFile string) err
 		}
 	}
 
-	return filesystem.CopyPath(filepath.Join(srcDir, databaseFile), destinationDir)
+	// TODO:  This is very boltdb module specific once again due to the filename.  Move to bolt module? Refactor for another day
+
+	// Prevent the possibility of having both databases.  Remove any default new instance
+	os.Remove(filepath.Join(destinationDir, boltdb.DatabaseFileName))
+	os.Remove(filepath.Join(destinationDir, boltdb.EncryptedDatabaseFileName))
+
+	// Now copy the database.  It'll be either portainer.db or portainer.edb
+
+	// Note: CopyPath does not return an error if the source file doesn't exist
+	err := filesystem.CopyPath(filepath.Join(srcDir, boltdb.EncryptedDatabaseFileName), destinationDir)
+	if err != nil {
+		return err
+	}
+
+	return filesystem.CopyPath(filepath.Join(srcDir, boltdb.DatabaseFileName), destinationDir)
 }
