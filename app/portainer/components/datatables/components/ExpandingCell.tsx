1a800d1c4c4de358746c089724c9d39f4212e877
cheloRydel <marcelorydel26@gmail.com>
2022-02-07 10:34:51 -0300

    wrapp expand arrow in button

--- app/portainer/components/datatables/components/ExpandingCell.tsx
+++ app/portainer/components/datatables/components/ExpandingCell.tsx
@@ -1,6 +1,8 @@
 import { PropsWithChildren } from 'react';
 import { Row } from 'react-table';
 
+import styles from './ExpandingCell.module.css';
+
 interface Props<D extends Record<string, unknown> = Record<string, unknown>> {
   row: Row<D>;
   showExpandArrow: boolean;
@@ -12,12 +14,14 @@ export function ExpandingCell<
   return (
     <>
       {showExpandArrow && (
-        <i
-          // eslint-disable-next-line react/jsx-props-no-spreading
-          {...row.getToggleRowExpandedProps()}
-          className={`fas ${arrowClass(row.isExpanded)} space-right`}
-          aria-hidden="true"
-        />
+        <button type="button" className={styles.expandButton}>
+          <i
+            // eslint-disable-next-line react/jsx-props-no-spreading
+            {...row.getToggleRowExpandedProps()}
+            className={`fas ${arrowClass(row.isExpanded)} space-right`}
+            aria-hidden="true"
+          />
+        </button>
       )}
       {children}
     </>
