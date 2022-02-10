34f4543b5419ae0e05cfc02f5176df7373f12c8f
cheloRydel <marcelorydel26@gmail.com>
2022-02-10 09:40:33 -0300

    remove unnecessary div

--- app/portainer/views/endpoints/kvm/KVMControl.tsx
+++ app/portainer/views/endpoints/kvm/KVMControl.tsx
@@ -14,16 +14,14 @@ export function KVMControl({ deviceId, server, token }: KVMControlProps) {
   if (!deviceId || !server || !token) return <div>Loading...</div>;
 
   return (
-    <div>
-      <KVM
-        deviceId={deviceId}
-        mpsServer={`https://${server}/mps/ws/relay`}
-        authToken={token}
-        mouseDebounceTime="200"
-        canvasHeight="100%"
-        canvasWidth="100%"
-      />
-    </div>
+    <KVM
+      deviceId={deviceId}
+      mpsServer={`https://${server}/mps/ws/relay`}
+      authToken={token}
+      mouseDebounceTime="200"
+      canvasHeight="100%"
+      canvasWidth="100%"
+    />
   );
 }
 
