b065b52940def57fa49b7c0bf6ed0ea548076b5f
Prabhat Khera <prabhat.khera@portainer.io>
2022-03-01 09:52:07 +1300

    DB upgrade failes if bucket does not exists

--- api/database/boltdb/db.go
+++ api/database/boltdb/db.go
@@ -407,6 +407,9 @@ func (connection *DbConnection) RestoreMetadata(s map[string]interface{}) error 
 
 		err = connection.Batch(func(tx *bolt.Tx) error {
 			bucket := tx.Bucket([]byte(bucketName))
+			if bucket == nil {
+				return nil
+			}
 			return bucket.SetSequence(uint64(id))
 		})
 	}
