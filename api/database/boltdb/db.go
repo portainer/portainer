eb6cdf1229adaded4437321f9520d3d2e198c132
Prabhat Khera <91852476+prabhat-org@users.noreply.github.com>
2022-03-03 09:10:26 +1300

    created bucket if not exists during restore sequence (#6614)

--- api/database/boltdb/db.go
+++ api/database/boltdb/db.go
@@ -419,9 +419,9 @@ func (connection *DbConnection) RestoreMetadata(s map[string]interface{}) error 
 		}
 
 		err = connection.Batch(func(tx *bolt.Tx) error {
-			bucket := tx.Bucket([]byte(bucketName))
-			if bucket == nil {
-				return nil
+			bucket, err := tx.CreateBucketIfNotExists([]byte(bucketName))
+			if err != nil {
+				return err
 			}
 			return bucket.SetSequence(uint64(id))
 		})
