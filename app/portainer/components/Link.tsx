2fd23c313cd7a27e7b0a0c192ee76a772e085189
Chaim Lev-Ari <chiptus@gmail.com>
2022-03-08 09:01:44 +0200

    refactor(home): wrap element with button

--- app/portainer/components/Link.tsx
+++ app/portainer/components/Link.tsx
@@ -1,32 +1,20 @@
-import { MouseEventHandler, PropsWithChildren } from 'react';
+import { PropsWithChildren } from 'react';
 import { UISref, UISrefProps } from '@uirouter/react';
 
 interface Props {
   title?: string;
-  onClick?: MouseEventHandler<HTMLAnchorElement>;
 }
 
 export function Link({
   title = '',
   children,
-  onClick,
   ...props
 }: PropsWithChildren<Props> & UISrefProps) {
   return (
     // eslint-disable-next-line react/jsx-props-no-spreading
     <UISref {...props}>
-      {/* eslint-disable-next-line jsx-a11y/anchor-is-valid,jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions */}
-      <a
-        title={title}
-        onClick={(e) => {
-          e.stopPropagation();
-          if (onClick) {
-            onClick(e);
-          }
-        }}
-      >
-        {children}
-      </a>
+      {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+      <a title={title}>{children}</a>
     </UISref>
   );
 }
