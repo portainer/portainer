07294c19bb292b997f76827346096033c0164b2b
Chao Geng <93526589+chaogeng77977@users.noreply.github.com>
2022-03-16 08:32:12 +0800

    fix(k8s/application): check name unique in k8s cluster (#6610)
    * EE-2353 Check unique name when creating new deployment in kubernetes
    
    * EE-2353 fix warning from gofmt
    
    * EE-2353 add miss methon in kubernetes_mock.go
    
    * EE-2353 add missing space
    
    * EE-2353 Use kubernetes cli to instead exec.command
    
    * EE-2353 remove useless parameter
    
    * EE-2353 remove unnecessary log in handle
    
    * EE-2353 fix gofmt warning
    
    * EE-2353 use ListOptions to filter the list
    
    * EE-2353 add function description
    
    * EE-2353 fix error
    
    * Update api/kubernetes/cli/deploment.go
    
    Co-authored-by: Chaim Lev-Ari <chiptus@users.noreply.github.com>
    
    * EE-2353 change function name to HasStackName
    
    Co-authored-by: Chaim Lev-Ari <chiptus@users.noreply.github.com>

--- api/portainer.go
+++ api/portainer.go
@@ -1262,6 +1262,7 @@ type (
 		GetServiceAccountBearerToken(userID int) (string, error)
 		CreateUserShellPod(ctx context.Context, serviceAccountName, shellPodImage string) (*KubernetesShellPod, error)
 		StartExecProcess(token string, useAdminToken bool, namespace, podName, containerName string, command []string, stdin io.Reader, stdout io.Writer, errChan chan error)
+		HasStackName(namespace string, stackName string) (bool, error)
 		NamespaceAccessPoliciesDeleteNamespace(namespace string) error
 		GetNodesLimits() (K8sNodesLimits, error)
 		GetNamespaceAccessPolicies() (map[string]K8sNamespaceAccessPolicy, error)
