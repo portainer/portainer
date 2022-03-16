ead2c5e27c9a1c79a59664b6a648732a8f3b8d6c
Prabhat Khera <prabhat.khera@portainer.io>
2022-03-16 15:00:44 +1300

    added validation method for version 32 and 33

--- api/datastore/migrator/migrate_dbversion32.go
+++ api/datastore/migrator/migrate_dbversion32.go
@@ -1,13 +1,17 @@
 package migrator
 
-import portainer "github.com/portainer/portainer/api"
+import (
+	"fmt"
+
+	portainer "github.com/portainer/portainer/api"
+)
 
 func (m *Migrator) migrateDBVersionToDB33() error {
 	if err := m.migrateSettingsToDB33(); err != nil {
 		return err
 	}
 
-	return nil
+	return m.validateSettingsToDB33()
 }
 
 func (m *Migrator) migrateSettingsToDB33() error {
@@ -19,4 +23,17 @@ func (m *Migrator) migrateSettingsToDB33() error {
 	migrateLog.Info("Setting default kubectl shell image")
 	settings.KubectlShellImage = portainer.DefaultKubectlShellImage
 	return m.settingsService.UpdateSettings(settings)
+}
+
+func (m *Migrator) validateSettingsToDB33() error {
+	settings, err := m.settingsService.Settings()
+	if err != nil {
+		return err
+	}
+
+	if settings.KubectlShellImage != portainer.DefaultKubectlShellImage {
+		return fmt.Errorf("settings KubectlShellImage is not same as portainer.DefaultKubectlShellImage")
+	}
+
+	return nil
 }
--- api/datastore/migrator/migrate_dbversion33.go
+++ api/datastore/migrator/migrate_dbversion33.go
@@ -1,6 +1,10 @@
 package migrator
 
-import "github.com/portainer/portainer/api/dataservices"
+import (
+	"fmt"
+
+	"github.com/portainer/portainer/api/dataservices"
+)
 
 func (m *Migrator) migrateDBVersionToDB34() error {
 	migrateLog.Info("Migrating stacks")
@@ -9,7 +13,7 @@ func (m *Migrator) migrateDBVersionToDB34() error {
 		return err
 	}
 
-	return nil
+	return validateStackEntryPoint(m.stackService)
 }
 
 // MigrateStackEntryPoint exported for testing (blah.)
@@ -26,6 +30,24 @@ func MigrateStackEntryPoint(stackService dataservices.StackService) error {
 		stack.GitConfig.ConfigFilePath = stack.EntryPoint
 		if err := stackService.UpdateStack(stack.ID, stack); err != nil {
 			return err
+		}
+	}
+	return nil
+}
+
+// validateStackEntryPoint validates if MigrateStackEntryPoint was successful
+func validateStackEntryPoint(stackService dataservices.StackService) error {
+	stacks, err := stackService.Stacks()
+	if err != nil {
+		return err
+	}
+	for i := range stacks {
+		stack := &stacks[i]
+		if stack.GitConfig == nil {
+			continue
+		}
+		if stack.GitConfig.ConfigFilePath == "" {
+			return fmt.Errorf("GitConfig.ConfigFilePath is empty for stack - name=%s, ID=%d", stack.Name, stack.ID)
 		}
 	}
 	return nil
