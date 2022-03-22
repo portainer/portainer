f37de64c20db4ff4fb083922f34590c52932715e
sunportainer <eric.sun@portainer.io>
2022-03-22 10:17:11 +0800

    update code according to review comments

--- app/portainer/views/custom-templates/custom-templates-view/customTemplatesViewController.js
+++ app/portainer/views/custom-templates/custom-templates-view/customTemplatesViewController.js
@@ -215,7 +215,7 @@ class CustomTemplatesViewController {
     try {
       var template = _.find(this.templates, { Id: templateId });
       await this.CustomTemplateService.remove(templateId);
-      this.Notifications.success('Template successfully deleted', template.Title);
+      this.Notifications.success('Template successfully deleted', template && template.Title);
       _.remove(this.templates, { Id: templateId });
     } catch (err) {
       this.Notifications.error('Failure', err, 'Failed to delete template');
