fc25cf5a72b0c09b88a0413c2c8c43dd2bf4281c
Felix Han <felix.han@portainer.io>
2022-04-03 08:54:25 +1200

    fix(container): debugging in Windows.

--- app/docker/views/containers/create/createContainerController.js
+++ app/docker/views/containers/create/createContainerController.js
@@ -701,7 +701,7 @@ angular.module('portainer.docker').controller('CreateContainerController', [
         .then(function success(networks) {
           networks.push({ Name: 'container' });
           $scope.availableNetworks = networks.sort((a, b) => a.Name.localeCompare(b.Name));
-
+          console.log($scope.availableNetworks);
           if (_.find(networks, { Name: 'nat' })) {
             $scope.config.HostConfig.NetworkMode = 'nat';
           }
