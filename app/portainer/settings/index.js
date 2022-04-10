9d51422321d613c9c20319c404605de1929ff7d3
Chaim Lev-Ari <chiptus@gmail.com>
2022-04-10 19:00:33 +0300

    refactor(endpoints): use edge script form in endpoints edit

--- app/portainer/settings/index.js
+++ app/portainer/settings/index.js
@@ -6,9 +6,11 @@ import generalModule from './general';
 import { SettingsFDOAngular } from './edge-compute/SettingsFDO';
 import { SettingsOpenAMTAngular } from './edge-compute/SettingsOpenAMT';
 import { EdgeComputeSettingsViewAngular } from './edge-compute/EdgeComputeSettingsView';
+import { EdgeScriptFormAngular } from './edge-compute/AutomaticEdgeEnvCreation/EdgeScriptForm';
 
 export default angular
   .module('portainer.settings', [authenticationModule, generalModule])
   .component('settingsEdgeCompute', EdgeComputeSettingsViewAngular)
   .component('settingsFdo', SettingsFDOAngular)
-  .component('settingsOpenAmt', SettingsOpenAMTAngular).name;
+  .component('settingsOpenAmt', SettingsOpenAMTAngular)
+  .component('edgeScriptForm', EdgeScriptFormAngular).name;
