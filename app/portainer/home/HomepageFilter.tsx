5ef25ca38ed042fd513257229a2c8db46f401d1c
Prabhat Khera <prabhat.khera@portainer.io>
2022-04-21 15:40:34 +1200

    fix home page filters

--- app/portainer/home/HomepageFilter.tsx
+++ app/portainer/home/HomepageFilter.tsx
@@ -1,7 +1,12 @@
 import { components, OptionProps } from 'react-select';
 
+import { useLocalStorage } from '@/portainer/hooks/useLocalStorage';
 import { Select } from '@/portainer/components/form-components/ReactSelect';
 import { Filter } from '@/portainer/home/types';
+import {
+  EnvironmentType,
+  EnvironmentStatus,
+} from '@/portainer/environments/types';
 
 interface Props {
   filterOptions: Filter[];
@@ -41,4 +46,131 @@ export function HomepageFilter({
       onChange={(option) => onChange(option as Filter[])}
     />
   );
+}
+
+export function useHomePageStatusTypeState(
+  key: string
+): [
+  EnvironmentStatus[] | undefined,
+  (value: EnvironmentStatus[] | undefined) => void
+] {
+  const filterKey = keyBuilder(key);
+  const [value, setValue] = useLocalStorage(filterKey, '[]', sessionStorage);
+
+  const v = JSON.parse(value);
+  // eslint-disable-next-line func-style
+  const setValueParsed = (value: EnvironmentStatus[] | undefined) => {
+    setValue(JSON.stringify(value));
+  };
+
+  return [v, setValueParsed];
+
+  function keyBuilder(key: string) {
+    return `datatable_home_filter_${key}`;
+  }
+}
+
+export function useHomePageNumberTypeState(
+  key: string,
+  defaultValue: number[]
+): [number[] | undefined, (value: number[] | undefined) => void] {
+  const filterKey = keyBuilder(key);
+  const [value, setValue] = useLocalStorage(
+    filterKey,
+    JSON.stringify(defaultValue),
+    sessionStorage
+  );
+
+  const v = JSON.parse(value);
+  // eslint-disable-next-line func-style
+  const setValueParsed = (value: number[] | undefined) => {
+    setValue(JSON.stringify(value));
+  };
+
+  return [v, setValueParsed];
+
+  function keyBuilder(key: string) {
+    return `datatable_home_filter_${key}`;
+  }
+}
+
+export function useHomePageFilterState(
+  key: string
+): [
+  EnvironmentType[] | EnvironmentStatus[] | undefined,
+  (value: EnvironmentType[] | EnvironmentStatus[] | undefined) => void
+] {
+  const filterKey = keyBuilder(key);
+  const [value, setValue] = useLocalStorage(filterKey, '[]', sessionStorage);
+
+  const v = JSON.parse(value);
+  // eslint-disable-next-line func-style
+  const setValueParsed = (
+    value: EnvironmentType[] | EnvironmentStatus[] | undefined
+  ) => {
+    setValue(JSON.stringify(value));
+  };
+
+  return [v, setValueParsed];
+
+  function keyBuilder(key: string) {
+    return `datatable_home_filter_${key}`;
+  }
+}
+
+export function useHomePageFilterBoolState(
+  key: string
+): [boolean, (value: boolean) => void] {
+  const filterKey = keyBuilder(key);
+  const [value, setValue] = useLocalStorage(filterKey, 'false', sessionStorage);
+
+  const v = JSON.parse(value);
+  // eslint-disable-next-line func-style
+  const setValueParsed = (value: boolean) => {
+    setValue(JSON.stringify(value));
+  };
+
+  return [v, setValueParsed];
+
+  function keyBuilder(key: string) {
+    return `datatable_home_filter_${key}`;
+  }
+}
+
+export function useHomePageFilterTypeState(
+  key: string
+): [Filter[], (value: Filter[]) => void] {
+  const filterKey = keyBuilder(key);
+  const [value, setValue] = useLocalStorage(filterKey, '[]', sessionStorage);
+
+  const v = JSON.parse(value);
+  // eslint-disable-next-line func-style
+  const setValueParsed = (value: Filter[]) => {
+    setValue(JSON.stringify(value));
+  };
+
+  return [v, setValueParsed];
+
+  function keyBuilder(key: string) {
+    return `datatable_home_filter_type_${key}`;
+  }
+}
+
+export function useHomePageSingleFilterTypeState(
+  key: string
+): [Filter | undefined, (value: Filter | undefined) => void] {
+  const filterKey = keyBuilder(key);
+  const [value, setValue] = useLocalStorage(filterKey, '', sessionStorage);
+
+  const v = value && JSON.parse(value);
+  // eslint-disable-next-line func-style
+  const setValueParsed = (value: Filter | undefined) => {
+    setValue(JSON.stringify(value));
+  };
+
+  return [v, setValueParsed];
+
+  function keyBuilder(key: string) {
+    return `datatable_home_filter_type_${key}`;
+  }
 }
