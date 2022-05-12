d1a1832654b43a85bf30d54ea26b01270a3d5e12
cong meng <mcpacino@gmail.com>
2022-05-12 13:16:56 +1200

    fix(pwd) EE-3161 ease the minimum password restrictions to 12 characters (#6920)
    * fix(pwd): EE-3161 ease the minimum password restrictions to 12 characters

--- app/portainer/helpers/password.ts
+++ app/portainer/helpers/password.ts
@@ -4,19 +4,6 @@ function lengthCheck(password: string) {
   return password.length >= MinPasswordLen;
 }
 
-function comboCheck(password: string) {
-  let count = 0;
-  const regexps = [/[a-z]/, /[A-Z]/, /[0-9]/, /[\W_]/];
-
-  regexps.forEach((re) => {
-    if (password.match(re) != null) {
-      count += 1;
-    }
-  });
-
-  return count >= 3;
-}
-
 export function StrengthCheck(password: string) {
-  return lengthCheck(password) && comboCheck(password);
+  return lengthCheck(password);
 }
