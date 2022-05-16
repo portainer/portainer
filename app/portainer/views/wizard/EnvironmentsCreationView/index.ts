094eb8ed73da823845ec5a578457af62ca203db9
Chaim Lev-Ari <chiptus@users.noreply.github.com>
2022-05-16 19:19:24 +0300

    refactor(wizard): migrate kube form to react (#1625)

--- app/portainer/views/wizard/EnvironmentsCreationView/index.ts
+++ app/portainer/views/wizard/EnvironmentsCreationView/index.ts
@@ -3,7 +3,9 @@ import angular from 'angular';
 import { r2a } from '@/react-tools/react2angular';
 
 import { WizardDocker } from './WizardDocker';
+import { WizardKubernetes } from './WizardKubernetes';
 
 export const environmentCreationViewModule = angular
   .module('portainer.wizard.environmentCreation', [])
-  .component('wizardDocker', r2a(WizardDocker, ['onCreate'])).name;
+  .component('wizardDocker', r2a(WizardDocker, ['onCreate']))
+  .component('wizardKubernetes', r2a(WizardKubernetes, ['onCreate'])).name;
