c83e82fdc7fdaed03ddda86546b912bb3180c479
chaogeng77977 <chao.geng@portainer.io>
2022-05-29 23:14:38 +0800

    EE-2141 separate kubeconfigPrompt from kubeconfigButton.tsx

--- app/portainer/home/EnvironmentList/KubeconfigButton.tsx
+++ app/portainer/home/EnvironmentList/KubeconfigButton.tsx
@@ -7,13 +7,10 @@ import * as notifications from '@/portainer/services/notifications';
 import { Environment, EnvironmentId } from '@/portainer/environments/types';
 import { EnvironmentsQueryParams } from '@/portainer/environments/environment.service/index';
 import { isKubernetesEnvironment } from '@/portainer/environments/utils';
-import { useEnvironmentList } from '@/portainer/environments/queries';
 import { trackEvent } from '@/angulartics.matomo/analytics-services';
 import { Button } from '@/portainer/components/Button';
-import { Widget } from '@/portainer/components/widget';
-import { PaginationControls } from '@/portainer/components/pagination-controls';
-import { usePaginationLimitState } from '@/portainer/hooks/usePaginationLimitState';
 
+import { KubeconfigPrompt } from './KubeconfigPrompt';
 import styles from './KubeconfigButton.module.css';
 import '@reach/dialog/styles.css';
 
@@ -80,7 +77,7 @@ export function KubeconfigButton({
             </button>
             <h5 className="modal-title">Download kubeconfig file</h5>
           </div>
-          <KubeconfigPormpt
+          <KubeconfigPrompt
             kubeServiceExpiry={kubeServiceExpiryQuery.data}
             selection={selection}
             envQueryParams={envQueryParams}
@@ -137,90 +134,5 @@ export function KubeconfigButton({
     } catch (e) {
       notifications.error('Failed downloading kubeconfig file', e as Error);
     }
-  }
-}
-
-export interface KubeconfigPormptProps {
-  kubeServiceExpiry: string | undefined;
-  selection: Record<EnvironmentId, boolean>;
-  envQueryParams: EnvironmentsQueryParams;
-  onToggleSelection: (id: EnvironmentId, selected: boolean) => void;
-}
-const storageKey = 'home_endpoints';
-
-export function KubeconfigPormpt({
-  kubeServiceExpiry,
-  selection,
-  envQueryParams,
-  onToggleSelection,
-}: KubeconfigPormptProps) {
-  const [page, setPage] = useState(1);
-  const [pageLimit, setPageLimit] = usePaginationLimitState(storageKey);
-
-  const { environments, totalCount } = useEnvironmentList({
-    page,
-    pageLimit,
-    ...envQueryParams,
-  });
-  const kubeEnvs = environments.filter((env) =>
-    isKubernetesEnvironment(env.Type)
-  );
-
-  const isAllPageSelected = kubeEnvs.every((env) => selection[env.Id]);
-
-  return (
-    <div className="modal-body">
-      <form className="bootbox-form">
-        <div className="bootbox-prompt-message">
-          <p>
-            Select the kubernetes environment(s) to add to the kubeconfig file.
-            <br />
-            {kubeServiceExpiry}
-          </p>
-        </div>
-      </form>
-      <div className={styles.checkbox}>
-        <label>
-          <input
-            type="checkbox"
-            checked={isAllPageSelected}
-            onChange={handleSelectAll}
-          />
-          &nbsp;Select all (in this page)
-        </label>
-      </div>
-      <div className="datatable">
-        <div className="bootbox-checkbox-list">
-          {kubeEnvs.map((env) => (
-            <div className={styles.checkbox} key={env.Id}>
-              <label>
-                <input
-                  type="checkbox"
-                  checked={!!selection[env.Id]}
-                  onChange={() => onToggleSelection(env.Id, !selection[env.Id])}
-                />
-                &nbsp;{env.Name} ({env.URL})
-              </label>
-            </div>
-          ))}
-        </div>
-        <Widget>
-          <div className="footer">
-            <PaginationControls
-              showAll={totalCount <= 100}
-              page={page}
-              onPageChange={setPage}
-              pageLimit={pageLimit}
-              onPageLimitChange={setPageLimit}
-              totalCount={totalCount}
-            />
-          </div>
-        </Widget>
-      </div>
-    </div>
-  );
-
-  function handleSelectAll() {
-    kubeEnvs.forEach((env) => onToggleSelection(env.Id, !isAllPageSelected));
   }
 }
