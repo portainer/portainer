e8a8b71daa36ad1b17735a2a6d1de9bde5eaeec0
Matt Hook <hookenz@gmail.com>
2022-06-03 13:50:37 +1200

    feat(compose): upgrade to docker compose v2 EE-2096  (#6994)
    Upgrade to compose v2 + new helm + new kubectl

--- build/download_kompose_binary.sh
+++ build/download_kompose_binary.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
+set -euo pipefail
+
+if [[ $# -ne 3 ]]; then
+    echo "Illegal number of parameters" >&2
+    exit 1
+fi
 
 PLATFORM=$1
 ARCH=$2
 KOMPOSE_VERSION=$3
 
-if [ "${PLATFORM}" == 'windows' ]; then
+
+if [[ ${PLATFORM} == "windows" ]]; then
   wget -O "dist/kompose.exe" "https://github.com/kubernetes/kompose/releases/download/${KOMPOSE_VERSION}/kompose-windows-amd64.exe"
   chmod +x "dist/kompose.exe"
 else
   wget -O "dist/kompose" "https://github.com/kubernetes/kompose/releases/download/${KOMPOSE_VERSION}/kompose-${PLATFORM}-${ARCH}"
   chmod +x "dist/kompose"
 fi
-
-exit 0
