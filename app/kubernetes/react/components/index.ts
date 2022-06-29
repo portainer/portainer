3e31cd0d6656d79e4a9959a09ecc0a0385f558eb
Chaim Lev-Ari <chiptus@gmail.com>
2022-06-29 08:07:33 +0300

    refactor(kube): use namespaces-selector

--- app/kubernetes/react/components/index.ts
+++ app/kubernetes/react/components/index.ts
@@ -1,6 +1,19 @@
 import angular from 'angular';
 
-export const componentsModule = angular.module(
-  'portainer.kubernetes.react.components',
-  []
-).name;
+import { r2a } from '@/react-tools/react2angular';
+import { NamespacesSelector } from '@/react/kubernetes/cluster/RegistryAccessView/NamespacesSelector';
+
+export const componentsModule = angular
+  .module('portainer.kubernetes.react.components', [])
+  .component(
+    'namespacesSelector',
+    r2a(NamespacesSelector, [
+      'dataCy',
+      'inputId',
+      'name',
+      'namespaces',
+      'onChange',
+      'placeholder',
+      'value',
+    ])
+  ).name;
