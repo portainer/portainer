2c5ea30695b09fc3ff303744a28ed87c94ab3550
Chaim Lev-Ari <chiptus@gmail.com>
2022-06-29 14:25:53 +0300

    refactor(kube): use react-select in storage class

--- app/kubernetes/react/components/index.ts
+++ app/kubernetes/react/components/index.ts
@@ -2,6 +2,7 @@ import angular from 'angular';
 
 import { r2a } from '@/react-tools/react2angular';
 import { NamespacesSelector } from '@/react/kubernetes/cluster/RegistryAccessView/NamespacesSelector';
+import { StorageAccessModeSelector } from '@/react/kubernetes/cluster/ConfigureView/StorageAccessModeSelector';
 
 export const componentsModule = angular
   .module('portainer.kubernetes.react.components', [])
@@ -15,5 +16,15 @@ export const componentsModule = angular
       'onChange',
       'placeholder',
       'value',
+    ])
+  )
+  .component(
+    'storageAccessModeSelector',
+    r2a(StorageAccessModeSelector, [
+      'inputId',
+      'onChange',
+      'options',
+      'value',
+      'storageClassName',
     ])
   ).name;
