2352c0358ee3d31db1c798cc99821971f99896d0
Chaim Lev-Ari <chiptus@gmail.com>
2022-07-03 16:34:00 +0300

    refactor(app): redesign dashboard-item component

--- app/react/components/DashboardItem/DashboardItem.tsx
+++ app/react/components/DashboardItem/DashboardItem.tsx
@@ -1,6 +1,8 @@
 import { ReactNode } from 'react';
+import clsx from 'clsx';
 
 import { Icon, IconProps } from '@/react/components/Icon';
+import { pluralize } from '@/portainer/helpers/strings';
 
 import { Widget, WidgetBody } from '@@/Widget';
 
@@ -18,21 +20,30 @@ export function DashboardItem({
   featherIcon,
 }: Props) {
   return (
-    <div className="col-sm-12 col-md-6" aria-label={type}>
-      <Widget>
-        <WidgetBody>
-          <div className="widget-icon blue pull-left">
+    <Widget aria-label={type}>
+      <WidgetBody
+        className={clsx(
+          'border-solid rounded-lg border hover:border-2 hover:-m-px border-gray-5 hover:border-blue-7',
+          'bg-gray-2 hover:bg-blue-2'
+        )}
+      >
+        <div className="flex">
+          <div className="widget-icon blue">
             <Icon icon={icon} feather={featherIcon} />
           </div>
-          <div className="pull-right">{children}</div>
-          <div className="title" aria-label="value">
-            {value}
+
+          <div className="flex flex-col justify-around">
+            <div className="text-gray-9 font-medium text-xl" aria-label="value">
+              {typeof value !== 'undefined' ? value : '-'}
+            </div>
+            <div className="text-gray-7 text-lg" aria-label="resourceType">
+              {pluralize(value || 0, type)}
+            </div>
           </div>
-          <div className="comment" aria-label="resourceType">
-            {type}
-          </div>
-        </WidgetBody>
-      </Widget>
-    </div>
+
+          <div className="ml-auto">{children}</div>
+        </div>
+      </WidgetBody>
+    </Widget>
   );
 }
