ecce21171ace0240f954eceec1bfeb0bd3cb1672
Hao Zhang <hao.zhang@portainer.io>
2022-07-03 22:53:32 +0800

    feat(UI): docker container  related page

--- app/portainer/components/index.js
+++ app/portainer/components/index.js
@@ -1,5 +1,8 @@
 import angular from 'angular';
 
+import { r2a } from '@/react-tools/react2angular';
+import { Tooltip } from '@@/Tip/Tooltip';
+import { TooltipAngular } from 'Portainer/components/Tip/Tooltip';
 import formComponentsModule from './form-components';
 import gitFormModule from './forms/git-form';
 import porAccessManagementModule from './accessManagement';
@@ -7,7 +10,6 @@ import widgetModule from './widget';
 import { boxSelectorModule } from './BoxSelector';
 import { pageHeaderModule } from './PageHeader';
 
-import { TooltipAngular } from './Tip/Tooltip';
 import { beFeatureIndicator } from './BEFeatureIndicator';
 import { InformationPanelAngular } from './InformationPanel';
 
@@ -16,4 +18,5 @@ export default angular
   .component('informationPanel', InformationPanelAngular)
 
   .component('portainerTooltip', TooltipAngular)
+  .component('tooltip', r2a(Tooltip, ['message', 'position']))
   .component('beFeatureIndicator', beFeatureIndicator).name;
