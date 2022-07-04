69baa279d475b0b965b7459ffc377590b7c670f0
Richard Wei <54336863+WaysonWei@users.noreply.github.com>
2022-07-04 14:11:13 +1200

    feat(ui): break css into module EE-3629 (#7180)
    * break css into module and fix icon mode

--- app/portainer/react/components/index.ts
+++ app/portainer/react/components/index.ts
@@ -34,5 +34,8 @@ export const componentsModule = angular
     'pageHeader',
     r2a(PageHeader, ['title', 'breadcrumbs', 'loading', 'onReload', 'reload'])
   )
-  .component('prIcon', r2a(Icon, ['className', 'feather', 'icon']))
+  .component(
+    'prIcon',
+    r2a(Icon, ['className', 'feather', 'icon', 'mode', 'size'])
+  )
   .component('reactQueryDevTools', r2a(ReactQueryDevtoolsWrapper, [])).name;
