aee251f1f19752fbb4003663a730c8d000c1812f
Chaim Lev-Ari <chiptus@gmail.com>
2022-07-05 19:12:52 +0300

    refactor(app): remove widget for dashboard item

--- app/react/components/DashboardItem/DashboardItem.tsx
+++ app/react/components/DashboardItem/DashboardItem.tsx
@@ -4,8 +4,6 @@ import clsx from 'clsx';
 import { Icon, IconProps } from '@/react/components/Icon';
 import { pluralize } from '@/portainer/helpers/strings';
 
-import { Widget, WidgetBody } from '@@/Widget';
-
 interface Props extends IconProps {
   value?: number;
   type: string;
@@ -20,30 +18,29 @@ export function DashboardItem({
   featherIcon,
 }: Props) {
   return (
-    <Widget>
-      <WidgetBody
-        className={clsx(
-          'border-solid rounded-lg border hover:border-2 hover:-m-px border-gray-5 hover:border-blue-7',
-          'bg-gray-2 hover:bg-blue-2'
-        )}
-      >
-        <div className="flex" aria-label={type}>
-          <div className="widget-icon blue">
-            <Icon icon={icon} feather={featherIcon} />
-          </div>
-
-          <div className="flex flex-col justify-around">
-            <div className="text-gray-9 font-medium text-xl" aria-label="value">
-              {typeof value !== 'undefined' ? value : '-'}
-            </div>
-            <div className="text-gray-7 text-lg" aria-label="resourceType">
-              {pluralize(value || 0, type)}
-            </div>
-          </div>
-
-          <div className="ml-auto">{children}</div>
+    <div
+      className={clsx(
+        'border-solid rounded-lg border-2 hover:border-2 border-gray-5 hover:border-blue-7',
+        'bg-gray-2 hover:bg-blue-2',
+        'p-3'
+      )}
+    >
+      <div className="flex items-center" aria-label={type}>
+        <div className="icon-badge text-2xl bg-blue-3 text-blue-8 mr-4">
+          <Icon icon={icon} feather={featherIcon} className="feather" />
         </div>
-      </WidgetBody>
-    </Widget>
+
+        <div className="flex flex-col justify-around">
+          <div className="text-gray-9 font-medium text-2xl" aria-label="value">
+            {typeof value !== 'undefined' ? value : '-'}
+          </div>
+          <div className="text-gray-7 text-xl" aria-label="resourceType">
+            {pluralize(value || 0, type)}
+          </div>
+        </div>
+
+        <div className="ml-auto">{children}</div>
+      </div>
+    </div>
   );
 }
