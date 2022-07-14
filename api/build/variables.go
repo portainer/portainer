443eaf496c3d3fe79ea90cdca8c09d32eebbd3b6
itsconquest <william.conquest@portainer.io>
2022-07-15 11:09:38 +1200

    feat(buildinfo): ability to see build info [EE-2552] (#7107)
    * feat(buildinfo): ability to see build info [EE-2252]
    
    * handle dark theme
    
    * feat: add build info to status version
    
    * feat: include ldflags in azure pipeline
    
    * echo shell commands in azure build
    
    * clean up main log
    
    * allow tests to pass
    
    * use data from backend
    
    * allow clicking off modal to dismiss
    
    * add placeholder versions
    
    * refactor
    
    * update button class
    
    * fix modal displaying behind elements
    
    Co-authored-by: Dmitry Salakhov <to@dimasalakhov.com>

---
+++ api/build/variables.go
@@ -0,0 +1,9 @@
+package build
+
+// Variables to be set during the build time
+var BuildNumber string
+var ImageTag string
+var NodejsVersion string
+var YarnVersion string
+var WebpackVersion string
+var GoVersion string
