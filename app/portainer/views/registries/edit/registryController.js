7fb3691dedeb6b6525443ecb4aedf247af0c35e0
congs <mcpacino@gmail.com>
2022-07-21 12:28:58 +1200

    feat(ui): update registries css (#7249) [EE-3562]

--- app/portainer/views/registries/edit/registryController.js
+++ app/portainer/views/registries/edit/registryController.js
@@ -3,7 +3,8 @@ import { RegistryTypes } from '@/portainer/models/registryTypes';
 
 export default class RegistryController {
   /* @ngInject */
-  constructor($async, $state, RegistryService, Notifications) {
+  constructor($scope, $async, $state, RegistryService, Notifications) {
+    this.$scope = $scope;
     Object.assign(this, { $async, $state, RegistryService, Notifications });
 
     this.RegistryTypes = RegistryTypes;
@@ -14,6 +15,21 @@ export default class RegistryController {
     };
 
     this.Password = '';
+
+    this.toggleAuthentication = this.toggleAuthentication.bind(this);
+    this.toggleQuayUseOrganisation = this.toggleQuayUseOrganisation.bind(this);
+  }
+
+  toggleAuthentication(newValue) {
+    this.$scope.$evalAsync(() => {
+      this.registry.Authentication = newValue;
+    });
+  }
+
+  toggleQuayUseOrganisation(newValue) {
+    this.$scope.$evalAsync(() => {
+      this.registry.Quay.UseOrganisation = newValue;
+    });
   }
 
   passwordLabel() {
