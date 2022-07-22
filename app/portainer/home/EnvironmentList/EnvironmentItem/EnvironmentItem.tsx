3645ff7459050e6846e9c6e3a9b5e49655d01cc1
Prabhat Khera <91852476+prabhat-org@users.noreply.github.com>
2022-07-22 14:16:50 +1200

    feat(ui): cluster setup page done EE-3705 (#7267)

--- app/portainer/home/EnvironmentList/EnvironmentItem/EnvironmentItem.tsx
+++ app/portainer/home/EnvironmentList/EnvironmentItem/EnvironmentItem.tsx
@@ -101,7 +101,7 @@ export function EnvironmentItem({ environment, onClick, groupName }: Props) {
                           <i className="fa fa-memory space-left space-right" />
                           {humanize(environment.Snapshots[0].TotalMemory)}
                           <i className="fa fa-digital-tachograph space-left space-right" />
-                          {environment.Gpus.length}
+                          {environment.Gpus?.length}
                         </span>
                       )}
                       <span className="space-left space-right">-</span>
