65c050dc87ac810ccb005723a2f9274bbcdef818
Richard Wei <54336863+WaysonWei@users.noreply.github.com>
2022-08-03 11:15:00 +1200

    feat(ui): ui change for edge compute settings EE-3800 (#7365)
    * added style to edge compute under settings

--- app/react/components/form-components/FormError.tsx
+++ app/react/components/form-components/FormError.tsx
@@ -1,11 +1,14 @@
 import { PropsWithChildren } from 'react';
 
+import { Icon } from '@@/Icon';
+
 export function FormError({ children }: PropsWithChildren<unknown>) {
   return (
-    <div className="small text-warning">
-      <i
-        className="fa fa-exclamation-triangle space-right"
-        aria-hidden="true"
+    <div className="small text-warning vertical-center">
+      <Icon
+        icon="alert-triangle"
+        feather
+        className="icon icon-sm icon-warning"
       />
       {children}
     </div>
