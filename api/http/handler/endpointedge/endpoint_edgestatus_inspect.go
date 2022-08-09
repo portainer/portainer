fd832595a4cb0b4b051f24872148fbc873904383
matias.spinarolli <matias.spinarolli@portainer.io>
2022-08-09 16:01:17 -0300

    fix(edge): parse agent platform on every polling request to avoid endpoint misconfiguration

--- api/http/handler/endpointedge/endpoint_edgestatus_inspect.go
+++ api/http/handler/endpointedge/endpoint_edgestatus_inspect.go
@@ -77,13 +77,13 @@ func (handler *Handler) endpointEdgeStatusInspect(w http.ResponseWriter, r *http
 	if endpoint.EdgeID == "" {
 		edgeIdentifier := r.Header.Get(portainer.PortainerAgentEdgeIDHeader)
 		endpoint.EdgeID = edgeIdentifier
-
-		agentPlatform, agentPlatformErr := parseAgentPlatform(r)
-		if agentPlatformErr != nil {
-			return httperror.BadRequest("agent platform header is not valid", err)
-		}
-		endpoint.Type = agentPlatform
 	}
+
+	agentPlatform, agentPlatformErr := parseAgentPlatform(r)
+	if agentPlatformErr != nil {
+		return httperror.BadRequest("agent platform header is not valid", err)
+	}
+	endpoint.Type = agentPlatform
 
 	endpoint.LastCheckInDate = time.Now().Unix()
 
