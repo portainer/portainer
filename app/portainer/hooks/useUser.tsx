edb01c63a0c7d7558ec6690b09b4496ecc8c2f21
Chaim Lev-Ari <chiptus@gmail.com>
2022-08-14 16:40:54 +0300

    feat(ui): create withUser hoc

--- app/portainer/hooks/useUser.tsx
+++ app/portainer/hooks/useUser.tsx
@@ -8,6 +8,7 @@ import {
   useState,
   useMemo,
   PropsWithChildren,
+  ComponentType,
 } from 'react';
 
 import { isAdmin } from '@/portainer/users/user.helpers';
@@ -23,6 +24,7 @@ interface State {
 }
 
 export const UserContext = createContext<State | null>(null);
+UserContext.displayName = 'UserContext';
 
 export function useUser() {
   const context = useContext(UserContext);
@@ -180,4 +182,23 @@ export function UserProvider({ children }: UserProviderProps) {
 export function useIsAdmin() {
   const { user } = useUser();
   return !!user && isAdmin(user);
+}
+
+export function withCurrentUser<T>(WrappedComponent: ComponentType<T>) {
+  // Try to create a nice displayName for React Dev Tools.
+  const displayName =
+    WrappedComponent.displayName || WrappedComponent.name || 'Component';
+
+  function WrapperComponent(props: T) {
+    return (
+      <UserProvider>
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        <WrappedComponent {...props} />
+      </UserProvider>
+    );
+  }
+
+  WrapperComponent.displayName = displayName;
+
+  return WrapperComponent;
 }
