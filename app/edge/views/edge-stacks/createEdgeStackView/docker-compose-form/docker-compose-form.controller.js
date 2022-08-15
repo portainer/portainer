f3909e1c275544b93820a1601ca15b1e0b360a1c
Chaim Lev-Ari <chiptus@gmail.com>
2022-08-15 10:50:24 +0300

    fix(edge-stacks): load edge stack build selector

--- app/edge/views/edge-stacks/createEdgeStackView/docker-compose-form/docker-compose-form.controller.js
+++ app/edge/views/edge-stacks/createEdgeStackView/docker-compose-form/docker-compose-form.controller.js
@@ -1,13 +1,15 @@
+import { Edit, FileText, GitHub, Upload } from 'react-feather';
+
 class DockerComposeFormController {
   /* @ngInject */
   constructor($async, EdgeTemplateService, Notifications) {
     Object.assign(this, { $async, EdgeTemplateService, Notifications });
 
     this.methodOptions = [
-      { id: 'method_editor', icon: 'edit', featherIcon: true, label: 'Web editor', description: 'Use our Web editor', value: 'editor' },
-      { id: 'method_upload', icon: 'upload', featherIcon: true, label: 'Upload', description: 'Upload from your computer', value: 'upload' },
-      { id: 'method_repository', icon: 'github', featherIcon: true, label: 'Repository', description: 'Use a git repository', value: 'repository' },
-      { id: 'method_template', icon: 'file-text', featherIcon: true, label: 'Template', description: 'Use an Edge stack template', value: 'template' },
+      { id: 'method_editor', icon: Edit, featherIcon: true, label: 'Web editor', description: 'Use our Web editor', value: 'editor' },
+      { id: 'method_upload', icon: Upload, featherIcon: true, label: 'Upload', description: 'Upload from your computer', value: 'upload' },
+      { id: 'method_repository', icon: GitHub, featherIcon: true, label: 'Repository', description: 'Use a git repository', value: 'repository' },
+      { id: 'method_template', icon: FileText, featherIcon: true, label: 'Template', description: 'Use an Edge stack template', value: 'template' },
     ];
 
     this.selectedTemplate = null;
--- app/edge/views/edge-stacks/createEdgeStackView/kube-manifest-form/kube-manifest-form.controller.js
+++ app/edge/views/edge-stacks/createEdgeStackView/kube-manifest-form/kube-manifest-form.controller.js
@@ -1,12 +1,14 @@
+import { Edit, GitHub, Upload } from 'react-feather';
+
 class KubeManifestFormController {
   /* @ngInject */
   constructor($async) {
     Object.assign(this, { $async });
 
     this.methodOptions = [
-      { id: 'method_editor', icon: 'edit', featherIcon: true, label: 'Web editor', description: 'Use our Web editor', value: 'editor' },
-      { id: 'method_upload', icon: 'upload', featherIcon: true, label: 'Upload', description: 'Upload from your computer', value: 'upload' },
-      { id: 'method_repository', icon: 'github', featherIcon: true, label: 'Repository', description: 'Use a git repository', value: 'repository' },
+      { id: 'method_editor', icon: Edit, featherIcon: true, label: 'Web editor', description: 'Use our Web editor', value: 'editor' },
+      { id: 'method_upload', icon: Upload, featherIcon: true, label: 'Upload', description: 'Upload from your computer', value: 'upload' },
+      { id: 'method_repository', icon: GitHub, featherIcon: true, label: 'Repository', description: 'Use a git repository', value: 'repository' },
     ];
 
     this.onChangeFileContent = this.onChangeFileContent.bind(this);
