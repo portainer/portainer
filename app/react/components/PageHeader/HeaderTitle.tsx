c5f5269366958612707bb391116e88f8d31976bd
Chaim Lev-Ari <chiptus@users.noreply.github.com>
2022-08-16 18:08:04 +0300

    fix(ui/header): change font sizes [EE-3966] (#7484)

--- app/react/components/PageHeader/HeaderTitle.tsx
+++ app/react/components/PageHeader/HeaderTitle.tsx
@@ -26,7 +26,7 @@ export function HeaderTitle({ title, children }: PropsWithChildren<Props>) {
   return (
     <div className="flex justify-between whitespace-normal pt-3">
       <div className="flex items-center gap-2">
-        <div className="font-medium text-3xl text-gray-11 th-dark:text-white th-highcontrast:text-white">
+        <div className="font-medium text-2xl text-gray-11 th-dark:text-white th-highcontrast:text-white">
           {title}
         </div>
         {children && <span>{children}</span>}
