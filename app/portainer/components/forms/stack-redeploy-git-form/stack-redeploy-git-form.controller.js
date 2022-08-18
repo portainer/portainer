3bd54fe530563eda3d66d8c6f416c98832a85f52
Simon Meng <simon.meng@portainer.io>
2022-08-19 10:23:22 +1200

    fix(stack): EE-3908 broken modal when updating/redeploying stacks: update prompt text

--- app/portainer/components/forms/stack-redeploy-git-form/stack-redeploy-git-form.controller.js
+++ app/portainer/components/forms/stack-redeploy-git-form/stack-redeploy-git-form.controller.js
@@ -103,7 +103,7 @@ class StackRedeployGitFormController {
 
   async submit() {
     const tplCrop =
-      '<div>Any changes to this stack or application made locally in Portainer will be overridden, which may cause service interruption.</div>' +
+      '<div>Any changes to this stack or application made locally in Portainer will be overridden, which may cause service interruption. Do you wish to continue?</div>' +
       '<div"><div style="position: absolute; right: 5px; top: 68px; z-index: 999">' +
       '<be-feature-indicator feature="stackPullImageFeature"></be-feature-indicator></div></div>';
     const template = angular.element(tplCrop);
