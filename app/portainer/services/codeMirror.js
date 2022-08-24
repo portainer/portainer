d4f4bb532f65a8613bf86773a29279865b51dfbf
Matt Hook <hookenz@gmail.com>
2022-08-25 10:11:25 +1200

    fix(web-editor): add search hint text [EE-3967] (#7496)

--- app/portainer/services/codeMirror.js
+++ app/portainer/services/codeMirror.js
@@ -8,7 +8,7 @@ import 'codemirror/addon/search/search.js';
 import 'codemirror/addon/search/searchcursor.js';
 import 'codemirror/addon/search/jump-to-line.js';
 import 'codemirror/addon/dialog/dialog.js';
-import 'codemirror/addon/dialog/dialog.css';
+import './codeMirrorDialog.css';
 
 angular.module('portainer.app').factory('CodeMirrorService', function CodeMirrorService() {
   'use strict';
