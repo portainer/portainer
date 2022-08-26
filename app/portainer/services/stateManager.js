c66cd7a7f63003dbeb05d713a0c1646e8acccf7d
itsconquest <william.conquest@portainer.io>
2022-08-26 17:50:38 +1200

    fix(sidebar): rework the update notification [EE-4119]

--- app/portainer/services/stateManager.js
+++ app/portainer/services/stateManager.js
@@ -26,6 +26,7 @@ function StateManagerFactory(
       dismissedInfoPanels: {},
       dismissedInfoHash: '',
       timesPasswordChangeSkipped: {},
+      dismissedUpdateVersion: '',
     },
   };
 
