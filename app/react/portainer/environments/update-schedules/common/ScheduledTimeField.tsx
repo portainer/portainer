005c6bff5e20be78eb9472a099ac728c787f161b
Chaim Lev-Ari <chaim.levi-ari@portainer.io>
2022-09-07 12:48:43 +0300

    fix(edge/update): show correct scheduled time when disabled

--- app/react/portainer/environments/update-schedules/common/ScheduledTimeField.tsx
+++ app/react/portainer/environments/update-schedules/common/ScheduledTimeField.tsx
@@ -1,8 +1,12 @@
 import { useField } from 'formik';
 import DateTimePicker from 'react-datetime-picker';
 import { Calendar, X } from 'react-feather';
+import { useMemo } from 'react';
+
+import { isoDateFromTimestamp } from '@/portainer/filters/filters';
 
 import { FormControl } from '@@/form-components/FormControl';
+import { Input } from '@@/form-components/Input';
 
 import { FormValues } from './types';
 
@@ -14,20 +18,25 @@ export function ScheduledTimeField({ disabled }: Props) {
   const [{ name, value }, { error }, { setValue }] =
     useField<FormValues['time']>('time');
 
+  const dateValue = useMemo(() => new Date(value * 1000), [value]);
+
   return (
     <FormControl label="Schedule date & time" errors={error}>
-      <DateTimePicker
-        format="y-MM-dd HH:mm:ss"
-        minDate={new Date()}
-        className="form-control [&>div]:border-0"
-        onChange={(date) => setValue(Math.floor(date.getTime() / 1000))}
-        name={name}
-        value={new Date(value * 1000)}
-        calendarIcon={<Calendar className="feather" />}
-        clearIcon={<X className="feather" />}
-        disabled={disabled}
-        disableClock
-      />
+      {!disabled ? (
+        <DateTimePicker
+          format="y-MM-dd HH:mm:ss"
+          minDate={new Date()}
+          className="form-control [&>div]:border-0"
+          onChange={(date) => setValue(Math.floor(date.getTime() / 1000))}
+          name={name}
+          value={dateValue}
+          calendarIcon={<Calendar className="feather" />}
+          clearIcon={<X className="feather" />}
+          disableClock
+        />
+      ) : (
+        <Input defaultValue={isoDateFromTimestamp(value)} disabled />
+      )}
     </FormControl>
   );
 }
