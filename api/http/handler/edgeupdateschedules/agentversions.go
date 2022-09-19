6731200882a97ada1110fd5b212f1dd91dc7ad0f
Chaim Lev-Ari <chaim.levi-ari@portainer.io>
2022-09-19 22:49:07 +0300

    feat(edge/updates): add dev agent versions

--- api/http/handler/edgeupdateschedules/agentversions.go
+++ api/http/handler/edgeupdateschedules/agentversions.go
@@ -2,6 +2,8 @@ package edgeupdateschedules
 
 import (
 	"net/http"
+	"os"
+	"strings"
 
 	httperror "github.com/portainer/libhttp/error"
 	"github.com/portainer/libhttp/response"
@@ -20,13 +22,22 @@ import (
 // @failure 500 "Server error"
 // @router /edge_update_schedules/agent_versions [get]
 func (h *Handler) agentVersions(w http.ResponseWriter, r *http.Request) *httperror.HandlerError {
-	return response.JSON(w, []string{
+	versions := []string{
 		"2.13.0",
 		"2.13.1",
 		"2.14.0",
 		"2.14.1",
 		"2.14.2",
-		"2.15",    // for develop only
-		"develop", // for develop only
-	})
+		"2.15.0",
+		"2.15.1",
+		"2.16.0",
+	}
+
+	env := os.Getenv("TEST_UPDATE_AGENT_VERSIONS")
+	if env != "" {
+		testVersions := strings.Split(env, ",")
+		versions = append(versions, testVersions...)
+	}
+
+	return response.JSON(w, versions)
 }
