c6b07592cd9556e222ce1c4bee684824239c1357
Prabhat Khera <prabhat.khera@portainer.io>
2022-09-22 13:31:15 +1200

    fix ingress screen loading

--- app/kubernetes/react/views/index.ts
+++ app/kubernetes/react/views/index.ts
@@ -3,8 +3,20 @@ import angular from 'angular';
 import { r2a } from '@/react-tools/react2angular';
 import { IngressesDatatableView } from '@/kubernetes/react/views/networks/ingresses/IngressDatatable';
 import { CreateIngressView } from '@/kubernetes/react/views/networks/ingresses/CreateIngressView';
+import { withCurrentUser } from '@/react-tools/withCurrentUser';
+import { withReactQuery } from '@/react-tools/withReactQuery';
+import { withUIRouter } from '@/react-tools/withUIRouter';
 
 export const viewsModule = angular
   .module('portainer.kubernetes.react.views', [])
-  .component('kubernetesIngressesView', r2a(IngressesDatatableView, []))
-  .component('kubernetesIngressesCreateView', r2a(CreateIngressView, [])).name;
+  .component(
+    'kubernetesIngressesView',
+    r2a(
+      withUIRouter(withReactQuery(withCurrentUser(IngressesDatatableView))),
+      []
+    )
+  )
+  .component(
+    'kubernetesIngressesCreateView',
+    r2a(withUIRouter(withReactQuery(withCurrentUser(CreateIngressView))), [])
+  ).name;
