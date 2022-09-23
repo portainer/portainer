4e20d70a99b3eccd91e46a76bb42042bbebe7d53
Ali <83188384+testA113@users.noreply.github.com>
2022-09-23 16:35:47 +1200

    feat(secrets): allow creating secrets beyond opaque [EE-2625] (#7709)

--- app/kubernetes/components/kubernetes-configuration-data/kubernetesConfigurationDataController.js
+++ app/kubernetes/components/kubernetes-configuration-data/kubernetesConfigurationDataController.js
@@ -6,11 +6,12 @@ import { Base64 } from 'js-base64';
 import KubernetesFormValidationHelper from 'Kubernetes/helpers/formValidationHelper';
 import KubernetesConfigurationHelper from 'Kubernetes/helpers/configurationHelper';
 import { KubernetesConfigurationFormValuesEntry } from 'Kubernetes/models/configuration/formvalues';
+import { KubernetesConfigurationKinds, KubernetesSecretTypes } from 'Kubernetes/models/configuration/models';
 
 class KubernetesConfigurationDataController {
   /* @ngInject */
-  constructor($async) {
-    this.$async = $async;
+  constructor($async, Notifications) {
+    Object.assign(this, { $async, Notifications });
 
     this.editorUpdate = this.editorUpdate.bind(this);
     this.editorUpdateAsync = this.editorUpdateAsync.bind(this);
@@ -18,12 +19,16 @@ class KubernetesConfigurationDataController {
     this.onFileLoadAsync = this.onFileLoadAsync.bind(this);
     this.showSimpleMode = this.showSimpleMode.bind(this);
     this.showAdvancedMode = this.showAdvancedMode.bind(this);
+    this.KubernetesConfigurationKinds = KubernetesConfigurationKinds;
+    this.KubernetesSecretTypes = KubernetesSecretTypes;
   }
 
   onChangeKey(entry) {
     if (entry && entry.Used) {
       return;
     }
+
+    this.onChangeValidation();
 
     this.state.duplicateKeys = KubernetesFormValidationHelper.getDuplicates(_.map(this.formValues.Data, (data) => data.Key));
     this.state.invalidKeys = KubernetesFormValidationHelper.getInvalidKeys(_.map(this.formValues.Data, (data) => data.Key));
@@ -32,6 +37,85 @@ class KubernetesConfigurationDataController {
 
   addEntry() {
     this.formValues.Data.push(new KubernetesConfigurationFormValuesEntry());
+
+    // logic for setting required keys for new entries, based on the secret type
+    if (this.formValues.Kind === this.KubernetesConfigurationKinds.SECRET) {
+      const newDataIndex = this.formValues.Data.length - 1;
+      const typeValue = typeof this.formValues.Type === 'string' ? this.formValues.Type : this.formValues.Type.value;
+      switch (typeValue) {
+        case this.KubernetesSecretTypes.DOCKERCFG.value:
+          this.addMissingKeys(['dockercfg'], newDataIndex);
+          break;
+        case this.KubernetesSecretTypes.DOCKERCONFIGJSON.value:
+          this.addMissingKeys(['.dockerconfigjson'], newDataIndex);
+          break;
+        case this.KubernetesSecretTypes.BASICAUTH.value:
+          // only add a required key if there is no required key out of username and password
+          if (!this.formValues.Data.some((entry) => entry.Key === 'username' || entry.Key === 'password')) {
+            this.addMissingKeys(['username', 'password'], newDataIndex);
+          }
+          break;
+        case this.KubernetesSecretTypes.SSHAUTH.value:
+          this.addMissingKeys(['ssh-privatekey'], newDataIndex);
+          break;
+        case this.KubernetesSecretTypes.TLS.value:
+          this.addMissingKeys(['tls.crt', 'tls.key'], newDataIndex);
+          break;
+        case this.KubernetesSecretTypes.BOOTSTRAPTOKEN.value:
+          this.addMissingKeys(['token-id', 'token-secret'], newDataIndex);
+          break;
+        default:
+          break;
+      }
+    }
+
+    this.onChangeValidation();
+  }
+
+  // addMissingKeys adds the keys in the keys array to the entry at the index provided and stops when the first one is added
+  addMissingKeys(keys, newDataIndex) {
+    for (let key of keys) {
+      if (this.formValues.Data.every((entry) => entry.Key !== key)) {
+        this.formValues.Data[newDataIndex].Key = key;
+        return;
+      }
+    }
+  }
+
+  isRequiredKey(key) {
+    if (this.formValues.Kind === this.KubernetesConfigurationKinds.SECRET) {
+      const secretTypeValue = typeof this.formValues.Type === 'string' ? this.formValues.Type : this.formValues.Type.value;
+      switch (secretTypeValue) {
+        case this.KubernetesSecretTypes.DOCKERCONFIGJSON.value:
+          if (key === '.dockerconfigjson') {
+            return true;
+          }
+          break;
+        case this.KubernetesSecretTypes.DOCKERCFG.value:
+          if (key === '.dockercfg') {
+            return true;
+          }
+          break;
+        case this.KubernetesSecretTypes.SSHAUTH.value:
+          if (key === 'ssh-privatekey') {
+            return true;
+          }
+          break;
+        case this.KubernetesSecretTypes.TLS.value:
+          if (key === 'tls.crt' || key === 'tls.key') {
+            return true;
+          }
+          break;
+        case this.KubernetesSecretTypes.BOOTSTRAPTOKEN.value:
+          if (key === 'token-id' || key === 'token-secret') {
+            return true;
+          }
+          break;
+        default:
+          break;
+      }
+    }
+    return false;
   }
 
   removeEntry(index, entry) {
@@ -55,22 +139,75 @@ class KubernetesConfigurationDataController {
   }
 
   async onFileLoadAsync(event) {
-    const entry = new KubernetesConfigurationFormValuesEntry();
-    const encoding = chardet.detect(Buffer.from(event.target.result));
-    const decoder = new TextDecoder(encoding);
-
-    entry.Key = event.target.fileName;
-    entry.IsBinary = KubernetesConfigurationHelper.isBinary(encoding);
-
-    if (!entry.IsBinary) {
-      entry.Value = decoder.decode(event.target.result);
-    } else {
-      const stringValue = decoder.decode(event.target.result);
-      entry.Value = Base64.encode(stringValue);
+    // exit if the file is too big
+    const maximumFileSizeBytes = 1024 * 1024; // 1MB
+    if (event.target.result.byteLength > maximumFileSizeBytes) {
+      this.Notifications.error('File size is too big', 'File size is too big', 'Select a file that is 1MB or smaller.');
+      return;
     }
 
-    this.formValues.Data.push(entry);
+    const entry = new KubernetesConfigurationFormValuesEntry();
+    try {
+      const encoding = chardet.detect(Buffer.from(event.target.result));
+      const decoder = new TextDecoder(encoding);
+
+      entry.IsBinary = KubernetesConfigurationHelper.isBinary(encoding);
+
+      if (!entry.IsBinary) {
+        entry.Value = decoder.decode(event.target.result);
+      } else {
+        const stringValue = decoder.decode(event.target.result);
+        entry.Value = Base64.encode(stringValue);
+      }
+    } catch (error) {
+      this.Notifications.error('Failed to upload file', error, 'Failed to upload file');
+      return;
+    }
+
+    entry.Key = event.target.fileName;
+
+    if (this.formValues.Kind === this.KubernetesConfigurationKinds.SECRET) {
+      if (this.isDockerConfig) {
+        if (this.formValues.Type.name === this.KubernetesSecretTypes.DOCKERCFG.name) {
+          entry.Key = '.dockercfg';
+        } else {
+          entry.Key = '.dockerconfigjson';
+        }
+      }
+
+      if (this.formValues.Type.name === this.KubernetesSecretTypes.TLS.name) {
+        const isCrt = entry.Value.indexOf('BEGIN CERTIFICATE') !== -1;
+        if (isCrt) {
+          entry.Key = 'tls.crt';
+        }
+        const isKey = entry.Value.indexOf('PRIVATE KEY') !== -1;
+        if (isKey) {
+          entry.Key = 'tls.key';
+        }
+      }
+    }
+
+    // if this.formValues.Data has a key that matches an existing key, then replace it
+    const existingEntryIndex = this.formValues.Data.findIndex((data) => data.Key === entry.Key || (data.Value === '' && data.Key === ''));
+    if (existingEntryIndex !== -1) {
+      this.formValues.Data[existingEntryIndex] = entry;
+    } else {
+      this.formValues.Data.push(entry);
+    }
+
     this.onChangeKey();
+  }
+
+  isEntryRequired() {
+    if (this.formValues.Kind === this.KubernetesConfigurationKinds.SECRET) {
+      const typeValue = typeof this.formValues.Type === 'string' ? this.formValues.Type : this.formValues.Type.value;
+      if (this.formValues.Data.length === 1) {
+        if (typeValue !== this.KubernetesSecretTypes.SERVICEACCOUNTTOKEN.value) {
+          return true;
+        }
+      }
+    }
+    return false;
   }
 
   onFileLoad(event) {
--- app/kubernetes/converters/configuration.js
+++ app/kubernetes/converters/configuration.js
@@ -1,12 +1,13 @@
 import _ from 'lodash-es';
-import { KubernetesConfiguration, KubernetesConfigurationTypes } from 'Kubernetes/models/configuration/models';
+import { KubernetesConfiguration, KubernetesConfigurationKinds } from 'Kubernetes/models/configuration/models';
 
 class KubernetesConfigurationConverter {
   static secretToConfiguration(secret) {
     const res = new KubernetesConfiguration();
-    res.Type = KubernetesConfigurationTypes.SECRET;
+    res.Kind = KubernetesConfigurationKinds.SECRET;
     res.Id = secret.Id;
     res.Name = secret.Name;
+    res.Type = secret.Type;
     res.Namespace = secret.Namespace;
     res.CreationDate = secret.CreationDate;
     res.Yaml = secret.Yaml;
@@ -21,7 +22,7 @@ class KubernetesConfigurationConverter {
 
   static configMapToConfiguration(configMap) {
     const res = new KubernetesConfiguration();
-    res.Type = KubernetesConfigurationTypes.CONFIGMAP;
+    res.Kind = KubernetesConfigurationKinds.CONFIGMAP;
     res.Id = configMap.Id;
     res.Name = configMap.Name;
     res.Namespace = configMap.Namespace;
