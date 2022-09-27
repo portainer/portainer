56b35b6344a9e8a08121b771bf091e93ad62d35a
oscarzhou <oscar.zhou@portainer.io>
2022-09-28 09:49:46 +1300

    feat(gitops): merge RepoConfigV2 and RepoConfig

--- api/git/types/types.go
+++ api/git/types/types.go
@@ -13,10 +13,12 @@ type RepoConfig struct {
 	URL string `example:"https://github.com/portainer/portainer.git"`
 	// The reference name
 	ReferenceName string `example:"refs/heads/branch_name"`
-	// Path to where the config file is in this url/refName
-	ConfigFilePath string `example:"docker-compose.yml"`
 	// Git credentials
 	Authentication *GitAuthentication
+	// Path to where the config file is in this url/refName
+	ConfigFilePath string `example:"docker-compose.yml"`
+	// Applicable when deploying with multiple stack files
+	AdditionalFiles []string `example:"[nz.compose.yml, uat.compose.yml]"`
 	// Repository hash
 	ConfigHash string `example:"bc4c183d756879ea4d173315338110b31004b8e0"`
 }
@@ -28,17 +30,4 @@ type GitAuthentication struct {
 	// When the value is 0, Username and Password are set without using saved credential
 	// This is introduced since 2.15.0
 	GitCredentialID int `example:"0"`
-}
-
-// RepoConfigV2 binds the basic git repository information with extra paths information
-type RepoConfigV2 struct {
-	RepoConfig
-	// Path to the Stack file inside the Git repository
-	ConfigFilePath string `example:"docker-compose.yml" default:"docker-compose.yml"`
-	// Applicable when deploying with multiple stack files
-	AdditionalFiles []string `example:"[nz.compose.yml, uat.compose.yml]"`
-	// The name of directory where the repository is cloned to
-	DirectoryName string `example:"myStack_jpofkc0i9uo9wtx1zesuk649w"`
-	// First git repository if true
-	Main bool
 }
