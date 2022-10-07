3be7f075317827bb96ddb49dbdb7920e75e6ca36
andres-portainer <andres-portainer@users.noreply.github.com>
2022-10-07 14:37:53 -0300

    fix(logging): convert missing cases to Zerolog EE-4400

--- api/git/service.go
+++ api/git/service.go
@@ -2,12 +2,12 @@ package git
 
 import (
 	"context"
-	"log"
 	"strings"
 	"sync"
 	"time"
 
 	lru "github.com/hashicorp/golang-lru"
+	"github.com/rs/zerolog/log"
 )
 
 var (
@@ -74,12 +74,12 @@ func newService(ctx context.Context, cacheSize int, cacheTTL time.Duration) *Ser
 		var err error
 		service.repoRefCache, err = lru.New(cacheSize)
 		if err != nil {
-			log.Printf("[DEBUG] [git] [message: failed to create ref cache: %v\n", err)
+			log.Debug().Err(err).Msg("failed to create ref cache")
 		}
 
 		service.repoFileCache, err = lru.New(cacheSize)
 		if err != nil {
-			log.Printf("[DEBUG] [git] [message: failed to create file cache: %v\n", err)
+			log.Debug().Err(err).Msg("failed to create file cache")
 		}
 
 		if cacheTTL > 0 {
