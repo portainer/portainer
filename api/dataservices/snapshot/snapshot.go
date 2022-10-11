0f51f9e44593e31444a3d4a40d9b5a9f26823926
andres-portainer <andres-portainer@users.noreply.github.com>
2022-10-11 16:25:52 -0300

    fix(logging): remove remaining traces of logrus EE-4414

--- api/dataservices/snapshot/snapshot.go
+++ api/dataservices/snapshot/snapshot.go
@@ -4,7 +4,8 @@ import (
 	"fmt"
 
 	portainer "github.com/portainer/portainer/api"
-	"github.com/sirupsen/logrus"
+
+	"github.com/rs/zerolog/log"
 )
 
 const (
@@ -51,7 +52,7 @@ func (service *Service) Snapshots() ([]portainer.Snapshot, error) {
 		func(obj interface{}) (interface{}, error) {
 			snapshot, ok := obj.(*portainer.Snapshot)
 			if !ok {
-				logrus.WithField("obj", obj).Errorf("Failed to convert to Snapshot object")
+				log.Debug().Str("obj", fmt.Sprintf("%#v", obj)).Msg("failed to convert to Snapshot object")
 				return nil, fmt.Errorf("failed to convert to Snapshot object: %s", obj)
 			}
 			snapshots = append(snapshots, *snapshot)
