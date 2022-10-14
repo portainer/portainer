191f8e17eea9ef2d255a7b6eb505d24581a63245
andres-portainer <91705312+andres-portainer@users.noreply.github.com>
2022-10-14 19:42:31 -0300

    fix(code): remove unused code EE-4431 (#7866)

--- api/http/handler/webhooks/handler.go
+++ api/http/handler/webhooks/handler.go
@@ -1,12 +1,11 @@
 package webhooks
 
 import (
-	"github.com/portainer/portainer/api/dataservices"
-	"github.com/portainer/portainer/api/internal/authorization"
 	"net/http"
 
+	"github.com/portainer/portainer/api/dataservices"
+
 	httperror "github.com/portainer/libhttp/error"
-	portainer "github.com/portainer/portainer/api"
 	"github.com/portainer/portainer/api/docker"
 	"github.com/portainer/portainer/api/http/security"
 
@@ -38,44 +37,4 @@ func NewHandler(bouncer *security.RequestBouncer) *Handler {
 	h.Handle("/webhooks/{token}",
 		bouncer.PublicAccess(httperror.LoggerHandler(h.webhookExecute))).Methods(http.MethodPost)
 	return h
-}
-
-func (handler *Handler) checkResourceAccess(r *http.Request, resourceID string, resourceControlType portainer.ResourceControlType) *httperror.HandlerError {
-	securityContext, err := security.RetrieveRestrictedRequestContext(r)
-	if err != nil {
-		return httperror.InternalServerError("Unable to retrieve user info from request context", err)
-	}
-	// non-admins
-	rc, err := handler.DataStore.ResourceControl().ResourceControlByResourceIDAndType(resourceID, resourceControlType)
-	if rc == nil || err != nil {
-		return httperror.InternalServerError("Unable to retrieve a resource control associated to the resource", err)
-	}
-	userTeamIDs := make([]portainer.TeamID, 0)
-	for _, membership := range securityContext.UserMemberships {
-		userTeamIDs = append(userTeamIDs, membership.TeamID)
-	}
-	canAccess := authorization.UserCanAccessResource(securityContext.UserID, userTeamIDs, rc)
-	if !canAccess {
-		return &httperror.HandlerError{StatusCode: http.StatusForbidden, Message: "This operation is disabled for non-admin users and unassigned access users"}
-	}
-	return nil
-}
-
-func (handler *Handler) checkAuthorization(r *http.Request, endpoint *portainer.Endpoint, authorizations []portainer.Authorization) (bool, *httperror.HandlerError) {
-	err := handler.requestBouncer.AuthorizedEndpointOperation(r, endpoint)
-	if err != nil {
-		return false, httperror.Forbidden("Permission denied to access environment", err)
-	}
-
-	securityContext, err := security.RetrieveRestrictedRequestContext(r)
-	if err != nil {
-		return false, httperror.InternalServerError("Unable to retrieve user info from request context", err)
-	}
-
-	authService := authorization.NewService(handler.DataStore)
-	isAdminOrAuthorized, err := authService.UserIsAdminOrAuthorized(securityContext.UserID, endpoint.ID, authorizations)
-	if err != nil {
-		return false, httperror.InternalServerError("Unable to get user authorizations", err)
-	}
-	return isAdminOrAuthorized, nil
 }
