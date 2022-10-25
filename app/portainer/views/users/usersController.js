cb9fe2606cffdc4fde57c1ef6c56de704e974032
Oscar Zhou <100548325+oscarzhou-portainer@users.noreply.github.com>
2022-10-25 14:39:24 +1300

    fix(team): disable team leader setting when external auth sync is enabled [EE-3579] (#7852)

--- app/portainer/views/users/usersController.js
+++ app/portainer/views/users/usersController.js
@@ -131,6 +131,7 @@ angular.module('portainer.app').controller('UsersController', [
           $scope.teams = _.orderBy(data.teams, 'Name', 'asc');
           $scope.AuthenticationMethod = data.settings.AuthenticationMethod;
           $scope.requiredPasswordLength = data.settings.RequiredPasswordLength;
+          $scope.teamSync = data.settings.TeamSync;
         })
         .catch(function error(err) {
           Notifications.error('Failure', err, 'Unable to retrieve users and teams');
