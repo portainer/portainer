9ef2e27aae24b7bd94691d99ad77bcdc252c0f9b
Chaim Lev-Ari <chiptus@users.noreply.github.com>
2022-11-02 12:44:31 +0200

    fix(environments): debounce name validation [EE-4177] (#7889)

--- app/react/portainer/environments/wizard/EnvironmentsCreationView/WizardDocker/APITab/APIForm.validation.tsx
+++ app/react/portainer/environments/wizard/EnvironmentsCreationView/WizardDocker/APITab/APIForm.validation.tsx
@@ -3,14 +3,14 @@ import { boolean, object, SchemaOf, string } from 'yup';
 import { gpusListValidation } from '@/react/portainer/environments/wizard/EnvironmentsCreationView/shared/Hardware/GpusList';
 
 import { metadataValidation } from '../../shared/MetadataFieldset/validation';
-import { nameValidation } from '../../shared/NameField';
+import { useNameValidation } from '../../shared/NameField';
 
 import { validation as certsValidation } from './TLSFieldset';
 import { FormValues } from './types';
 
-export function validation(): SchemaOf<FormValues> {
+export function useValidation(): SchemaOf<FormValues> {
   return object({
-    name: nameValidation(),
+    name: useNameValidation(),
     url: string().required('This field is required.'),
     tls: boolean().default(false),
     skipVerify: boolean(),
--- app/react/portainer/environments/wizard/EnvironmentsCreationView/shared/EdgeAgentTab/EdgeAgentForm/EdgeAgentForm.validation.ts
+++ app/react/portainer/environments/wizard/EnvironmentsCreationView/shared/EdgeAgentTab/EdgeAgentForm/EdgeAgentForm.validation.ts
@@ -3,14 +3,16 @@ import { number, object, SchemaOf } from 'yup';
 import { gpusListValidation } from '@/react/portainer/environments/wizard/EnvironmentsCreationView/shared/Hardware/GpusList';
 
 import { metadataValidation } from '../../MetadataFieldset/validation';
-import { nameValidation } from '../../NameField';
+import { useNameValidation } from '../../NameField';
 
 import { validation as urlValidation } from './PortainerUrlField';
 import { FormValues } from './types';
 
-export function validationSchema(): SchemaOf<FormValues> {
+export function useValidationSchema(): SchemaOf<FormValues> {
+  const nameValidation = useNameValidation();
+
   return object().shape({
-    name: nameValidation(),
+    name: nameValidation,
     portainerUrl: urlValidation(),
     pollFrequency: number().required(),
     meta: metadataValidation(),
