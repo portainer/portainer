71216bd585363710b25b12d26ece95e489469fc1
Oscar Zhou <100548325+oscarzhou-portainer@users.noreply.github.com>
2022-11-17 10:38:23 +1300

    fix(access/viwer): update the viwer filter key to user.id (#8056)

--- app/portainer/rbac/components/access-viewer/access-viewer.controller.js
+++ app/portainer/rbac/components/access-viewer/access-viewer.controller.js
@@ -34,7 +34,7 @@ export default class AccessViewerController {
         throw new Error('User not found');
       }
 
-      const userMemberships = _.filter(this.teamMemberships, { UserId: user.value });
+      const userMemberships = _.filter(this.teamMemberships, { UserId: user.Id });
 
       for (const [, endpoint] of _.entries(this.endpoints)) {
         let role = this.getRoleFromUserEndpointPolicy(user, endpoint);
