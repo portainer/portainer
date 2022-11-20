a4661d562b38cbdb082ce75ec012ac9e467c5360
Chaim Lev-Ari <chaim.levi-ari@portainer.io>
2022-11-20 12:03:08 +0200

    feat(system): style loading dialog

--- app/react/sidebar/UpgradeBEBanner/LoadingDialog.tsx
+++ app/react/sidebar/UpgradeBEBanner/LoadingDialog.tsx
@@ -1,11 +1,28 @@
+import CircleNotch from '@/assets/ico/circle-notch.svg?c';
+
 import { Modal } from '@@/modals/Modal';
+import { Icon } from '@@/Icon';
 
 export function LoadingDialog() {
   return (
     <Modal aria-label="Upgrade Portainer to Business Edition">
       <Modal.Header title={null} />
 
-      <Modal.Body>Loading</Modal.Body>
+      <Modal.Body>
+        <div className="flex flex-col items-center justify-center w-full">
+          <Icon
+            icon={CircleNotch}
+            className="animate-spin-slow !text-8xl !text-blue-8"
+            aria-label="loading"
+          />
+
+          <h1 className="!text-2xl">Upgrading Portainer...</h1>
+
+          <p className="text-center text-gray-6 text-xl">
+            Please wait while we upgrade your Portainer to Business Edition.
+          </p>
+        </div>
+      </Modal.Body>
     </Modal>
   );
 }
