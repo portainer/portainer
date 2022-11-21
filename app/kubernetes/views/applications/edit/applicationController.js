a61107aba9302dea3657cdeb698a21d4114ab986
Matt Hook <hookenz@gmail.com>
2022-11-21 21:46:15 +1300

    rolling restart teaser button

--- app/kubernetes/views/applications/edit/applicationController.js
+++ app/kubernetes/views/applications/edit/applicationController.js
@@ -1,6 +1,7 @@
 import angular from 'angular';
 import _ from 'lodash-es';
 import * as JsonPatch from 'fast-json-patch';
+//import { FeatureId } from '@/react/portainer/feature-flags/enums';
 
 import {
   KubernetesApplicationDataAccessPolicies,
