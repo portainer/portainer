28b4eb5605d785c815af48038e3d3ac5d9167c73
testA113 <alex.harris@portainer.io>
2022-12-01 21:20:11 +0000

    make random string kube friendly

--- api/internal/randomstring/random_string.go
+++ api/internal/randomstring/random_string.go
@@ -2,9 +2,9 @@ package randomstring
 
 import "math/rand"
 
-const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+const letterBytes = "abcdefghijklmnopqrstuvwxyz0123456789"
 
-// RandomString returns a random alphanumeric string of length n
+// RandomString returns a random lowercase alphanumeric string of length n
 func RandomString(n int) string {
 	b := make([]byte, n)
 	for i := range b {
