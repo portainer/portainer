a006f3ab4dccc338cdcd55d6dd8ffb15b115a2c3
Jakob <72307968+jakob30061@users.noreply.github.com>
2022-12-22 09:53:46 +0100

    feat(containers): add link to stack for stack names #7617

--- app/react/docker/containers/ListView/ContainersDatatable/columns/stack.tsx
+++ app/react/docker/containers/ListView/ContainersDatatable/columns/stack.tsx
@@ -1,13 +1,33 @@
 import { Column } from 'react-table';
+import { useSref } from '@uirouter/react';
 
 import type { DockerContainer } from '@/react/docker/containers/types';
 
 export const stack: Column<DockerContainer> = {
   Header: 'Stack',
-  accessor: (row) => row.StackName || '-',
+  accessor: (row) => row.StackName || '',
   id: 'stack',
   sortType: 'string',
   disableFilters: true,
+  Cell: StackCell,
   canHide: true,
   Filter: () => null,
 };
+
+interface Props {
+  value: string;
+}
+
+function StackCell({ value: stackName }: Props) {
+  const linkProps = useSref('docker.stacks.stack', { name: stackName });
+
+  if (!stackName) {
+    return '-';
+  }
+
+  return (
+    <a href={linkProps.href} onClick={linkProps.onClick}>
+      {stackName}
+    </a>
+  );
+}
