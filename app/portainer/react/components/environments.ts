a0ce75ce5e25b39ef3c3df50e33f668a9405c9da
Chaim Lev-Ari <chaim.levi-ari@portainer.io>
2023-01-01 11:07:24 +0200

    refactor(environments): convert ListView to react

--- app/portainer/react/components/environments.ts
+++ app/portainer/react/components/environments.ts
@@ -3,15 +3,11 @@ import angular from 'angular';
 import { r2a } from '@/react-tools/react2angular';
 import { EdgeKeyDisplay } from '@/react/portainer/environments/ItemView/EdgeKeyDisplay';
 import { KVMControl } from '@/react/portainer/environments/KvmView/KVMControl';
-import { withReactQuery } from '@/react-tools/withReactQuery';
-import { withUIRouter } from '@/react-tools/withUIRouter';
-import { EnvironmentsDatatable } from '@/react/portainer/environments/ListView/EnvironmentsDatatable';
 
 export const environmentsModule = angular
   .module('portainer.app.react.components.environments', [])
   .component('edgeKeyDisplay', r2a(EdgeKeyDisplay, ['edgeKey']))
-  .component('kvmControl', r2a(KVMControl, ['deviceId', 'server', 'token']))
   .component(
-    'environmentsDatatable',
-    r2a(withUIRouter(withReactQuery(EnvironmentsDatatable)), ['onRemove'])
+    'kvmControl',
+    r2a(KVMControl, ['deviceId', 'server', 'token'])
   ).name;
