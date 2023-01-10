6c193a8a45b40c327bd12698838c78338d3fabf1
Prabhat Khera <91852476+prabhat-org@users.noreply.github.com>
2023-01-10 15:12:24 +1300

    refactor(log): log.fatal usage review EE-4607 (#8280)

--- api/adminmonitor/admin_monitor.go
+++ api/adminmonitor/admin_monitor.go
@@ -54,7 +54,8 @@ func (m *Monitor) Start() {
 		case <-time.After(m.timeout):
 			initialized, err := m.WasInitialized()
 			if err != nil {
-				log.Fatal().Err(err).Msg("")
+				log.Error().Err(err).Msg("AdminMonitor failed to determine if Portainer is Initialized")
+				return
 			}
 
 			if !initialized {
