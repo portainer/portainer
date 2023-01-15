350ed6106d06e32b2e776a78da9feb08712cbf23
Chaim Lev-Ari <chaim.levi-ari@portainer.io>
2023-01-15 15:10:31 +0200

    fix(edge/update): remove schedule date for old envs [EE-3023]
    fix [EE-3023]

--- app/react/portainer/environments/update-schedules/common/validation.ts
+++ app/react/portainer/environments/update-schedules/common/validation.ts
@@ -1,27 +1,32 @@
-import { array, object, string } from 'yup';
+import { array, object, SchemaOf, string } from 'yup';
+
+import { parseIsoDate } from '@/portainer/filters/filters';
 
 import { EdgeUpdateSchedule, ScheduleType } from '../types';
 
 import { nameValidation } from './NameField';
 import { typeValidation } from './ScheduleTypeSelector';
+import { FormValues } from './types';
 
 export function validation(
   schedules: EdgeUpdateSchedule[],
   currentId?: EdgeUpdateSchedule['id']
-) {
+): SchemaOf<FormValues> {
   return object({
     groupIds: array().min(1, 'At least one group is required'),
     name: nameValidation(schedules, currentId),
     type: typeValidation(),
-    // time: number()
-    //   .min(Date.now() / 1000)
-    //   .required(),
-    version: string().when('type', {
-      is: ScheduleType.Update,
-      // update type
-      then: (schema) => schema.required('Version is required'),
-      // rollback
-      otherwise: (schema) => schema.required('No rollback options available'),
-    }),
+    scheduledTime: string()
+      .default('')
+      .test('valid', (value) => !value || parseIsoDate(value) !== null),
+    version: string()
+      .default('')
+      .when('type', {
+        is: ScheduleType.Update,
+        // update type
+        then: (schema) => schema.required('Version is required'),
+        // rollback
+        otherwise: (schema) => schema.required('No rollback options available'),
+      }),
   });
 }
