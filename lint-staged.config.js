73db588080bce2c75eb1a737d25cc0465a9cc6e0
Prabhat Khera <91852476+prabhat-org@users.noreply.github.com>
2023-01-18 15:20:42 +1300

    chore(linting): configure go linter EE-4871 (#8288)

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,4 +2,5 @@ module.exports = {
   '*.(js|ts){,x}': 'eslint --cache --fix',
   '*.(ts){,x}': () => 'tsc --noEmit',
   '*.{js,ts,tsx,css,md,html,json}': 'prettier --write',
+  '*.go': 'golangci-lint run -c api/.golangci.yaml',
 };
