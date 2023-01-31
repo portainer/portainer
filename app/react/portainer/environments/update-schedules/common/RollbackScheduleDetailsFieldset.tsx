95a788dfe2399969d7a945f4ca6c585c517723fd
oscarzhou-portainer <oscar.zhou@portainer.io>
2023-02-01 08:37:12 +0900

    fix(schedule): move the environment state to the parent component

--- app/react/portainer/environments/update-schedules/common/RollbackScheduleDetailsFieldset.tsx
+++ app/react/portainer/environments/update-schedules/common/RollbackScheduleDetailsFieldset.tsx
@@ -1,32 +1,15 @@
-import { useFormikContext } from 'formik';
-import { useEffect } from 'react';
-
 import { RollbackOptions } from './RollbackOptions';
-import { FormValues } from './types';
-import { useEdgeGroupsEnvironmentIds } from './useEdgeGroupsEnvironmentIds';
-import { useEnvironments } from './useEnvironments';
-import { defaultValue, ScheduledTimeField } from './ScheduledTimeField';
+import { ScheduledTimeField } from './ScheduledTimeField';
 
-export function RollbackScheduleDetailsFieldset() {
-  const { values, setFieldValue } = useFormikContext<FormValues>();
+interface Props {
+  hasTimeZone: boolean;
+  hasGroupSelected: boolean;
+}
 
-  const environmentIdsQuery = useEdgeGroupsEnvironmentIds(values.groupIds);
-
-  const edgeGroupsEnvironmentIds = environmentIdsQuery.data || [];
-  const environments = useEnvironments(edgeGroupsEnvironmentIds);
-
-  // old version is version that doesn't support scheduling of updates
-  const hasTimeZone = environments.some((env) => env.LocalTimeZone);
-  const hasGroupSelected = values.groupIds.length > 0;
-
-  useEffect(() => {
-    if (!hasTimeZone || !hasGroupSelected) {
-      setFieldValue('scheduledTime', '');
-    } else if (!values.scheduledTime) {
-      setFieldValue('scheduledTime', defaultValue());
-    }
-  }, [setFieldValue, hasTimeZone, values.scheduledTime, hasGroupSelected]);
-
+export function RollbackScheduleDetailsFieldset({
+  hasTimeZone,
+  hasGroupSelected,
+}: Props) {
   return (
     <div className="mt-3">
       <RollbackOptions />
