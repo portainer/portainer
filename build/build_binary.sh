6c425faac4cd55477a8f0bdf097c33f350b076c7
Chaim Lev-Ari <chaim.levi-ari@portainer.io>
2023-02-06 12:22:13 +0530

    fix(build): get versions as paramater

--- build/build_binary.sh
+++ build/build_binary.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
-
-set -x
+DEBUG=${DEBUG:-""}
+if [ -n "$DEBUG" ]; then
+	set -x
+fi
 
 mkdir -p dist
 
 # populate tool versions
-BUILDNUMBER="N/A"
-CONTAINER_IMAGE_TAG="N/A"
-NODE_VERSION="0"
-YARN_VERSION="0"
-WEBPACK_VERSION="0"
-GO_VERSION="0"
+BUILD_NUMBER=${BUILD_NUMBER:-"N/A"}
+CONTAINER_IMAGE_TAG=${CONTAINER_IMAGE_TAG:-"N/A"}
+NODE_VERSION=${NODE_VERSION:-"0"}
+YARN_VERSION=${YARN_VERSION:-"0"}
+WEBPACK_VERSION=${WEBPACK_VERSION:-"0"}
+GO_VERSION=${GO_VERSION:-"0"}
 
 # copy templates
 cp -r "./mustache-templates" "./dist"
-
 
 cd api
 # the go get adds 8 seconds
@@ -25,7 +26,7 @@ GOOS=$1 GOARCH=$2 CGO_ENABLED=0 go build \
 	-trimpath \
 	--installsuffix cgo \
 	--ldflags "-s \
-	--X 'github.com/portainer/portainer/api/build.BuildNumber=${BUILDNUMBER}' \
+	--X 'github.com/portainer/portainer/api/build.BuildNumber=${BUILD_NUMBER}' \
 	--X 'github.com/portainer/portainer/api/build.ImageTag=${CONTAINER_IMAGE_TAG}' \
 	--X 'github.com/portainer/portainer/api/build.NodejsVersion=${NODE_VERSION}' \
 	--X 'github.com/portainer/portainer/api/build.YarnVersion=${YARN_VERSION}' \
