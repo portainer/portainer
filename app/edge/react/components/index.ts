c7d11cef23d994c42c6ac05221e51f64c2389df3
Chaim Lev-Ari <chaim.levi-ari@portainer.io>
2023-02-16 12:57:26 +0530

    refactor(edge/groups): load edge groups in selector
    fix(edge/stacks): remove double groups title

--- app/edge/react/components/index.ts
+++ app/edge/react/components/index.ts
@@ -12,7 +12,7 @@ export const componentsModule = angular
   .module('portainer.edge.react.components', [])
   .component(
     'edgeGroupsSelector',
-    r2a(EdgeGroupsSelector, ['items', 'onChange', 'value'])
+    r2a(withReactQuery(EdgeGroupsSelector), ['onChange', 'value', 'error'])
   )
   .component(
     'edgeScriptForm',
--- app/react/edge/edge-stacks/components/EdgeGroupsSelector.tsx
+++ app/react/edge/edge-stacks/components/EdgeGroupsSelector.tsx
@@ -3,33 +3,59 @@ import _ from 'lodash';
 import { EdgeGroup } from '@/react/edge/edge-groups/types';
 
 import { Select } from '@@/form-components/ReactSelect';
+import { FormSection } from '@@/form-components/FormSection';
+import { FormError } from '@@/form-components/FormError';
+import { Link } from '@@/Link';
+
+import { useEdgeGroups } from '../../edge-groups/queries/useEdgeGroups';
 
 type SingleValue = EdgeGroup['Id'];
 
 interface Props {
-  items: EdgeGroup[];
   value: SingleValue[];
   onChange: (value: SingleValue[]) => void;
+  error?: string | string[];
 }
 
-export function EdgeGroupsSelector({ items, value, onChange }: Props) {
-  const valueGroups = _.compact(
-    value.map((id) => items.find((item) => item.Id === id))
-  );
+export function EdgeGroupsSelector({ value, onChange, error }: Props) {
+  const edgeGroupsQuery = useEdgeGroups();
+
+  const items = edgeGroupsQuery.data;
+
+  const valueGroups =
+    items && _.compact(value.map((id) => items.find((item) => item.Id === id)));
 
   return (
-    <Select
-      aria-label="Edge groups"
-      options={items}
-      isMulti
-      getOptionLabel={(item) => item.Name}
-      getOptionValue={(item) => String(item.Id)}
-      value={valueGroups}
-      onChange={(value) => {
-        onChange(value.map((item) => item.Id));
-      }}
-      placeholder="Select one or multiple group(s)"
-      closeMenuOnSelect={false}
-    />
+    <FormSection title="Edge Groups">
+      <div className="form-group">
+        <div className="col-sm-12">
+          {items ? (
+            <Select
+              aria-label="Edge groups"
+              options={items}
+              isMulti
+              getOptionLabel={(item) => item.Name}
+              getOptionValue={(item) => String(item.Id)}
+              value={valueGroups}
+              onChange={(value) => {
+                onChange(value.map((item) => item.Id));
+              }}
+              placeholder="Select one or multiple group(s)"
+              closeMenuOnSelect={false}
+            />
+          ) : (
+            <div className="small text-muted">
+              No Edge groups are available. Head over to the{' '}
+              <Link to="edge.groups">Edge groups view</Link> to create one.
+            </div>
+          )}
+        </div>
+        {error && (
+          <div className="col-sm-12">
+            <FormError>{error}</FormError>
+          </div>
+        )}
+      </div>
+    </FormSection>
   );
 }
