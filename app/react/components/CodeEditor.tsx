79fb8a20dd880e7057c881ceb77170bd804492f8
Chaim Lev-Ari <chaim.levi-ari@portainer.io>
2023-02-28 10:44:14 +0200

    fix(ui/code-editor): apply theme colors [EE-5104]
    fix [EE-5104]

--- app/react/components/CodeEditor.tsx
+++ app/react/components/CodeEditor.tsx
@@ -2,6 +2,10 @@ import CodeMirror from '@uiw/react-codemirror';
 import { StreamLanguage } from '@codemirror/language';
 import { yaml } from '@codemirror/legacy-modes/mode/yaml';
 import { useMemo } from 'react';
+import { createTheme } from '@uiw/codemirror-themes';
+import { tags as highlightTags } from '@lezer/highlight';
+
+import styles from './CodeEditor.module.css';
 
 interface Props {
   id: string;
@@ -12,6 +16,28 @@ interface Props {
   value: string;
   height?: string;
 }
+
+const theme = createTheme({
+  theme: 'light',
+  settings: {
+    background: 'var(--bg-codemirror-color)',
+    foreground: 'var(--text-codemirror-color)',
+    caret: 'var(--border-codemirror-cursor-color)',
+    selection: 'var(--bg-codemirror-selected-color)',
+    selectionMatch: 'var(--bg-codemirror-selected-color)',
+
+    gutterBackground: 'var(--bg-codemirror-gutters-color)',
+  },
+  styles: [
+    { tag: highlightTags.atom, color: 'var(--text-cm-default-color)' },
+    { tag: highlightTags.meta, color: 'var(--text-cm-meta-color)' },
+    {
+      tag: [highlightTags.string, highlightTags.special(highlightTags.brace)],
+      color: 'var(--text-cm-string-color)',
+    },
+    { tag: highlightTags.number, color: 'var(--text-cm-number-color)' },
+  ],
+});
 
 export function CodeEditor({
   id,
@@ -29,6 +55,8 @@ export function CodeEditor({
 
   return (
     <CodeMirror
+      className={styles.root}
+      theme={theme}
       value={value}
       onChange={onChange}
       readOnly={readonly}
