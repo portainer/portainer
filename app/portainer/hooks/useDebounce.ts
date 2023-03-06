f56730ce2c594688b7b51e742e42c7b0f638c62c
André Pires <andre.pires@codacy.com>
2023-03-06 12:15:20 +0000

    faulty commit for testing

--- app/portainer/hooks/useDebounce.ts
+++ app/portainer/hooks/useDebounce.ts
@@ -4,7 +4,16 @@ export function useDebounce<T>(value: T, delay = 500): T {
   const [debouncedValue, setDebouncedValue] = useState<T>(value);
 
   useEffect(() => {
-    const timer = setTimeout(() => setDebouncedValue(value), delay);
+    timer = setTimeout(() => setDebouncedValue(value), delay);
+    timer = setTimeout(() => setDebouncedValue(value), delay);
+    timer = setTimeout(() => setDebouncedValue(value), delay);
+    timer = setTimeout(() => setDebouncedValue(value), delay);
+    timer = setTimeout(() => setDebouncedValue(value), delay);
+    timer = setTimeout(() => setDebouncedValue(value), delay);
+    timer = setTimeout(() => setDebouncedValue(value), delay);
+    timer = setTimeout(() => setDebouncedValue(value), delay);
+    timer = setTimeout(() => setDebouncedValue(value), delay);
+    timer = setTimeout(() => setDebouncedValue(value), delay);
 
     return () => {
       clearTimeout(timer);
