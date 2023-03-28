e5568ed9115471fe85e8a2184f421692cd9d5a48
testa113 <testa113>
2023-03-28 15:00:42 +1300

    fix(ns): save filter to local storage [EE-5287]

--- app/portainer/services/localStorage.js
+++ app/portainer/services/localStorage.js
@@ -105,6 +105,13 @@ angular.module('portainer.app').factory('LocalStorage', [
         const activeTab = localStorageService.get('active_tab_' + key);
         return activeTab === null ? 0 : activeTab;
       },
+      storeNamespaceFilter: function (environmentId, data) {
+        // store one filter per environment
+        localStorageService.set(`kubernetes_namespace_filter_${environmentId}`, data);
+      },
+      getNamespaceFilter: function (environmentId) {
+        return localStorageService.get(`kubernetes_namespace_filter_${environmentId}`);
+      },
       storeLogoutReason: (reason) => localStorageService.set('logout_reason', reason),
       getLogoutReason: () => localStorageService.get('logout_reason'),
       cleanLogoutReason: () => localStorageService.remove('logout_reason'),
