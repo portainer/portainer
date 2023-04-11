40e8c17d11241ebec13115ef3dd1ddbdaa3b98bd
LP B <xAt0mZ@users.noreply.github.com>
2023-04-11 17:06:16 +0200

    fix(app/home): environment item hover

--- app/react/portainer/HomeView/EnvironmentList/EnvironmentItem/EditButtons.tsx
+++ app/react/portainer/HomeView/EnvironmentList/EnvironmentItem/EditButtons.tsx
@@ -85,8 +85,13 @@ function ButtonsGrid({
   return (
     <div
       className={clsx(
-        'grid rounded-r-lg border border-solid',
-        'border-gray-5 th-highcontrast:border-white th-dark:border-gray-9',
+        'grid',
+        // trace borders but make them transparent so
+        // * hovering the env item won't make env box borders clip with the grid borders
+        // * hovering the buttons won't make the button's icon flicker
+        'rounded-r-lg border border-solid',
+        'border-y-transparent border-r-transparent',
+        'border-l-gray-5 th-highcontrast:border-l-white th-dark:border-l-gray-9',
         'overflow-hidden',
         className
       )}
@@ -95,7 +100,7 @@ function ButtonsGrid({
         <div
           key={index}
           className={clsx({
-            'border-0 border-b border-solid border-b-inherit':
+            'border-0 border-b border-solid border-b-gray-5 th-highcontrast:border-b-white th-dark:border-b-gray-9':
               index < children.length - 1,
           })}
         >
