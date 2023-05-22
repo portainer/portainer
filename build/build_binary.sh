0dba9b709d6d5557ce01a917214d292bec0464b2
Matt Hook <hookenz@gmail.com>
2023-05-22 15:51:17 +1200

    fix(makefile): revert makefile build process for 2.18 (#8977)
    * Revert "build/makefile fixes from develop (#8926)"
    
    This reverts commit 65d609861314b20bc5fc56124c9ab24ba6b82dcb.
    
    * Revert "chore(build): remove grunt and add makefile [EE-4824] (#8803)"
    
    This reverts commit 5fd36ee986186884ecf54141a6b5ce022334dad8.

--- build/build_binary.sh
+++ build/build_binary.sh
@@ -1,5 +1,6 @@
-#!/usr/bin/env bash
-set -euo pipefail
+#!/bin/sh
+
+set -x
 
 mkdir -p dist
 
@@ -15,27 +16,20 @@ GO_VERSION="0"
 cp -r "./mustache-templates" "./dist"
 
 
-cd api || exit 1
+cd api
 # the go get adds 8 seconds
 go get -t -d -v ./...
 
-
-ldflags="-s -X 'github.com/portainer/liblicense.LicenseServerBaseURL=https://api.portainer.io' \
--X 'github.com/portainer/portainer-ee/api/build.BuildNumber=${BUILDNUMBER}' \
--X 'github.com/portainer/portainer-ee/api/build.ImageTag=${CONTAINER_IMAGE_TAG}' \
--X 'github.com/portainer/portainer-ee/api/build.NodejsVersion=${NODE_VERSION}' \
--X 'github.com/portainer/portainer-ee/api/build.YarnVersion=${YARN_VERSION}' \
--X 'github.com/portainer/portainer-ee/api/build.WebpackVersion=${WEBPACK_VERSION}' \
--X 'github.com/portainer/portainer-ee/api/build.GoVersion=${GO_VERSION}'"
-
-BINARY_VERSION_FILE="../binary-version.json"
-
-echo "$ldflags"
-
 # the build takes 2 seconds
-GOOS=${1:-$(go env GOOS)} GOARCH=${2:-$(go env GOARCH)} CGO_ENABLED=0 go build \
+GOOS=$1 GOARCH=$2 CGO_ENABLED=0 go build \
 	-trimpath \
 	--installsuffix cgo \
-	--ldflags "$ldflags" \
+	--ldflags "-s \
+	--X 'github.com/portainer/portainer/api/build.BuildNumber=${BUILDNUMBER}' \
+	--X 'github.com/portainer/portainer/api/build.ImageTag=${CONTAINER_IMAGE_TAG}' \
+	--X 'github.com/portainer/portainer/api/build.NodejsVersion=${NODE_VERSION}' \
+	--X 'github.com/portainer/portainer/api/build.YarnVersion=${YARN_VERSION}' \
+	--X 'github.com/portainer/portainer/api/build.WebpackVersion=${WEBPACK_VERSION}' \
+	--X 'github.com/portainer/portainer/api/build.GoVersion=${GO_VERSION}'" \
 	-o "../dist/portainer" \
 	./cmd/portainer/
