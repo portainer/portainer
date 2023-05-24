6de85e7c36aeb6316b6b5be3ef2e16b8d84d18e2
oscarzhou <oscar.zhou@portainer.io>
2023-05-24 14:34:58 +1200

    refactor(container): sync EE solution with CE

--- api/docker/consts/labels.go
+++ api/docker/consts/labels.go
@@ -3,4 +3,6 @@ package consts
 const (
 	ComposeStackNameLabel = "com.docker.compose.project"
 	SwarmStackNameLabel   = "com.docker.stack.namespace"
+	SwarmServiceIdLabel   = "com.docker.swarm.service.id"
+	SwarmNodeIdLabel      = "com.docker.swarm.node.id"
 )
