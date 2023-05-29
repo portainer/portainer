8e10573832d36ba549d2419b0ee6c038d3c09940
Chaim Lev-Ari <chaim.levi-ari@portainer.io>
2023-05-29 13:15:21 +0700

    fix(registries): override config with registry fields

--- api/http/handler/registries/registry_configure.go
+++ api/http/handler/registries/registry_configure.go
@@ -125,31 +125,23 @@ func (handler *Handler) registryConfigure(w http.ResponseWriter, r *http.Request
 		return httperror.InternalServerError("Unable to find a registry with the specified identifier inside the database", err)
 	}
 
-	registry.ManagementConfiguration = &portainer.RegistryManagementConfiguration{
-		Type: registry.Type,
-	}
-	registry.ManagementConfiguration = syncConfig(registry)
-
 	if payload.Authentication {
 		registry.Authentication = true
-		registry.ManagementConfiguration.Authentication = true
 
 		registry.Username = payload.Username
-		registry.ManagementConfiguration.Username = payload.Username
 
 		if payload.Password != "" {
-			registry.ManagementConfiguration.Password = payload.Password
 			registry.Password = payload.Password
 		}
 
 		if payload.Region != "" {
-			registry.ManagementConfiguration.Ecr.Region = payload.Region
 			registry.Ecr.Region = payload.Region
 		}
 	}
 
+	var tlsConfig portainer.TLSConfiguration
 	if payload.TLS {
-		registry.ManagementConfiguration.TLSConfig = portainer.TLSConfiguration{
+		tlsConfig = portainer.TLSConfiguration{
 			TLS:           true,
 			TLSSkipVerify: payload.TLSSkipVerify,
 		}
@@ -161,21 +153,24 @@ func (handler *Handler) registryConfigure(w http.ResponseWriter, r *http.Request
 			if err != nil {
 				return httperror.InternalServerError("Unable to persist TLS certificate file on disk", err)
 			}
-			registry.ManagementConfiguration.TLSConfig.TLSCertPath = certPath
+			tlsConfig.TLSCertPath = certPath
 
 			keyPath, err := handler.FileService.StoreRegistryManagementFileFromBytes(folder, "key.pem", payload.TLSKeyFile)
 			if err != nil {
 				return httperror.InternalServerError("Unable to persist TLS key file on disk", err)
 			}
-			registry.ManagementConfiguration.TLSConfig.TLSKeyPath = keyPath
+			tlsConfig.TLSKeyPath = keyPath
 
 			cacertPath, err := handler.FileService.StoreRegistryManagementFileFromBytes(folder, "ca.pem", payload.TLSCACertFile)
 			if err != nil {
 				return httperror.InternalServerError("Unable to persist TLS CA certificate file on disk", err)
 			}
-			registry.ManagementConfiguration.TLSConfig.TLSCACertPath = cacertPath
+			tlsConfig.TLSCACertPath = cacertPath
 		}
 	}
+
+	registry.ManagementConfiguration = syncConfig(registry)
+	registry.ManagementConfiguration.TLSConfig = tlsConfig
 
 	err = handler.DataStore.Registry().UpdateRegistry(registry.ID, registry)
 	if err != nil {
