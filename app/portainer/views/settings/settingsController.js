642764efc7422cdcebf6bd3360c018406d9e2264
Chaim Lev-Ari <chaim.levi-ari@portainer.io>
2023-06-06 13:05:41 +0700

    style(settings): remove unused feature id

--- app/portainer/views/settings/settingsController.js
+++ app/portainer/views/settings/settingsController.js
@@ -11,7 +11,6 @@ angular.module('portainer.app').controller('SettingsController', [
   'BackupService',
   'FileSaver',
   function ($scope, Notifications, SettingsService, StateManager, BackupService, FileSaver) {
-    $scope.customBannerFeatureId = FeatureId.CUSTOM_LOGIN_BANNER;
     $scope.s3BackupFeatureId = FeatureId.S3_BACKUP_SETTING;
     $scope.enforceDeploymentOptions = FeatureId.ENFORCE_DEPLOYMENT_OPTIONS;
     $scope.updateSettings = updateSettings;
