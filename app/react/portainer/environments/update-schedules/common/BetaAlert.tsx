8129e7590bfa1d67fca8b2acff5e3fb815d66de7
Chaim Lev-Ari <chiptus@users.noreply.github.com>
2023-06-08 06:02:36 +0700

    feat(waiting-room): add beta alert to assignment [EE-5384] (#9028)

--- app/react/portainer/environments/update-schedules/common/BetaAlert.tsx
+++ app/react/portainer/environments/update-schedules/common/BetaAlert.tsx
@@ -1,24 +1,26 @@
+import { ReactNode } from 'react';
+
+import betaIcon from '@/assets/ico/beta.svg?c';
+
 import { TextTip } from '@@/Tip/TextTip';
 
 interface Props {
-  message: string;
+  message: ReactNode;
   className?: string;
   isHtml?: boolean;
 }
 
 export function BetaAlert({ message, className, isHtml }: Props) {
   return (
-    <TextTip
-      icon="svg-beta"
-      className={className}
-      childrenWrapperClassName="text-warning"
-    >
-      {!isHtml ? (
-        message
-      ) : (
-        // eslint-disable-next-line react/no-danger
-        <span dangerouslySetInnerHTML={{ __html: message }} />
-      )}
+    <TextTip icon={betaIcon} className={className}>
+      <div className="text-warning">
+        {isHtml && typeof message === 'string' ? (
+          // eslint-disable-next-line react/no-danger
+          <span dangerouslySetInnerHTML={{ __html: message }} />
+        ) : (
+          message
+        )}
+      </div>
     </TextTip>
   );
 }
