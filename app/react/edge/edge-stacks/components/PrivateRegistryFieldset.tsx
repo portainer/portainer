dacd99d44dc4f90edb2e9090b0ec5e0dbfb739f3
Chaim Lev-Ari <chaim.levi-ari@portainer.io>
2023-06-13 10:34:27 +0700

    fix(edge/stacks): fix ui issues [EE-5578]
    fix [EE-5578]

--- app/react/edge/edge-stacks/components/PrivateRegistryFieldset.tsx
+++ app/react/edge/edge-stacks/components/PrivateRegistryFieldset.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { RefreshCw } from 'lucide-react';
 
 import { Registry } from '@/react/portainer/registries/types';
 
@@ -75,26 +76,32 @@ export function PrivateRegistryFieldset({
       {checked && (
         <>
           {method !== 'repository' && (
-            <>
-              <TextTip color="blue">
-                If you make any changes to the image urls in your yaml, please
-                reload or select registry manually
-              </TextTip>
-
-              <Button onClick={reload}>Reload</Button>
-            </>
+            <TextTip color="blue">
+              If you make any changes to the image urls in your yaml, please
+              reload or select registry manually
+            </TextTip>
           )}
+
           {!errorMessage ? (
             <FormControl label="Registry" inputId="users-selector">
-              <Select
-                value={registries.filter(
-                  (registry) => registry.Id === selected
-                )}
-                options={registries}
-                getOptionLabel={(registry) => registry.Name}
-                getOptionValue={(registry) => registry.Id.toString()}
-                onChange={(value) => onSelect(value?.Id)}
-              />
+              <div className="flex">
+                <Select
+                  value={registries.filter(
+                    (registry) => registry.Id === selected
+                  )}
+                  options={registries}
+                  getOptionLabel={(registry) => registry.Name}
+                  getOptionValue={(registry) => registry.Id.toString()}
+                  onChange={(value) => onSelect(value?.Id)}
+                  className="w-full"
+                />
+                <Button
+                  onClick={reload}
+                  title="Reload"
+                  icon={RefreshCw}
+                  color="light"
+                />
+              </div>
             </FormControl>
           ) : (
             <FormError>{errorMessage}</FormError>
