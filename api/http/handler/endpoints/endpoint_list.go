44b436be8730912c75c8091213c5dd49af46b0bb
LP B <xAt0mZ@users.noreply.github.com>
2023-06-19 11:23:49 +0200

    fix(api/endpoints): rebase conflicts on imports

--- api/http/handler/endpoints/endpoint_list.go
+++ api/http/handler/endpoints/endpoint_list.go
@@ -5,12 +5,6 @@ import (
 	"sort"
 	"strconv"
 
-	portainer "github.com/portainer/portainer/api"
-	"github.com/portainer/portainer/api/http/security"
-	"github.com/portainer/portainer/api/internal/endpointutils"
-
-	"github.com/portainer/libhttp/request"
-
 	httperror "github.com/portainer/libhttp/error"
 	"github.com/portainer/libhttp/request"
 	"github.com/portainer/libhttp/response"
