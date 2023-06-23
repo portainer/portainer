549775de429edfe268402a479abfbf89c7f51b74
oscarzhou <oscar.zhou@portainer.io>
2023-06-23 14:52:38 +1200

    Revert "feat(edgestack): add env vars fields"
    This reverts commit ed384d0ed5b0fd4054054739287d948bbc8740be.

--- api/portainer.go
+++ api/portainer.go
@@ -303,7 +303,6 @@ type (
 	StackDeploymentInfo struct {
 		Version    int    `json:"Version"`
 		ConfigHash string `json:"ConfigHash"`
-		EnvVars    []Pair `json:"EnvVars"`
 	}
 
 	//EdgeStack represents an edge stack
