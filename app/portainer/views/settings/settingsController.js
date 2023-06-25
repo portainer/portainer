48dc89e4fb0784449d208a767779a0498924754c
Chaim Lev-Ari <chaim.levi-ari@portainer.io>
2023-06-25 19:34:21 +0700

    refactor(settings): remove ng hidden containers table

--- app/portainer/views/settings/settingsController.js
+++ app/portainer/views/settings/settingsController.js
@@ -1,53 +1,20 @@
 import angular from 'angular';
 
-angular.module('portainer.app').controller('SettingsController', [
-  '$scope',
-  'Notifications',
-  'SettingsService',
-  'StateManager',
-  function ($scope, Notifications, SettingsService, StateManager) {
-    $scope.updateSettings = updateSettings;
-    $scope.handleSuccess = handleSuccess;
+angular.module('portainer.app').controller('SettingsController', SettingsController);
 
-    $scope.state = {
-      actionInProgress: false,
-      showHTTPS: !window.ddExtension,
-    };
+/* @ngInject */
+function SettingsController($scope, StateManager) {
+  $scope.handleSuccess = handleSuccess;
 
-    $scope.formValues = {};
+  $scope.state = {
+    showHTTPS: !window.ddExtension,
+  };
 
-    function updateSettings(settings, successMessage = 'Settings updated') {
-      return SettingsService.update(settings)
-        .then(function success(settings) {
-          Notifications.success('Success', successMessage);
-          handleSuccess(settings);
-        })
-        .catch(function error(err) {
-          Notifications.error('Failure', err, 'Unable to update settings');
-        })
-        .finally(function final() {
-          $scope.state.actionInProgress = false;
-        });
+  function handleSuccess(settings) {
+    if (settings) {
+      StateManager.updateLogo(settings.LogoURL);
+      StateManager.updateSnapshotInterval(settings.SnapshotInterval);
+      StateManager.updateEnableTelemetry(settings.EnableTelemetry);
     }
-
-    function handleSuccess(settings) {
-      if (settings) {
-        StateManager.updateLogo(settings.LogoURL);
-        StateManager.updateSnapshotInterval(settings.SnapshotInterval);
-        StateManager.updateEnableTelemetry(settings.EnableTelemetry);
-      }
-    }
-
-    function initView() {
-      SettingsService.settings()
-        .then(function success(settings) {
-          $scope.settings = settings;
-        })
-        .catch(function error(err) {
-          Notifications.error('Failure', err, 'Unable to retrieve application settings');
-        });
-    }
-
-    initView();
-  },
-]);
+  }
+}
