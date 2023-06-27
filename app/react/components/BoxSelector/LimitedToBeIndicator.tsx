287029fd0fd74008f791fd828ee26f8d47d19981
LP B <xAt0mZ@users.noreply.github.com>
2023-06-27 14:16:21 +0200

    fix(app): review comments and prettier format

--- app/react/components/BoxSelector/LimitedToBeIndicator.tsx
+++ app/react/components/BoxSelector/LimitedToBeIndicator.tsx
@@ -11,7 +11,7 @@ interface Props {
 export function LimitedToBeIndicator({ tooltipId, url }: Props) {
   return (
     <div className="absolute left-0 top-0 w-full">
-      <div className="mx-auto flex max-w-fit items-center gap-1 rounded-b-lg bg-warning-4 th-dark:bg-[color:var(--bg-BE-only)] py-1 px-3 text-sm">
+      <div className="th-dark:bg-[--bg-BE-only] mx-auto flex max-w-fit items-center gap-1 rounded-b-lg bg-warning-4 py-1 px-3 text-sm">
         <a
           className="text-warning-9 th-dark:text-blue-8"
           href={url}
@@ -26,7 +26,10 @@ export function LimitedToBeIndicator({ tooltipId, url }: Props) {
           heading="Business Edition feature."
           message="This feature is currently limited to Business Edition users only."
         >
-          <HelpCircle className="ml-1 !text-warning-7 th-dark:!text-blue-8" aria-hidden="true" />
+          <HelpCircle
+            className="ml-1 !text-warning-7 th-dark:!text-blue-8"
+            aria-hidden="true"
+          />
         </TooltipWithChildren>
       </div>
     </div>
