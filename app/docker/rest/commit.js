599d214e50f68fd665d8e30c0eb033c1d4c13856
Chaim Lev-Ari <chiptus@users.noreply.github.com>
2023-06-28 08:02:43 +0700

    refactor(docker): remove EndpointProvider from commit [EE-5641] (#9123)

--- app/docker/rest/commit.js
+++ app/docker/rest/commit.js
@@ -1,14 +1,11 @@
 angular.module('portainer.docker').factory('Commit', [
   '$resource',
   'API_ENDPOINT_ENDPOINTS',
-  'EndpointProvider',
-  function CommitFactory($resource, API_ENDPOINT_ENDPOINTS, EndpointProvider) {
+  function CommitFactory($resource, API_ENDPOINT_ENDPOINTS) {
     'use strict';
     return $resource(
-      API_ENDPOINT_ENDPOINTS + '/:endpointId/docker/commit',
-      {
-        endpointId: EndpointProvider.endpointID,
-      },
+      API_ENDPOINT_ENDPOINTS + '/:environmentId/docker/commit',
+      {},
       {
         commitContainer: { method: 'POST', params: { container: '@id', repo: '@repo' }, ignoreLoadingBar: true },
       }
