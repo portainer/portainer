8b11e1678e18f4b503b91c3b9491d1990b1e0213
Chaim Lev-Ari <chiptus@users.noreply.github.com>
2023-07-10 03:39:11 +0700

    refactor(settings): migrate hidden containers panel to react [EE-5507] (#9119)

--- app/portainer/views/settings/settingsController.js
+++ app/portainer/views/settings/settingsController.js
@@ -1,78 +1,20 @@
 import angular from 'angular';
 
-angular.module('portainer.app').controller('SettingsController', [
-  '$scope',
-  'Notifications',
-  'SettingsService',
-  'StateManager',
-  function ($scope, Notifications, SettingsService, StateManager) {
-    $scope.updateSettings = updateSettings;
-    $scope.handleSuccess = handleSuccess;
+angular.module('portainer.app').controller('SettingsController', SettingsController);
 
-    $scope.state = {
-      actionInProgress: false,
-      showHTTPS: !window.ddExtension,
-    };
+/* @ngInject */
+function SettingsController($scope, StateManager) {
+  $scope.handleSuccess = handleSuccess;
 
-    $scope.formValues = {
-      BlackListedLabels: [],
-      labelName: '',
-      labelValue: '',
-    };
+  $scope.state = {
+    showHTTPS: !window.ddExtension,
+  };
 
-    $scope.removeFilteredContainerLabel = function (index) {
-      const filteredSettings = $scope.formValues.BlackListedLabels.filter((_, i) => i !== index);
-      const filteredSettingsPayload = { BlackListedLabels: filteredSettings };
-      updateSettings(filteredSettingsPayload, 'Hidden container settings updated');
-    };
-
-    $scope.addFilteredContainerLabel = function () {
-      var label = {
-        name: $scope.formValues.labelName,
-        value: $scope.formValues.labelValue,
-      };
-
-      const filteredSettings = [...$scope.formValues.BlackListedLabels, label];
-      const filteredSettingsPayload = { BlackListedLabels: filteredSettings };
-      updateSettings(filteredSettingsPayload, 'Hidden container settings updated');
-    };
-
-    function updateSettings(settings, successMessage = 'Settings updated') {
-      return SettingsService.update(settings)
-        .then(function success(settings) {
-          Notifications.success('Success', successMessage);
-          handleSuccess(settings);
-        })
-        .catch(function error(err) {
-          Notifications.error('Failure', err, 'Unable to update settings');
-        })
-        .finally(function final() {
-          $scope.state.actionInProgress = false;
-        });
+  function handleSuccess(settings) {
+    if (settings) {
+      StateManager.updateLogo(settings.LogoURL);
+      StateManager.updateSnapshotInterval(settings.SnapshotInterval);
+      StateManager.updateEnableTelemetry(settings.EnableTelemetry);
     }
-
-    function handleSuccess(settings) {
-      if (settings) {
-        StateManager.updateLogo(settings.LogoURL);
-        StateManager.updateSnapshotInterval(settings.SnapshotInterval);
-        StateManager.updateEnableTelemetry(settings.EnableTelemetry);
-        $scope.formValues.BlackListedLabels = settings.BlackListedLabels;
-      }
-    }
-
-    function initView() {
-      SettingsService.settings()
-        .then(function success(data) {
-          var settings = data;
-          $scope.settings = settings;
-
-          $scope.formValues.BlackListedLabels = settings.BlackListedLabels;
-        })
-        .catch(function error(err) {
-          Notifications.error('Failure', err, 'Unable to retrieve application settings');
-        });
-    }
-
-    initView();
-  },
-]);
+  }
+}
