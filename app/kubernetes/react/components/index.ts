e54dc9934567880e9325fb551397ec296555a4bf
testa113 <testa113>
2023-08-11 14:30:55 +0600

    fix(app): use correct withCurrentUser wrapper [EE-5928]

--- app/kubernetes/react/components/index.ts
+++ app/kubernetes/react/components/index.ts
@@ -16,7 +16,6 @@ import {
   ApplicationSummaryWidget,
   ApplicationDetailsWidget,
 } from '@/react/kubernetes/applications/DetailsView';
-import { withUserProvider } from '@/react/test-utils/withUserProvider';
 import { withFormValidation } from '@/react-tools/withFormValidation';
 import { withCurrentUser } from '@/react-tools/withCurrentUser';
 import { PlacementsDatatable } from '@/react/kubernetes/applications/ItemView/PlacementsDatatable';
@@ -105,7 +104,7 @@ export const ngModule = angular
   .component(
     'applicationDetailsWidget',
     r2a(
-      withUIRouter(withReactQuery(withUserProvider(ApplicationDetailsWidget))),
+      withUIRouter(withReactQuery(withCurrentUser(ApplicationDetailsWidget))),
       []
     )
   )
