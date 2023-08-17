dc76900a286c9a384d902836f113839f52a3fb86
Chaim Lev-Ari <chiptus@users.noreply.github.com>
2023-08-17 14:09:43 +0300

    feat(edge/stacks): reload edge stacks from server [EE-5970] (#10062)

--- app/react/edge/edge-stacks/ListView/ListView.tsx
+++ app/react/edge/edge-stacks/ListView/ListView.tsx
@@ -1,11 +1,22 @@
+import { useQueryClient } from 'react-query';
+
 import { PageHeader } from '@@/PageHeader';
+
+import { queryKeys } from '../queries/query-keys';
 
 import { EdgeStacksDatatable } from './EdgeStacksDatatable';
 
 export function ListView() {
+  const queryClient = useQueryClient();
+
   return (
     <>
-      <PageHeader title="Edge Stacks list" breadcrumbs="Edge Stacks" reload />
+      <PageHeader
+        title="Edge Stacks list"
+        breadcrumbs="Edge Stacks"
+        reload
+        onReload={() => queryClient.invalidateQueries(queryKeys.base())}
+      />
 
       <EdgeStacksDatatable />
     </>
