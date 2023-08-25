c0ba221021e537d854311361b35da264a58886f5
Dakota Walsh <101994734+dakota-portainer@users.noreply.github.com>
2023-08-25 13:12:41 +1200

    fix(registry): ecr secret fix [EE-5673] (#10107)

--- api/http/proxy/factory/kubernetes/deployments.go
+++ api/http/proxy/factory/kubernetes/deployments.go
@@ -6,7 +6,7 @@ import (
 
 func (transport *baseTransport) proxyDeploymentsRequest(request *http.Request, namespace, requestPath string) (*http.Response, error) {
 	switch request.Method {
-	case http.MethodPost, http.MethodPatch:
+	case http.MethodPost, http.MethodPatch, http.MethodPut:
 		transport.refreshRegistry(request, namespace)
 	}
 
