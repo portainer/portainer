2463ed50cc0c713ac2df14f26ae159e9ddfd2673
Chaim Lev-Ari <chaim.levi-ari@portainer.io>
2023-09-04 16:28:19 +0100

    fix(ui/datatable): withGlobalFilter generics

--- app/react/components/datatables/extend-options/withGlobalFilter.ts
+++ app/react/components/datatables/extend-options/withGlobalFilter.ts
@@ -3,9 +3,12 @@ import { TableOptions } from '@tanstack/react-table';
 import { defaultGlobalFilterFn } from '../Datatable';
 import { DefaultType } from '../types';
 
-export function withGlobalFilter<D extends DefaultType>(
-  filterFn: typeof defaultGlobalFilterFn
-) {
+export function withGlobalFilter<
+  D extends DefaultType,
+  TFilter extends {
+    search: string;
+  },
+>(filterFn: typeof defaultGlobalFilterFn<D, TFilter>) {
   return function extendOptions(options: TableOptions<D>) {
     return {
       ...options,
