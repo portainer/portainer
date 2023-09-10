805d57dc486235248601b51dc2390a84528ca68f
Chaim Lev-Ari <chaim.levi-ari@portainer.io>
2023-09-10 16:55:55 +0100

    fix(stacks): redirect api to new api

--- api/http/handler/stacks/handler.go
+++ api/http/handler/stacks/handler.go
@@ -59,7 +59,7 @@ func NewHandler(bouncer security.BouncerService) *Handler {
 	h.Handle("/stacks/create/{type}/{method}",
 		bouncer.AuthenticatedAccess(httperror.LoggerHandler(h.stackCreate))).Methods(http.MethodPost)
 	h.Handle("/stacks",
-		bouncer.AuthenticatedAccess(httperror.LoggerHandler(h.stackCreate))).Methods(http.MethodPost)
+		bouncer.AuthenticatedAccess(httperror.LoggerHandler(h.stackCreateDeprecated))).Methods(http.MethodPost) // Deprecated
 	h.Handle("/stacks",
 		bouncer.AuthenticatedAccess(httperror.LoggerHandler(h.stackList))).Methods(http.MethodGet)
 	h.Handle("/stacks/{id}",
