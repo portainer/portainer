8560930b9a74064bc99df55255899f2db00f945e
Chaim Lev-Ari <chaim.levi-ari@portainer.io>
2023-09-18 15:54:11 +0300

    refactor(docker): remove ng component definition for quick actions

--- app/docker/react/components/services.ts
+++ app/docker/react/components/services.ts
@@ -3,7 +3,6 @@ import angular from 'angular';
 import { r2a } from '@/react-tools/react2angular';
 import { withUIRouter } from '@/react-tools/withUIRouter';
 import { withCurrentUser } from '@/react-tools/withCurrentUser';
-import { TaskTableQuickActions } from '@/react/docker/services/common/TaskTableQuickActions';
 import { ServicesDatatable } from '@/react/docker/services/ListView/ServicesDatatable';
 import { TasksDatatable } from '@/react/docker/services/ItemView/TasksDatatable';
 
@@ -15,13 +14,6 @@ export const servicesModule = angular
       'serviceName',
       'dataset',
       'isSlotColumnVisible',
-    ])
-  )
-  .component(
-    'dockerTaskTableQuickActions',
-    r2a(withUIRouter(withCurrentUser(TaskTableQuickActions)), [
-      'state',
-      'taskId',
     ])
   )
   .component(
