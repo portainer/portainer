d678b155baca202608ed95cd0a1f9322be19b347
Ali <83188384+testA113@users.noreply.github.com>
2023-09-28 11:32:58 +0100

    fix(teasers): updated muted styles from qa feedback [EE-6035] (#10390)
    * fix(teasers): updated muted styles from qa feedback [EE-6035]

--- app/react/components/BoxSelector/LimitedToBeBoxSelectorIndicator.tsx
+++ app/react/components/BoxSelector/LimitedToBeBoxSelectorIndicator.tsx
@@ -5,9 +5,13 @@ import { Tooltip } from '@@/Tip/Tooltip';
 
 interface Props {
   url?: string;
+  showTooltip?: boolean;
 }
 
-export function LimitedToBeBoxSelectorIndicator({ url }: Props) {
+export function LimitedToBeBoxSelectorIndicator({
+  url,
+  showTooltip = true,
+}: Props) {
   return (
     <div className="absolute left-0 top-0 w-full">
       <div className="mx-auto flex max-w-fit items-center rounded-b-lg border border-t-0 border-solid border-gray-6 bg-transparent px-3 py-1 text-gray-6">
@@ -20,10 +24,12 @@ export function LimitedToBeBoxSelectorIndicator({ url }: Props) {
           <Icon icon={Briefcase} className="!mr-1" />
           <span>Business Feature</span>
         </a>
-        <Tooltip
-          size="sm"
-          message="Select this option to preview this business feature."
-        />
+        {showTooltip && (
+          <Tooltip
+            size="sm"
+            message="Select this option to preview this business feature."
+          />
+        )}
       </div>
     </div>
   );
