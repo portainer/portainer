bf7c701cb4d009f6a7cde8fa50314d84f9639bea
Joel Santos <joe@joesantos.io>
2023-10-02 22:41:34 +0100

    Fix #10278 Bitbucket commit url was wrong

--- app/react/docker/stacks/ListView/StacksDatatable/columns/helper.ts
+++ app/react/docker/stacks/ListView/StacksDatatable/columns/helper.ts
@@ -3,3 +3,16 @@ import { createColumnHelper } from '@tanstack/react-table';
 import { DecoratedStack } from '../types';
 
 export const columnHelper = createColumnHelper<DecoratedStack>();
+
+export const getGitCommitUrl = (gitObj: { URL: string; ConfigHash: string}): string => {
+  if (gitObj == null) {
+    return '';
+  }
+
+  const urlPaths = [gitObj.URL];
+  const isBitbucket = gitObj.URL.indexOf('bitbucket.org') !== -1;
+  urlPaths.push(isBitbucket ? 'commits' : 'commit');
+  urlPaths.push(gitObj.ConfigHash);
+
+  return urlPaths.join('/');
+};
