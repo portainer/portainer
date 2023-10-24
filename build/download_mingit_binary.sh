f9e9431c7dbc7cb7ff3d66d04fdff215efbf0303
Chaim Lev-Ari <chaim.levi-ari@portainer.io>
2023-10-24 10:31:10 +0300

    chore(ci): retry failed minigit download

--- build/download_mingit_binary.sh
+++ build/download_mingit_binary.sh
@@ -7,13 +7,12 @@ if [[ $# -ne 3 ]]; then
 fi
 
 PLATFORM=$1
-ARCH=$2
 MINGIT_VERSION=$3
 
 if [[ ${PLATFORM} == "windows" ]]; then
   GIT_VERSION=$(echo $MINGIT_VERSION | cut -d "." -f 1-3)
   GIT_PATCH_VERSION=$(echo $MINGIT_VERSION | cut -d "." -f 4)
 
-  wget https://github.com/git-for-windows/git/releases/download/v$GIT_VERSION.windows.$GIT_PATCH_VERSION/MinGit-$GIT_VERSION.$GIT_PATCH_VERSION-busybox-64-bit.zip
-  unzip MinGit-$GIT_VERSION.$GIT_PATCH_VERSION-busybox-64-bit.zip -d dist/mingit
+  wget --tries=3 --waitretry=30 --quiet "https://github.com/git-for-windows/git/releases/download/v$GIT_VERSION.windows.$GIT_PATCH_VERSION/MinGit-$GIT_VERSION.$GIT_PATCH_VERSION-busybox-64-bit.zip"
+  unzip "MinGit-$GIT_VERSION.$GIT_PATCH_VERSION-busybox-64-bit.zip" -d dist/mingit
 fi
