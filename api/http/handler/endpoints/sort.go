5734ca11d29efe5f4d6a298cc4b9da04a7d285ab
Chaim Lev-Ari <chaim.levi-ari@portainer.io>
2023-11-01 15:15:50 +0200

    refactor(endpoints): sort

--- api/http/handler/endpoints/sort.go
+++ api/http/handler/endpoints/sort.go
@@ -2,23 +2,31 @@ package endpoints
 
 import (
 	"slices"
-	"sort"
 	"strings"
 
 	"github.com/fvbommel/sortorder"
 	portainer "github.com/portainer/portainer/api"
 )
 
-func sortEnvironmentsByField(environments []portainer.Endpoint, environmentGroups []portainer.EndpointGroup, sortField sortKey, isSortDesc bool) {
+type comp[T any] func(a, b T) int
 
+func stringComp(a, b string) int {
+	if sortorder.NaturalLess(a, b) {
+		return -1
+	} else if sortorder.NaturalLess(b, a) {
+		return 1
+	} else {
+		return 0
+	}
+}
+
+func sortEnvironmentsByField(environments []portainer.Endpoint, environmentGroups []portainer.EndpointGroup, sortField sortKey, isSortDesc bool) {
+	var less comp[portainer.Endpoint]
 	switch sortField {
 	case sortKeyName:
-		sortData := EnvironmentsByKey{
-			environments: environments,
-			valueGetter:  func(environment portainer.Endpoint) string { return environment.Name },
-			desc:         isSortDesc,
+		less = func(a, b portainer.Endpoint) int {
+			return stringComp(a.Name, b.Name)
 		}
-		sort.Stable(sortData)
 
 	case sortKeyGroup:
 		environmentGroupNames := make(map[portainer.EndpointGroupID]string, 0)
@@ -26,42 +34,37 @@ func sortEnvironmentsByField(environments []portainer.Endpoint, environmentGroup
 			environmentGroupNames[group.ID] = group.Name
 		}
 
-		sortData := EnvironmentsByKey{
-			environments: environments,
-			valueGetter:  func(environment portainer.Endpoint) string { return environmentGroupNames[environment.GroupID] },
-			desc:         isSortDesc,
-		}
+		less = func(a, b portainer.Endpoint) int {
+			aGroup := environmentGroupNames[a.GroupID]
+			bGroup := environmentGroupNames[b.GroupID]
 
-		sort.Stable(sortData)
+			return stringComp(aGroup, bGroup)
+		}
 
 	case sortKeyStatus:
-		if isSortDesc {
-			sort.Slice(environments, func(i, j int) bool {
-				return environments[i].Status > environments[j].Status
-			})
-		} else {
-			sort.Slice(environments, func(i, j int) bool {
-				return environments[i].Status < environments[j].Status
-			})
+		less = func(a, b portainer.Endpoint) int {
+			return int(a.Status - b.Status)
 		}
-	case sortKeyLastCheckInDate:
-		slices.SortStableFunc(environments, func(a, b portainer.Endpoint) int {
-			mul := 1
-			if isSortDesc {
-				mul = -1
-			}
 
-			return int(a.LastCheckInDate-b.LastCheckInDate) * mul
-		})
-	case sortKeyEdgeID:
-		sortData := EnvironmentsByKey{
-			environments: environments,
-			valueGetter:  func(environment portainer.Endpoint) string { return environment.EdgeID },
-			desc:         isSortDesc,
+	case sortKeyLastCheckInDate:
+		less = func(a, b portainer.Endpoint) int {
+			return int(a.LastCheckInDate - b.LastCheckInDate)
 		}
-		sort.Stable(sortData)
+	case sortKeyEdgeID:
+		less = func(a, b portainer.Endpoint) int {
+			return stringComp(a.EdgeID, b.EdgeID)
+		}
 
 	}
+
+	slices.SortStableFunc(environments, func(a, b portainer.Endpoint) int {
+		mul := 1
+		if isSortDesc {
+			mul = -1
+		}
+
+		return less(a, b) * mul
+	})
 
 }
 
