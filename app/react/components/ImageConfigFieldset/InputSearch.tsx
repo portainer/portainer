c23a4bb8ecc2512d245269b6e2233a67129bc73f
cmeng <cmenginnz@gmail.com>
2023-11-03 16:21:02 +1300

    fix(container): allow creating non-existing option EE-6287

--- app/react/components/ImageConfigFieldset/InputSearch.tsx
+++ app/react/components/ImageConfigFieldset/InputSearch.tsx
@@ -22,6 +22,7 @@ export function InputSearch({
 
   return (
     <Select
+      isCreatable
       options={options}
       value={selectValue}
       onChange={(option) => option && onChange(option.value)}
