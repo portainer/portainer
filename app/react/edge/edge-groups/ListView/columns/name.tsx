99b39da03d958328390318f8e1a9c4da671e2559
Chaim Lev-Ari <chiptus@users.noreply.github.com>
2023-11-14 12:57:27 +0200

    refactor(edge/groups): migrate view to react [EE-4683] (#10592)

---
+++ app/react/edge/edge-groups/ListView/columns/name.tsx
@@ -0,0 +1,34 @@
+import { CellContext } from '@tanstack/react-table';
+
+import { Link } from '@@/Link';
+
+import { EdgeGroupListItemResponse } from '../../queries/useEdgeGroups';
+
+import { columnHelper } from './helper';
+
+export const name = columnHelper.accessor('Name', {
+  header: 'Name',
+  cell: NameCell,
+});
+
+function NameCell({
+  renderValue,
+  row: { original: item },
+}: CellContext<EdgeGroupListItemResponse, unknown>) {
+  const name = renderValue() || '';
+
+  if (typeof name !== 'string') {
+    return null;
+  }
+
+  return (
+    <>
+      <Link to=".edit" params={{ groupId: item.Id }} title={name}>
+        {name}
+      </Link>
+      {(item.HasEdgeJob || item.HasEdgeStack) && (
+        <span className="label label-info image-tag space-left">in use</span>
+      )}
+    </>
+  );
+}
--- app/react/edge/edge-groups/queries/build-url.ts
+++ app/react/edge/edge-groups/queries/build-url.ts
@@ -1,3 +1,10 @@
-export function buildUrl() {
-  return '/edge_groups';
+import { EdgeGroup } from '../types';
+
+export function buildUrl({
+  action,
+  id,
+}: { id?: EdgeGroup['Id']; action?: string } = {}) {
+  const baseUrl = '/edge_groups';
+  const url = id ? `${baseUrl}/${id}` : baseUrl;
+  return action ? `${url}/${action}` : url;
 }
