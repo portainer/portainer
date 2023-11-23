664277d765806fadabc085a9b4f580e489589e26
Chaim Lev-Ari <chaim.levi-ari@portainer.io>
2023-11-23 02:39:04 +0200

    fix(users): use users keys

--- app/react/portainer/account/access-tokens/queries/query-keys.ts
+++ app/react/portainer/account/access-tokens/queries/query-keys.ts
@@ -1,4 +1,4 @@
-import { queryKeys as userQueryKeys } from '@/portainer/users/queries/queryKeys';
+import { userQueryKeys } from '@/portainer/users/queries/queryKeys';
 import { UserId } from '@/portainer/users/types';
 
 export const queryKeys = {
