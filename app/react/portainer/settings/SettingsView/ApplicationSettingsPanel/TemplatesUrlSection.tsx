9c0698e4b47e044f288fdee1c375a45b2940eb02
Chaim Lev-Ari <chaim.levi-ari@portainer.io>
2023-11-29 08:11:47 +0200

    fix(app/templates): show default url in settings [EE-6393]
    fix [EE-6393]

--- app/react/portainer/settings/SettingsView/ApplicationSettingsPanel/TemplatesUrlSection.tsx
+++ app/react/portainer/settings/SettingsView/ApplicationSettingsPanel/TemplatesUrlSection.tsx
@@ -4,30 +4,40 @@ import { FormControl } from '@@/form-components/FormControl';
 import { FormSection } from '@@/form-components/FormSection';
 import { Input } from '@@/form-components/Input';
 
+// this value is taken from https://github.com/portainer/portainer/blob/develop/api/portainer.go#L1628
+const DEFAULT_URL =
+  'https://raw.githubusercontent.com/portainer/templates/v3/templates.json';
+
 export function TemplatesUrlSection() {
   const [{ name }, { error }] = useField<string>('templatesUrl');
+
   return (
     <FormSection title="App Templates">
       <div className="form-group">
-        <span className="col-sm-12 text-muted small">
-          You can specify the URL to your own template definitions file here.
-          See{' '}
-          <a
-            href="https://docs.portainer.io/advanced/app-templates/build"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Portainer documentation
-          </a>{' '}
-          for more details.
-        </span>
+        <div className="col-sm-12 text-muted small">
+          <p>
+            You can specify the URL to your own template definitions file here.
+            See{' '}
+            <a
+              href="https://docs.portainer.io/advanced/app-templates/build"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Portainer documentation
+            </a>{' '}
+            for more details.
+          </p>
+          <p>
+            The default value is <a href={DEFAULT_URL}>{DEFAULT_URL}</a>
+          </p>
+        </div>
       </div>
 
       <FormControl label="URL" inputId="templates_url" errors={error}>
         <Field
           as={Input}
           id="templates_url"
-          placeholder="https://myserver.mydomain/templates.json"
+          placeholder={DEFAULT_URL}
           data-cy="settings-templateUrl"
           name={name}
         />
