166ccf308a68d99becde8525f6dd8a73e40f8ca5
Chaim Lev-Ari <chaim.levi-ari@portainer.io>
2023-12-06 09:00:40 +0100

    fix(users): sync with EE

--- app/react/portainer/users/ListView/UsersDatatable/columns/role.tsx
+++ app/react/portainer/users/ListView/UsersDatatable/columns/role.tsx
@@ -2,13 +2,22 @@ import { User, UserPlus } from 'lucide-react';
 
 import { isEdgeAdmin } from '@/portainer/users/user.helpers';
 import { RoleNames } from '@/portainer/users/types';
+import { isBE } from '@/react/portainer/feature-flags/feature-flags.service';
 
 import { Icon } from '@@/Icon';
 
 import { helper } from './helper';
 
 export const role = helper.accessor(
-  (item) => (item.isTeamLeader ? 'team leader' : RoleNames[item.Role]),
+  (item) => {
+    if (isBE) {
+      return `${RoleNames[item.Role]} ${
+        item.isTeamLeader ? ' - team leader' : ''
+      }`.trim();
+    }
+
+    return item.isTeamLeader ? 'team leader' : RoleNames[item.Role];
+  },
   {
     header: 'Role',
     cell: ({ getValue, row: { original: item } }) => {
