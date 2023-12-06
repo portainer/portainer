0362b70d746683df6b8d4028e4720f1f0ce959da
Chaim Lev-Ari <chaim.levi-ari@portainer.io>
2023-12-06 09:31:55 +0100

    refactor(rbac): migrate roles table to react [EE-4711]
    fix [EE-4711]

--- app/portainer/rbac/index.js
+++ app/portainer/rbac/index.js
@@ -2,6 +2,7 @@ import { AccessHeaders } from '../authorization-guard';
 import { rolesView } from './views/roles';
 import { accessViewer } from './components/access-viewer';
 import { rolesDatatable } from './components/roles-datatable';
+import { accessViewerDatatable } from './components/access-viewer/access-viewer-datatable';
 
 import { RoleService } from './services/role.service';
 import { RolesFactory } from './services/role.rest';
@@ -11,6 +12,7 @@ angular
   .constant('API_ENDPOINT_ROLES', 'api/roles')
   .component('accessViewer', accessViewer)
   .component('rolesDatatable', rolesDatatable)
+  .component('accessViewerDatatable', accessViewerDatatable)
   .component('rolesView', rolesView)
   .factory('RoleService', RoleService)
   .factory('Roles', RolesFactory)
