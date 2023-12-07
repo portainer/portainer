f94fb7fedde94429744d99fdf2934744dac33bb5
Chaim Lev-Ari <chaim.levi-ari@portainer.io>
2023-12-07 10:04:02 +0100

    fix(gitops): fix cursor jumps

--- app/react/portainer/gitops/AuthFieldset/AuthFieldset.tsx
+++ app/react/portainer/gitops/AuthFieldset/AuthFieldset.tsx
@@ -1,5 +1,6 @@
 import { FormikErrors } from 'formik';
 import { boolean, number, object, SchemaOf, string } from 'yup';
+import { useState } from 'react';
 
 import { GitAuthModel } from '@/react/portainer/gitops/types';
 import { useDebounce } from '@/react/hooks/useDebounce';
@@ -23,11 +24,12 @@ interface Props {
 }
 
 export function AuthFieldset({
-  value,
+  value: initialValue,
   onChange,
   isAuthExplanationVisible,
   errors,
 }: Props) {
+  const [value, setValue] = useState(initialValue); // TODO: remove this state when form is not inside angularjs
   const [username, setUsername] = useDebounce(
     value.RepositoryUsername || '',
     (username) => handleChange({ RepositoryUsername: username })
@@ -139,6 +141,7 @@ export function AuthFieldset({
 
   function handleChange(partialValue: Partial<GitAuthModel>) {
     onChange(partialValue);
+    setValue((value) => ({ ...value, ...partialValue }));
   }
 }
 
