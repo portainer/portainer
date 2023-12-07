e0c49d1d23730c1ce6c6e697ac61c710ebe4eeb0
Chaim Lev-Ari <chaim.levi-ari@portainer.io>
2023-12-07 11:52:09 +0100

    refactor(registries): migrate gitlab projects table to react [EE-4709]
    fix [EE-4709]

--- app/portainer/react/components/registries.ts
+++ app/portainer/react/components/registries.ts
@@ -5,6 +5,7 @@ import { withReactQuery } from '@/react-tools/withReactQuery';
 import { withUIRouter } from '@/react-tools/withUIRouter';
 import { RepositoriesDatatable } from '@/react/portainer/registries/repositories/ListView/RepositoriesDatatable';
 import { TagsDatatable } from '@/react/portainer/registries/repositories/ItemView/TagsDatatable/TagsDatatable';
+import { GitlabProjectTable } from '@/react/portainer/registries/CreateView/GitlabProjectsTable/GitlabProjectsTable';
 
 export const registriesModule = angular
   .module('portainer.app.react.components.registries', [])
@@ -20,4 +21,8 @@ export const registriesModule = angular
       'onRemove',
       'onRetag',
     ])
+  )
+  .component(
+    'gitlabProjectSelector',
+    r2a(GitlabProjectTable, ['dataset', 'onChange', 'value'])
   ).name;
