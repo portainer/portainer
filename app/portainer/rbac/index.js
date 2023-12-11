9dfcbfb2570092564dfd6e66fd4def039ddcf932
Chaim Lev-Ari <chaim.levi-ari@portainer.io>
2023-12-11 12:48:59 +0200

    refactor(rbac): migrate access viewer table to react [EE-6447]
    fix [EE-6447]

--- app/portainer/rbac/index.js
+++ app/portainer/rbac/index.js
@@ -1,7 +1,6 @@
 import { AccessHeaders } from '../authorization-guard';
 import { rolesView } from './views/roles';
 import { accessViewer } from './components/access-viewer';
-import { accessViewerDatatable } from './components/access-viewer/access-viewer-datatable';
 import { rolesDatatable } from './components/roles-datatable';
 
 import { RoleService } from './services/role.service';
@@ -11,7 +10,6 @@ angular
   .module('portainer.rbac', ['ngResource'])
   .constant('API_ENDPOINT_ROLES', 'api/roles')
   .component('accessViewer', accessViewer)
-  .component('accessViewerDatatable', accessViewerDatatable)
   .component('rolesDatatable', rolesDatatable)
   .component('rolesView', rolesView)
   .factory('RoleService', RoleService)
---
+++ app/react/portainer/users/RolesView/AccessViewer/EffectiveAccessViewerDatatable.tsx
@@ -0,0 +1,36 @@
+import { TextTip } from '@@/Tip/TextTip';
+import { Datatable } from '@@/datatables';
+import { useTableStateWithStorage } from '@@/datatables/useTableState';
+
+import { AccessViewerPolicyModel } from './model';
+import { columns } from './columns';
+
+export function EffectiveAccessViewerDatatable({
+  dataset,
+}: {
+  dataset?: Array<AccessViewerPolicyModel>;
+}) {
+  const tableState = useTableStateWithStorage('access-viewer', 'Environment');
+
+  return (
+    <Datatable
+      dataset={dataset || []}
+      columns={columns}
+      settingsManager={tableState}
+      noWidget
+      title="Access"
+      description={
+        <TextTip color="blue">
+          Effective role for each environment will be displayed for the selected
+          user
+        </TextTip>
+      }
+      emptyContentLabel={
+        dataset
+          ? 'The selected user does not have access to any environment(s)'
+          : 'Select a user to show associated access and role'
+      }
+      disableSelect
+    />
+  );
+}
