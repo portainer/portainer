25741e8c4c7351b8e75db92b96847bae2036da3f
Chaim Lev-Ari <chiptus@users.noreply.github.com>
2023-12-13 11:10:29 +0200

    feat(edge): sort waiting room table [EE-6259] (#10577)

--- app/react/edge/edge-devices/WaitingRoomView/Datatable/columns.ts
+++ app/react/edge/edge-devices/WaitingRoomView/Datatable/columns.ts
@@ -8,27 +8,32 @@ const columnHelper = createColumnHelper<WaitingRoomEnvironment>();
 export const columns = [
   columnHelper.accessor('Name', {
     header: 'Name',
-    id: 'name',
+    id: 'Name',
   }),
   columnHelper.accessor('EdgeID', {
     header: 'Edge ID',
-    id: 'edge-id',
+    id: 'EdgeID',
   }),
-  columnHelper.accessor((row) => row.EdgeGroups.join(', ') || '-', {
+  columnHelper.accessor((row) => row.EdgeGroups.join(', '), {
     header: 'Edge Groups',
     id: 'edge-groups',
+    enableSorting: false,
+    cell: ({ getValue }) => getValue() || '-',
   }),
-  columnHelper.accessor((row) => row.Group || '-', {
+  columnHelper.accessor((row) => row.Group, {
     header: 'Group',
-    id: 'group',
+    id: 'Group',
+    cell: ({ getValue }) => getValue() || '-',
   }),
-  columnHelper.accessor((row) => row.Tags.join(', ') || '-', {
+  columnHelper.accessor((row) => row.Tags.join(', '), {
     header: 'Tags',
     id: 'tags',
+    enableSorting: false,
+    cell: ({ getValue }) => getValue() || '-',
   }),
   columnHelper.accessor((row) => row.LastCheckInDate, {
     header: 'Last Check-in',
-    id: 'last-check-in',
+    id: 'LastCheckIn',
     cell: ({ getValue }) => {
       const value = getValue();
       return value ? moment(value * 1000).fromNow() : '-';
