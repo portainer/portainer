23ceb70d27cfdce66788d726352d9c1c969f9b81
prabhat khera <prabhat.khera@portainer.io>
2023-12-19 13:49:35 +1300

    revert #10765

--- app/react/portainer/settings/SettingsView/BackupSettingsView/BackupSettingsPanel.tsx
+++ app/react/portainer/settings/SettingsView/BackupSettingsView/BackupSettingsPanel.tsx
@@ -1,6 +1,5 @@
 import { Download } from 'lucide-react';
-
-import { useLocalStorage } from '@/react/hooks/useLocalStorage';
+import { useState } from 'react';
 
 import { Widget, WidgetBody, WidgetTitle } from '@@/Widget';
 import { FormSection } from '@@/form-components/FormSection';
@@ -11,10 +10,7 @@ import { BackupFileForm } from './BackupFileForm';
 import { BackupS3Form } from './BackupS3Form';
 
 export function BackupSettingsPanel() {
-  const [backupType, setBackupType] = useLocalStorage(
-    'settings_backup_type',
-    options[0].value
-  );
+  const [backupType, setBackupType] = useState(options[0].value);
 
   return (
     <Widget>
