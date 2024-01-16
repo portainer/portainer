f0b8d57abdd69d842b0f114f563ad4e9c2904730
testa113 <testa113>
2024-01-16 15:10:44 +1300

    fix(app): remove canUndo function from environment variables [EE-6232]

--- app/react/kubernetes/applications/components/EnvironmentVariablesFormSection/kubeEnvVarValidationSchema.ts
+++ app/react/kubernetes/applications/components/EnvironmentVariablesFormSection/kubeEnvVarValidationSchema.ts
@@ -14,6 +14,7 @@ export function kubeEnvVarValidationSchema(): SchemaOf<EnvVar[]> {
         ),
       value: string().default(''),
       needsDeletion: bool().default(false),
+      isNew: bool().default(false),
     })
   ).test(
     'unique',
