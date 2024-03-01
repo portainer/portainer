7c1ab29050855c1708b922295c4f1113918cf909
hookenz <hookenz@gmail.com>
2024-03-01 15:05:47 +1300

    match EE

--- app/react/components/PageHeader/PageHeader.test.tsx
+++ app/react/components/PageHeader/PageHeader.test.tsx
@@ -1,24 +1,18 @@
-import { QueryClient, QueryClientProvider } from 'react-query';
-
 import { UserContext } from '@/react/hooks/useUser';
 import { UserViewModel } from '@/portainer/models/user';
-import { render } from '@/react-tools/test-utils';
+import { renderWithQueryClient } from '@/react-tools/test-utils';
 
 import { PageHeader } from './PageHeader';
 
 test('should display a PageHeader', async () => {
   const username = 'username';
   const user = new UserViewModel({ Username: username });
-  const queryClient = new QueryClient();
 
   const title = 'title';
-
-  const { queryByText } = render(
-    <QueryClientProvider client={queryClient}>
-      <UserContext.Provider value={{ user }}>
-        <PageHeader title={title} />
-      </UserContext.Provider>
-    </QueryClientProvider>
+  const { queryByText } = renderWithQueryClient(
+    <UserContext.Provider value={{ user }}>
+      <PageHeader title={title} />
+    </UserContext.Provider>
   );
 
   const heading = queryByText(title);
