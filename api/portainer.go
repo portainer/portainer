1b004d130c0d52313bc0c0d52bc28ea66c8e9cc9
prabhat khera <prabhat.khera@portainer.io>
2024-03-21 15:45:36 +1300

    chore(version): version bump to 2.22.0 [EE-6897]

--- api/portainer.go
+++ api/portainer.go
@@ -1595,7 +1595,7 @@ type (
 
 const (
 	// APIVersion is the version number of the Portainer API
-	APIVersion = "2.21.0"
+	APIVersion = "2.22.0"
 	// Edition is what this edition of Portainer is called
 	Edition = PortainerCE
 	// ComposeSyntaxMaxVersion is a maximum supported version of the docker compose syntax
