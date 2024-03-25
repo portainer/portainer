44ef5bb12abe818078c2a037367aa9b9e60c6827
andres-portainer <91705312+andres-portainer@users.noreply.github.com>
2024-03-25 14:19:38 -0300

    fix(kubernetes): avoid a deadlock EE-6901 (#11442)

--- api/kubernetes/cli/client.go
+++ api/kubernetes/cli/client.go
@@ -81,20 +81,29 @@ func (factory *ClientFactory) RemoveKubeClient(endpointID portainer.EndpointID) 
 // If no client is registered, it will create a new client, register it, and returns it.
 func (factory *ClientFactory) GetKubeClient(endpoint *portainer.Endpoint) (*KubeClient, error) {
 	factory.mu.Lock()
+	key := strconv.Itoa(int(endpoint.ID))
+	if client, ok := factory.endpointClients[key]; ok {
+		factory.mu.Unlock()
+		return client, nil
+	}
+	factory.mu.Unlock()
+
+	// EE-6901: Do not lock
+	client, err := factory.createCachedAdminKubeClient(endpoint)
+	if err != nil {
+		return nil, err
+	}
+
+	factory.mu.Lock()
 	defer factory.mu.Unlock()
 
-	key := strconv.Itoa(int(endpoint.ID))
-	client, ok := factory.endpointClients[key]
-	if !ok {
-		var err error
-
-		client, err = factory.createCachedAdminKubeClient(endpoint)
-		if err != nil {
-			return nil, err
-		}
-
-		factory.endpointClients[key] = client
+	// The lock was released before the client was created,
+	// so we need to check again
+	if c, ok := factory.endpointClients[key]; ok {
+		return c, nil
 	}
+
+	factory.endpointClients[key] = client
 
 	return client, nil
 }
