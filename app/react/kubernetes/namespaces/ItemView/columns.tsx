9c68c6c9f3f5fbc084d1875ac17a048644c6b951
Chaim Lev-Ari <chiptus@users.noreply.github.com>
2024-04-02 22:55:34 +0300

    refactor(kube/namespaces): migrate item apps table to react [EE-4693] (#11012)

---
+++ app/react/kubernetes/namespaces/ItemView/columns.tsx
@@ -0,0 +1,55 @@
+import { createColumnHelper } from '@tanstack/react-table';
+
+import { humanize, truncate } from '@/portainer/filters/filters';
+
+import { Link } from '@@/Link';
+import { ExternalBadge } from '@@/Badge/ExternalBadge';
+
+import { isExternalApplication } from '../../applications/utils';
+import { cpuHumanValue } from '../../applications/utils/cpuHumanValue';
+
+import { NamespaceApp } from './types';
+
+const columnHelper = createColumnHelper<NamespaceApp>();
+
+export const columns = [
+  columnHelper.accessor('Name', {
+    header: 'Name',
+    cell: ({ row: { original: item } }) => (
+      <>
+        <Link
+          to="kubernetes.applications.application"
+          params={{ name: item.Name, namespace: item.ResourcePool }}
+        >
+          {item.Name}
+        </Link>
+        {isExternalApplication({ metadata: item.Metadata }) && (
+          <div className="ml-2">
+            <ExternalBadge />
+          </div>
+        )}
+      </>
+    ),
+  }),
+  columnHelper.accessor('StackName', {
+    header: 'Stack',
+    cell: ({ getValue }) => getValue() || '-',
+  }),
+  columnHelper.accessor('Image', {
+    header: 'Image',
+    cell: ({ row: { original: item } }) => (
+      <>
+        {truncate(item.Image, 64)}
+        {item.Containers?.length > 1 && <>+ {item.Containers.length - 1}</>}
+      </>
+    ),
+  }),
+  columnHelper.accessor('CPU', {
+    header: 'CPU',
+    cell: ({ getValue }) => cpuHumanValue(getValue()),
+  }),
+  columnHelper.accessor('Memory', {
+    header: 'Memory',
+    cell: ({ getValue }) => humanize(getValue()),
+  }),
+];
