194b6e491d1b4742a51029119d218d82c68726fc
Ali <83188384+testA113@users.noreply.github.com>
2024-04-04 08:18:13 +1300

    fix(namespace): wait for system ns setting to load before selecting existing ns [EE-6917] (#11481)
    Co-authored-by: testa113 <testa113>

--- app/react/kubernetes/datatables/SystemResourceDescription.tsx
+++ app/react/kubernetes/datatables/SystemResourceDescription.tsx
@@ -3,11 +3,11 @@ import { Authorized } from '@/react/hooks/useUser';
 import { TextTip } from '@@/Tip/TextTip';
 
 interface Props {
-  showSystemResources: boolean;
+  showSystemResources?: boolean;
 }
 
 export function SystemResourceDescription({ showSystemResources }: Props) {
-  return !showSystemResources ? (
+  return showSystemResources === false ? (
     <Authorized authorizations="K8sAccessSystemNamespaces" adminOnlyCE>
       <TextTip color="blue" className="!mb-0">
         System resources are hidden, this can be changed in the table settings
