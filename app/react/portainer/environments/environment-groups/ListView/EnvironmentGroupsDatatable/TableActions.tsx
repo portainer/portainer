d1ba484be11b84105c347c1813058a1669a99b3d
Chaim Lev-Ari <chiptus@users.noreply.github.com>
2024-04-04 18:54:57 +0300

    refactor(env/groups): migrate list view to react [EE-4703] (#10671)

---
+++ app/react/portainer/environments/environment-groups/ListView/EnvironmentGroupsDatatable/TableActions.tsx
@@ -0,0 +1,37 @@
+import { notifySuccess } from '@/portainer/services/notifications';
+
+import { DeleteButton } from '@@/buttons/DeleteButton';
+import { AddButton } from '@@/buttons';
+
+import { EnvironmentGroup } from '../../types';
+
+import { useDeleteEnvironmentGroupsMutation } from './useDeleteEnvironmentGroupsMutation';
+
+export function TableActions({
+  selectedItems,
+}: {
+  selectedItems: EnvironmentGroup[];
+}) {
+  const deleteMutation = useDeleteEnvironmentGroupsMutation();
+
+  return (
+    <>
+      <DeleteButton
+        disabled={selectedItems.length === 0}
+        confirmMessage="Are you sure you want to remove the selected environment group(s)?"
+        onConfirmed={handleRemove}
+      />
+
+      <AddButton>Add group</AddButton>
+    </>
+  );
+
+  function handleRemove() {
+    const ids = selectedItems.map((item) => item.Id);
+    deleteMutation.mutate(ids, {
+      onSuccess() {
+        notifySuccess('Success', 'Environment Group(s) removed');
+      },
+    });
+  }
+}
