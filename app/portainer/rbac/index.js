8a36cd02080ca2d0c1f48c351f26d2b5f8c0d8f0
Chaim Lev-Ari <chaim.levi-ari@portainer.io>
2024-04-08 17:48:21 +0300

    fix(rbac): remove unused ng definitions

--- app/portainer/rbac/index.js
+++ app/portainer/rbac/index.js
@@ -1,8 +1,6 @@
 import { AccessHeaders } from '../authorization-guard';
 import { rolesView } from './views/roles';
 import { accessViewer } from './components/access-viewer';
-import { rolesDatatable } from './components/roles-datatable';
-import { accessViewerDatatable } from './components/access-viewer/access-viewer-datatable';
 
 import { RoleService } from './services/role.service';
 import { RolesFactory } from './services/role.rest';
@@ -11,8 +9,6 @@ angular
   .module('portainer.rbac', ['ngResource'])
   .constant('API_ENDPOINT_ROLES', 'api/roles')
   .component('accessViewer', accessViewer)
-  .component('rolesDatatable', rolesDatatable)
-  .component('accessViewerDatatable', accessViewerDatatable)
   .component('rolesView', rolesView)
   .factory('RoleService', RoleService)
   .factory('Roles', RolesFactory)
