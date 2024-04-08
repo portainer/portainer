45be6c2b45d68147cf5978d865e69ab42143bce7
Chaim Lev-Ari <chiptus@users.noreply.github.com>
2024-04-08 17:23:49 +0300

    refactor(tags): migrate tags to react [EE-4707] (#10771)

--- app/portainer/react/components/environments.ts
+++ app/portainer/react/components/environments.ts
@@ -3,11 +3,10 @@ import angular from 'angular';
 import { r2a } from '@/react-tools/react2angular';
 import { EdgeKeyDisplay } from '@/react/portainer/environments/ItemView/EdgeKeyDisplay';
 import { KVMControl } from '@/react/portainer/environments/KvmView/KVMControl';
+import { TagsDatatable } from '@/react/portainer/environments/TagsView/TagsDatatable';
 
 export const environmentsModule = angular
   .module('portainer.app.react.components.environments', [])
   .component('edgeKeyDisplay', r2a(EdgeKeyDisplay, ['edgeKey']))
-  .component(
-    'kvmControl',
-    r2a(KVMControl, ['deviceId', 'server', 'token'])
-  ).name;
+  .component('kvmControl', r2a(KVMControl, ['deviceId', 'server', 'token']))
+  .component('tagsDatatable', r2a(TagsDatatable, ['dataset', 'onRemove'])).name;
