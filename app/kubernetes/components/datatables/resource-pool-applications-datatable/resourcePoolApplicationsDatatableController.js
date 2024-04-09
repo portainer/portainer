f69338370ffbc837f396f8ed864fbd924e77b32a
testa113 <aliharriss1995@gmail.com>
2024-04-09 15:08:55 +1200

    fix(stacks): conditionally hide node and namespace stacks [EE-6949]

--- app/kubernetes/components/datatables/resource-pool-applications-datatable/resourcePoolApplicationsDatatableController.js
+++ app/kubernetes/components/datatables/resource-pool-applications-datatable/resourcePoolApplicationsDatatableController.js
@@ -1,10 +1,12 @@
 import KubernetesApplicationHelper from 'Kubernetes/helpers/application';
+import { getDeploymentOptions } from '@/react/portainer/environments/environment.service';
 
 angular.module('portainer.docker').controller('KubernetesResourcePoolApplicationsDatatableController', [
   '$scope',
   '$controller',
+  '$async',
   'DatatableService',
-  function ($scope, $controller, DatatableService) {
+  function ($scope, $controller, $async, DatatableService) {
     angular.extend(this, $controller('GenericDatatableController', { $scope: $scope }));
 
     this.isExternalApplication = function (item) {
@@ -12,36 +14,40 @@ angular.module('portainer.docker').controller('KubernetesResourcePoolApplication
     };
 
     this.$onInit = function () {
-      this.setDefaults();
-      this.prepareTableFromDataset();
+      return $async(async () => {
+        this.setDefaults();
+        this.prepareTableFromDataset();
 
-      this.state.orderBy = this.orderBy;
-      var storedOrder = DatatableService.getDataTableOrder(this.tableKey);
-      if (storedOrder !== null) {
-        this.state.reverseOrder = storedOrder.reverse;
-        this.state.orderBy = storedOrder.orderBy;
-      }
+        this.deploymentOptions = await getDeploymentOptions();
 
-      var textFilter = DatatableService.getDataTableTextFilters(this.tableKey);
-      if (textFilter !== null) {
-        this.state.textFilter = textFilter;
-        this.onTextFilterChange();
-      }
+        this.state.orderBy = this.orderBy;
+        var storedOrder = DatatableService.getDataTableOrder(this.tableKey);
+        if (storedOrder !== null) {
+          this.state.reverseOrder = storedOrder.reverse;
+          this.state.orderBy = storedOrder.orderBy;
+        }
 
-      var storedFilters = DatatableService.getDataTableFilters(this.tableKey);
-      if (storedFilters !== null) {
-        this.filters = storedFilters;
-      }
-      if (this.filters && this.filters.state) {
-        this.filters.state.open = false;
-      }
+        var textFilter = DatatableService.getDataTableTextFilters(this.tableKey);
+        if (textFilter !== null) {
+          this.state.textFilter = textFilter;
+          this.onTextFilterChange();
+        }
 
-      var storedSettings = DatatableService.getDataTableSettings(this.tableKey);
-      if (storedSettings !== null) {
-        this.settings = storedSettings;
-        this.settings.open = false;
-      }
-      this.onSettingsRepeaterChange();
+        var storedFilters = DatatableService.getDataTableFilters(this.tableKey);
+        if (storedFilters !== null) {
+          this.filters = storedFilters;
+        }
+        if (this.filters && this.filters.state) {
+          this.filters.state.open = false;
+        }
+
+        var storedSettings = DatatableService.getDataTableSettings(this.tableKey);
+        if (storedSettings !== null) {
+          this.settings = storedSettings;
+          this.settings.open = false;
+        }
+        this.onSettingsRepeaterChange();
+      });
     };
   },
 ]);
