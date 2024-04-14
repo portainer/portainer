4a0f97a8eacb5248383385c7f90af0223639d3a2
Chaim Lev-Ari <chaim.levi-ari@portainer.io>
2024-04-14 18:57:28 +0300

    fix(app): use tanstack query

--- app/react/portainer/environments/queries/useDisassociateEdgeEnvironment.ts
+++ app/react/portainer/environments/queries/useDisassociateEdgeEnvironment.ts
@@ -1,4 +1,4 @@
-import { useMutation } from 'react-query';
+import { useMutation } from '@tanstack/react-query';
 
 import { useAnalytics } from '@/react/hooks/useAnalytics';
 import axios, { parseAxiosError } from '@/portainer/services/axios';
