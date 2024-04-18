cb8b6bdf1e8f9091a7d4d1ed36be5b742166ec01
andres-portainer <andres-portainer@users.noreply.github.com>
2024-04-18 14:55:35 -0300

    Fix binary downloader script.

--- build/download_mingit_binary.sh
+++ build/download_mingit_binary.sh
@@ -13,6 +13,6 @@ if [[ ${PLATFORM} == "windows" ]]; then
   GIT_VERSION=$(echo $MINGIT_VERSION | cut -d "." -f 1-3)
   GIT_PATCH_VERSION=$(echo $MINGIT_VERSION | cut -d "." -f 4)
 
-  wget --tries=3 --waitretry=30 --quiet "https://github.com/git-for-windows/git/releases/download/v$GIT_VERSION.windows.$GIT_PATCH_VERSION/MinGit-$GIT_VERSION.$GIT_PATCH_VERSION-busybox-64-bit.zip"
-  unzip "MinGit-$GIT_VERSION.$GIT_PATCH_VERSION-busybox-64-bit.zip" -d dist/mingit
+  wget --tries=3 --waitretry=30 --quiet "https://github.com/git-for-windows/git/releases/download/v$GIT_VERSION.windows.$GIT_PATCH_VERSION/MinGit-$GIT_VERSION-busybox-64-bit.zip"
+  unzip "MinGit-$GIT_VERSION-busybox-64-bit.zip" -d dist/mingit
 fi
