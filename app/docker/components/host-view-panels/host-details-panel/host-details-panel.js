2ecc4df7f24c03e919ddb2feb1943e88963cc99b
cmeng <cmenginnz@gmail.com>
2024-05-29 14:44:05 +1200

    fix(host-info) hide browse button if /host folder does not exist EE-7075

--- app/docker/components/host-view-panels/host-details-panel/host-details-panel.js
+++ app/docker/components/host-view-panels/host-details-panel/host-details-panel.js
@@ -1,8 +1,12 @@
+import { HostDetailsPanelController } from './host-details-panel-controller';
+
 angular.module('portainer.docker').component('hostDetailsPanel', {
   templateUrl: './host-details-panel.html',
+  controller: HostDetailsPanelController,
   bindings: {
     host: '<',
     isBrowseEnabled: '<',
     browseUrl: '@',
+    environmentId: '<',
   },
 });
