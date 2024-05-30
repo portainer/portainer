96ee114dbbbdd9e1b9a41235007a97917196d0df
Chaim Lev-Ari <chaim.levi-ari@portainer.io>
2024-05-30 14:23:44 +0300

    refactor(edge/jobs): use recurring request method

--- app/react/edge/edge-jobs/ItemView/UpdateEdgeJobForm/getPayload.tsx
+++ app/react/edge/edge-jobs/ItemView/UpdateEdgeJobForm/getPayload.tsx
@@ -1,6 +1,5 @@
-import moment from 'moment';
-
 import { UpdatePayload } from '../../queries/useUpdateEdgeJobMutation';
+import { toRecurringRequest } from '../../components/EdgeJobForm/parseRecurringValues';
 
 import { FormValues } from './types';
 
@@ -9,42 +8,7 @@ export function getPayload(values: FormValues): UpdatePayload {
     name: values.name,
     edgeGroups: values.edgeGroupIds,
     endpoints: values.environmentIds,
-    ...getRecurringConfig(values),
     fileContent: values.fileContent,
+    ...toRecurringRequest(values),
   };
-
-  function getRecurringConfig(values: FormValues): {
-    recurring: boolean;
-    cronExpression: string;
-  } {
-    if (values.cronMethod !== 'basic') {
-      return {
-        recurring: true,
-        cronExpression: values.cronExpression,
-      };
-    }
-
-    if (values.recurring) {
-      return {
-        recurring: true,
-        cronExpression: values.recurringOption,
-      };
-    }
-
-    return {
-      recurring: false,
-      cronExpression: dateTimeToCron(values.dateTime),
-    };
-
-    function dateTimeToCron(datetime: Date) {
-      const date = moment(datetime);
-      return [
-        date.minutes(),
-        date.hours(),
-        date.date(),
-        date.month() + 1,
-        '*',
-      ].join(' ');
-    }
-  }
 }
