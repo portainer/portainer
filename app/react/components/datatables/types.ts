bf0c15f9ad4cc90b1f6d86f002614a209b46e87d
Chaim Lev-Ari <chaim.levi-ari@portainer.io>
2024-05-30 17:33:26 +0300

    feat(edge/stacks): default refresh rate to 10s

--- app/react/components/datatables/types.ts
+++ app/react/components/datatables/types.ts
@@ -53,10 +53,11 @@ export interface SettableColumnsTableSettings {
 }
 
 export function hiddenColumnsSettings<T extends SettableColumnsTableSettings>(
-  set: ZustandSetFunc<T>
+  set: ZustandSetFunc<T>,
+  initialHiddenColumns: string[] = []
 ): SettableColumnsTableSettings {
   return {
-    hiddenColumns: [],
+    hiddenColumns: initialHiddenColumns,
     setHiddenColumns: (hiddenColumns: string[]) =>
       set((s) => ({ ...s, hiddenColumns })),
   };
@@ -68,10 +69,11 @@ export interface RefreshableTableSettings {
 }
 
 export function refreshableSettings<T extends RefreshableTableSettings>(
-  set: ZustandSetFunc<T>
+  set: ZustandSetFunc<T>,
+  autoRefreshRate = 0
 ): RefreshableTableSettings {
   return {
-    autoRefreshRate: 0,
+    autoRefreshRate,
     setAutoRefreshRate: (autoRefreshRate: number) =>
       set((s) => ({ ...s, autoRefreshRate })),
   };
@@ -113,6 +115,7 @@ export function createPersistedStore<T extends BasicTableSettings>(
         }) as T,
       {
         name: keyBuilder(`datatable_settings_${storageKey}`),
+        version: 1,
       }
     )
   );
