878e416a7299afe391ae504f9a234aec0ca83336
Chaim Lev-Ari <chaim.levi-ari@portainer.io>
2024-06-10 15:22:45 +0300

    refactor(users): use use mutation correctly

--- app/react/portainer/users/queries/useCreateUserMutation.ts
+++ app/react/portainer/users/queries/useCreateUserMutation.ts
@@ -1,11 +1,7 @@
 import { useQueryClient, useMutation } from '@tanstack/react-query';
 
 import axios, { parseAxiosError } from '@/portainer/services/axios';
-import {
-  mutationOptions,
-  withError,
-  withInvalidate,
-} from '@/react-tools/react-query';
+import { withGlobalError, withInvalidate } from '@/react-tools/react-query';
 import { userQueryKeys } from '@/portainer/users/queries/queryKeys';
 import { buildUrl } from '@/portainer/users/user.service';
 import { Role, User } from '@/portainer/users/types';
@@ -22,8 +18,9 @@ interface CreateUserPayload {
 
 export function useCreateUserMutation() {
   const queryClient = useQueryClient();
-  return useMutation(
-    async (values: CreateUserPayload) => {
+
+  return useMutation({
+    mutationFn: async (values: CreateUserPayload) => {
       const user = await createUser(values);
       return Promise.all(
         values.teams.map((id) =>
@@ -31,11 +28,9 @@ export function useCreateUserMutation() {
         )
       );
     },
-    mutationOptions(
-      withInvalidate(queryClient, [userQueryKeys.base()]),
-      withError('Unable to create user')
-    )
-  );
+    ...withInvalidate(queryClient, [userQueryKeys.base()]),
+    ...withGlobalError('Unable to create user'),
+  });
 }
 
 async function createUser(payload: CreateUserPayload) {
--- app/react/portainer/users/queries/useDeleteUserMutation.ts
+++ app/react/portainer/users/queries/useDeleteUserMutation.ts
@@ -1,24 +1,18 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 import axios, { parseAxiosError } from '@/portainer/services/axios';
-import {
-  mutationOptions,
-  withError,
-  withInvalidate,
-} from '@/react-tools/react-query';
+import { withGlobalError, withInvalidate } from '@/react-tools/react-query';
 import { UserId } from '@/portainer/users/types';
 import { buildUrl } from '@/portainer/users/user.service';
+import { userQueryKeys } from '@/portainer/users/queries/queryKeys';
 
 export function useDeleteUserMutation() {
   const queryClient = useQueryClient();
-  return useMutation(
-    (id: UserId) => deleteUser(id),
-
-    mutationOptions(
-      withError('Unable to delete user'),
-      withInvalidate(queryClient, [['users']])
-    )
-  );
+  return useMutation({
+    mutationFn: (id: UserId) => deleteUser(id),
+    ...withGlobalError('Unable to delete user'),
+    ...withInvalidate(queryClient, [userQueryKeys.base()]),
+  });
 }
 
 export async function deleteUser(id: UserId) {
