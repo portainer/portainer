4343005405b228356e706c8100543bffccd5ea43
KaySar12 <shintheman12@gmail.com>
2024-07-13 00:48:39 +0700

    update

--- app/react/portainer/settings/SettingsView/ApplicationSettingsPanel/EnableTelemetryField.tsx
+++ app/react/portainer/settings/SettingsView/ApplicationSettingsPanel/EnableTelemetryField.tsx
@@ -17,18 +17,6 @@ export function EnableTelemetryField() {
           onChange={(checked) => setValue(checked)}
         />
       </div>
-
-      <div className="col-sm-12 text-muted small mt-2">
-        You can find more information about this in our{' '}
-        <a
-          href="https://www.portainer.io/documentation/in-app-analytics-and-privacy-policy/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          privacy policy
-        </a>
-        .
-      </div>
     </div>
   );
 }
