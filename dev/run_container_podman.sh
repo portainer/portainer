a4cb517e7d46980046498c5f167f7b1992f4009e
testA113 <aliharriss1995@gmail.com>
2024-08-13 09:03:39 +1200

    update dev script

--- dev/run_container_podman.sh
+++ dev/run_container_podman.sh
@@ -7,16 +7,14 @@ PORTAINER_FLAGS=${PORTAINER_FLAGS:-}
 
 podman rm -f portainer
 
-# rootful podman
-
-# must run as sudo for rootful podman
+# rootful podman (sudo required)
 sudo podman run -d \
   -p 8000:8000 \
   -p 9000:9000 \
   -p 9443:9443 \
   -v "$PORTAINER_PROJECT/dist:/app" \
   -v "$PORTAINER_DATA:/data" \
-  -v /run/podman/podman.sock:/run/podman/podman.sock:z \
+  -v /run/podman/podman.sock:/run/podman/podman.sock \
   -v /tmp:/tmp \
   --privileged \
   --name portainer \
