809302efca126be9575f9ad95411b0041916aa0b
oscarzhou <oscar.zhou@portainer.io>
2024-09-16 19:55:03 +1200

    fix: lint error

--- api/http/handler/docker/images/images_list.go
+++ api/http/handler/docker/images/images_list.go
@@ -78,7 +78,7 @@ func (handler *Handler) imagesList(w http.ResponseWriter, r *http.Request) *http
 
 	imagesList := make([]ImageResponse, len(images))
 	for i, image := range images {
-		if (image.RepoTags == nil || len(image.RepoTags) == 0) && (image.RepoDigests != nil && len(image.RepoDigests) > 0) {
+		if len(image.RepoTags) == 0 && len(image.RepoDigests) > 0 {
 			for _, repoDigest := range image.RepoDigests {
 				image.RepoTags = append(image.RepoTags, repoDigest[0:strings.Index(repoDigest, "@")]+":<none>")
 			}
