639678fbc7cd3103d3c56d2804ff8f0f98fab7ae
stevensbkang <skan070@gmail.com>
2024-09-30 16:52:13 +1300

    fixed the standard user not being able to access the default namespace

--- api/kubernetes/cli/access.go
+++ api/kubernetes/cli/access.go
@@ -124,17 +124,13 @@ func (kcl *KubeClient) UpdateNamespaceAccessPolicies(accessPolicies map[string]p
 }
 
 // GetNonAdminNamespaces retrieves namespaces for a non-admin user, excluding the default namespace if restricted.
-func (kcl *KubeClient) GetNonAdminNamespaces(userID int, isRestrictDefaultNamespace bool) ([]string, error) {
+func (kcl *KubeClient) GetNonAdminNamespaces(userID int) ([]string, error) {
 	accessPolicies, err := kcl.GetNamespaceAccessPolicies()
 	if err != nil {
 		return nil, fmt.Errorf("an error occurred during the getNonAdminNamespaces operation, unable to get namespace access policies via portainer-config. check if portainer-config configMap exists in the Kubernetes cluster: %w", err)
 	}
 
-	nonAdminNamespaces := []string{}
-	if !isRestrictDefaultNamespace {
-		nonAdminNamespaces = append(nonAdminNamespaces, defaultNamespace)
-	}
-
+	nonAdminNamespaces := []string{defaultNamespace}
 	for namespace, accessPolicy := range accessPolicies {
 		if hasUserAccessToNamespace(userID, nil, accessPolicy) {
 			nonAdminNamespaces = append(nonAdminNamespaces, namespace)
