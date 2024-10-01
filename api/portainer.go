b4d8bc22546f94a0edd025684ee335290d18408e
stevensbkang <skan070@gmail.com>
2024-10-01 14:56:05 +1300

    release cadence: version bump to 2.22.0-rc1

--- api/portainer.go
+++ api/portainer.go
@@ -1599,7 +1599,7 @@ type (
 
 const (
 	// APIVersion is the version number of the Portainer API
-	APIVersion = "2.22.0"
+	APIVersion = "2.22.0-rc1"
 	// Edition is what this edition of Portainer is called
 	Edition = PortainerCE
 	// ComposeSyntaxMaxVersion is a maximum supported version of the docker compose syntax
