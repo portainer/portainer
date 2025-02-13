4bd9569e63fc5912df926d3e6a0a9f6852891577
Steven Kang <skan070@gmail.com>
2025-02-14 08:39:05 +1300

    version: bump version to 2.27.0-rc3 - release 2.27 (#427)

--- api/portainer.go
+++ api/portainer.go
@@ -1636,7 +1636,7 @@ type (
 
 const (
 	// APIVersion is the version number of the Portainer API
-	APIVersion = "2.27.0-rc2"
+	APIVersion = "2.27.0-rc3"
 	// Support annotation for the API version ("STS" for Short-Term Support or "LTS" for Long-Term Support)
 	APIVersionSupport = "LTS"
 	// Edition is what this edition of Portainer is called
