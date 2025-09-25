7450501b7abc87609543d61cd7419ae30c8b6940
Steven Kang <skan070@gmail.com>
2025-09-25 14:29:28 +1200

    chore: version bump 2.33.2 (#1257)

--- api/portainer.go
+++ api/portainer.go
@@ -1782,7 +1782,7 @@ type (
 
 const (
 	// APIVersion is the version number of the Portainer API
-	APIVersion = "2.33.1"
+	APIVersion = "2.33.2"
 	// Support annotation for the API version ("STS" for Short-Term Support or "LTS" for Long-Term Support)
 	APIVersionSupport = "LTS"
 	// Edition is what this edition of Portainer is called
