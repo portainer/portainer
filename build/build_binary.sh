9c79d6dc7daba7f9cd014853a4c99a52e60969bf
Yajith Dayarathna <yajith.dayarathna@portainer.io>
2025-12-08 14:12:43 +1300

    chore(ci): minor ci workflow updates (#1492)

--- build/build_binary.sh
+++ build/build_binary.sh
@@ -59,9 +59,24 @@ ldflags="-s -X 'github.com/portainer/liblicense.LicenseServerBaseURL=https://api
 
 echo "$ldflags"
 
-GOOS=${1:-$(go env GOOS)} GOARCH=${2:-$(go env GOARCH)} CGO_ENABLED=0 go build \
+
+# See: https://gist.github.com/asukakenji/f15ba7e588ac42795f421b48b8aede63
+# For a list of valid GOOS and GOARCH values
+PLATFORM=${1:-$(go env GOOS)}
+ARCH=${2:-$(go env GOARCH)}
+# if the default platform is darwin, set it to linux to allow it to run in the portainer/base image (which doesn't support darwin)
+if [ "$PLATFORM" = "darwin" ]; then
+  PLATFORM="linux"
+fi
+
+BINARY_NAME="portainer"
+if [ "$PLATFORM" = "windows" ]; then
+  BINARY_NAME="portainer.exe"
+fi
+
+GOOS=${PLATFORM} GOARCH=${ARCH} CGO_ENABLED=0 go build \
 	-trimpath \
 	--installsuffix cgo \
 	--ldflags "$ldflags" \
-	-o "../dist/portainer" \
+	-o "../dist/${BINARY_NAME}" \
 	./cmd/portainer/
