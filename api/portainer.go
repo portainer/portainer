f4ea7a24fdc7991c43b3e24abd0558c9914efbb2
Dakota Walsh <101994734+dakota-portainer@users.noreply.github.com>
2026-06-25 12:55:33 +1200

    feat(bump): LTS patch version bump from 2.39.3 to 2.39.4 (#3035)

--- api/portainer.go
+++ api/portainer.go
@@ -1875,7 +1875,7 @@ type (
 
 const (
 	// APIVersion is the version number of the Portainer API
-	APIVersion = "2.39.3"
+	APIVersion = "2.39.4"
 	// Support annotation for the API version ("STS" for Short-Term Support or "LTS" for Long-Term Support)
 	APIVersionSupport = "LTS"
 	// Edition is what this edition of Portainer is called
