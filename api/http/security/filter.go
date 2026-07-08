7ddf825c0b77a0f169b696111e063b7ff1e21515
andres-portainer <91705312+andres-portainer@users.noreply.github.com>
2026-07-08 18:59:38 -0300

    fix(security): stop FilterUserTeams from dropping teams for standard users BE-13187 (#3148)

--- api/http/security/filter.go
+++ api/http/security/filter.go
@@ -12,19 +12,14 @@ func FilterUserTeams(teams []portainer.Team, context *RestrictedRequestContext) 
 		return teams
 	}
 
-	n := 0
+	memberOf := map[portainer.TeamID]bool{}
 	for _, membership := range context.UserMemberships {
-		for _, team := range teams {
-			if team.ID == membership.TeamID {
-				teams[n] = team
-				n++
-
-				break
-			}
-		}
+		memberOf[membership.TeamID] = true
 	}
 
-	return teams[:n]
+	return slicesx.FilterInPlace(teams, func(team portainer.Team) bool {
+		return memberOf[team.ID]
+	})
 }
 
 // FilterLeaderTeams filters teams based on user role.
